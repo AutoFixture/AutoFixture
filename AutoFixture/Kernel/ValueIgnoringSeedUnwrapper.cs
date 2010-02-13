97b00a6473c1ac8b987850a078049d88c03dd181
Mark Seemann <mark@seemann.se>
2010-02-13 19:51:19 +0000

    Added ValueIgnoringSeedUnwrapper to the new Kernel. This type is pretty mundane in itself, since its main purpose is to serve as a fallback if no other InstanceGenerator can handle the seed. It does, however, provide an opportunity for other InstanceGenerators to intercept Seeds for specific types (such as strings). Adding this type also fixes the CA warning about too few types in the Kernel sub-namespace.

---
+++ AutoFixture/Kernel/ValueIgnoringSeedUnwrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Unwraps a request for a <see cref="Seed"/> to a request for its
+    /// <see cref="Seed.TargetType"/> while ignoring the <see cref="Seed.Value"/>.
+    /// </summary>
+    public class ValueIgnoringSeedUnwrapper : InstanceGeneratorNode
+    {
+        /// <summary>
+        /// Initialized a new instance of the <see cref="ValueIgnoringSeedUnwrapper"/> class with
+        /// the supplied parent.
+        /// </summary>
+        /// <param name="parent">The parent generator.</param>
+        public ValueIgnoringSeedUnwrapper(IInstanceGenerator parent)
+            : base(parent)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="InstanceGeneratorNode.GeneratorStrategy" /> instance that
+        /// implements the behavior of <see cref="ValueIgnoringSeedUnwrapper" />.
+        /// </summary>
+        /// <param name="attributeProvider">
+        /// A <see cref="ICustomAttributeProvider" /> instance.
+        /// </param>
+        /// <returns>
+        /// A <see cref="InstanceGeneratorNode.GeneratorStrategy"/> that implements the behavior of 
+        /// <see cref="ValueIgnoringSeedUnwrapper" />.
+        /// </returns>
+        protected override InstanceGeneratorNode.GeneratorStrategy CreateStrategy(ICustomAttributeProvider attributeProvider)
+        {
+            return new TargetTypeForwardingGeneratorStrategy(this.Parent, attributeProvider);
+        }
+
+        private class TargetTypeForwardingGeneratorStrategy : GeneratorStrategy
+        {
+            private readonly Seed seed;
+
+            internal TargetTypeForwardingGeneratorStrategy(IInstanceGenerator parent, ICustomAttributeProvider attributeProvider)
+                : base(parent, attributeProvider)
+            {
+                this.seed = attributeProvider as Seed;
+            }
+
+            public override bool CanGenerate()
+            {
+                return this.seed != null
+                    && this.Parent.CanGenerate(this.seed.TargetType);
+            }
+
+            public override object Generate()
+            {
+                return this.Parent.Generate(this.seed.TargetType);
+            }
+        }
+
+    }
+}
---
+++ AutoFixtureUnitTest/ValueIgnoringSeedUnwrapperTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    [TestClass]
+    public class ValueIgnoringSeedUnwrapperTest
+    {
+        [TestMethod]
+        public void SutIsInstanceGeneratorNode()
+        {
+            // Fixture setup
+            var dummyParent = new MockInstanceGenerator();
+            // Exercise system
+            var sut = new ValueIgnoringSeedUnwrapper(dummyParent);
+            // Verify outcome
+            Assert.IsInstanceOfType(sut, typeof(InstanceGeneratorNode));
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGeneratorForNullProviderWillReturnFalse()
+        {
+            // Fixture setup
+            var sut = new ValueIgnoringSeedUnwrapper(new MockInstanceGenerator());
+            // Exercise system
+            var result = sut.CanGenerate(null);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForAnonymousProviderWillReturnFalse()
+        {
+            // Fixture setup
+            var anonymousProvider = typeof(PropertyHolder<string>).GetProperty("Property");
+            var sut = new ValueIgnoringSeedUnwrapper(new MockInstanceGenerator());
+            // Exercise system
+            var result = sut.CanGenerate(anonymousProvider);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForSeedWhenParentCannotGenerateSeedTypeWillReturnFalse()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(ASCIIEncoding), new object());
+            var sut = new ValueIgnoringSeedUnwrapper(new MockInstanceGenerator { CanGenerateCallback = ap => false });
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateWhenParentCanGenerateTargetTypeWillReturnFalse()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(ConcreteType), new ConcreteType());
+
+            var parentMock = new MockInstanceGenerator();
+            parentMock.CanGenerateCallback = seed.TargetType.Equals;
+
+            var sut = new ValueIgnoringSeedUnwrapper(parentMock);
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsTrue(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void GenerateWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(ConcreteType), null);
+            var expectedInstance = new object();
+
+            var parentMock = new MockInstanceGenerator();
+            parentMock.CanGenerateCallback = ap => true;
+            parentMock.GenerateCallback = ap => expectedInstance;
+
+            var sut = new ValueIgnoringSeedUnwrapper(parentMock);
+            // Exercise system
+            var result = sut.Generate(seed);
+            // Verify outcome
+            Assert.AreEqual(expectedInstance, result, "Generate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void GenerateWillInvokeParentCorrectly()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(FtpStyleUriParser), TimeSpan.FromDays(1));
+
+            var parentMock = new MockInstanceGenerator();
+            parentMock.CanGenerateCallback = seed.TargetType.Equals;
+            parentMock.GenerateCallback = ap =>
+                {
+                    Assert.AreEqual(seed.TargetType, ap, "Generate");
+                    return new object();
+                };
+
+            var sut = new ValueIgnoringSeedUnwrapper(parentMock);
+            // Exercise system
+            sut.Generate(seed);
+            // Verify outcome (done by mock)
+            // Teardown
+        }
+    }
+}
