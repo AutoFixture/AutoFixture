494279fee87773b7a3247145af9c2838cb7f6d80
Mark Seemann <mark@seemann.se>
2010-02-14 08:25:09 +0000

    Added StringSeedUnwrapper. The purpose of this class is to unwrap a seeded string request to a request for a string instance, and then append the seed to the generated string.

---
+++ AutoFixture/Kernel/StringSeedUnwrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Unwraps a request for a string <see cref="Seed"/> to a request for a string and appends the
+    /// seed to the result.
+    /// </summary>
+    public class StringSeedUnwrapper : InstanceGeneratorNode
+    {
+        /// <summary>
+        /// Initialized a new instance of the the <see cref="StringSeedUnwrapper"/> class with the
+        /// supplied parent.
+        /// </summary>
+        /// <param name="parent"></param>
+        public StringSeedUnwrapper(IInstanceGenerator parent)
+            : base(parent)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="InstanceGeneratorNode.GeneratorStrategy" /> instance that
+        /// implements the behavior of <see cref="StringSeedUnwrapper" />.
+        /// </summary>
+        /// <param name="attributeProvider">
+        /// A <see cref="ICustomAttributeProvider" /> instance.
+        /// </param>
+        /// <returns>
+        /// A <see cref="InstanceGeneratorNode.GeneratorStrategy"/> that implements the behavior of 
+        /// <see cref="StringSeedUnwrapper" />.
+        /// </returns>
+        protected override InstanceGeneratorNode.GeneratorStrategy CreateStrategy(ICustomAttributeProvider attributeProvider)
+        {
+            return new StringSeedUnwrapStrategy(this.Parent, attributeProvider);
+        }
+
+        private class StringSeedUnwrapStrategy : GeneratorStrategy
+        {
+            private readonly Seed seed;
+
+            internal StringSeedUnwrapStrategy(IInstanceGenerator parent, ICustomAttributeProvider attributeProvider)
+                : base(parent, attributeProvider)
+            {
+                this.seed = attributeProvider as Seed;
+            }
+
+            public override bool CanGenerate()
+            {
+                return this.seed != null
+                    && this.seed.TargetType == typeof(string)
+                    && this.seed.Value is string
+                    && this.Parent.CanGenerate(typeof(string));
+            }
+
+            public override object Generate()
+            {
+                return (string)this.seed.Value + this.Parent.Generate(typeof(string));
+            }
+        }
+
+    }
+}
---
+++ AutoFixtureUnitTest/StringSeedUnwrapperTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    [TestClass]
+    public class StringSeedUnwrapperTest
+    {
+        [TestMethod]
+        public void SutIsInstanceGneeratorNode()
+        {
+            // Fixture setup
+            var dummyParent = new MockInstanceGenerator();
+            // Exercise system
+            var sut = new StringSeedUnwrapper(dummyParent);
+            // Verify outcome
+            Assert.IsInstanceOfType(sut, typeof(InstanceGeneratorNode));
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForNullProviderWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var dummyParent = new MockInstanceGenerator();
+            var sut = new StringSeedUnwrapper(dummyParent);
+            // Exercise system
+            var result = sut.CanGenerate(null);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForAnonymousProviderWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var anonymousProvider = typeof(GenericUriParser);
+            var sut = new StringSeedUnwrapper(new MockInstanceGenerator());
+            // Exercise system
+            var result = sut.CanGenerate(anonymousProvider);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForSeedWithWrongTypeWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(HttpStyleUriParser), new object());
+            var sut = new StringSeedUnwrapper(new MockInstanceGenerator());
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenereateForSeedWithWrongValueTypeWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(string), new ModuleHandle());
+            var sut = new StringSeedUnwrapper(new MockInstanceGenerator());
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForCorrectSeedWhenParentCannotGenerateStringsWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(string), "Anonymous value");
+            var sut = new StringSeedUnwrapper(new MockInstanceGenerator { CanGenerateCallback = ap => false });
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsFalse(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void CanGenerateForCorrectSeedWhenParentCanGenerateStringsWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var correctType = typeof(string);
+            var seed = new Seed(correctType, "Anonymous value");
+            var sut = new StringSeedUnwrapper(new MockInstanceGenerator { CanGenerateCallback = correctType.Equals });
+            // Exercise system
+            var result = sut.CanGenerate(seed);
+            // Verify outcome
+            Assert.IsTrue(result, "CanGenerate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void GenerateWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(string), "Anonymous seed");
+            var parentString = "Anonymous parent-generated text";
+            var expectedInstance = seed.Value + parentString;
+
+            var parentStub = new MockInstanceGenerator();
+            parentStub.CanGenerateCallback = ap => true;
+            parentStub.GenerateCallback = ap => parentString;
+
+            var sut = new StringSeedUnwrapper(parentStub);
+            // Exercise system
+            var result = sut.Generate(seed);
+            // Verify outcome
+            Assert.AreEqual(expectedInstance, result, "Generate");
+            // Teardown
+        }
+
+        [TestMethod]
+        public void GenerateWillInvokeParentCorectly()
+        {
+            // Fixture setup
+            var seed = new Seed(typeof(string), "Anonymous seed");
+
+            var parentMock = new MockInstanceGenerator();
+            parentMock.CanGenerateCallback = seed.TargetType.Equals;
+            parentMock.GenerateCallback = ap =>
+                {
+                    Assert.AreEqual(seed.TargetType, ap, "Generate");
+                    return "Anonymous result";
+                };
+
+            var sut = new StringSeedUnwrapper(parentMock);
+            // Exercise system
+            sut.Generate(seed);
+            // Verify outcome (done by mock)
+            // Teardown
+        }
+    }
+}
