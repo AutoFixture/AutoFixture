88af922e49a655188d6aa7b3246d72abf842c404
Mark Seemann <mark@seemann.ms>
2010-04-26 13:35:52 +0200

    Added Postprocessor as initial work to enable Do/With functionality. At this stage, Do functionality should be possible.

---
+++ AutoFixture/Kernel/Postprocessor.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Performs post-processing on a created specimen.
+    /// </summary>
+    public class Postprocessor : ISpecimenBuilder
+    {
+        private readonly ISpecimenBuilder builder;
+        private readonly Action<object> action;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Postprocessor"/> class with the supplied
+        /// parameters.
+        /// </summary>
+        /// <param name="builder">The <see cref="ISpecimenBuilder"/> to decorate.</param>
+        /// <param name="action">The action to perform on the created specimen.</param>
+        public Postprocessor(ISpecimenBuilder builder, Action<object> action)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            this.builder = builder;
+            this.action = action;
+        }
+
+        #region ISpecimenBuilder Members
+
+        /// <summary>
+        /// Creates a new specimen based on a request and performs an action on the created
+        /// specimen.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="container">A container that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="request"/> can be any object, but will often be a
+        /// <see cref="Type"/> or other <see cref="System.Reflection.MemberInfo"/> instances.
+        /// </para>
+        /// </remarks>
+        public object Create(object request, ISpecimenContainer container)
+        {
+            var specimen = this.builder.Create(request, container);
+            this.action(specimen);
+            return specimen;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Performs post-processing on a created specimen.
+    /// </summary>
+    /// <typeparam name="T">The type of specimen.</typeparam>
+    public class Postprocessor<T> : ISpecimenBuilder
+    {
+        private readonly ISpecimenBuilder builder;
+        private readonly Action<T> action;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Postprocessor"/> class with the supplied
+        /// parameters.
+        /// </summary>
+        /// <param name="builder">The <see cref="ISpecimenBuilder"/> to decorate.</param>
+        /// <param name="action">The action to perform on the created specimen.</param>
+        public Postprocessor(ISpecimenBuilder builder, Action<T> action)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            this.builder = builder;
+            this.action = action;
+        }
+
+        #region ISpecimenBuilder Members
+
+        /// <summary>
+        /// Creates a new specimen based on a request and performs an action on the created
+        /// specimen.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="container">A container that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="request"/> can be any object, but will often be a
+        /// <see cref="Type"/> or other <see cref="System.Reflection.MemberInfo"/> instances.
+        /// </para>
+        /// </remarks>
+        public object Create(object request, ISpecimenContainer container)
+        {
+            var specimen = this.builder.Create(request, container);
+            if (!(specimen is T))
+            {
+                throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture,
+                    "The specimen returned by the decorated ISpecimenBuilder is not compatible with {0}.", typeof(T)));
+            }
+            var item = (T)specimen;
+            this.action(item);
+            return specimen;
+        }
+
+        #endregion
+    }
+}
---
+++ AutoFixtureUnitTest/Kernel/PostprocessorGenericTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class PostprocessorGenericTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            Action<string> dummyAction = s => { };
+            // Exercise system
+            var sut = new Postprocessor<string>(dummyBuilder, dummyAction);
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullBuilderThrows()
+        {
+            // Fixture setup
+            Action<int> dummyAction = s => { };
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new Postprocessor<int>(null, dummyAction));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullActionThrows()
+        {
+            // Fixture setup
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new Postprocessor<Guid>(dummyBuilder, null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateInvokesDecoratedBuilderWithCorrectParameters()
+        {
+            // Fixture setup
+            var expectedRequest = new object();
+            var expectedContainer = new DelegatingSpecimenContainer();
+
+            var verified = false;
+            var builderMock = new DelegatingSpecimenBuilder { OnCreate = (r, c) => verified = r == expectedRequest && c == expectedContainer };
+
+            Action<bool> dummyAction = s => { };
+            var sut = new Postprocessor<bool>(builderMock, dummyAction);
+            // Exercise system
+            sut.Create(expectedRequest, expectedContainer);
+            // Verify outcome
+            Assert.True(verified, "Mock verified");
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectResult()
+        {
+            // Fixture setup
+            var expectedResult = 1m;
+            var builder = new DelegatingSpecimenBuilder { OnCreate = (r, c) => expectedResult };
+            Action<decimal> dummyAction = s => { };
+            var sut = new Postprocessor<decimal>(builder, dummyAction);
+            // Exercise system
+            var dummyRequest = new object();
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(dummyRequest, dummyContainer);
+            // Verify outcome
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateThrowsWhenBuilderReturnsIncompatibleType()
+        {
+            // Fixture setup
+            var nonInt = "Anonymous variable";
+            var builder = new DelegatingSpecimenBuilder { OnCreate = (r, c) => nonInt };
+
+            Action<int> dummyAction = s => { };
+            var sut = new Postprocessor<int>(builder, dummyAction);
+            // Exercise system and verify outcome
+            var dummyRequest = new object();
+            var dummyContainer = new DelegatingSpecimenContainer();
+            Assert.Throws<InvalidOperationException>(() => sut.Create(dummyRequest, dummyContainer));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateInvokesActionWithCreatedSpecimen()
+        {
+            // Fixture setup
+            var expectedSpecimen = new DateTime(2010, 4, 26);
+            var builder = new DelegatingSpecimenBuilder { OnCreate = (r, c) => expectedSpecimen };
+
+            var verified = false;
+            Action<DateTime> mock = s => verified = s == expectedSpecimen;
+
+            var sut = new Postprocessor<DateTime>(builder, mock);
+            // Exercise system
+            var dummyRequest = new object();
+            var dummyContainer = new DelegatingSpecimenContainer();
+            sut.Create(dummyRequest, dummyContainer);
+            // Verify outcome
+            Assert.True(verified, "Mock verified");
+            // Teardown
+        }
+    }
+}
