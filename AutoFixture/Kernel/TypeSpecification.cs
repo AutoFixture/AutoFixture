b6518c06d561662c06fd1c28501efaf449874060
Mark Seemann <mark@seemann.ms>
2010-05-03 17:16:39 +0200

    Added TypeSpecification, which encapsulates a Specification that filters on a target type.

---
+++ AutoFixture/Kernel/TypeSpecification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// A specification that evaluates requests for types against a target type.
+    /// </summary>
+    public class TypeSpecification : IRequestSpecification
+    {
+        private readonly Type specType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeSpecification"/> class.
+        /// </summary>
+        /// <param name="type">The target type.</param>
+        public TypeSpecification(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            this.specType = type;
+        }
+
+        #region IRequestSpecification Members
+
+        /// <summary>
+        /// Evaluates a request for a specimen.
+        /// </summary>
+        /// <param name="request">The specimen request.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="request"/> matches the target type;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsSatisfiedBy(object request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            return this.specType.Equals(request);
+        }
+
+        #endregion
+    }
+}
---
+++ AutoFixtureUnitTest/Kernel/TypeSpecificationTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class TypeSpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            // Exercise system
+            var sut = new TypeSpecification(dummyType);
+            // Verify outcome
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullTypeThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new TypeSpecification(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void IsSatisfiedByNullThrows()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            var sut = new TypeSpecification(dummyType);
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => sut.IsSatisfiedBy(null));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(object), typeof(object), true)]
+        [InlineData(typeof(string), typeof(string), true)]
+        [InlineData(typeof(string), typeof(int), false)]
+        [InlineData(typeof(PropertyHolder<string>), typeof(FieldHolder<string>), false)]
+        public void IsSatisfiedByReturnsCorrectResult(Type specType, Type requestType, bool expectedResult)
+        {
+            // Fixture setup
+            var sut = new TypeSpecification(specType);
+            // Exercise system
+            var result = sut.IsSatisfiedBy(requestType);
+            // Verify outcome
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+    }
+}
