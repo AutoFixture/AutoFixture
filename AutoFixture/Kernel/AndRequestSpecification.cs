7b3e7e62c2ce9fc5179123feedfe7507438c73b4
Mark Seemann <mark@seemann.ms>
2010-05-03 11:02:29 +0200

    Added AndRequestSpecification.

---
+++ AutoFixture/Kernel/AndRequestSpecification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// A boolean 'And' Composite <see cref="IRequestSpecification"/>.
+    /// </summary>
+    public class AndRequestSpecification : IRequestSpecification
+    {
+        private readonly IEnumerable<IRequestSpecification> specifications;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AndRequestSpecification"/> with the
+        /// supplied specifications.
+        /// </summary>
+        /// <param name="specifications">An array of <see cref="IRequestSpecification"/>.</param>
+        public AndRequestSpecification(params IRequestSpecification[] specifications)
+        {
+            if (specifications == null)
+            {
+                throw new ArgumentNullException("specifications");
+            }
+
+            this.specifications = specifications;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AndRequestSpecification"/> class with the
+        /// supplied specifications.
+        /// </summary>
+        /// <param name="specifications">A sequence of <see cref="IRequestSpecification"/>.</param>
+        public AndRequestSpecification(IEnumerable<IRequestSpecification> specifications)
+            : this(specifications.ToArray())
+        {
+        }
+
+        /// <summary>
+        /// Gets the decorated specifications.
+        /// </summary>
+        public IEnumerable<IRequestSpecification> Specifications
+        {
+            get { return this.specifications; }
+        }
+
+        #region IRequestSpecification Members
+
+        /// <summary>
+        /// Evaluates a request for a specimen.
+        /// </summary>
+        /// <param name="request">The specimen request.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="request"/> is satisfied by all
+        /// <see cref="Specifications"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsSatisfiedBy(object request)
+        {
+            return this.specifications.All(s => s.IsSatisfiedBy(request));
+        }
+
+        #endregion
+    }
+}
