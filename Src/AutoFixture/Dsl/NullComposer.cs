ac21dc3562aa431ae0b2bce0db0d9144ed6541b6
Mark Seemann <mark@seemann.ms>
2010-06-15 15:48:30 +0200

    Added NullComposer, which can be used to carry an already composed ISpecimenBuilder instance forward.

---
+++ Src/AutoFixture/Dsl/NullComposer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+using System.Linq.Expressions;
+
+namespace Ploeh.AutoFixture.Dsl
+{
+    public class NullComposer<T> : ICustomizationComposer<T>
+    {
+        private readonly Func<ISpecimenBuilder> compose;
+
+        public NullComposer()
+            : this(new CompositeSpecimenBuilder())
+        {
+        }
+
+        public NullComposer(ISpecimenBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+
+            this.compose = () => builder;
+        }
+
+        public NullComposer(Func<ISpecimenBuilder> factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException("factory");
+            }
+
+            this.compose = factory;
+        }
+
+        #region IFactoryComposer<T> Members
+
+        public IPostprocessComposer<T> FromSeed(Func<T, T> factory)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> FromFactory(Func<T> factory)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput>(Func<TInput, T> factory)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2>(Func<TInput1, TInput2, T> factory)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2, TInput3>(Func<TInput1, TInput2, TInput3, T> factory)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2, TInput3, TInput4>(Func<TInput1, TInput2, TInput3, TInput4, T> factory)
+        {
+            return this;
+        }
+
+        #endregion
+
+        #region IPostprocessComposer<T> Members
+
+        public IPostprocessComposer<T> Do(Action<T> action)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> OmitAutoProperties()
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> With<TProperty>(Expression<Func<T, TProperty>> propertyPicker)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> With<TProperty>(Expression<Func<T, TProperty>> propertyPicker, TProperty value)
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> WithAutoProperties()
+        {
+            return this;
+        }
+
+        public IPostprocessComposer<T> Without<TProperty>(Expression<Func<T, TProperty>> propertyPicker)
+        {
+            return this;
+        }
+
+        #endregion
+
+        #region ISpecimenBuilderComposer Members
+
+        public ISpecimenBuilder Compose()
+        {
+            return this.compose();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Dsl/NullComposerTest.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Dsl;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixtureUnitTest.Dsl
+{
+    public class NullComposerTest
+    {
+        [Fact]
+        public void SutIsCustomizationComposer()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new NullComposer<object>();
+            // Verify outcome
+            Assert.IsAssignableFrom<ICustomizationComposer<object>>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullBuilderThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new NullComposer<object>((ISpecimenBuilder)null));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullFuncThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new NullComposer<object>((Func<ISpecimenBuilder>)null));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializedWithDefaultConstructorCorrectlyComposes()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.Compose();
+            // Verify outcome
+            var composite = Assert.IsAssignableFrom<CompositeSpecimenBuilder>(result);
+            Assert.Empty(composite.Builders);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializedWithBuilderCorrectlyComposes()
+        {
+            // Fixture setup
+            var expectedBuilder = new DelegatingSpecimenBuilder();
+            var sut = new NullComposer<object>(expectedBuilder);
+            // Exercise system
+            var result = sut.Compose();
+            // Verify outcome
+            Assert.Equal(expectedBuilder, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializedWithFuncCorrectlyComposes()
+        {
+            // Fixture setup
+            var expectedBuilder = new DelegatingSpecimenBuilder();
+            var sut = new NullComposer<object>(() => expectedBuilder);
+            // Exercise system
+            var result = sut.Compose();
+            // Verify outcome
+            Assert.Equal(expectedBuilder, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromSeedReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromSeed(s => s);
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromFactoryReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromFactory(() => new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromSingleParameterFactoryReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromFactory((object x) => new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromDoubleParameterFactoryReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromFactory((object x, object y) => new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromTripeParameterFactoryReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromFactory((object x, object y, object z) => new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void FromQuadrupleParameterFactoryReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.FromFactory((object x, object y, object z, object æ) => new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void DoReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.Do(x => { });
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void OmitAutoPropertiesReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.OmitAutoProperties();
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void AnonymousWithReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<PropertyHolder<object>>();
+            // Exercise system
+            var result = sut.With(x => x.Property);
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void WithReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<PropertyHolder<object>>();
+            // Exercise system
+            var result = sut.With(x => x.Property, new object());
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void WithAutoPropertiesReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<object>();
+            // Exercise system
+            var result = sut.WithAutoProperties();
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void WithoutReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NullComposer<PropertyHolder<object>>();
+            // Exercise system
+            var result = sut.Without(x => x.Property);
+            // Verify outcome
+            Assert.Same(sut, result);
+            // Teardown
+        }
+    }
+}
