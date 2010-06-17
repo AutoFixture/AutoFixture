5589b740d9c751be58edf15bdb42e1bd0ee02d71
Mark Seemann <mark@seemann.ms>
2010-06-17 13:04:36 +0200

    Made DoubleSequenceGenerator an ISpecimenBuilder.

--- Src/AutoFixture/DoubleSequenceGenerator.cs
+++ Src/AutoFixture/DoubleSequenceGenerator.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
     /// <summary>
     /// Creates a sequence of consecutive numbers, starting at 1.
     /// </summary>
-    public class DoubleSequenceGenerator
+    public class DoubleSequenceGenerator : ISpecimenBuilder
     {
         private double d;
         private readonly object syncRoot;
@@ -39,5 +40,28 @@ namespace Ploeh.AutoFixture
         {
             return this.CreateAnonymous();
         }
+
+        #region ISpecimenBuilder Members
+
+        /// <summary>
+        /// Creates an anonymous number.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="container">Not used.</param>
+        /// <returns>
+        /// The next number in a consequtive sequence, if <paramref name="request"/> is a request
+        /// for a <see cref="System.Double"/>; otherwise, a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContainer container)
+        {
+            if (request != typeof(double))
+            {
+                return new NoSpecimen(request);
+            }
+
+            return this.CreateAnonymous();
+        }
+
+        #endregion
     }
 }
--- Src/AutoFixtureUnitTest/DoubleSequenceGeneratorTest.cs
+++ Src/AutoFixtureUnitTest/DoubleSequenceGeneratorTest.cs
@@ -1,14 +1,12 @@
 ﻿using Ploeh.AutoFixture;
 using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
 
 namespace Ploeh.AutoFixtureUnitTest
 {
     public class DoubleSequenceGeneratorTest
     {
-        public DoubleSequenceGeneratorTest()
-        {
-        }
-
         [Fact]
         public void CreateWillReturnOneOnFirstCall()
         {
@@ -25,6 +23,95 @@ namespace Ploeh.AutoFixtureUnitTest
         public void CreateWillReturnTenOnTenthCall()
         {
             new LoopTest<DoubleSequenceGenerator, double>(sut => sut.CreateAnonymous()).Execute(10);
+        }
+
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new DoubleSequenceGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var sut = new DoubleSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(null, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContainerDoesNotThrow()
+        {
+            // Fixture setup
+            var sut = new DoubleSequenceGenerator();
+            // Exercise system
+            var dummyRequest = new object();
+            sut.Create(dummyRequest, null);
+            // Verify outcome (no exception indicates success)
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNonDoubleRequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var nonDoubleRequest = new object();
+            var sut = new DoubleSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(nonDoubleRequest, dummyContainer);
+            // Verify outcome
+            var expectedResult = new NoSpecimen(nonDoubleRequest);
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithDoubleRequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var doubleRequest = typeof(double);
+            var sut = new DoubleSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(doubleRequest, dummyContainer);
+            // Verify outcome
+            Assert.Equal(1d, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithDoubleRequestWillReturnCorrectResultOnSecondCall()
+        {
+            // Fixture setup
+            var doubleRequest = typeof(double);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<DoubleSequenceGenerator, double>(sut => (double)sut.Create(doubleRequest, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(2);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithDoubleRequestWillReturnCorrectResultOnTenthCall()
+        {
+            // Fixture setup
+            var doubleRequest = typeof(double);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<DoubleSequenceGenerator, double>(sut => (double)sut.Create(doubleRequest, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(10);
+            // Teardown
         }
     }
 }
