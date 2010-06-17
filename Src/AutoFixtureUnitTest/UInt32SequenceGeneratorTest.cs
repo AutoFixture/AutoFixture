6f00f18c919e810eb08a5c938879117e2e3ca1d6
Mark Seemann <mark@seemann.ms>
2010-06-17 12:43:03 +0200

    Made UInt32SequenceGenerator an ISpecimenBuilder.

--- Src/AutoFixtureUnitTest/UInt32SequenceGeneratorTest.cs
+++ Src/AutoFixtureUnitTest/UInt32SequenceGeneratorTest.cs
@@ -4,15 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Ploeh.AutoFixture;
 using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
 
 namespace Ploeh.AutoFixtureUnitTest
 {
     public class UInt32SequenceGeneratorTest
     {
-        public UInt32SequenceGeneratorTest()
-        {
-        }
-
         [Fact]
         public void CreateWillReturnOneOnFirstCall()
         {
@@ -29,6 +27,95 @@ namespace Ploeh.AutoFixtureUnitTest
         public void CreateWillReturnTenOnTenthCall()
         {
             new LoopTest<UInt32SequenceGenerator, uint>(sut => sut.CreateAnonymous()).Execute(10);
+        }
+
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new UInt32SequenceGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var sut = new UInt32SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(null, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContainerDoesNotThrow()
+        {
+            // Fixture setup
+            var sut = new UInt32SequenceGenerator();
+            // Exercise system
+            var dummyRequest = new object();
+            sut.Create(dummyRequest, null);
+            // Verify outcome (no exception indicates success)
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNonUInt32RequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var nonUInt32Request = new object();
+            var sut = new UInt32SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(nonUInt32Request, dummyContainer);
+            // Verify outcome
+            var expectedResult = new NoSpecimen(nonUInt32Request);
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt32RequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var uInt32Request = typeof(uint);
+            var sut = new UInt32SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(uInt32Request, dummyContainer);
+            // Verify outcome
+            Assert.Equal(1u, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt32RequestWillReturnCorrectResultOnSecondCall()
+        {
+            // Fixture setup
+            var uInt32Request = typeof(uint);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<UInt32SequenceGenerator, uint>(sut => (uint)sut.Create(uInt32Request, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(2);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt32RequestWillReturnCorrectResultOnTenthCall()
+        {
+            // Fixture setup
+            var uInt32Request = typeof(uint);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<UInt32SequenceGenerator, uint>(sut => (uint)sut.Create(uInt32Request, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(10);
+            // Teardown
         }
     }
 }
