b7bb68b37ec7021b75b185c67cb2458e0edb77b2
Mark Seemann <mark@seemann.ms>
2010-06-17 12:36:51 +0200

    Made UInt16SequenceGenerator an ISpecimenBuilder.

--- Src/AutoFixtureUnitTest/UInt16SequenceGeneratorTest.cs
+++ Src/AutoFixtureUnitTest/UInt16SequenceGeneratorTest.cs
@@ -4,15 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Ploeh.AutoFixture;
 using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
 
 namespace Ploeh.AutoFixtureUnitTest
 {
     public class UInt16SequenceGeneratorTest
     {
-        public UInt16SequenceGeneratorTest()
-        {
-        }
-
         [Fact]
         public void CreateWillReturnOneOnFirstCall()
         {
@@ -29,6 +27,95 @@ namespace Ploeh.AutoFixtureUnitTest
         public void CreateWillReturnTenOnTenthCall()
         {
             new LoopTest<UInt16SequenceGenerator, ushort>(sut => sut.CreateAnonymous()).Execute(10);
+        }
+
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new UInt16SequenceGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var sut = new UInt16SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(null, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContainerDoesNotThrow()
+        {
+            // Fixture setup
+            var sut = new UInt16SequenceGenerator();
+            // Exercise system
+            var dummyRequest = new object();
+            sut.Create(dummyRequest, null);
+            // Verify outcome (no exception indicates success)
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNonUInt16RequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var nonUInt16Request = new object();
+            var sut = new UInt16SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(nonUInt16Request, dummyContainer);
+            // Verify outcome
+            var expectedResult = new NoSpecimen(nonUInt16Request);
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt16RequestWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var uInt16Request = typeof(ushort);
+            var sut = new UInt16SequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var result = sut.Create(uInt16Request, dummyContainer);
+            // Verify outcome
+            Assert.Equal((ushort)1, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt16RequestWillReturnCorrectResultOnSecondCall()
+        {
+            // Fixture setup
+            var uInt16Request = typeof(ushort);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<UInt16SequenceGenerator, ushort>(sut => (ushort)sut.Create(uInt16Request, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(2);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithUInt16RequestWillReturnCorrectResultOnTenthCall()
+        {
+            // Fixture setup
+            var uInt16Request = typeof(ushort);
+            var dummyContainer = new DelegatingSpecimenContainer();
+            var loopTest = new LoopTest<UInt16SequenceGenerator, ushort>(sut => (ushort)sut.Create(uInt16Request, dummyContainer));
+            // Exercise system and verify outcome
+            loopTest.Execute(10);
+            // Teardown
         }
     }
 }
