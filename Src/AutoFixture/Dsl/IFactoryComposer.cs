475205ef6abfcb8fa623e5e54ef31698dec5cf10
Mark Seemann <mark@seemann.ms>
2010-06-24 10:15:04 +0200

    Moved ISpecimenBuilderComposer from Dsl to Kernel namespace, as it looks like this interface will play a much more central role than was first assumed.

--- Src/AutoFixture/Dsl/IFactoryComposer.cs
+++ Src/AutoFixture/Dsl/IFactoryComposer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.Dsl
 {
--- Src/AutoFixture/SpecimenBuilderComposer.cs
+++ Src/AutoFixture/SpecimenBuilderComposer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Ploeh.AutoFixture.Dsl;
+﻿using Ploeh.AutoFixture.Dsl;
+using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
