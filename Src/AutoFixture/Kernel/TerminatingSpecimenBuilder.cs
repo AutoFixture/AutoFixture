59448cac7714e005ed6e60dbfe4a4d56f162d42c
Mark Seemann <mark@seemann.ms>
2010-06-30 14:52:10 +0200

    Added TerminatingSpecimenBuilder

---
+++ Src/AutoFixture/Kernel/TerminatingSpecimenBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Throws an <see cref="ObjectCreationException"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This <see cref="ISpecimenBuilder"/> can be used as the last item in a
+    /// <see cref="CompositeSpecimenBuilder"/> to throw an exception instead of letting the
+    /// containing builder return a <see cref="NoSpecimen"/> instance when it can't satisfy a
+    /// request.
+    /// </para>
+    /// </remarks>
+    public class TerminatingSpecimenBuilder : ISpecimenBuilder
+    {
+        #region ISpecimenBuilder Members
+
+        /// <summary>
+        /// Throws an <see cref="ObjectCreationException"/>.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">
+        /// A context that can be used to create other specimens. Not used.
+        /// </param>
+        /// <returns>
+        /// This method never returns. It always throws an <see cref="ObjectCreationException"/>.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            throw new ObjectCreationException(
+                string.Format(
+                    CultureInfo.CurrentCulture,
+                    "AutoFixture was unable to create an instance from {0}, most likely because it has no public constructor.",
+                    request
+                    )
+                );
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/TerminatingSpecimenBuilderTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class TerminatingSpecimenBuilderTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new TerminatingSpecimenBuilder();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateThrowsCorrectException()
+        {
+            // Fixture setup
+            var sut = new TerminatingSpecimenBuilder();
+            var dummyRequest = new object();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system and verify outcome
+            Assert.Throws<ObjectCreationException>(() =>
+                sut.Create(dummyRequest, dummyContext));
+            // Teardown
+        }
+
+        [Fact]
+        public void ExceptionContainsInformationAboutRequest()
+        {
+            // Fixture setup
+            var sut = new TerminatingSpecimenBuilder();
+            var request = Guid.NewGuid();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system
+            var e = Assert.Throws<ObjectCreationException>(() =>
+                sut.Create(request, dummyContext));
+            // Verify outcome
+            Assert.Contains(request.ToString(), e.Message);
+            // Teardown
+        }
+    }
+}
