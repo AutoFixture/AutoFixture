50355d1f8c8e61ee0f735182cb5a24cf0712bb56
Mark Seemann <mark@seemann.ms>
2010-06-30 15:52:49 +0200

    Added SeedRequestSpecification.

---
+++ Src/AutoFixture/Kernel/SeedRequestSpecification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// A specification that evaluates seeded requests for types against a target type.
+    /// </summary>
+    public class SeedRequestSpecification : IRequestSpecification
+    {
+        private readonly Type targetType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SeedRequestSpecification"/> class.
+        /// </summary>
+        /// <param name="type">The target type.</param>
+        public SeedRequestSpecification(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            this.targetType = type;
+        }
+
+        /// <summary>
+        /// Gets the type targeted by this <see cref="IRequestSpecification"/>.
+        /// </summary>
+        public Type TargetType
+        {
+            get { return this.targetType; }
+        }
+
+        #region IRequestSpecification Members
+
+        /// <summary>
+        /// Evaluates a request for a specimen.
+        /// </summary>
+        /// <param name="request">The specimen request.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="request"/> is a <see cref="SeededRequest"/>
+        /// for a type that matches <see cref="TargetType"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsSatisfiedBy(object request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            var sr = request as SeededRequest;
+            if (sr != null)
+            {
+                return this.TargetType.Equals(sr.Request);
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/SeedRequestSpecificationTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class SeedRequestSpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            // Exercise system
+            var sut = new SeedRequestSpecification(dummyType);
+            // Verify outcome
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullTypeThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new SeedRequestSpecification(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void TargetTypeIsCorrect()
+        {
+            // Fixture setup
+            var expectedType = typeof(DayOfWeek);
+            var sut = new SeedRequestSpecification(expectedType);
+            // Exercise system
+            Type result = sut.TargetType;
+            // Verify outcome
+            Assert.Equal(expectedType, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void IsSatisfiedByNullThrows()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            var sut = new SeedRequestSpecification(dummyType);
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.IsSatisfiedBy(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void IsSatisfiedByNonSeedReturnsCorrectResult()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            var sut = new SeedRequestSpecification(dummyType);
+            var nonSeedRequest = new object();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(nonSeedRequest);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(object), typeof(object), true)]
+        [InlineData(typeof(string), typeof(string), true)]
+        [InlineData(typeof(string), typeof(int), false)]
+        [InlineData(typeof(PropertyHolder<string>), typeof(FieldHolder<string>), false)]
+        public void IsSatisfiedByReturnsCorrectResult(Type specType, Type seedRequestType, bool expectedResult)
+        {
+            // Fixture setup
+            var sut = new SeedRequestSpecification(specType);
+            var dummySeed = new object();
+            var seededRequest = new SeededRequest(seedRequestType, dummySeed);
+            // Exercise system
+            var result = sut.IsSatisfiedBy(seededRequest);
+            // Verify outcome
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+    }
+}
