4d5cb01475834300e39b5e6e1c0c917c2920f7eb
Mark Seemann <mark@seemann.ms>
2010-07-28 09:53:14 +0200

    Added NoSpecimenOutputGuard to enable scenarios where the Build<T> method call chain should throw if it can't create specimens.

---
+++ Src/AutoFixture/Kernel/NoSpecimenOutputGuard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Prevents a decorated <see cref="ISpecimenBuilder"/> from returning a
+    /// <see cref="NoSpecimen"/> instance.
+    /// </summary>
+    public class NoSpecimenOutputGuard : ISpecimenBuilder
+    {
+        private readonly ISpecimenBuilder builder;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoSpecimenOutputGuard"/> class with an 
+        /// <see cref="ISpecimenBuilder"/> to decorate.
+        /// </summary>
+        /// <param name="builder">The builder to decorate.</param>
+        public NoSpecimenOutputGuard(ISpecimenBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+
+            this.builder = builder;
+        }
+
+        /// <summary>
+        /// Gets the decorated builder.
+        /// </summary>
+        /// <value>The <see cref="ISpecimenBuilder"/> supplied via the constructor.</value>
+        /// <seealso cref="NoSpecimenOutputGuard"/>
+        public ISpecimenBuilder Builder
+        {
+            get { return this.builder; }
+        }
+
+        #region ISpecimenBuilder Members
+
+        /// <summary>
+        /// Creates a new specimen by delegating to the decorated <see cref="Builder"/>.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise an exception is thrown.
+        /// </returns>
+        /// <exception cref="ObjectCreationException">
+        /// The decorated <see cref="Builder"/> returned a <see cref="NoSpecimen"/> result.
+        /// </exception>
+        public object Create(object request, ISpecimenContext context)
+        {
+            var result = this.Builder.Create(request, context);
+            if (result is NoSpecimen)
+            {
+                throw new ObjectCreationException(string.Format(CultureInfo.CurrentCulture, "The decorated ISpecimenBuilder could not create a specimen based on the request: {0}. This can happen if the request represents an interface or abstract class; if this is the case, register an ISpecimenBuilder that can create specimens based on the request. If this happens in a strongly typed Build<T> expression, try supplying a factory using one of the IFactoryComposer<T> methods.", request));
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/NoSpecimenOutputGuardTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class NoSpecimenOutputGuardTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            // Exercise system
+            var sut = new NoSpecimenOutputGuard(dummyBuilder);
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullBuilderThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new NoSpecimenOutputGuard(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void BuilderIsCorrect()
+        {
+            // Fixture setup
+            var expectedBuilder = new DelegatingSpecimenBuilder();
+            var sut = new NoSpecimenOutputGuard(expectedBuilder);
+            // Exercise system
+            ISpecimenBuilder result = sut.Builder;
+            // Verify outcome
+            Assert.Equal(expectedBuilder, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectResult()
+        {
+            // Fixture setup
+            var request = new object();
+            var context = new DelegatingSpecimenContext();
+            var expectedResult = new object();
+            var builder = new DelegatingSpecimenBuilder { OnCreate = (r, c) => r == request && c == context ? expectedResult : new NoSpecimen(r) };
+
+            var sut = new NoSpecimenOutputGuard(builder);
+            // Exercise system
+            var result = sut.Create(request, context);
+            // Verify outcome
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateThrowsWhenDecoratedBuilderReturnsNoSpecimen()
+        {
+            // Fixture setup
+            var builder = new DelegatingSpecimenBuilder { OnCreate = (r, c) => new NoSpecimen(r) };
+            var sut = new NoSpecimenOutputGuard(builder);
+            // Exercise system and verify outcome
+            var dummyRequest = new object();
+            var dummyContext = new DelegatingSpecimenContext();
+            Assert.Throws<ObjectCreationException>(() =>
+                sut.Create(dummyRequest, dummyContext));
+            // Teardown
+        }
+    }
+}
