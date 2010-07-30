c22c7cc80b84ff4b7c9c4fe593b302f10b8908bb
Mark Seemann <mark@seemann.ms>
2010-07-30 12:31:05 +0200

    First, very primitive, proof-of-concept of AutoData.

---
+++ Src/AutoFixture.xUnit.net.UnitTest/AutoDataAttributeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class AutoDataAttributeTest
+    {
+        [Fact]
+        public void SutIsDataAttribute()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new AutoDataAttribute();
+            // Verify outcome
+            Assert.IsAssignableFrom<DataAttribute>(sut);
+            // Teardown
+        }
+    }
+}
---
+++ Src/AutoFixture.xUnit.net.UnitTest/Scenario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class Scenario
+    {
+        [Theory, AutoData]
+        public void AutoDataProvidesCorrectInteger(int primitiveValue)
+        {
+            Assert.NotEqual(0, primitiveValue);
+        }
+
+        [Theory, AutoData]
+        public void AutoDataProvidesCorrectString(string text)
+        {
+            Assert.True(text.StartsWith("text"));
+        }
+
+        [Theory, AutoData]
+        public void AutoDataProvidesCorrectObject(PropertyHolder<Version> ph)
+        {
+            Assert.NotNull(ph);
+            Assert.NotNull(ph.Property);
+        }
+
+        [Theory, AutoData]
+        public void AutoDataProvidesMultipleObjects(PropertyHolder<Version> ph, SingleParameterType<OperatingSystem> spt)
+        {
+            Assert.NotNull(ph);
+            Assert.NotNull(ph.Property);
+
+            Assert.NotNull(spt);
+            Assert.NotNull(spt.Parameter);
+        }
+    }
+}
