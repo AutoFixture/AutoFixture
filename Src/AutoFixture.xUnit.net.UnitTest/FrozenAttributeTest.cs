0a04caa49c1345b2c027c6d0051c2f3ce7c94e75
Mark Seemann <mark@seemann.ms>
2010-08-02 11:16:37 +0200

    Added FrozenAttribute, but did not yet implement anything because it waits for ICustomization that should be implemented in the default branch.

---
+++ Src/AutoFixture.xUnit.net.UnitTest/FrozenAttributeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class FrozenAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new FrozenAttribute();
+            // Verify outcome
+            Assert.IsAssignableFrom<Attribute>(sut);
+            // Teardown
+        }
+    }
+}
