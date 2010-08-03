0b296ec1718f5350b5bd31dc3b1194b3586f2708
Mark Seemann <mark@seemann.ms>
2010-08-03 11:20:18 +0200

    Added FixedBuilder, which can be used to implement weakly typed Freeze functionality.

---
+++ Src/AutoFixtureUnitTest/Kernel/FixedBuilderTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class FixedBuilderTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            var dummySpecimen = new object();
+            // Exercise system
+            var sut = new FixedBuilder(dummySpecimen);
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectResult()
+        {
+            // Fixture setup
+            var expectedSpecimen = new object();
+            var sut = new FixedBuilder(expectedSpecimen);
+            // Exercise system
+            var dummyRequest = new object();
+            var dummyContext = new DelegatingSpecimenContext();
+            var result = sut.Create(dummyRequest, dummyContext);
+            // Verify outcome
+            Assert.Equal(expectedSpecimen, result);
+            // Teardown
+        }
+    }
+}
