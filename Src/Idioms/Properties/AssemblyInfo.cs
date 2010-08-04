186e28184b02d99118b65aebb247ee66831ec36a
Mark Seemann <mark@seemann.ms>
2010-08-04 07:42:28 +0200

    Updated assembly version numbers to 2.0.0.1 (the "1" signifies the beta version, for lack of a better scheme).

--- Src/Idioms/Properties/AssemblyInfo.cs
+++ Src/Idioms/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Idioms")]
+[assembly: AssemblyProduct("AutoFixture Idioms")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
