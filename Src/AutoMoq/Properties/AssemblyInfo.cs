f1dfe38df6b27d38042b56a2ee7fd06934906883
Mark Seemann <mark@seemann.ms>
2010-10-08 22:10:41 +0200

    Updated assembly version numbers to 2.0.0.2 (the "2" signifies the RTW version, for lack of a better scheme).

--- Src/AutoMoq/Properties/AssemblyInfo.cs
+++ Src/AutoMoq/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.2")]
 
 [assembly: CLSCompliant(true)]
