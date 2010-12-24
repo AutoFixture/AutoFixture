0d3688a1b3226228028806875afbb77ed85918da
Mark Seemann <mark@seemann.ms>
2010-12-24 15:20:23 +0100

    Added DefaultBoundaryConvention as preparation for removing IBoundaryConventionFactory and derived types.

---
+++ Src/Idioms/DefaultBoundaryConvention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class DefaultBoundaryConvention : CompositeBoundaryConvention
+    {
+        public DefaultBoundaryConvention()
+            : base(
+                new GuidBoundaryConvention(),
+                new ValueTypeBoundaryConvention(),
+                new StringBoundaryConvention(),
+                new ReferenceTypeBoundaryConvention())
+        {
+        }
+    }
+}
---
+++ Src/IdiomsUnitTest/DefaultBoundaryConventionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class DefaultBoundaryConventionTest
+    {
+        [Fact]
+        public void SutIsConvention()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new DefaultBoundaryConvention();
+            // Verify outcome
+            Assert.IsAssignableFrom<CompositeBoundaryConvention>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void ConventionTypesAreCorrect()
+        {
+            // Fixture setup
+            var expected = new[]{
+                typeof(GuidBoundaryConvention),
+                typeof(ValueTypeBoundaryConvention),
+                typeof(StringBoundaryConvention),
+                typeof(ReferenceTypeBoundaryConvention)
+            };
+
+            var sut = new DefaultBoundaryConvention();
+            // Exercise system
+            // Verify outcome
+            var result = (from c in sut.Conventions
+                          select c.GetType()).ToList();
+            Assert.True(expected.SequenceEqual(result));
+            // Teardown
+        }
+    }
+}
