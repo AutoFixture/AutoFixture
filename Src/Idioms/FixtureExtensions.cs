14cbaf6f85d63795ff99deda50f031bf5d5d73ad
Mark Seemann <mark@seemann.ms>
2010-12-27 09:53:43 +0100

    Switched off Code Analysis rule 1006 (about nested generics) as this rule is turned off for the rest of AutoFixture too.

--- Src/Idioms/FixtureExtensions.cs
+++ Src/Idioms/FixtureExtensions.cs
@@ -5,7 +5,6 @@ namespace Ploeh.AutoFixture.Idioms
 {
     public static class FixtureExtensions
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Nested generics and Expression's is a very happy marriage.")]
         public static PickedProperty<T, TProperty> PickProperty<T, TProperty>(this IFixture fixture, Expression<Func<T, TProperty>> property)
         {
             if (property == null)
