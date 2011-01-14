6a4eb4867874b0fc128479b9f4b2980248b99078
Mark Seemann <mark@seemann.ms>
2011-01-14 10:40:39 +0100

    Added EnumerableFavoringConstructorQuery. The purpose of this query is to favor constructors that takes an IEnumerable<T> input. The primary customer is intended to be List<T>(IEnumerable<T>).

---
+++ Src/AutoFixture/Kernel/EnumerableFavoringConstructorQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class EnumerableFavoringConstructorQuery : IConstructorQuery
+    {
+        #region IConstructorQuery Members
+
+        public IEnumerable<IMethod> SelectConstructors(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            return from ci in type.GetConstructors()
+                   let parameters = ci.GetParameters()
+                   orderby EnumerableFavoringConstructorQuery.Score(type, parameters) descending
+                   select new ConstructorMethod(ci) as IMethod;
+        }
+
+        #endregion
+
+        private static int Score(Type type, ParameterInfo[] parameters)
+        {
+            var genericParameterTypes = type.GetGenericArguments();
+            if (genericParameterTypes.Length != 1)
+            {
+                return 0;
+            }
+            var genericParameterType = genericParameterTypes.Single();
+
+            var enumerableType = typeof(IEnumerable<>).MakeGenericType(genericParameterType);
+            return parameters.Count(p => enumerableType.IsAssignableFrom(p.ParameterType));
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/EnumerableFavoringConstructorQueryTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.TestTypeFoundation;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class EnumerableFavoringConstructorQueryTest
+    {
+        [Fact]
+        public void SutIsConstructorQuery()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new EnumerableFavoringConstructorQuery();
+            // Verify outcome
+            Assert.IsAssignableFrom<IConstructorQuery>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void SelectConstructorsFromNullTypeThrows()
+        {
+            // Fixture setup
+            var sut = new EnumerableFavoringConstructorQuery();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.SelectConstructors(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void SelectFromTypeWithNoPublicConstructorReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new EnumerableFavoringConstructorQuery();
+            var typeWithNoPublicConstructors = typeof(AbstractType);
+            // Exercise system
+            var result = sut.SelectConstructors(typeWithNoPublicConstructors);
+            // Verify outcome
+            Assert.False(result.Any());
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(SingleParameterType<object>))]
+        [InlineData(typeof(ConcreteType))]
+        [InlineData(typeof(MultiUnorderedConstructorType))]
+        public void SelectFromTypeReturnsAllAppropriateResults(Type type)
+        {
+            // Fixture setup
+            var expectedConstructors = from ci in type.GetConstructors()
+                                       let parameters = ci.GetParameters()
+                                       select new ConstructorMethod(ci) as IMethod;
+
+            var sut = new EnumerableFavoringConstructorQuery();
+            // Exercise system
+            var result = sut.SelectConstructors(type);
+            // Verify outcome
+            Assert.True(expectedConstructors.All(m => result.Any(m.Equals)));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(List<object>))]
+        [InlineData(typeof(List<string>))]
+        [InlineData(typeof(List<int>))]
+        [InlineData(typeof(List<Version>))]
+        [InlineData(typeof(HashSet<object>))]
+        [InlineData(typeof(HashSet<string>))]
+        [InlineData(typeof(HashSet<int>))]
+        [InlineData(typeof(HashSet<Version>))]
+        public void SelectConstructorsFromTypeReturnsFirstMethodThatTakesEnumerableAsArgument(Type type)
+        {            
+            var sut = new EnumerableFavoringConstructorQuery();
+            // Exercise system
+            var result = sut.SelectConstructors(type);
+            // Verify outcome
+            var genericParameterType = type.GetGenericArguments().Single();
+            Assert.True(result.First().Parameters.Any(p => typeof(IEnumerable<>).MakeGenericType(genericParameterType).IsAssignableFrom(p.ParameterType)));
+            // Teardown
+        }
+    }
+}
