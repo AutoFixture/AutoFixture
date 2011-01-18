fc78333a4acb177f24984f358254466b687c38f2
Mark Seemann <mark@seemann.ms>
2011-01-18 15:24:12 +0100

    Added EnumerableSpecification to have a reusable filter to test whether a request is a request for a constructed IEnumerable<T>

---
+++ Src/AutoFixture/Kernel/EnumerableSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class EnumerableSpecification : IRequestSpecification
+    {
+        #region IRequestSpecification Members
+
+        public bool IsSatisfiedBy(object request)
+        {
+            var type = request as Type;
+            if (type == null)
+            {
+                return false;
+            }
+
+            return type.IsGenericType
+                && typeof(IEnumerable<>) == type.GetGenericTypeDefinition();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/EnumerableSpecificationTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class EnumerableSpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new EnumerableSpecification();
+            // Verify outcome
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(1)]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(Version))]
+        [InlineData(typeof(object[]))]
+        [InlineData(typeof(string[]))]
+        [InlineData(typeof(int[]))]
+        [InlineData(typeof(Version[]))]
+        public void IsSatisfiedByNonEnumerableRequestReturnsCorrectResult(object request)
+        {
+            // Fixture setup
+            var sut = new EnumerableSpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(IEnumerable<object>))]
+        [InlineData(typeof(IEnumerable<string>))]
+        [InlineData(typeof(IEnumerable<int>))]
+        [InlineData(typeof(IEnumerable<Version>))]
+        public void IsSatisfiedByEnumerableRequestReturnsCorrectResult(Type request)
+        {
+            // Fixture setup
+            var sut = new EnumerableSpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.True(result);
+            // Teardown
+        }
+    }
+}
