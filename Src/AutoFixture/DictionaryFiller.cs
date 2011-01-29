0d65d68dad9ddec2c8d06f9ebfb777bf6011cc59
Mark Seemann <mark@seemann.ms>
2011-01-29 14:21:47 +0100

    Added DictionaryFiller to implement automatick population of dictionaries. Currently this class looks rather different than CollectionFiller, but it's conceivable that they may converge in functionality later.

---
+++ Src/AutoFixture/DictionaryFiller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+using System.Collections;
+
+namespace Ploeh.AutoFixture
+{
+    public static class DictionaryFiller
+    {
+        public static void AddMany(object specimen, ISpecimenContext context)
+        {
+            if (specimen == null)
+            {
+                throw new ArgumentNullException("specimen");
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var typeArguments = specimen.GetType().GetGenericArguments();
+            if (typeArguments.Length != 2)
+            {
+                throw new ArgumentException("The specimen must be an instance of IDictionary<TKey, TValue>.", "specimen");
+            }
+
+            if (!typeof(IDictionary<,>).MakeGenericType(typeArguments).IsAssignableFrom(specimen.GetType()))
+            {
+                throw new ArgumentException("The specimen must be an instance of IDictionary<TKey, TValue>.", "specimen");
+            }
+
+            var kvpType = typeof(KeyValuePair<,>).MakeGenericType(typeArguments);
+            var enumerable = context.Resolve(new MultipleRequest(kvpType)) as IEnumerable;
+            foreach (var item in enumerable)
+            {
+                var addMethod = typeof(ICollection<>).MakeGenericType(kvpType).GetMethod("Add", new[] { kvpType });
+                addMethod.Invoke(specimen, new[] { item });
+            }
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/DictionaryFillerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Ploeh.AutoFixture;
+using Xunit.Extensions;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class DictionaryFillerTest
+    {
+        [Fact]
+        public void AddManyToNullSpecimenThrows()
+        {
+            // Fixture setup
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                DictionaryFiller.AddMany(null, dummyContext));
+            // Teardown
+        }
+
+        [Fact]
+        public void AddManyWithNullContextThrows()
+        {
+            // Fixture setup
+            var dummyDictionary = new Dictionary<object, object>();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                DictionaryFiller.AddMany(dummyDictionary, null));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(1)]
+        [InlineData(true)]
+        [InlineData(false)]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        public void AddManyToNonDictionaryThrows(object specimen)
+        {
+            // Fixture setup
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentException>(() =>
+                DictionaryFiller.AddMany(specimen, dummyContext));
+            // Teardown
+        }
+
+        [Fact]
+        public void AddManyFillsDictionary()
+        {
+            // Fixture setup
+            var dictionary = new Dictionary<int, string>();
+
+            var expectedRequest = new MultipleRequest(typeof(KeyValuePair<int, string>));
+            var expectedResult = Enumerable.Range(1, 3).Select(i => new KeyValuePair<int, string>(i, i.ToString()));
+            var context = new DelegatingSpecimenContext { OnResolve = r => expectedRequest.Equals(r) ? (object)expectedResult : new NoSpecimen(r) };
+            // Exercise system
+            DictionaryFiller.AddMany(dictionary, context);
+            // Verify outcome
+            Assert.True(expectedResult.SequenceEqual(dictionary));
+            // Teardown
+        }
+    }
+}
