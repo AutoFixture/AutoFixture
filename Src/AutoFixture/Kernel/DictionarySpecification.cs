041ab1b2d5aee2d0fcbaaa5b3e0f362cb4c8d24c
Mark Seemann <mark@seemann.ms>
2011-01-29 15:04:00 +0100

    Added DictionarySpecification to identify requests for any constructed Dictionary<TKey, TValue>.

---
+++ Src/AutoFixture/Kernel/DictionarySpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class DictionarySpecification : IRequestSpecification
+    {
+        #region IRequestSpecification Members
+
+        public bool IsSatisfiedBy(object request)
+        {
+            var type = request as Type;
+            if (type == null)
+            {
+                return false;
+            }
+
+            return type.IsGenericType
+                && typeof(Dictionary<,>) == type.GetGenericTypeDefinition();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/DictionarySpecificationTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class DictionarySpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new DictionarySpecification();
+            // Verify outcome
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(1)]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(Version))]
+        [InlineData(typeof(object[]))]
+        [InlineData(typeof(string[]))]
+        [InlineData(typeof(int[]))]
+        [InlineData(typeof(Version[]))]
+        public void IsSatisfiedByNonDictionaryRequestReturnsCorrectResult(object request)
+        {
+            // Fixture setup
+            var sut = new DictionarySpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(Dictionary<object, object>))]
+        [InlineData(typeof(Dictionary<int, string>))]
+        [InlineData(typeof(Dictionary<string, int>))]
+        [InlineData(typeof(Dictionary<Version, OperatingSystem>))]
+        public void IsSatisfiedByDictionaryRequestReturnsCorrectResult(Type request)
+        {
+            // Fixture setup
+            var sut = new DictionarySpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.True(result);
+            // Teardown
+        }
+    }
+}
