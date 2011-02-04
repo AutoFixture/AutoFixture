73431c5869cd5d7a6eb6394b94c93c425946613d
Mark Seemann <mark@seemann.ms>
2011-02-04 21:00:21 +0100

    Loosened the API of FixtureExtensions to work with any ISpecimenBuilderComposer.

--- Src/Idioms/FixtureExtensions.cs
+++ Src/Idioms/FixtureExtensions.cs
@@ -2,63 +2,64 @@ using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.Idioms
 {
     public static class FixtureExtensions
     {
-        public static PropertyContext ForProperty<T, TProperty>(this IFixture fixture, Expression<Func<T, TProperty>> property)
+        public static PropertyContext ForProperty<T, TProperty>(this ISpecimenBuilderComposer composer, Expression<Func<T, TProperty>> property)
         {
             var propertyInfo = Reflect<T>.GetProperty(property);
-            return fixture.ForProperty(propertyInfo);
+            return composer.ForProperty(propertyInfo);
         }
 
-        public static PropertyContext ForProperty(this IFixture fixture, PropertyInfo propertyInfo)
+        public static PropertyContext ForProperty(this ISpecimenBuilderComposer composer, PropertyInfo propertyInfo)
         {
-            if (fixture == null)
+            if (composer == null)
             {
-                throw new ArgumentNullException("fixture");
+                throw new ArgumentNullException("composer");
             }
             if (propertyInfo == null)
             {
                 throw new ArgumentNullException("propertyInfo");
             }
 
-            return new PropertyContext(fixture, propertyInfo);
+            return new PropertyContext(composer, propertyInfo);
         }
 
-        public static MethodContext ForMethod<T>(this IFixture fixture, Expression<Action<T>> methodPicker)
+        public static MethodContext ForMethod<T>(this ISpecimenBuilderComposer composer, Expression<Action<T>> methodPicker)
         {
-            return fixture.ForMethod(Reflect<T>.GetMethod(methodPicker));
+            return composer.ForMethod(Reflect<T>.GetMethod(methodPicker));
         }
 
-        public static MethodContext ForMethod<T, TInput>(this IFixture fixture, Expression<Action<T, TInput>> methodPicker)
+        public static MethodContext ForMethod<T, TInput>(this ISpecimenBuilderComposer composer, Expression<Action<T, TInput>> methodPicker)
         {
-            return fixture.ForMethod(Reflect<T>.GetMethod(methodPicker));
+            return composer.ForMethod(Reflect<T>.GetMethod(methodPicker));
         }
 
-        public static MethodContext ForMethod<T, TInput1, TInput2>(this IFixture fixture, Expression<Action<T, TInput1, TInput2>> methodPicker)
+        public static MethodContext ForMethod<T, TInput1, TInput2>(this ISpecimenBuilderComposer composer, Expression<Action<T, TInput1, TInput2>> methodPicker)
         {
-            return fixture.ForMethod(Reflect<T>.GetMethod(methodPicker));
+            return composer.ForMethod(Reflect<T>.GetMethod(methodPicker));
         }
 
-        public static MethodContext ForMethod<T, TInput1, TInput2, TInput3>(this IFixture fixture, Expression<Action<T, TInput1, TInput2, TInput3>> methodPicker)
+        public static MethodContext ForMethod<T, TInput1, TInput2, TInput3>(this ISpecimenBuilderComposer composer, Expression<Action<T, TInput1, TInput2, TInput3>> methodPicker)
         {
-            return fixture.ForMethod(Reflect<T>.GetMethod(methodPicker));
+            return composer.ForMethod(Reflect<T>.GetMethod(methodPicker));
         }
 
-        public static MethodContext ForMethod(this IFixture fixture, MethodBase methodBase)
+        public static MethodContext ForMethod(this ISpecimenBuilderComposer composer, MethodBase methodBase)
         {
-            if (fixture == null)
+            if (composer == null)
             {
-                throw new ArgumentNullException("fixture");
+                throw new ArgumentNullException("composer");
             }
             if (methodBase == null)
             {
                 throw new ArgumentNullException("methodBase");
             }
 
-            return new MethodContext(fixture, methodBase);
+            return new MethodContext(composer, methodBase);
         }
     }
 }
