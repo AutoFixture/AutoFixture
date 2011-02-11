651aa8b63279daf196273a425ba1e119324c4d65
Mark Seemann <mark@seemann.ms>
2011-02-11 10:44:41 +0100

    Added CompositeMethodContext to enable sweeping assertions on a set of IMethodContexts.

---
+++ Src/Idioms/CompositeMethodContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class CompositeMethodContext : IMethodContext
+    {
+        private readonly IEnumerable<IMethodContext> methodContexts;
+
+        public CompositeMethodContext(params IMethodContext[] methodContexts)
+        {
+            if (methodContexts == null)
+            {
+                throw new ArgumentNullException("methodContexts");
+            }
+
+            this.methodContexts = methodContexts;
+        }
+
+        public CompositeMethodContext(IEnumerable<IMethodContext> methodContexts)
+            : this(methodContexts.ToArray())
+        {
+        }
+
+        public IEnumerable<IMethodContext> MethodContexts
+        {
+            get { return this.methodContexts; }
+        }
+
+        #region IMemberContext Members
+
+        public void VerifyBoundaries(IBoundaryConvention convention)
+        {
+            if (convention == null)
+            {
+                throw new ArgumentNullException("convention");
+            }
+
+            foreach (var ctx in this.MethodContexts)
+            {
+                ctx.VerifyBoundaries(convention);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/DelegatingMethodContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class DelegatingMethodContext : IMethodContext
+    {
+        public DelegatingMethodContext()
+        {
+            this.OnVerifyBoundaries = convention => { };
+        }
+
+        public Action<IBoundaryConvention> OnVerifyBoundaries { get; set; }
+
+        #region IMemberContext Members
+
+        public void VerifyBoundaries(IBoundaryConvention convention)
+        {
+            this.OnVerifyBoundaries(convention);
+        }
+
+        #endregion
+    }
+}
