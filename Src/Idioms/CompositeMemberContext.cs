67544fb7e272b68b0c9fd608194ba1c12a172243
Mark Seemann <mark@seemann.ms>
2011-03-04 14:36:31 +0100

    Added CompositeMemberContext to support sweeping assertions of a complete class at a time.

---
+++ Src/Idioms/CompositeMemberContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class CompositeMemberContext : IMemberContext
+    {
+        private readonly IEnumerable<IMemberContext> memberContexts;
+
+        public CompositeMemberContext(params IMemberContext[] memberContexts)
+        {
+            if (memberContexts == null)
+            {
+                throw new ArgumentNullException("memberContexts");
+            }
+
+            this.memberContexts = memberContexts;
+        }
+
+        public CompositeMemberContext(IEnumerable<IMemberContext> memberContexts)
+            : this(memberContexts.ToArray())
+        {
+        }
+
+        public IEnumerable<IMemberContext> MemberContexts
+        {
+            get { return this.memberContexts; }
+        }
+
+        #region IMemberContext Members
+
+        public void VerifyBoundaries(IBoundaryConvention convention)
+        {
+            if (convention == null)
+            {
+                throw new ArgumentNullException("convention");
+            }
+
+            foreach (var ctx in this.memberContexts)
+            {
+                ctx.VerifyBoundaries(convention);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/CompositeMemberContextTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class CompositeMemberContextTest
+    {
+        [Fact]
+        public void SutIsMethodContext()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new CompositeMemberContext();
+            // Verify outcome
+            Assert.IsAssignableFrom<IMemberContext>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void MethodContextsIsCorrectWhenInitializedByArray()
+        {
+            // Fixture setup
+            var expectedMethodContexts = new[] { new DelegatingMethodContext(), new DelegatingMethodContext(), new DelegatingMethodContext() };
+            var sut = new CompositeMemberContext(expectedMethodContexts);
+            // Exercise system
+            IEnumerable<IMemberContext> result = sut.MemberContexts;
+            // Verify outcome
+            Assert.True(expectedMethodContexts.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullArrayThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new CompositeMemberContext((IMemberContext[])null));
+            // Teardown
+        }
+
+        [Fact]
+        public void MethodContextIsCorrectWhenInitializedByEnumerable()
+        {
+            // Fixture setup
+            var expectedMethodContexts = new[] { new DelegatingMemberContext(), new DelegatingMemberContext(), new DelegatingMemberContext() }.AsEnumerable().Cast<IMemberContext>();
+            var sut = new CompositeMemberContext(expectedMethodContexts);
+            // Exercise system
+            var result = sut.MemberContexts;
+            // Verify outcome
+            Assert.True(expectedMethodContexts.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullEnumerableThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new CompositeMemberContext((IEnumerable<IMemberContext>)null));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyBoundariesVerifiesBoundariesForAllContexts()
+        {
+            // Fixture setup
+            var convention = new DelegatingBoundaryConvention();
+            var invocationCount = 0;
+            var contexts = new[]
+                {
+                    new DelegatingMemberContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}},
+                    new DelegatingMemberContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}},
+                    new DelegatingMemberContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}}
+                };
+            var sut = new CompositeMemberContext(contexts);
+            // Exercise system
+            sut.VerifyBoundaries(convention);
+            // Verify outcome
+            Assert.Equal(contexts.Length, invocationCount);
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyBoundariesWithNullConventionThrows()
+        {
+            // Fixture setup
+            var sut = new CompositeMemberContext();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.VerifyBoundaries(null));
+            // Teardown
+        }
+    }
+}
