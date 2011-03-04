4c41357b02afcab044c75e30212fb0e3d385df72
Mark Seemann <mark@seemann.ms>
2011-03-04 15:06:31 +0100

    Added CompositePropertyContext to support swepping assertions across all properties of a class.

---
+++ Src/Idioms/CompositePropertyContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class CompositePropertyContext : IPropertyContext
+    {
+        private readonly IEnumerable<IPropertyContext> propertyContexts;
+
+        public CompositePropertyContext(params IPropertyContext[] propertyContexts)
+        {
+            if (propertyContexts == null)
+            {
+                throw new ArgumentNullException("propertyContexts");
+            }
+
+            this.propertyContexts = propertyContexts;
+        }
+
+        public CompositePropertyContext(IEnumerable<IPropertyContext> propertyContexts)
+            : this(propertyContexts.ToArray())
+        {
+        }
+
+        public IEnumerable<IPropertyContext> PropertyContexts
+        {
+            get { return this.propertyContexts; }
+        }
+
+        #region IPropertyContext Members
+
+        public void VerifyWritable()
+        {
+            foreach (var ctx in this.propertyContexts)
+            {
+                ctx.VerifyWritable();
+            }
+        }
+
+        #endregion
+
+        #region IMemberContext Members
+
+        public void VerifyBoundaries(IBoundaryConvention convention)
+        {
+            if (convention == null)
+            {
+                throw new ArgumentNullException("convention");
+            }
+
+            foreach (var ctx in this.propertyContexts)
+            {
+                ctx.VerifyBoundaries(convention);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/CompositePropertyContextTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class CompositePropertyContextTest
+    {
+        [Fact]
+        public void SutIsMethodContext()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new CompositePropertyContext();
+            // Verify outcome
+            Assert.IsAssignableFrom<IPropertyContext>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void MethodContextsIsCorrectWhenInitializedByArray()
+        {
+            // Fixture setup
+            var expectedPropertyContexts = new[] { new DelegatingPropertyContext(), new DelegatingPropertyContext(), new DelegatingPropertyContext() };
+            var sut = new CompositePropertyContext(expectedPropertyContexts);
+            // Exercise system
+            IEnumerable<IPropertyContext> result = sut.PropertyContexts;
+            // Verify outcome
+            Assert.True(expectedPropertyContexts.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullArrayThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new CompositePropertyContext((IPropertyContext[])null));
+            // Teardown
+        }
+
+        [Fact]
+        public void MethodContextIsCorrectWhenInitializedByEnumerable()
+        {
+            // Fixture setup
+            var expectedPropertyContexts = new[] { new DelegatingPropertyContext(), new DelegatingPropertyContext(), new DelegatingPropertyContext() }.AsEnumerable().Cast<IPropertyContext>();
+            var sut = new CompositePropertyContext(expectedPropertyContexts);
+            // Exercise system
+            var result = sut.PropertyContexts;
+            // Verify outcome
+            Assert.True(expectedPropertyContexts.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullEnumerableThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new CompositePropertyContext((IEnumerable<IPropertyContext>)null));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyBoundariesVerifiesBoundariesForAllContexts()
+        {
+            // Fixture setup
+            var convention = new DelegatingBoundaryConvention();
+            var invocationCount = 0;
+            var contexts = new[]
+                {
+                    new DelegatingPropertyContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}},
+                    new DelegatingPropertyContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}},
+                    new DelegatingPropertyContext{ OnVerifyBoundaries = c => { if (c == convention){ invocationCount++;}}}
+                };
+            var sut = new CompositePropertyContext(contexts);
+            // Exercise system
+            sut.VerifyBoundaries(convention);
+            // Verify outcome
+            Assert.Equal(contexts.Length, invocationCount);
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyBoundariesWithNullConventionThrows()
+        {
+            // Fixture setup
+            var sut = new CompositePropertyContext();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.VerifyBoundaries(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyWritableVerifiesWritableForAllContexts()
+        {
+            // Fixture setup
+            var invocations = new List<int>();
+            var contexts = new[]
+                {
+                    new DelegatingPropertyContext{ OnVerifyWritable = () => invocations.Add(1) },
+                    new DelegatingPropertyContext{ OnVerifyWritable = () => invocations.Add(2) },
+                    new DelegatingPropertyContext{ OnVerifyWritable = () => invocations.Add(3) }
+                };
+            var sut = new CompositePropertyContext(contexts);
+            // Exercise system
+            sut.VerifyWritable();
+            // Verify outcome
+            Assert.True(Enumerable.Range(1, 3).SequenceEqual(invocations));
+            // Teardown
+        }
+    }
+}
