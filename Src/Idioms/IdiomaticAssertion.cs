edd00208789fa91d0546b7f0c09fa315acb1f475
Mark Seemann <mark@seemann.ms>
2011-05-21 10:29:24 -0700

    Added IdiomaticAssertion and associated interface as a placeholder. Not yet implemented.

---
+++ Src/Idioms/IdiomaticAssertion.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public abstract class IdiomaticAssertion : IIdiomaticAssertion
+    {
+        #region IIdiomaticAssertion Members
+
+        public void Verify(System.Reflection.Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(params Type[] types)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(IEnumerable<Type> types)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(params System.Reflection.MemberInfo[] memberInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(IEnumerable<System.Reflection.MemberInfo> memberInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(System.Reflection.MemberInfo memberInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(params System.Reflection.ConstructorInfo[] constructorInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(IEnumerable<System.Reflection.ConstructorInfo> constructorInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(System.Reflection.ConstructorInfo constructorInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(params System.Reflection.MethodInfo[] methodInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(IEnumerable<System.Reflection.MethodInfo> methodInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(System.Reflection.MethodInfo methodInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(params System.Reflection.PropertyInfo[] propertyInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(IEnumerable<System.Reflection.PropertyInfo> propertyInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(System.Reflection.PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/IdiomaticAssertionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class IdiomaticAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticAssertion()
+        {
+            // Fixture setup
+            // Exercise system
+            // Verify outcome
+            Assert.True(typeof(IIdiomaticAssertion).IsAssignableFrom(typeof(IdiomaticAssertion)));
+            // Teardown
+        }
+
+        [Fact]
+        public void TestableSutIsSut()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new DelegatingIdiomaticAssertion();
+            // Verify outcome
+            Assert.IsAssignableFrom<IdiomaticAssertion>(sut);
+            // Teardown
+        }
+    }
+}
