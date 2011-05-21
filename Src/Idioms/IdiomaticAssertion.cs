f30e6f472605d331c1ad16b9db30216b7ed2ae4e
Mark Seemann <mark@seemann.ms>
2011-05-21 10:32:13 -0700

    Removed all the NotImplementedExceptions since they aren't necessary for void methods.

--- Src/Idioms/IdiomaticAssertion.cs
+++ Src/Idioms/IdiomaticAssertion.cs
@@ -11,82 +11,66 @@ namespace Ploeh.AutoFixture.Idioms
 
         public void Verify(System.Reflection.Assembly assembly)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(params Type[] types)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(IEnumerable<Type> types)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(Type type)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(params System.Reflection.MemberInfo[] memberInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(IEnumerable<System.Reflection.MemberInfo> memberInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(System.Reflection.MemberInfo memberInfo)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(params System.Reflection.ConstructorInfo[] constructorInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(IEnumerable<System.Reflection.ConstructorInfo> constructorInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(System.Reflection.ConstructorInfo constructorInfo)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(params System.Reflection.MethodInfo[] methodInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(IEnumerable<System.Reflection.MethodInfo> methodInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(System.Reflection.MethodInfo methodInfo)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(params System.Reflection.PropertyInfo[] propertyInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(IEnumerable<System.Reflection.PropertyInfo> propertyInfos)
         {
-            throw new NotImplementedException();
         }
 
         public void Verify(System.Reflection.PropertyInfo propertyInfo)
         {
-            throw new NotImplementedException();
         }
 
         #endregion
