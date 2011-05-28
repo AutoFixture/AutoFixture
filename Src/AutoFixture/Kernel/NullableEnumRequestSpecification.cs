d1dab0df2cc648f4a3fe80f37b090ebdf152aab7
Mark Seemann <mark@seemann.ms>
2011-05-28 22:50:48 +0200

    Added NullableEnumRequestSpecification in order to specifically deal with the situation where a nullable enum is requested.

---
+++ Src/AutoFixture/Kernel/NullableEnumRequestSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class NullableEnumRequestSpecification : IRequestSpecification
+    {
+        #region IRequestSpecification Members
+
+        public bool IsSatisfiedBy(object request)
+        {
+            return (from t in request.Maybe().OfType<Type>()
+                    where t.IsGenericType
+                    let gtd = t.GetGenericTypeDefinition()
+                    where typeof(Nullable<>).IsAssignableFrom(gtd)
+                    let ga = t.GetGenericArguments()
+                    where ga.Length == 1
+                    select ga.Single().IsEnum).DefaultIfEmpty(false).Single();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/NullableEnumRequestSpecificationTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class NullableEnumRequestSpecificationTest
+    {
+        [Fact]
+        public void SutIsRequestSpecification()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new NullableEnumRequestSpecification();
+            // Verify outcome
+            Assert.IsAssignableFrom<IRequestSpecification>(sut);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("foo")]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(true)]
+        [InlineData(false)]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(Version))]
+        [InlineData(typeof(TriState))]
+        [InlineData(typeof(int?))]
+        [InlineData(typeof(PropertyHolder<int>))]
+        [InlineData(typeof(DoubleFieldHolder<int, string>))]
+        public void IsSatisfiedReturnsFalseOnRequestWhichIsNotRequestForNullableEnum(object request)
+        {
+            // Fixture setup
+            var sut = new NullableEnumRequestSpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(TriState?))]
+        [InlineData(typeof(ConsoleColor?))]
+        public void IsSatisfiedReturnsTrueForRequestForNullableEnum(object request)
+        {
+            // Fixture setup
+            var sut = new NullableEnumRequestSpecification();
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.True(result);
+            // Teardown
+        }
+    }
+}
