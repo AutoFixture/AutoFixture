4050aea5e5907546763e8e56e3ebc745e5255ba2
Mark Seemann <mark@seemann.ms>
2011-06-01 23:35:14 +0200

    Added assertion class intended to replace PropertyContext.VerifyWritable()

---
+++ Src/IdiomsUnitTest/WritablePropertyAssertionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.TestTypeFoundation;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class WritablePropertyAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticAssertion()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new WritablePropertyAssertion();
+            // Verify outcome
+            Assert.IsAssignableFrom<IdiomaticAssertion>(sut);
+            // Teardown
+        }
+    }
+}
