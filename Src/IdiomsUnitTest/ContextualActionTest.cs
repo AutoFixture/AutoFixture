27edd019f1028b59e70c023c76c564cc811fcc6e
Mark Seemann <mark@seemann.ms>
2011-06-02 19:52:31 +0200

    Introducing IContextualAction. This interface is intended to wrap an action while also being able to provide some information about the context of the action.

---
+++ Src/IdiomsUnitTest/ContextualActionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class ContextualActionTest
+    {
+        [Fact]
+        public void SutIsContextualAction()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ContextualAction();
+            // Verify outcome
+            Assert.IsAssignableFrom<IContextualAction>(sut);
+            // Teardown
+        }
+    }
+}
