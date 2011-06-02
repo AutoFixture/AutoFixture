f23f1700699c863bfa958eb7c810a24abc314115
Mark Seemann <mark@seemann.ms>
2011-06-02 20:16:20 +0200

    Added PropertyInfo to PropertySetCommand as the first of expected three constructor parameters. Yes, this will require a bit of test refactoring.

--- Src/Idioms/PropertySetCommand.cs
+++ Src/Idioms/PropertySetCommand.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace Ploeh.AutoFixture.Idioms
 {
     public class PropertySetCommand : IContextualCommand
     {
+        private readonly PropertyInfo propertyInfo;
+
+        public PropertySetCommand(PropertyInfo propertyInfo)
+        {
+            this.propertyInfo = propertyInfo;
+        }
+
+        public System.Reflection.PropertyInfo PropertyInfo
+        {
+            get { return this.propertyInfo; }
+        }
     }
 }
--- Src/IdiomsUnitTest/PropertySetCommandTest.cs
+++ Src/IdiomsUnitTest/PropertySetCommandTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Ploeh.AutoFixture.Idioms;
+using Ploeh.TestTypeFoundation;
+using System.Reflection;
 
 namespace Ploeh.AutoFixture.IdiomsUnitTest
 {
@@ -13,10 +15,24 @@ namespace Ploeh.AutoFixture.IdiomsUnitTest
         public void SutIsContextualAction()
         {
             // Fixture setup
+            var dummyProperty = typeof(PropertyHolder<object>).GetProperty("Property");
             // Exercise system
-            var sut = new PropertySetCommand();
+            var sut = new PropertySetCommand(dummyProperty);
             // Verify outcome
             Assert.IsAssignableFrom<IContextualCommand>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void PropertyInfoIsCorrect()
+        {
+            // Fixture setup
+            var propertyInfo = typeof(PropertyHolder<object>).GetProperty("Property");
+            var sut = new PropertySetCommand(propertyInfo);
+            // Exercise system
+            PropertyInfo result = sut.PropertyInfo;
+            // Verify outcome
+            Assert.Equal(propertyInfo, result);
             // Teardown
         }
     }
