e77f8815ab4ed48d872c8be6b91aa1c631e7c301
Mark Seemann <mark@seemann.ms>
2011-06-02 20:41:57 +0200

    Added class to unwrap TargetInvocationExceptions, as these will be thrown when APIs are invoked through Reflection (such as by PropertySetCommand).

---
+++ Src/Idioms/ReflectionExceptionUnwrappingCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class ReflectionExceptionUnwrappingCommand : IContextualCommand
+    {
+        #region IContextualCommand Members
+
+        public void Execute()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/ReflectionExceptionUnwrappingCommandTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class ReflectionExceptionUnwrappingCommandTest
+    {
+        [Fact]
+        public void SutIsContextualCommand()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ReflectionExceptionUnwrappingCommand();
+            // Verify outcome
+            Assert.IsAssignableFrom<IContextualCommand>(sut);
+            // Teardown
+        }
+    }
+}
