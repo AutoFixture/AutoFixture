7e10b0a3e5e46e9980d57496c5e8acf5859e4696
Mark Seemann <mark@seemann.ms>
2011-06-02 20:52:10 +0200

    ReflectionExceptionUnwrappingCommand is a Decorator, so added the decorated instance.

--- Src/Idioms/ReflectionExceptionUnwrappingCommand.cs
+++ Src/Idioms/ReflectionExceptionUnwrappingCommand.cs
@@ -7,6 +7,18 @@ namespace Ploeh.AutoFixture.Idioms
 {
     public class ReflectionExceptionUnwrappingCommand : IContextualCommand
     {
+        private IContextualCommand command;
+
+        public ReflectionExceptionUnwrappingCommand(IContextualCommand command)
+        {
+            this.command = command;
+        }
+
+        public IContextualCommand Command
+        {
+            get { return this.command; }
+        }
+
         #region IContextualCommand Members
 
         public void Execute()
--- Src/IdiomsUnitTest/ReflectionExceptionUnwrappingCommandTest.cs
+++ Src/IdiomsUnitTest/ReflectionExceptionUnwrappingCommandTest.cs
@@ -13,10 +13,24 @@ namespace Ploeh.AutoFixture.IdiomsUnitTest
         public void SutIsContextualCommand()
         {
             // Fixture setup
+            var dummyCommand = new DelegatingContextualCommand();
             // Exercise system
-            var sut = new ReflectionExceptionUnwrappingCommand();
+            var sut = new ReflectionExceptionUnwrappingCommand(dummyCommand);
             // Verify outcome
             Assert.IsAssignableFrom<IContextualCommand>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CommandIsCorrect()
+        {
+            // Fixture setup
+            var expectedCommand = new DelegatingContextualCommand();
+            var sut = new ReflectionExceptionUnwrappingCommand(expectedCommand);
+            // Exercise system
+            IContextualCommand result = sut.Command;
+            // Verify outcome
+            Assert.Equal(expectedCommand, result);
             // Teardown
         }
     }
