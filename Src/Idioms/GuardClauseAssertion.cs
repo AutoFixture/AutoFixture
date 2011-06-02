3f8881054b39f3c62fd3eb5bbeda72c3acf23339
Mark Seemann <mark@seemann.ms>
2011-06-02 15:01:16 +0200

    Added GuardClauseAssertion, which is intended to replace IMemberContext.VerifyBoundaries.

---
+++ Src/Idioms/GuardClauseAssertion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class GuardClauseAssertion : IdiomaticAssertion
+    {
+    }
+}
---
+++ Src/IdiomsUnitTest/GuardClauseAssertionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class GuardClauseAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticAssertion()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new GuardClauseAssertion();
+            // Verify outcome
+            Assert.IsAssignableFrom<IdiomaticAssertion>(sut);
+            // Teardown
+        }
+    }
+}
