e3ca2f880ee1264c90f3223ea2b29c715dc5681a
Mark Seemann <mark@seemann.ms>
2011-06-03 18:39:51 +0200

    Added a few tests for exception. More to follow.

---
+++ Src/IdiomsUnitTest/GuardClauseExceptionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class GuardClauseExceptionTest
+    {
+        [Fact]
+        public void SutIsException()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new GuardClauseException();
+            // Verify outcome
+            Assert.IsAssignableFrom<Exception>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void MessageIsNotNull()
+        {
+            // Fixture setup
+            var sut = new GuardClauseException();
+            // Exercise system
+            var result = sut.Message;
+            // Verify outcome
+            Assert.NotNull(result);
+            // Teardown
+        }
+    }
+}
