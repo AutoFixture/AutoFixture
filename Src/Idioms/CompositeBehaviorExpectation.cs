850d77a1e434abbb503dcea41a66d645ce7980a2
Mark Seemann <mark@seemann.ms>
2011-06-03 21:59:31 +0200

    Introducing the CompositeBehaviorExpectation. Not yet implemented.

---
+++ Src/Idioms/CompositeBehaviorExpectation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class CompositeBehaviorExpectation : IBehaviorExpectation
+    {
+        #region IBehaviorExpectation Members
+
+        public void Verify(IGuardClauseCommand command)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/CompositeBehaviorExpectationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class CompositeBehaviorExpectationTest
+    {
+        [Fact]
+        public void SutIsBehaviorExpectation()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new CompositeBehaviorExpectation();
+            // Verify outcome
+            Assert.IsAssignableFrom<IBehaviorExpectation>(sut);
+            // Teardown
+        }
+    }
+}
