ac2c3c9d8616bb0c3cd8a9c8d59499aa80439d1b
Mark Seemann <mark@seemann.ms>
2011-06-05 14:26:20 +0200

    Introducing the ConstructorInvokeCommand class, which will encapsulat invocation of a constructor.

---
+++ Src/Idioms/ConstructorInvokeCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class ConstructorInvokeCommand : IGuardClauseCommand
+    {
+        #region IGuardClauseCommand Members
+
+        public Type ValueType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Execute(object value)
+        {
+        }
+
+        public Exception CreateException(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Exception CreateException(string value, Exception innerException)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/IdiomsUnitTest/ConstructorInvokeCommandTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class ConstructorInvokeCommandTest
+    {
+        [Fact]
+        public void SutIsGuardClauseCommand()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ConstructorInvokeCommand();
+            // Verify outcome
+            Assert.IsAssignableFrom<IGuardClauseCommand>(sut);
+            // Teardown
+        }
+    }
+}
