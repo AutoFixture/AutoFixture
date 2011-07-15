eaf3ef2fa1659314bbba49a4c15ef5e4e2931e8f
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-07-15 22:35:03 +0300

    TypeWithFactoryProperty now acts as a factory (before it was acting like singleton).

--- Src/TestTypeFoundation/TypeWithFactoryProperty.cs
+++ Src/TestTypeFoundation/TypeWithFactoryProperty.cs
@@ -2,30 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
 namespace Ploeh.TestTypeFoundation
 {
     public class TypeWithFactoryProperty
     {
-        private static TypeWithFactoryProperty value;
-
         private TypeWithFactoryProperty()
         {
         }
 
-        public static TypeWithFactoryProperty Instance
+        public static TypeWithFactoryProperty Factory
         {
             get
             {
-                if (value != null)
-                {
-                    return value;
-                }
-
-                Interlocked.CompareExchange(ref value, new TypeWithFactoryProperty(), null);
-
-                return value;
+                return new TypeWithFactoryProperty();
             }
         }
     }
