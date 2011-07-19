7199e54559dd46d5d56e971d74ef4d4a40f1a579
Mark Seemann <mark@seemann.ms>
2011-07-19 21:57:27 +0200

    Introducing InstanceMethod. The idea is to use it in Idioms to implement invocations of instance methods (duh!)

---
+++ Src/AutoFixture/Kernel/InstanceMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class InstanceMethod : IMethod
+    {
+        public InstanceMethod(MethodInfo dummyMethod)
+        {
+        }
+
+        #region IMethod Members
+
+        public IEnumerable<ParameterInfo> Parameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Invoke(IEnumerable<object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/InstanceMethodTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class InstanceMethodTest
+    {
+        [Fact]
+        public void SutIsMethod()
+        {
+            // Fixture setup
+            var dummyMethod = typeof(object).GetMethods(BindingFlags.Public | BindingFlags.Instance).First();
+            // Exercise system
+            var sut = new InstanceMethod(dummyMethod);
+            // Verify outcome
+            Assert.IsAssignableFrom<IMethod>(sut);
+            // Teardown
+        }
+    }
+}
