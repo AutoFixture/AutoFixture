6960ceb38364960849309428c2d427015cd30dae
Mark Seemann <mark@seemann.ms>
2011-07-23 17:38:55 +0200

    Added a few XML comments/documentation.

--- Src/Idioms/EmptyGuidBehaviorExpectation.cs
+++ Src/Idioms/EmptyGuidBehaviorExpectation.cs
@@ -5,10 +5,29 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Idioms
 {
+    /// <summary>
+    /// Encapsulates the expected behavior when an <see cref="IGuardClauseCommand" /> (typically
+    /// representing a method or constructor) is invoked with a <see cref="Guid.Empty" /> argument.
+    /// </summary>
+    /// <seealso cref="Verify(IGuardClauseCommand)" />
     public class EmptyGuidBehaviorExpectation : IBehaviorExpectation
     {
         #region IBehaviorExpectation Members
 
+        /// <summary>
+        /// Verifies the behavior of the command when invoked with <see cref="Guid.Empty" />.
+        /// </summary>
+        /// <param name="command">The command whose behavior must be examined.</param>
+        /// <remarks>
+        /// <para>
+        /// This method encapsulates the behavior which is expected when a method or constructor is
+        /// invoked with <see cref="Guid.Empty" /> as one of the method arguments. In that case it's
+        /// expected that invoking <paramref name="command" /> with Guid.Empty throws an
+        /// <see cref="ArgumentException" />, causing the Verify method to succeed. If other
+        /// exceptions are thrown, or no exception is thrown when invoking the command, the Verify
+        /// method throws an exception.
+        /// </para>
+        /// </remarks>
         public void Verify(IGuardClauseCommand command)
         {
             if (command.RequestedType != typeof(Guid))
--- Src/Idioms/IBehaviorExpectation.cs
+++ Src/Idioms/IBehaviorExpectation.cs
@@ -5,8 +5,22 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Idioms
 {
+    /// <summary>
+    /// Represents an expectation about the behavior when an <see cref="IGuardClauseCommand" /> is
+    /// invoked.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This interface is a rather specialized interface supporting the implementation of
+    /// <see cref="GuardClauseAssertion" />.
+    /// </para>
+    /// </remarks>
     public interface IBehaviorExpectation
     {
+        /// <summary>
+        /// Verifies the behavior of the command.
+        /// </summary>
+        /// <param name="command">The command whose behavior must be examined.</param>
         void Verify(IGuardClauseCommand command);
     }
 }
