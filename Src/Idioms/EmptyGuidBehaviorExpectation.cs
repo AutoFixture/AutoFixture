c1eb382134a65faa191aa450f8caed00445c8b16
Mark Seemann <mark@seemann.ms>
2011-07-23 12:30:40 +0200

    Introducing EmptyGuidBehaviorExpectation, which will check for Guard Clauses against Guid.Empty. Implementation follows.

---
+++ Src/Idioms/EmptyGuidBehaviorExpectation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    public class EmptyGuidBehaviorExpectation : IBehaviorExpectation
+    {
+        #region IBehaviorExpectation Members
+
+        public void Verify(IGuardClauseCommand command)
+        {
+        }
+
+        #endregion
+    }
+}
