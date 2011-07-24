40fe49798a9f21989e050d1a21cbb1ac51867e9d
Mark Seemann <mark@seemann.ms>
2011-07-24 22:59:40 +0200

    Added XML comment as documentation for added method override.

--- Src/Idioms/WritablePropertyException.cs
+++ Src/Idioms/WritablePropertyException.cs
@@ -81,6 +81,18 @@ namespace Ploeh.AutoFixture.Idioms
             get { return this.propertyInfo; }
         }
 
+        /// <summary>
+        /// Adds <see cref="PropertyInfo" /> to a
+        /// <see cref="T:System.Runtime.Serialization.SerializationInfo"/>.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the
+        /// serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains
+        /// contextual information about the source or destination.
+        /// </param>
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
