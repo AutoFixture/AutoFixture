1ac1dce72a92de2e82f4903b8441e27622b654b6
Mark Seemann <mark@seemann.ms>
2011-08-03 17:17:29 +0200

    Added XML comments/documentation.

--- Src/AutoFixture/TypeGenerator.cs
+++ Src/AutoFixture/TypeGenerator.cs
@@ -6,10 +6,22 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// Generates Type instances.
+    /// </summary>
     public class TypeGenerator : ISpecimenBuilder
     {
         #region ISpecimenBuilder Members
 
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The <see cref="object"/> <see cref="Type"/> if <paramref name="request"/> is a Type
+        /// Type; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
         public object Create(object request, ISpecimenContext context)
         {
             if (typeof(Type).Equals(request))
