a99f567d7828a075cb7392ca5a49022e43c1cd1f
Mark Seemann <mark@seemann.ms>
2011-08-10 13:30:14 +0200

    Added StableFiniteSequenceCustomization to provide a packaged customization for stable sequences as outlined here: http://blog.ploeh.dk/2011/04/18/EnumerablesAreDynamicalsoInAutoFixture.aspx

---
+++ Src/AutoFixture/StableFiniteSequenceCustomization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    public class StableFiniteSequenceCustomization : ICustomization
+    {
+        #region ICustomization Members
+
+        public void Customize(IFixture fixture)
+        {
+            if (fixture == null)
+                throw new ArgumentNullException("fixture");
+
+            fixture.Customizations.Add(new StableFiniteSequenceRelay());
+        }
+
+        #endregion
+    }
+}
---
+++ Src/AutoFixtureUnitTest/StableFiniteSequenceCustomizationTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class StableFiniteSequenceCustomizationTest
+    {
+        [Fact]
+        public void SutIsCustomization()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new StableFiniteSequenceCustomization();
+            // Verify outcome
+            Assert.IsAssignableFrom<ICustomization>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeNullFixtureThrows()
+        {
+            // Fixture setup
+            var sut = new StableFiniteSequenceCustomization();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.Customize(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeAddsCorrectItemToCustomizations()
+        {
+            // Fixture setup
+            var sut = new StableFiniteSequenceCustomization();
+            var fixture = new Fixture();
+            // Exercise system
+            sut.Customize(fixture);
+            // Verify outcome
+            Assert.True(fixture.Customizations.OfType<StableFiniteSequenceRelay>().Any());
+            // Teardown
+        }
+    }
+}
