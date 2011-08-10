99ee1b7a4d3795802097646e082146953593683c
Mark Seemann <mark@seemann.ms>
2011-08-10 21:28:20 +0200

    Added FavorEnumerableAttribute which can be used to declaratively make AutoFixture favor constructors with IEnumerable<T> parameters.

---
+++ Src/AutoFixture.xUnit.net.UnitTest/FavorEnumerableAttributeTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Xunit;
+using Ploeh.TestTypeFoundation;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class FavorEnumerableAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new FavorEnumerableAttribute();
+            // Verify outcome
+            Assert.IsAssignableFrom<CustomizeAttribute>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void GetCustomizationFromNullParameterThrows()
+        {
+            // Fixture setup
+            var sut = new FavorEnumerableAttribute();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.GetCustomization(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void GetCustomizationReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new FavorEnumerableAttribute();
+            var parameter = typeof(TypeWithOverloadedMembers).GetMethod("DoSomething", new[] { typeof(object) }).GetParameters().Single();
+            // Exercise system
+            var result = sut.GetCustomization(parameter);
+            // Verify outcome
+            var invoker = Assert.IsAssignableFrom<ConstructorCustomization>(result);
+            Assert.Equal(parameter.ParameterType, invoker.TargetType);
+            Assert.IsAssignableFrom<EnumerableFavoringConstructorQuery>(invoker.Query);
+            // Teardown
+        }
+    }
+}
