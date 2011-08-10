97f7fab4ddc7cfb635e7737125253028c61bc6ef
Mark Seemann <mark@seemann.ms>
2011-08-10 13:40:21 +0200

    Added documentation in form of XML comments.

--- Src/AutoFixture/StableFiniteSequenceCustomization.cs
+++ Src/AutoFixture/StableFiniteSequenceCustomization.cs
@@ -6,10 +6,33 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// A customization that makes sequences (i.e. <see cref="IEnumerable{T}" /> stable instead of
+    /// dynamic.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A sequence (i.e. an instance of <see cref="IEnumerable{T}" /> is not guaranteed to be
+    /// stable or finite; it might, in fact, represent a Generator that creates an Iterator that
+    /// can go on for ever. Even if the sequence is finite, it may not be stable because it may
+    /// yield new items every time it's iterated.
+    /// </para>
+    /// <para>
+    /// The StableFiniteSequenceCustomization class encapsulates a change in behavior that
+    /// guarantees that an <see cref="IFixture" /> instance will resolve a request for a sequence
+    /// into a stable sequence.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="StableFiniteSequenceRelay" />
     public class StableFiniteSequenceCustomization : ICustomization
     {
         #region ICustomization Members
 
+        /// <summary>
+        /// Adds a <see cref="StableFiniteSequenceRelay" /> to an <see cref="IFixture" />.
+        /// </summary>
+        /// <param name="fixture">The fixture to customize.</param>
+        /// <seealso cref="StableFiniteSequenceCustomization" />
         public void Customize(IFixture fixture)
         {
             if (fixture == null)
