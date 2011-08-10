be29a077e9a88b5e3028ced17267d18e4f4925d5
Mark Seemann <mark@seemann.ms>
2011-08-10 15:26:00 +0200

    Implemented FavorArraysAttribute which can be used to declaratively make AutoFixture favor constructors with array parameters.

---
+++ Src/AutoFixture.xUnit.net.UnitTest/FavorArraysAttributeTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Xunit;
+using Ploeh.TestTypeFoundation;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class FavorArraysAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new FavorArraysAttribute();
+            // Verify outcome
+            Assert.IsAssignableFrom<CustomizeAttribute>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void GetCustomizationFromNullParameterThrows()
+        {
+            // Fixture setup
+            var sut = new FavorArraysAttribute();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.GetCustomization(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void GetCustomizationReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new FavorArraysAttribute();
+            var parameter = typeof(TypeWithOverloadedMembers).GetMethod("DoSomething", new[] { typeof(object) }).GetParameters().Single();
+            // Exercise system
+            var result = sut.GetCustomization(parameter);
+            // Verify outcome
+            var invoker = Assert.IsAssignableFrom<ConstructorCustomization>(result);
+            Assert.Equal(parameter.ParameterType, invoker.TargetType);
+            Assert.IsAssignableFrom<ArrayFavoringConstructorQuery>(invoker.Query);
+            // Teardown
+        }
+    }
+}
