0895d28eb54e8ed72be321b3f9b7e2525757d09d
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-01 22:04:03 +0300

    Added RangedNumberGeneratorDataTest representing a different Test Class Structure for RangedNumberGenerator. Added more test cases.

---
+++ Src/AutoFixtureUnitTest/RangedNumberGeneratorDataTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class RangedNumberGeneratorDataTest : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue:  1, expectedResult: 11);
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue:  2, expectedResult: 12);
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue:  3, expectedResult: 13);
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue: 10, expectedResult: 10);
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue: 20, expectedResult: 20);
+            yield return CreateTestCase(operandType: typeof(int), minimum: 10, maximum: 20, contextValue: 21, expectedResult: 10);
+
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue:  1.0, expectedResult: 11.0);
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue:  2.0, expectedResult: 12.0);
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue:  3.0, expectedResult: 13.0);
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue: 10.0, expectedResult: 10.0);
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue: 20.0, expectedResult: 20.0);
+            yield return CreateTestCase(operandType: typeof(double), minimum: 10.0, maximum: 20.0, contextValue: 21.0, expectedResult: 10.0);
+
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue:  1, expectedResult: 11);
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue:  2, expectedResult: 12);
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue:  3, expectedResult: 13);
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue: 10, expectedResult: 10);
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue: 20, expectedResult: 20);
+            yield return CreateTestCase(operandType: typeof(long), minimum: 10, maximum: 20, contextValue: 21, expectedResult: 10);
+
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue:  1.0, expectedResult: 11.0);
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue:  2.0, expectedResult: 12.0);
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue:  3.0, expectedResult: 13.0);
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue: 10.0, expectedResult: 10.0);
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue: 20.0, expectedResult: 20.0);
+            yield return CreateTestCase(operandType: typeof(decimal), minimum: 10.0, maximum: 20.0, contextValue: 21.0, expectedResult: 10.0);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private static object[] CreateTestCase(Type operandType, object minimum, object maximum, object contextValue, object expectedResult)
+        {
+            return new object[] { operandType, minimum, maximum, contextValue, expectedResult };
+        }
+    }
+}
