e73a880dd2acad942c7f89c76f02995cd8da29fa
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-01 11:58:41 +0300

    Added RangedNumberGenerator in Ploeh.AutoFixture Added RangedNumberGeneratorTest in Ploeh.AutoFixtureUnitTest

---
+++ Src/AutoFixture/RangedNumberGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    public class RangedNumberGenerator : ISpecimenBuilder
+    {
+        private readonly object syncRoot;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RangedNumberGenerator"/> class.
+        /// </summary>
+        public RangedNumberGenerator()
+        {
+            this.syncRoot = new object();
+        }
+
+        /// <summary>
+        /// Creates a new number based on a RangedNumberRequest.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested number if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null)
+            {
+                return new NoSpecimen();
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var rangedNumberRequest = request as RangedNumberRequest;
+            if (rangedNumberRequest == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            var value = context.Resolve(request);
+            if (value == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            return CreateAnonymous(rangedNumberRequest, value);
+        }
+
+        /// <summary>
+        /// Creates an anonymous number within a range specified by a RangeNumberRequest.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="value">The context value.</param>
+        /// <returns>
+        /// The next number in a consequtive sequence within a range specified by a RangeNumberRequest.
+        /// </returns>
+        private object CreateAnonymous(RangedNumberRequest request, object value)
+        {
+            var minimum = (IComparable)request.Minimum;
+            if (minimum.CompareTo(value) == 0)
+            {
+                return minimum;
+            }
+
+            var maximum = (IComparable)request.Maximum;
+            if (maximum.CompareTo(value) == 0)
+            {
+                return maximum;
+            }
+
+            if (minimum.CompareTo(value) <= 0 &&
+                maximum.CompareTo(value) <= 0)
+            {
+                return minimum;
+            }
+
+            return Add(minimum, value);
+        }
+
+        private object Add(object a, object b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/RangedNumberGeneratorTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class RangedNumberGeneratorTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new RangedNumberGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new RangedNumberGenerator();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system
+            var result = sut.Create(null, dummyContext);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContextThrows()
+        {
+            // Fixture setup
+            var sut = new RangedNumberGenerator();
+            var dummyRequest = new object();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => sut.Create(dummyRequest, null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithAnonymousRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new RangedNumberGenerator();
+            var dummyRequest = new object();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system
+            var result = sut.Create(dummyRequest, dummyContext);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(dummyRequest), result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(int), 10, 20, 10, 10)]
+        [InlineData(typeof(int), 10, 20, 20, 20)]
+        [InlineData(typeof(int), 10, 20, 21, 10)]
+        [InlineData(typeof(int), 10, 20, 1, 11)]
+        public void CreateReturnsCorrectResult(Type operandType, object minimum, object maximum, object contextValue, object expectedResult)
+        {
+            // Fixture setup
+            var sut = new RangedNumberGenerator();
+            var dummyRequest = new RangedNumberRequest(operandType, minimum, maximum);
+            var dummyContext = new DelegatingSpecimenContext
+            {
+                OnResolve = r =>
+                {
+                    Assert.Equal(dummyRequest, r);
+                    return contextValue;
+                }
+            };
+            // Exercise system
+            var result = sut.Create(dummyRequest, dummyContext);
+            // Verify outcome
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+    }
+}
