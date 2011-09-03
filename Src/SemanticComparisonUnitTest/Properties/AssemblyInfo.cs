6a5ff8f1112694721a815b26e6aa458c105e6269
Mark Seemann <mark@seemann.ms>
2011-09-03 21:10:27 +0200

    Ran a build.

--- Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
+++ Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.41.0")]
-[assembly: AssemblyFileVersion("2.2.41.0")]
+[assembly: AssemblyVersion("2.2.42.0")]
+[assembly: AssemblyFileVersion("2.2.42.0")]
