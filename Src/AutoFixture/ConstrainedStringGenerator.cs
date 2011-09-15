bf447508e47f080a5aa0d9e8b00f1dadcd8e461b
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-16 01:02:25 +0300

    Added ConstrainedStringGeneratorTest and ConstrainedStringGenerator.

---
+++ Src/AutoFixture/ConstrainedStringGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Creates a constrained string.
+    /// </summary>
+    public class ConstrainedStringGenerator : ISpecimenBuilder
+    {
+        private readonly object syncRoot;
+        private string constrainedString;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RangedNumberGenerator"/> class.
+        /// </summary>
+        public ConstrainedStringGenerator()
+        {
+            this.syncRoot = new object();
+            this.constrainedString = string.Empty;
+        }
+
+        /// <summary>
+        /// Creates a constrained string based on a ConstrainedStringRequest.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested number if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null)
+            {
+                return new NoSpecimen();
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var constrain = request as ConstrainedStringRequest;
+            if (constrain == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            this.CreateAnonymous(constrain.MaximumLength, context);
+
+            return this.constrainedString;
+        }
+
+        private void CreateAnonymous(int maximumLength, ISpecimenContext context)
+        {
+            lock (this.syncRoot)
+            {
+                var s = string.Empty;
+                while (s.Length < 1)
+                {
+                    s += context.Resolve(typeof(string));
+                }
+                if (s.Length > maximumLength)
+                {
+                    s = s.Substring(0, maximumLength);
+                }
+
+                this.constrainedString = s;
+            }
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/ConstrainedStringGeneratorTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class ConstrainedStringGeneratorTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ConstrainedStringGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringGenerator();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system
+            var result = sut.Create(null, dummyContext);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContextThrows()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringGenerator();
+            var dummyRequest = new object();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => sut.Create(dummyRequest, null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithAnonymousRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringGenerator();
+            var dummyRequest = new object();
+            var dummyContext = new DelegatingSpecimenContext();
+            // Exercise system
+            var result = sut.Create(dummyRequest, dummyContext);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(dummyRequest), result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(2, 5)]
+        [InlineData(5, 2)]
+        [InlineData(9, 9)]
+        public void CreateReturnsCorrectResultOnMultipleCall(int maximumLength, int loopCount)
+        {
+            // Fixture setup
+            var request = new ConstrainedStringRequest(maximumLength);
+            var context = new DelegatingSpecimenContext
+            {
+                OnResolve = r =>
+                {
+                    Assert.Equal(request.MaximumLength, maximumLength);
+                    return maximumLength;
+                }
+            };
+            var sut = new ConstrainedStringGenerator();
+            // Exercise system
+            var result = (from n in Enumerable.Range(1, loopCount).Select(i => ((string)sut.Create(request, context)).Length)
+                          where (n > request.MaximumLength)
+                          select n);
+            // Verify outcome
+            Assert.False(result.Any());
+            // Teardown
+        }
+    }
+}
