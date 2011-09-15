554d4f388d51a3e77f2162e1b56633097e33510e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-15 22:26:23 +0300

    Added StringLengthValidatedType on Ploeh.AutoFixtureUnitTest.DataAnnotations namespace.

---
+++ Src/AutoFixtureUnitTest/DataAnnotations/StringLengthValidatedType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ploeh.AutoFixtureUnitTest.DataAnnotations
+{
+    public class StringLengthValidatedType
+    {
+        [StringLength(3)]
+        public string Property { get; set; }
+    }
+}
