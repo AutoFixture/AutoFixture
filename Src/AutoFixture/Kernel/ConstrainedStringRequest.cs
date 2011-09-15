59c445d5428baf0dba4f408785cc4817dc73a232
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-15 23:58:01 +0300

    Added ConstrainedStringRequestTest and ConstrainedStringRequest.

---
+++ Src/AutoFixture/Kernel/ConstrainedStringRequest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Encapsulates a maximum length for a string.
+    /// </summary>
+    public class ConstrainedStringRequest : IEquatable<ConstrainedStringRequest>
+    {
+        private readonly int maximum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstrainedStringRequest"/> class.
+        /// </summary>
+        /// <param name="maximum">The maximum.</param>
+        public ConstrainedStringRequest(int maximum)
+        {
+            if (maximum < 1)
+            {
+                throw new ArgumentOutOfRangeException("maximum", "Maximum must be equal or greater than 1.");
+            }
+
+            this.maximum = maximum;
+        }
+
+        /// <summary>
+        /// Gets the maximum value.
+        /// </summary>
+        public int Maximum
+        {
+            get
+            {
+                return this.maximum;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="T:System.NullReferenceException">
+        /// The <paramref name="obj"/> parameter is null.
+        ///   </exception>
+        public override bool Equals(object obj)
+        {
+            var other = obj as ConstrainedStringRequest;
+            if (other != null)
+            {
+                return this.Equals(other);
+            }
+
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.Maximum.GetHashCode();
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(ConstrainedStringRequest other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Maximum == other.Maximum;
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/ConstrainedStringRequestTest.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class ConstrainedStringRequestTest
+    {
+        [Fact]
+        public void MaximumIsCorrect()
+        {
+            // Fixture setup
+            var expectedMaximum = 3;
+            var sut = new ConstrainedStringRequest(expectedMaximum);
+            // Exercise system
+            var result = sut.Maximum;
+            // Verify outcome
+            Assert.Equal(expectedMaximum, result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-2)]
+        [InlineData(-3)]
+        public void CreateWithMaximumLowerThanOneWillThrow(int maximum)
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new ConstrainedStringRequest(maximum));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void CreateWithMaximumEqualsOrGreaterThanOneWillDoesNotThrow(int maximum)
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.DoesNotThrow(() => new ConstrainedStringRequest(maximum));
+            // Teardown
+        }
+
+        [Fact]
+        public void SutIsEquatable()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ConstrainedStringRequest(3);
+            // Verify outcome
+            Assert.IsAssignableFrom<IEquatable<ConstrainedStringRequest>>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualNullObject()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringRequest(3);
+            object other = null;
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualNullSut()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringRequest(3);
+            ConstrainedStringRequest other = null;
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualAnonymousObject()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringRequest(3);
+            object anonymousObject = new FileStyleUriParser();
+            // Exercise system
+            var result = sut.Equals(anonymousObject);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualOtherObjectWhenMaximumsDiffer()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringRequest(3);
+            object other = new ConstrainedStringRequest(4);
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualOtherSutWhenMaximumsDiffer()
+        {
+            // Fixture setup
+            var sut = new ConstrainedStringRequest(3);
+            var other = new ConstrainedStringRequest(4);
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutEqualsOtherObjectWhenConstructorParametersEquals()
+        {
+            // Fixture setup
+            int maximum = 3;
+            var sut = new ConstrainedStringRequest(maximum);
+            object other = new ConstrainedStringRequest(maximum);
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.True(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutEqualsOtherSutWhenConstructorParametersEquals()
+        {
+            // Fixture setup
+            int maximum = 3;
+            var sut = new ConstrainedStringRequest(maximum);
+            var other = new ConstrainedStringRequest(maximum);
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.True(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void GetHashCodeWillReturnCorrectResult()
+        {
+            // Fixture setup
+            int maximum = 3;
+            var sut = new ConstrainedStringRequest(maximum);
+            var expectedHashCode = maximum.GetHashCode();
+            // Exercise system
+            var result = sut.GetHashCode();
+            // Verify outcome
+            Assert.Equal(expectedHashCode, result);
+            // Teardown
+        }
+    }
+}
