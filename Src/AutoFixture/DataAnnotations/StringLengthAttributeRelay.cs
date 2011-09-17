8ff8e851efebed7767e1a2bb2a7edfabcaa20f2e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-09-18 01:44:29 +0300

    Added XML comments on a publicly visible type.

--- Src/AutoFixture/DataAnnotations/StringLengthAttributeRelay.cs
+++ Src/AutoFixture/DataAnnotations/StringLengthAttributeRelay.cs
@@ -6,8 +6,21 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.DataAnnotations
 {
+    /// <summary>
+    /// Relays a request for a constrained string to a <see cref="ConstrainedStringRequest"/>.
+    /// </summary>
     public class StringLengthAttributeRelay : ISpecimenBuilder
     {
+        /// <summary>
+        /// Creates a new specimen based on a specified length of characters that are allowed.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A container that can be used to create other specimens.</param>
+        /// <returns>
+        /// A specimen created from a <see cref="RangedNumberRequest"/> encapsulating the operand
+        /// type, the minimum and the maximum of the requested number, if possible; otherwise,
+        /// a <see cref="NoSpecimen"/> instance.
+        /// </returns>
         public object Create(object request, ISpecimenContext context)
         {
             if (request == null)
