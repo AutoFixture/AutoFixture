a577155908f6fbb8061c1c44a73448a8725c1d51
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-10-21 00:18:44 +0300

    Added DependencyConstraints.

---
+++ Src/AutoFakeItEasy/AutoFakeItEasyCustomization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ploeh.AutoFixture.AutoFakeItEasy
+{
+    public class AutoFakeItEasyCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFakeItEasyUnitTest/DependencyConstraints.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixture.AutoFakeItEasy.UnitTest
+{
+    public class DependencyConstraints
+    {
+        [Theory]
+        [InlineData("FakeItEasy")]
+        [InlineData("xunit")]
+        [InlineData("xunit.extensions")]
+        public void AutoMoqDoesNotReference(string assemblyName)
+        {
+            // Fixture setup
+            // Exercise system
+            var references = typeof(AutoFakeItEasyCustomization).Assembly.GetReferencedAssemblies();
+            // Verify outcome
+            Assert.False(references.Any(an => an.Name == assemblyName));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("FakeItEasy")]
+        public void AutoFixtureUnitTestsDoNotReference(string assemblyName)
+        {
+            // Fixture setup
+            // Exercise system
+            var references = this.GetType().Assembly.GetReferencedAssemblies();
+            // Verify outcome
+            Assert.False(references.Any(an => an.Name == assemblyName));
+            // Teardown
+        }
+    }
+}
