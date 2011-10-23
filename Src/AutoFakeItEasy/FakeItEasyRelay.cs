d17149901cb35df2ec3358bbffe973e190af63be
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-10-23 22:22:53 +0300

    Added FakeItEasyRelay.

---
+++ Src/AutoFakeItEasy/FakeItEasyRelay.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using FakeItEasy;
+using FakeItEasy.Configuration;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.AutoFakeItEasy
+{
+    /// <summary>
+    /// Relays a request for an interface or an abstract class to a request for a
+    /// <see cref="A{T}"/> of that class.
+    /// </summary>
+    public class FakeItEasyRelay : ISpecimenBuilder
+    {
+        private readonly Func<Type, bool> shouldBeFaked;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeItEasyRelay"/> class.
+        /// </summary>
+        public FakeItEasyRelay()
+            : this(FakeItEasyRelay.ShouldBeFaked)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeItEasyRelay"/> class with a specification
+        /// that determines whether a type should be mocked.
+        /// </summary>
+        /// <param name="fakeableSpecification">
+        /// A specification that determines whether a type should be mocked or not.
+        /// </param>
+        public FakeItEasyRelay(Func<Type, bool> fakeableSpecification)
+        {
+            if (fakeableSpecification == null)
+            {
+                throw new ArgumentNullException("fakeableSpecification");
+            }
+
+            this.shouldBeFaked = fakeableSpecification;
+        }
+
+        /// <summary>
+        /// Gets a specification that determines whether a given type should be mocked.
+        /// </summary>
+        /// <value>The specification.</value>
+        /// <remarks>
+        /// <para>
+        /// This specification determins whether a given type should be relayed as a request for a
+        /// mock of the same type. By default it only returns <see langword="true"/> for interfaces
+        /// and abstract classes, but a different specification can be supplied by using the
+        /// <see cref="FakeItEasyRelay(Func{Type, bool})"/> overloaded constructor that takes a
+        /// specification as input. In that case, this property returns the specification supplied
+        /// to the constructor.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="FakeItEasyRelay(Func{Type, bool})"/>
+        public Func<Type, bool> FakeableSpecification
+        {
+            get { return this.shouldBeFaked; }
+        }
+
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// A dynamic mock instance of the requested interface or abstract class if possible;
+        /// otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var type = request as Type;
+            if (!this.shouldBeFaked(type))
+            {
+                return new NoSpecimen(request);
+            }
+
+            var fake = FakeItEasyRelay.ResolveFake(type, context);
+            if (fake == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            return fake;
+        }
+
+        private static bool ShouldBeFaked(Type t)
+        {
+            return (t != null) 
+                && ((t.IsAbstract) || (t.IsInterface));
+        }
+
+        // The Fake class is static and does not relate with Fake<T> type. The only way to cast
+        // to a non-generic type of Fake<T> is by casting to IHideObjectMembers class which the
+        // Fake<T> class implements.
+        private static IHideObjectMembers ResolveFake(Type t, ISpecimenContext context)
+        {
+            var fakeType = typeof(Fake<>).MakeGenericType(t);
+            return context.Resolve(fakeType) as IHideObjectMembers;
+        }
+    }
+}
---
+++ Src/AutoFakeItEasyUnitTest/FakeItEasyRelayTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using FakeItEasy;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.TestTypeFoundation;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixture.AutoFakeItEasy.UnitTest
+{
+    public class FakeItEasyRelayTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new FakeItEasyRelay();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullSpecificationThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new FakeItEasyRelay(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void SpecificationIsCorrectWhenInitializedWithSpecification()
+        {
+            // Fixture setup
+            Func<Type, bool> expectedSpec = t => true;
+            var sut = new FakeItEasyRelay(expectedSpec);
+            // Exercise system
+            Func<Type, bool> result = sut.FakeableSpecification;
+            // Verify outcome
+            Assert.Equal(expectedSpec, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SpecificationIsNotNullWhenInitializedWithDefaultConstructor()
+        {
+            // Fixture setup
+            var sut = new FakeItEasyRelay();
+            // Exercise system
+            var result = sut.FakeableSpecification;
+            // Verify outcome
+            Assert.NotNull(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContextThrows()
+        {
+            // Fixture setup
+            var sut = new FakeItEasyRelay();
+            var dummyRequest = new object();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.Create(dummyRequest, null));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(1)]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        public void CreateWithNonAbstractionRequestReturnsCorrectResult(object request)
+        {
+            // Fixture setup
+            var sut = new FakeItEasyRelay();
+            var dummyContext = A.Fake<ISpecimenContext>();
+            // Exercise system
+            var result = sut.Create(request, dummyContext);
+            // Verify outcome
+            var expectedResult = new NoSpecimen(request);
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(AbstractType))]
+        [InlineData(typeof(IInterface))]
+        public void CreateWithAbstractionRequestReturnsCorrectResult(Type request)
+        {
+            // Fixture setup
+            var mockType = typeof(Fake<>).MakeGenericType(request);
+
+            var mock = Activator.CreateInstance(mockType);
+            var contextStub = A.Fake<ISpecimenContext>();
+            A.CallTo(() => contextStub.Resolve(mockType)).Returns(mock);
+
+            var sut = new FakeItEasyRelay();
+            // Exercise system
+            var result = sut.Create(request, contextStub);
+            // Verify outcome
+            Assert.Equal(mock, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectResultWhenContextReturnsNonMock()
+        {
+            // Fixture setup
+            var request = typeof(IInterface);
+            var mockType = typeof(Fake<>).MakeGenericType(request);
+
+            var contextStub = A.Fake<ISpecimenContext>();
+            A.CallTo(() => contextStub.Resolve(mockType)).Returns(new object());
+
+            var sut = new FakeItEasyRelay();
+            // Exercise system
+            var result = sut.Create(request, contextStub);
+            // Verify outcome
+            var expectedResult = new NoSpecimen(request);
+            Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(AbstractType))]
+        [InlineData(typeof(IInterface))]
+        public void CreateCorrectlyInvokesSpecification(Type request)
+        {
+            // Fixture setup
+            var verified = false;
+            Func<Type, bool> mockSpec = t => verified = t == request;
+            var sut = new FakeItEasyRelay(mockSpec);
+            // Exercise system
+            var contextDummy = A.Fake<ISpecimenContext>();
+            sut.Create(request, contextDummy);
+            // Verify outcome
+            Assert.True(verified, "Fake verified");
+            // Teardown
+        }
+    }
+}
