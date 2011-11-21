e4bf87937cbe9a5898efa5d3e481d0516b3cfee9
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-11-22 01:37:23 +0200

    Made FakeItEasyMethod class public.

--- Src/AutoFakeItEasy/FakeItEasyMethod.cs
+++ Src/AutoFakeItEasy/FakeItEasyMethod.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using FakeItEasy;
 using FakeItEasy.Creation;
@@ -8,53 +7,90 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.AutoFakeItEasy
 {
-    internal class FakeItEasyMethod : IMethod
+    /// <summary>
+    /// Encapsulates logic on how to create a mock instance with FakeIteasy, using a constructor with
+    /// appropriate parameters.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The main purpose of FakeItEasyMethod is to support creation of fake instances of
+    /// abstract classes with non-default constructors. In this case FakeItEasy must be supplied
+    /// with the appropriate parameter values to be able to properly initialize the fake instance,
+    /// since it needs to pass those parameters to the base class.
+    /// </para>
+    /// </remarks>
+    public class FakeItEasyMethod : IMethod
     {
         private readonly ParameterInfo[] paramInfos;
-        private readonly Type targetType;
+        private readonly Type fakeTargetType;
 
         private IEnumerable<object> parameters;
 
-        internal FakeItEasyMethod(Type fakeTargetType, ParameterInfo[] paramInfos)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeItEasyMethod"/> class.
+        /// </summary>
+        /// <param name="fakeTargetType">
+        /// The type of which a mock instance should be created.
+        /// </param>
+        /// <param name="parameterInfos">
+        /// The parameter information which can be used to identify the signature of the
+        /// constructor.
+        /// </param>
+        internal FakeItEasyMethod(Type fakeTargetType, ParameterInfo[] parameterInfos)
         {
             if (fakeTargetType == null)
             {
                 throw new ArgumentNullException("fakeTargetType");
             }
 
-            if (paramInfos == null)
+            if (parameterInfos == null)
             {
-                throw new ArgumentNullException("paramInfos");
+                throw new ArgumentNullException("parameterInfos");
             }
 
-            this.paramInfos = paramInfos;
-            this.targetType = fakeTargetType;
+            this.fakeTargetType = fakeTargetType;
+            this.paramInfos = parameterInfos;
         }
 
+        /// <summary>
+        /// Gets the type of which a mock instance should be created.
+        /// </summary>
+        /// <seealso cref="FakeItEasyMethod(Type, ParameterInfo[])" />
+        public Type FakeTargetType
+        {
+            get { return this.fakeTargetType; }
+        }
+
+        /// <summary>
+        /// Gets information about the parameters of the method.
+        /// </summary>
         public IEnumerable<ParameterInfo> Parameters
         {
             get { return this.paramInfos; }
         }
 
+        /// <summary>
+        /// Creates a mock instance using FakeItEasy.
+        /// </summary>
+        /// <param name="parameters">
+        /// The parameters which will be supplied to the base constructor.
+        /// </param>
+        /// <returns>A mock instance created with FakeItEasy.</returns>
         public object Invoke(IEnumerable<object> parameters)
         {
-            this.parameters = parameters;
-            MethodInfo fake = typeof(A)
-                .GetMethods().Where(x =>
-                       x.Name.StartsWith("Fake")
-                    && x.ContainsGenericParameters
-                    && x.GetParameters().Count() == 1).Single()
-                .MakeGenericMethod(new[] { this.targetType });
-
             MethodInfo argumentsForConstructor = this.GetType()
                 .GetMethod("SetArgumentsForConstructor", BindingFlags.Instance | BindingFlags.NonPublic)
-                .MakeGenericMethod(new[] { this.targetType });
+                .MakeGenericMethod(new[] { this.fakeTargetType });
 
+            this.parameters = parameters;
             Type actionType = typeof(Action<>).MakeGenericType(
-                 typeof(IFakeOptionsBuilder<>).MakeGenericType(this.targetType));
+                 typeof(IFakeOptionsBuilder<>).MakeGenericType(this.fakeTargetType));
             Delegate action = Delegate.CreateDelegate(actionType, this, argumentsForConstructor);
 
-            return fake.Invoke(null, new[] { action });
+            return typeof(Fake<>)
+                .MakeGenericType(this.fakeTargetType)
+                .GetConstructors()[1]
+                .Invoke(new[] { action });
         }
 
         private void SetArgumentsForConstructor<T>(IFakeOptionsBuilder<T> o)
