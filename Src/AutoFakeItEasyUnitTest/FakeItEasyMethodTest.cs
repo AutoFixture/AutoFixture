6dd2b331c506970c1c333e13d4d380466b2ba488
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-11-28 22:21:18 +0200

    Added tests in FakeItEasyMethodTest class.

--- Src/AutoFakeItEasyUnitTest/FakeItEasyMethodTest.cs
+++ Src/AutoFakeItEasyUnitTest/FakeItEasyMethodTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using FakeItEasy;
 using Ploeh.AutoFixture.Kernel;
 using Ploeh.TestTypeFoundation;
 using Xunit;
@@ -53,6 +54,50 @@ namespace Ploeh.AutoFixture.AutoFakeItEasy.UnitTest
             Type result = sut.TargetType;
             // Verify outcome
             Assert.Equal(t, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void ParametersIsCorrect()
+        {
+            // Fixture setup
+            var parameters = Enumerable.Empty<ParameterInfo>();
+            var sut = new FakeItEasyMethod(typeof(object), parameters.ToArray());
+            // Exercise system
+            var result = sut.Parameters;
+            // Verify outcome
+            Assert.Equal(parameters, result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(IInterface))]
+        [InlineData(typeof(AbstractType))]
+        public void InvokeReturnsCorrectResult(Type type)
+        {
+            // Fixture setup
+            var sut = new FakeItEasyMethod(type, new ParameterInfo[] { });
+            var expectedFake = typeof(Fake<>).MakeGenericType(type);
+            // Exercise system
+            var result = sut.Invoke(Enumerable.Empty<object>());
+            // Verify outcome
+            Assert.IsAssignableFrom(expectedFake, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void InvokeOnTypeWithNonDefaultConstructorReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new FakeItEasyMethod(
+                typeof(AbstractTypeWithNonDefaultConstructor<int>),
+                new ParameterInfo[] { }
+                );
+            // Exercise system
+            var result = sut.Invoke(new object[] { 1 });
+            // Verify outcome
+            var fake = Assert.IsAssignableFrom<Fake<AbstractTypeWithNonDefaultConstructor<int>>>(result);
+            Assert.Equal(1, fake.FakedObject.Property);
             // Teardown
         }
     }
