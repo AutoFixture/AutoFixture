1d62cb9b3dc756b466c07f0ea5c2e75e75b2c667
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2011-12-03 15:13:18 +0200

    Added RegularExpressionRequest and RegularExpressionAttributeRelay.

---
+++ Src/AutoFixture/DataAnnotations/RegularExpressionAttributeRelay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.DataAnnotations
+{
+    public class RegularExpressionAttributeRelay : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null)
+            {
+                return new NoSpecimen();
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var customAttributeProvider = request as ICustomAttributeProvider;
+            if (customAttributeProvider == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            var regularExpressionAttribute = customAttributeProvider.GetCustomAttributes(typeof(RegularExpressionAttribute), inherit: true).Cast<RegularExpressionAttribute>().SingleOrDefault();
+            if (regularExpressionAttribute == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            return context.Resolve(new RegularExpressionRequest(regularExpressionAttribute.Pattern));
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/RegularExpressionRequestTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class RegularExpressionRequestTest
+    {
+        [Fact]
+        public void CreateWithNullPatternThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new RegularExpressionRequest(null));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("[0-9]")]
+        [InlineData("[A-Z]")]
+        [InlineData("[a-z]")]
+        public void PatternIsCorrect(string pattern)
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest(pattern);
+            // Exercise system
+            var result = sut.Pattern;
+            // Verify outcome
+            Assert.Equal(pattern, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutIsEquatable()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new RegularExpressionRequest("[0-9]");
+            // Verify outcome
+            Assert.IsAssignableFrom<IEquatable<RegularExpressionRequest>>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualNullObject()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            object other = null;
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void SutDoesNotEqualNullSut()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            RegularExpressionRequest other = null;
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualAnonymousObject()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            object anonymousObject = new FileStyleUriParser();
+            // Exercise system
+            var result = sut.Equals(anonymousObject);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualOtherObjectWhenPatternsAreDifferent()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            object other = new RegularExpressionRequest("[A-Z]");
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutDoesNotEqualOtherSutWhenPatternsAreDifferent()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            var other = new RegularExpressionRequest("[A-Z]");
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.False(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void SutEqualsOtherObjectWhenPatternsMatch()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            object other = new RegularExpressionRequest("[0-9]");
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.True(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutEqualsOtherSutWhenPatternsMatch()
+        {
+            // Fixture setup
+            var sut = new RegularExpressionRequest("[0-9]");
+            var other = new RegularExpressionRequest("[0-9]");
+            // Exercise system
+            var result = sut.Equals(other);
+            // Verify outcome
+            Assert.True(result, "Equals");
+            // Teardown
+        }
+
+        [Fact]
+        public void GetHashCodeReturnsCorrectResult()
+        {
+            // Fixture setup
+            string pattern = "[0-9]";
+            var sut = new RegularExpressionRequest(pattern);
+            var expectedHashCode = pattern.GetHashCode();
+            // Exercise system
+            var result = sut.GetHashCode();
+            // Verify outcome
+            Assert.Equal(expectedHashCode, result);
+            // Teardown
+        }
+    }
+}
