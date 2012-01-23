d2c43da50f2eb3fa916230180f9c355d75eccf1a
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-01-23 13:21:08 +0200

    Added XML comments.

--- Src/SemanticComparison/SemanticComparer.cs
+++ Src/SemanticComparison/SemanticComparer.cs
@@ -6,11 +6,23 @@ using System.Reflection;
 
 namespace Ploeh.SemanticComparison
 {
+    /// <summary>
+    /// Provides a class with implementations of the System.Collections.Generic.IEqualityComparer<T>
+    /// and System.Collections.Generic.IEqualityComparer interfaces for convention-based object 
+    /// equality comparison for use when comparing two semantically equivalent, but different, objects.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source value (against which the destination value 
+    /// will be compared for equality).</typeparam>
+    /// <typeparam name="TDestination">The type of the destination value which will be compared for 
+    /// equality against the source value.</typeparam>
     public class SemanticComparer<TSource, TDestination> : IEqualityComparer, IEqualityComparer<object>
     {
         private readonly IEnumerable<MemberEvaluator<TSource, TDestination>> evaluators;
         private readonly Func<IEnumerable<MemberInfo>> defaultMembersGenerator;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemanticComparer&lt;TSource, TDestination&gt;"/> class.
+        /// </summary>
         public SemanticComparer()
             :this(Enumerable.Empty<MemberEvaluator<TSource, TDestination>>(), DefaultMembers.Generate<TDestination>)
         {
@@ -32,6 +44,17 @@ namespace Ploeh.SemanticComparison
             get { return defaultMembersGenerator; }
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="x">The source value (against which the destination value will be compared for
+        /// equality).</param>
+        /// <param name="y">The destination value which will be compared for equality against the 
+        /// source value.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; 
+        ///   otherwise, <c>false</c>.
+        /// </returns>
         public new bool Equals(object x, object y)
         {
             if ((x == null) && (y == null))
@@ -54,9 +77,6 @@ namespace Ploeh.SemanticComparison
                 return true;
             }
 
-            // Check whether each instance is compatible to the given type.
-            // NOTE: The type parameters 'TSource' and 'TDestination' cannot be used with the 'as'
-            // operator because they do not have a class type constraint nor a 'class' constraint.
             if (x is TSource && y is TDestination)
             {
                 return this.GetEvaluators().All(me => me.Evaluator((TSource)x, (TDestination)y));
@@ -65,16 +85,49 @@ namespace Ploeh.SemanticComparison
             return false;
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The obj.</param>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures 
+        /// like a hash table. 
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.
+        ///   </exception>
         public int GetHashCode(object obj)
         {
             return obj == null ? 0 : obj.GetHashCode();
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="x">The source value (against which the destination value will be compared for
+        /// equality).</param>
+        /// <param name="y">The destination value which will be compared for equality against the 
+        /// source value.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; 
+        ///   otherwise, <c>false</c>.
+        /// </returns>
         bool IEqualityComparer.Equals(object x, object y)
         {
             return this.Equals(x, y);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The obj.</param>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures 
+        /// like a hash table. 
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.
+        ///   </exception>
         int IEqualityComparer.GetHashCode(object obj)
         {
             return this.GetHashCode(obj);
