cfaaf2abe303f7985d93a43e4fbc6280ab31d890
Mark Seemann <mark@seemann.ms>
2012-03-18 19:51:31 +0100

    Suggested the removal of the ISpecimenBuilderPipe interface, as my current thinking is that instead, the kernel should be an explicit, immutable tree of ISpecimenBuilder instances.

--- Src/AutoFixture/Kernel/ISpecimenBuilderPipe.cs
+++ Src/AutoFixture/Kernel/ISpecimenBuilderPipe.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+#warning Consider removing this interface, as it's most likely redundant
     public interface ISpecimenBuilderPipe
     {
         IEnumerable<ISpecimenBuilder> Pipe(IEnumerable<ISpecimenBuilder> builders);
