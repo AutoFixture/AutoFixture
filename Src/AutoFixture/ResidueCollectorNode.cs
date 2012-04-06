9d03315a9b0e7288c4b4d6475b5af7c07e8e041d
Mark Seemann <mark@seemann.ms>
2012-04-06 14:46:19 +0200

    Added Marker nodes in order to be able to implement Fixture by creating a tree where Customizations and ResidueCollectors are contained within each marker node.

---
+++ Src/AutoFixture/ResidueCollectorNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    public class ResidueCollectorNode : CompositeSpecimenBuilder
+    {
+        public ResidueCollectorNode(params ISpecimenBuilder[] builders)
+            : base(builders)
+        {
+        }
+
+        public ResidueCollectorNode(IEnumerable<ISpecimenBuilder> builders)
+            : base(builders)
+        {
+        }
+
+        public override ISpecimenBuilderNode Compose(IEnumerable<ISpecimenBuilder> builders)
+        {
+            return new ResidueCollectorNode(builders);
+        }
+    }
+}
