cdc9c18ededf66aadb9ddc588aff125169dc2a98
Mark Seemann <mark@seemann.ms>
2012-04-06 19:30:42 +0200

    Added AutoPropertiesTargetNode as a marker node to signal that if OmitAutoProperties is set to false, this node should be composed to contain a Postprocessor.

---
+++ Src/AutoFixture/AutoPropertiesTargetNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    public class AutoPropertiesTargetNode : CompositeSpecimenBuilder
+    {
+        public AutoPropertiesTargetNode(params ISpecimenBuilder[] builders)
+            : base(builders)
+        {
+        }
+
+        public AutoPropertiesTargetNode(IEnumerable<ISpecimenBuilder> builders)
+            : base(builders)
+        {
+        }
+
+        public override ISpecimenBuilderNode Compose(IEnumerable<ISpecimenBuilder> builders)
+        {
+            return new AutoPropertiesTargetNode(builders);
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/AutoPropertiesTargetNodeTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class AutoPropertiesTargetNodeTest
+    {
+        [Fact]
+        public void SutIsCompositeSpecimenBuilder()
+        {
+            var sut = new AutoPropertiesTargetNode();
+            Assert.IsAssignableFrom<CompositeSpecimenBuilder>(sut);
+        }
+
+        [Fact]
+        public void SutYieldsInjectedArray()
+        {
+            var expected = new[]
+            {
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder()
+            };
+            var sut = new AutoPropertiesTargetNode(expected);
+
+            Assert.True(expected.SequenceEqual(sut));
+        }
+
+        [Fact]
+        public void SutYieldsInjectedSequence()
+        {
+            var expected = new ISpecimenBuilder[]
+            {
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder()
+            }.AsEnumerable();
+            var sut = new AutoPropertiesTargetNode(expected);
+
+            Assert.True(expected.SequenceEqual(sut));
+        }
+
+        [Fact]
+        public void ComposeReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new AutoPropertiesTargetNode();
+            // Exercise system
+            var expected = new[]
+            {
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder()
+            };
+            var actual = sut.Compose(expected);
+            // Verify outcome
+            var aptn = Assert.IsAssignableFrom<AutoPropertiesTargetNode>(actual);
+            Assert.True(expected.SequenceEqual(aptn));
+            // Teardown
+        }
+    }
+}
