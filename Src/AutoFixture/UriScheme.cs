5bae969b0108b08059ba4d72b5b1adfda59c2285
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-04-07 21:35:56 +0300

    Added UriScheme class.

---
+++ Src/AutoFixture/UriScheme.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Represents a URI scheme name. Scheme names consist of a sequence of characters beginning 
+    /// with a letter and followed by any combination of letters, digits, plus ('+'), period ('.'),
+    /// or hyphen ('-').
+    /// </summary>
+    public class UriScheme
+    {
+        private readonly string scheme;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UriScheme"/> class using "scheme" as the
+        /// default URI scheme name.
+        /// </summary>
+        public UriScheme()
+            : this("scheme")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UriScheme"/> class.
+        /// </summary>
+        /// <param name="scheme">The scheme name.</param>
+        public UriScheme(string scheme)
+        {
+            if (string.IsNullOrEmpty(scheme))
+            {
+                throw new ArgumentNullException("scheme");
+            }
+
+            if (!UriScheme.IsValid(scheme))
+            {
+                throw new ArgumentException("The provided scheme is not valid. Scheme names consist of a sequence of characters beginning with a letter and followed by any combination of letters, digits, plus ('+'), period ('.'), or hyphen ('-').");
+            }
+
+            this.scheme = scheme;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.scheme;
+        }
+
+        /// <summary>
+        /// Gets the scheme name.
+        /// </summary>
+        public string Scheme
+        {
+            get { return this.scheme; }
+        }
+
+        private static bool IsValid(string scheme)
+        {
+            return Regex.IsMatch(scheme, "^[a-zA-Z0-9+-.]*$");
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/UriSchemeTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class UriSchemeTest
+    {
+        [Fact]
+        public void InitializeWithDefaultConstructorDoesNotThrow()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.DoesNotThrow(() => new UriScheme());
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithDefaultConstructorSetsCorrectScheme()
+        {
+            // Fixture setup
+            var sut = new UriScheme();
+            string expectedScheme = "scheme";
+            // Exercise system
+            string result = sut.Scheme;
+            // Verify outcome
+            Assert.Equal(expectedScheme, result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void InitializeWithNullOrEmptySchemeThrows(string scheme)
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(
+                () => new UriScheme(scheme));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("scheme:")]
+        [InlineData("scheme/")]
+        [InlineData("scheme:/")]
+        public void InitializeWithInvalidSchemeThrows(string scheme)
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentException>(
+                () => new UriScheme(scheme));
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithSchemeParameterSetsCorrectScheme()
+        {
+            // Fixture setup
+            string expectedScheme = "http";
+            var sut = new UriScheme("http");
+            // Exercise system
+            string result = sut.Scheme;
+            // Verify outcome
+            Assert.Equal(expectedScheme, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void ToStringReturnsCorrectResult()
+        {
+            // Fixture setup
+            string expected = "http";
+            var sut = new UriScheme("http");
+            // Exercise system
+            var result = sut.ToString();
+            // Verify outcome
+            Assert.Equal(expected, result);
+            // Teardown
+        }
+    }
+}
