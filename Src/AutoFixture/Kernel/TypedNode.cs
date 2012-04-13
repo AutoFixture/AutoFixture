3332355ae5426f73543e0368f6a8544b993fbb7e
Mark Seemann <mark@seemann.ms>
2012-04-13 13:35:35 +0200

    Partially introducing TypedNode as an intended replacement of TypedBuilderComposer. However, before continuing, more composite ISpecimenBuilder implementations need to be turned into explicit node: FilteringSpecimenBuilder and NoSpecimenOutputGuard.

---
+++ Src/AutoFixture/Kernel/TypedNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class TypedNode : ISpecimenBuilderNode
+    {
+        private readonly Type targetType;
+        private readonly ISpecimenBuilder factory;
+
+        public TypedNode(Type targetType, ISpecimenBuilder factory)
+        {
+            if (targetType == null)
+                throw new ArgumentNullException("targetType");
+            if (factory == null)
+                throw new ArgumentNullException("factory");
+
+            this.targetType = targetType;
+            this.factory = factory;
+        }
+
+        public virtual ISpecimenBuilderNode Compose(IEnumerable<ISpecimenBuilder> builders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<ISpecimenBuilder> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type TargetType
+        {
+            get { return this.targetType; }
+        }
+
+        public ISpecimenBuilder Factory
+        {
+            get { return this.factory; }
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/TypedNodeTest.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class TypedNodeTest
+    {
+        [Fact]
+        public void SutIsNode()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            // Exercise system
+            var sut = new TypedNode(dummyType, dummyBuilder);
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilderNode>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void ConstructWithNullTypeThrows()
+        {
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            Assert.Throws<ArgumentNullException>(() =>
+                new TypedNode(null, dummyBuilder));
+        }
+
+        [Fact]
+        public void ConstructWithNullFactoryThrows()
+        {
+            var dummyType = typeof(object);
+            Assert.Throws<ArgumentNullException>(() =>
+                new TypedNode(dummyType, null));
+        }
+
+        [Theory]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(Guid))]
+        [InlineData(typeof(Version))]
+        public void TargetTypeIsCorrect(Type expected)
+        {
+            // Fixture setup
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            var sut = new TypedNode(expected, dummyBuilder);
+            // Exercise system
+            Type actual = sut.TargetType;
+            // Verify outcome
+            Assert.Equal(expected, actual);
+            // Teardown
+        }
+
+        [Fact]
+        public void FactoryIsCorrect()
+        {
+            // Fixture setup
+            var dummyType = typeof(object);
+            var expected = new DelegatingSpecimenBuilder();
+            var sut = new TypedNode(dummyType, expected);
+            // Exercise system
+            ISpecimenBuilder actual = sut.Factory;
+            // Verify outcome
+            Assert.Equal(expected, actual);
+            // Teardown
+        }
+
+        [Theory(Skip = "Waiting for builders to be turned into nodes.")]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(Guid))]
+        [InlineData(typeof(Version))]
+        public void SutYieldsCorrectDescendants(Type targetType)
+        {
+            // Fixture setup
+            var factory = new DelegatingSpecimenBuilder();
+            var sut = new TypedNode(targetType, factory);
+            // Exercise system
+            // Verify outcome
+            var expected = new FilteringSpecimenBuilder(
+                new CompositeSpecimenBuilder(
+                    new NoSpecimenOutputGuard(
+                        factory,
+                        new InverseRequestSpecification(
+                            new SeedRequestSpecification(
+                                targetType))),
+                    new SeedIgnoringRelay()),
+                new OrRequestSpecification(
+                    new SeedRequestSpecification(targetType),
+                    new ExactTypeSpecification(targetType)));
+            Assert.True(false, "Unspecified assertion");
+            // Teardown
+        }
+
+        private class TypedNodeComparer : IEqualityComparer<ISpecimenBuilder>
+        {
+            private readonly IEqualityComparer<IRequestSpecification> specificationComparer;
+
+            public TypedNodeComparer()
+            {
+                this.specificationComparer = new SpecificationComparer();
+            }
+
+            public bool Equals(ISpecimenBuilder x, ISpecimenBuilder y)
+            {
+                var fx = x as FilteringSpecimenBuilder;
+                var fy = y as FilteringSpecimenBuilder;
+                if (fx != null && fy != null)
+                {
+                    if (this.specificationComparer.Equals(fx.Specification, fy.Specification))
+                        return true;
+                }
+
+                if (x is CompositeSpecimenBuilder && y is CompositeSpecimenBuilder)
+                    return true;
+
+                var gx = x as NoSpecimenOutputGuard;
+                var gy = y as NoSpecimenOutputGuard;
+                if (gx != null && gy != null)
+                {
+                    if (this.specificationComparer.Equals(gx.Specification, gy.Specification))
+                        return true;
+                }
+
+                return EqualityComparer<ISpecimenBuilder>.Default.Equals(x, y);
+            }
+
+            public int GetHashCode(ISpecimenBuilder obj)
+            {
+                return EqualityComparer<ISpecimenBuilder>.Default.GetHashCode(obj);
+            }
+
+            private class SpecificationComparer : IEqualityComparer<IRequestSpecification>
+            {
+                public bool Equals(IRequestSpecification x, IRequestSpecification y)
+                {
+                    var invx = x as InverseRequestSpecification;
+                    var invy = y as InverseRequestSpecification;
+                    if (invx != null && invy != null)
+                    {
+                        if (this.Equals(invx.Specification, invy.Specification))
+                            return true;
+                    }
+
+                    var ox = x as OrRequestSpecification;
+                    var oy = y as OrRequestSpecification;
+                    if (ox != null && oy != null)
+                    {
+                        if (ox.Specifications.SequenceEqual(oy.Specifications, this))
+                            return true;
+                    }
+
+                    var sx = x as SeedRequestSpecification;
+                    var sy = y as SeedRequestSpecification;
+                    if (sx != null && sy != null)
+                    {
+                        if (sx.TargetType == sy.TargetType)
+                            return true;
+                    }
+
+                    var ex = x as ExactTypeSpecification;
+                    var ey = y as ExactTypeSpecification;
+                    if (ex != null && ey != null)
+                    {
+                        if (ex.TargetType == ey.TargetType)
+                            return true;
+                    }
+
+                    return EqualityComparer<IRequestSpecification>.Default.Equals(x, y);
+                }
+
+                public int GetHashCode(IRequestSpecification obj)
+                {
+                    return EqualityComparer<IRequestSpecification>.Default.GetHashCode(obj);
+                }
+            }
+        }
+    }
+}
