f5aeef9274e0ce7032bd51b78be4c249bcd037b7
Mark Seemann <mark@seemann.ms>
2012-04-13 09:46:52 +0200

    Added the OmitSpecimen class, which is a pure marker object that can be used to indicate that a particular request should be omitted. The primary purpose is to notify the AutoPropertiesCommand that a property or field should be omitted.

---
+++ Src/AutoFixture/Kernel/OmitSpecimen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class OmitSpecimen : IEquatable<OmitSpecimen>
+    {
+        public override bool Equals(object obj)
+        {
+            var other = obj as OmitSpecimen;
+            if (other != null)
+                return this.Equals(other);
+            return base.Equals(obj);
+        }
+
+        public bool Equals(OmitSpecimen other)
+        {
+            return other != null;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+    }
+}
