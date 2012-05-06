b4b33bf2e33d8184097c044502b7c1c28a5bff14
Mark Seemann <mark@seemann.ms>
2012-05-06 13:03:05 +0200

    Added OmitRecursionGuard.

---
+++ Src/AutoFixture/Kernel/OmitRecursionGuard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class OmitRecursionGuard : RecursionGuard
+    {
+        public OmitRecursionGuard(ISpecimenBuilder builder)
+            : base(builder)
+        {
+        }
+
+        public OmitRecursionGuard(ISpecimenBuilder builder, IEqualityComparer comparer)
+            : base(builder, comparer)
+        {
+        }
+
+        public override object HandleRecursiveRequest(object request)
+        {
+            return new OmitSpecimen();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/OmitRecursionGuardTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class OmitRecursionGuardTest
+    {
+        [Fact]
+        public void SutIsRecursionGuard()
+        {
+            // Fixture setup
+            // Exercise system
+            var dummy = new DelegatingSpecimenBuilder();
+            var sut = new OmitRecursionGuard(dummy);
+            // Verify outcome
+            Assert.IsAssignableFrom<RecursionGuard>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void HandleRecursiveRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var dummyBuilder = new DelegatingSpecimenBuilder();
+            var sut = new OmitRecursionGuard(dummyBuilder);
+            // Exercise system
+            var dummyRequest = new object();
+            var actual = sut.HandleRecursiveRequest(dummyRequest);
+            // Verify outcome
+            var expected = new OmitSpecimen();
+            Assert.Equal(expected, actual);
+            // Teardown
+        }
+
+        [Fact]
+        public void ConstructorThrowsOnNullComparer()
+        {
+            // Fixture setup
+            var dummy = new DelegatingSpecimenBuilder();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new OmitRecursionGuard(dummy, null));
+            // Teardown
+        }
+    }
+}
