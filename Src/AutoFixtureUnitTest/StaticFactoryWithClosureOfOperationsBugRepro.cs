d973f2326565db65e685263dd8d86434a82a9729
Mark Seemann <mark@seemann.ms>
2012-06-22 21:13:27 +0200

    Fixed a bug where MethodInvoker didn't understand and respect OmitSpecimen.

---
+++ Src/AutoFixtureUnitTest/StaticFactoryWithClosureOfOperationsBugRepro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    /* This class contains regression tests against this bug:
+     * http://autofixture.codeplex.com/workitem/4256 */
+    public class StaticFactoryWithClosureOfOperationsBugRepro
+    {
+        [Fact]
+        public void CreateWithOmitOnRecursionThrowsAppriateException()
+        {
+            // Fixture setup
+            var fixture = new Fixture();
+            fixture.Behaviors
+                .OfType<ThrowingRecursionBehavior>()
+                .ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            // Exercise system and verify outcome
+            Assert.Throws<ObjectCreationException>(() =>
+                fixture.CreateAnonymous<ClosureOfOperationsHost>());
+            // Teardown
+        }
+
+        private class ClosureOfOperationsHost
+        {
+            private ClosureOfOperationsHost()
+            {
+            }
+
+            public static ClosureOfOperationsHost CloseAround(
+                ClosureOfOperationsHost thing)
+            {
+                return thing;
+            }
+        }
+    }
+}
