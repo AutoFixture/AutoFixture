968c34b8dc2963caa3bb963a6a99ff5f52d3fbbc
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-09-20 15:24:05 +0300

    Extracted the minimum-maximum calculation to a nested type.

--- Src/AutoFixture/RandomNumericSequenceGenerator.cs
+++ Src/AutoFixture/RandomNumericSequenceGenerator.cs
@@ -8,13 +8,10 @@ namespace Ploeh.AutoFixture
     public class RandomNumericSequenceGenerator : ISpecimenBuilder
     {
         private readonly IEnumerable<int> boundaries;
-        private readonly int upperBound;
-        private readonly int lowerBound;
 
         private readonly Random random;
         private readonly List<int> numbers;
-        private int minimum;
-        private int maximum;
+        private readonly MinMax minMax;
 
         public RandomNumericSequenceGenerator()
             : this(Byte.MaxValue, Int16.MaxValue, Int32.MaxValue)
@@ -34,11 +31,10 @@ namespace Ploeh.AutoFixture
             }
 
             this.boundaries = boundaries;
-            this.lowerBound = this.boundaries.First();
-            this.upperBound = this.boundaries.Last();
 
             this.random = new Random();
             this.numbers = new List<int>();
+            this.minMax = new MinMax(boundaries);
         }
 
         public IEnumerable<int> Boundaries
@@ -112,38 +108,88 @@ namespace Ploeh.AutoFixture
 
         private int GetNextRandom()
         {
-            if (this.numbers.Count == this.upperBound)
+            MinMax current;
+            try
+            {
+                current = this.minMax.Calculate(this.numbers.Count);
+            }
+            catch (ArgumentException)
             {
                 throw new InvalidOperationException(
                     "The upper bound has been reached. Either increase the boundaries or use the default constructor to generate up to 2147483647 random numbers.");
             }
 
-            if (this.numbers.Count == 0)
+            if (current.GetIsBoundFromAbove())
             {
-                this.minimum = 1;
-                this.maximum = this.lowerBound + 1;
-            }
-            else if (this.numbers.Count == this.maximum - 1)
-            {
-                this.minimum = this.maximum;
-                this.maximum = this.boundaries.FirstOrDefault(x => x > this.minimum);
-
-                if (this.maximum == 0 && this.minimum == this.upperBound)
-                {
-                    this.numbers.Add(this.upperBound);
-                    return this.upperBound;
-                }
+                int upperBound = this.boundaries.Last();
+                this.numbers.Add(upperBound);
+                return upperBound;
             }
 
             int result;
             do
             {
-                result = this.random.Next(this.minimum, this.maximum);
+                result = this.random.Next(current.Minimum, current.Maximum);
             }
             while (this.numbers.Contains(result));
 
             this.numbers.Add(result);
             return result;
+        }
+
+        private sealed class MinMax
+        {
+            private readonly int[] boundaries;
+            private readonly int upperBound;
+
+            private int minimum;
+            private int maximum;
+
+            private MinMax(int[] boundaries, int minimum, int maximum)
+            {
+                this.boundaries = boundaries;
+                this.upperBound = boundaries[boundaries.Length - 1];
+
+                this.minimum = minimum;
+                this.maximum = maximum;
+            }
+
+            internal MinMax(int[] boundaries)
+                : this(boundaries, 1, boundaries[0] + 1)
+            {
+            }
+
+            internal int Minimum
+            {
+                get { return this.minimum; }
+            }
+
+            internal int Maximum
+            {
+                get { return this.maximum; }
+            }
+
+            internal MinMax Calculate(int number)
+            {
+                if (number == this.upperBound)
+                {
+                    throw new ArgumentException(
+                        "Number must be lower than the upper bound.");
+                }
+
+                if (number == this.maximum - 1)
+                {
+                    this.minimum = this.maximum;
+                    this.maximum = this.boundaries.FirstOrDefault(x => x > this.minimum);
+                }
+
+                return new MinMax(this.boundaries, this.minimum, this.maximum);
+            }
+
+            internal bool GetIsBoundFromAbove()
+            {
+                return this.maximum == 0 && this.minimum == this.upperBound;
+            }
         }
     }
 }
