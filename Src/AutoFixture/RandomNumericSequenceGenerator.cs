6d754e7867d90bbdbaee3f306e7e926f1031d531
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-09-20 14:42:58 +0300

    Created RandomNumericSequenceGenerator class.

---
+++ Src/AutoFixture/RandomNumericSequenceGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ploeh.AutoFixture
+{
+    public class RandomNumericSequenceGenerator : ISpecimenBuilder
+    {
+        private readonly IEnumerable<int> boundaries;
+
+        public RandomNumericSequenceGenerator(IEnumerable<int> boundaries)
+            : this(boundaries.ToArray())
+        {
+        }
+
+        public RandomNumericSequenceGenerator(params int[] boundaries)
+        {
+            if (boundaries == null)
+            {
+                throw new ArgumentNullException("boundaries");
+            }
+
+            this.boundaries = boundaries;
+        }
+
+        public IEnumerable<int> Boundaries
+        {
+            get { return this.boundaries; }
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var type = request as Type;
+            if (type == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            return null;
+        }
+    }
+}
