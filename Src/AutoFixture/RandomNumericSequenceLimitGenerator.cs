72d1123030cd694d79d391702db1b2808762bf96
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-09-23 01:29:41 +0300

    Added RandomNumericSequenceLimitGenerator class.

---
+++ Src/AutoFixture/RandomNumericSequenceLimitGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Creates new <see cref="RandomNumericSequenceLimit"/> instances.
+    /// </summary>
+    public class RandomNumericSequenceLimitGenerator : ISpecimenBuilder
+    {
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request != typeof(RandomNumericSequenceLimit))
+            {
+                return new NoSpecimen(request);
+            }
+
+            return new RandomNumericSequenceLimit();
+        }
+    }
+}
