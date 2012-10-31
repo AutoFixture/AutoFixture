4210b0463ea6b8910ecb920a897498cf8d49bc8b
Mark Seemann <mark@seemann.ms>
2012-10-31 16:34:51 +0100

    Incremented patch version,
    to indicate a bug fix.

--- Src/AutoFixtureUnitTest/Properties/AssemblyInfo.cs
+++ Src/AutoFixtureUnitTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.13.3.0")]
-[assembly: AssemblyFileVersion("2.13.3.0")]
+[assembly: AssemblyVersion("2.13.4.0")]
+[assembly: AssemblyFileVersion("2.13.4.0")]
