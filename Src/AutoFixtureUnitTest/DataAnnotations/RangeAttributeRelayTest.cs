a5b89aca2c9bb3ed0082a09ad26c227da9fc8175
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-11-09 00:35:51 +0200

    Modified RangeAttributeRelay to support Nullable Types.

--- Src/AutoFixtureUnitTest/DataAnnotations/RangeAttributeRelayTest.cs
+++ Src/AutoFixtureUnitTest/DataAnnotations/RangeAttributeRelayTest.cs
@@ -119,21 +119,31 @@ namespace Ploeh.AutoFixtureUnitTest.DataAnnotations
         }
 
         [Theory]
-        [InlineData(10, 20)]
-        [InlineData(-2, -1)]
-        [InlineData("10.1", "20.2")]
-        [InlineData("-2.2", "-1.1")]
-        [InlineData(10.0, 20.0)]
-        [InlineData(-2.0, -1.0)]
-        [InlineData(10, 20)]
-        [InlineData(-2, -1)]
-        public void CreateWithPropertyDecoratedWithRangeAttributeReturnsCorrectResult( 
+        [InlineData("Property", 10, 20)]
+        [InlineData("Property", -2, -1)]
+        [InlineData("Property", "10.1", "20.2")]
+        [InlineData("Property", "-2.2", "-1.1")]
+        [InlineData("Property", 10.0, 20.0)]
+        [InlineData("Property", -2.0, -1.0)]
+        [InlineData("Property", 10, 20)]
+        [InlineData("Property", -2, -1)]
+        [InlineData("NullableTypeProperty", 10, 20)]
+        [InlineData("NullableTypeProperty", -2, -1)]
+        [InlineData("NullableTypeProperty", "10.1", "20.2")]
+        [InlineData("NullableTypeProperty", "-2.2", "-1.1")]
+        [InlineData("NullableTypeProperty", 10.0, 20.0)]
+        [InlineData("NullableTypeProperty", -2.0, -1.0)]
+        [InlineData("NullableTypeProperty", 10, 20)]
+        [InlineData("NullableTypeProperty", -2, -1)]
+        public void CreateWithPropertyDecoratedWithRangeAttributeReturnsCorrectResult(
+            string name,
             object attributeMinimum, 
             object attributeMaximum)
         {
             // Fixture setup
-            var request = typeof(RangeValidatedType).GetProperty("Property");
-            Type target = request.PropertyType;
+            var request = typeof(RangeValidatedType).GetProperty(name);
+            Type target = Nullable.GetUnderlyingType(request.PropertyType) 
+                ?? request.PropertyType;
 
             var expectedRequest = new RangedNumberRequest(
                 target,
@@ -155,21 +165,31 @@ namespace Ploeh.AutoFixtureUnitTest.DataAnnotations
         }
 
         [Theory]
-        [InlineData(10, 20)]
-        [InlineData(-2, -1)]
-        [InlineData("10.1", "20.2")]
-        [InlineData("-2.2", "-1.1")]
-        [InlineData(10.0, 20.0)]
-        [InlineData(-2.0, -1.0)]
-        [InlineData(10, 20)]
-        [InlineData(-2, -1)]
+        [InlineData("Field", 10, 20)]
+        [InlineData("Field", -2, -1)]
+        [InlineData("Field", "10.1", "20.2")]
+        [InlineData("Field", "-2.2", "-1.1")]
+        [InlineData("Field", 10.0, 20.0)]
+        [InlineData("Field", -2.0, -1.0)]
+        [InlineData("Field", 10, 20)]
+        [InlineData("Field", -2, -1)]
+        [InlineData("NullableTypeField", 10, 20)]
+        [InlineData("NullableTypeField", -2, -1)]
+        [InlineData("NullableTypeField", "10.1", "20.2")]
+        [InlineData("NullableTypeField", "-2.2", "-1.1")]
+        [InlineData("NullableTypeField", 10.0, 20.0)]
+        [InlineData("NullableTypeField", -2.0, -1.0)]
+        [InlineData("NullableTypeField", 10, 20)]
+        [InlineData("NullableTypeField", -2, -1)]
         public void CreateWithFieldDecoratedWithRangeAttributeReturnsCorrectResult(
+            string name,
             object attributeMinimum,
             object attributeMaximum)
         {
             // Fixture setup
-            var request = typeof(RangeValidatedType).GetField("Field");
-            Type target = request.FieldType;
+            var request = typeof(RangeValidatedType).GetField(name);
+            Type target = Nullable.GetUnderlyingType(request.FieldType)
+                ?? request.FieldType;
 
             var expectedRequest = new RangedNumberRequest(
                 target,
