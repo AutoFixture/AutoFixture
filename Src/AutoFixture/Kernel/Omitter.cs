91ea2b87e57995b65e5507bce1507476cb166941
Mark Seemann <mark@seemann.ms>
2012-11-09 14:21:51 +0100

    Added Omitter,
    which is intended to be used to skip a particular property of field by
    issuing an OmitSpecimen instance.

---
+++ Src/AutoFixture/Kernel/Omitter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class Omitter : ISpecimenBuilder
+    {
+        private readonly IRequestSpecification specification;
+
+        public Omitter()
+            : this(new TrueRequestSpecification())
+        {
+        }
+
+        public Omitter(IRequestSpecification specification)
+        {
+            if (specification == null)
+                throw new ArgumentNullException("specification");
+
+            this.specification = specification;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
+            if (this.specification.IsSatisfiedBy(request))
+                return new OmitSpecimen();
+
+            return new NoSpecimen(request);
+        }
+
+        public IRequestSpecification Specification
+        {
+            get { return this.specification; }
+        }
+    }
+}
