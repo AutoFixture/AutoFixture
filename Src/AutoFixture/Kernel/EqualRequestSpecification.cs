649a1d4eb4a9443aa4d8582863d0e3e7dd797e7e
Mark Seemann <mark@seemann.ms>
2012-11-09 22:47:18 +0100

    Added XML docs to EqualRequestSpecification.

--- Src/AutoFixture/Kernel/EqualRequestSpecification.cs
+++ Src/AutoFixture/Kernel/EqualRequestSpecification.cs
@@ -6,16 +6,40 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+    /// <summary>
+    /// Evaluates whether a request is equal to an expected target.
+    /// </summary>
     public class EqualRequestSpecification : IRequestSpecification
     {
         private readonly object target;
         private readonly IEqualityComparer comparer;
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="EqualRequestSpecification" /> class.
+        /// </summary>
+        /// <param name="target">The expected target.</param>
+        /// <seealso cref="EqualRequestSpecification(object, IEqualityComparer)" />
+        /// <seealso cref="Target" />
+        /// <exception cref="System.ArgumentNullException">target</exception>
         public EqualRequestSpecification(object target)
             : this(target, EqualityComparer<object>.Default)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="EqualRequestSpecification" /> class.
+        /// </summary>
+        /// <param name="target">The expected target.</param>
+        /// <param name="comparer">
+        /// The comparer used to evaluate whether a request is equal to
+        /// <paramref name="target" />.
+        /// </param>
+        /// <seealso cref="EqualRequestSpecification(object)" />
+        /// <seealso cref="Target" />
+        /// <exception cref="System.ArgumentNullException">target</exception>
+        /// <exception cref="System.ArgumentNullException">comparer</exception>
         public EqualRequestSpecification(
             object target,
             IEqualityComparer comparer)
@@ -29,11 +53,28 @@ namespace Ploeh.AutoFixture.Kernel
             this.comparer = comparer;
         }
 
+        /// <summary>
+        /// Evaluates whether a request for a specimen is equal to the expected
+        /// target.
+        /// </summary>
+        /// <param name="request">The specimen request.</param>
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="request" /> is equal to
+        /// <see cref="Target" />; otherwise, <see langword="false" />.
+        /// </returns>
         public bool IsSatisfiedBy(object request)
         {
             return this.comparer.Equals(this.target, request);
         }
 
+        /// <summary>
+        /// Gets the expected target.
+        /// </summary>
+        /// <value>
+        /// The expected target, originally supplied via the constructor.
+        /// </value>
+        /// <seealso cref="EqualRequestSpecification(object)" />
+        /// <seealso cref="EqualRequestSpecification(object, IEqualityComparer)" />
         public object Target
         {
             get { return this.target; }
