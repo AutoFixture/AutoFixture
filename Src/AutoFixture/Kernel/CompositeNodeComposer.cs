1f7301a72382e550d894d1de9f99678e51bb8ea3
Mark Seemann <mark@seemann.ms>
2012-11-12 11:06:45 +0100

    Introduced CompositeNodeComposer.
    All implementation still remains, but the intended purpose is to decorate
    given graph of ISpecimenBuilders (that is, an ISpecimenBuilderNode) and
    delegate its behavior into any ICustomizationComposer<T> instances found
    in that graph.

---
+++ Src/AutoFixture/Kernel/CompositeNodeComposer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Dsl;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class CompositeNodeComposer<T> : ICustomizationComposer<T>
+    {
+        public IPostprocessComposer<T> FromSeed(Func<T, T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory(ISpecimenBuilder factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory(Func<T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput>(Func<TInput, T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2>(Func<TInput1, TInput2, T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2, TInput3>(Func<TInput1, TInput2, TInput3, T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> FromFactory<TInput1, TInput2, TInput3, TInput4>(Func<TInput1, TInput2, TInput3, TInput4, T> factory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecimenBuilder Compose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> Do(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> OmitAutoProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> With<TProperty>(System.Linq.Expressions.Expression<Func<T, TProperty>> propertyPicker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> With<TProperty>(System.Linq.Expressions.Expression<Func<T, TProperty>> propertyPicker, TProperty value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> WithAutoProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPostprocessComposer<T> Without<TProperty>(System.Linq.Expressions.Expression<Func<T, TProperty>> propertyPicker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Dsl/CompositeNodeComposerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture.Dsl;
+
+namespace Ploeh.AutoFixtureUnitTest.Dsl
+{
+    public class CompositeNodeComposerTests
+    {
+        [Fact]
+        public void SutIsComposer()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new CompositeNodeComposer<object>();
+            // Verify outcome
+            Assert.IsAssignableFrom<ICustomizationComposer<object>>(sut);
+            // Teardown
+        }
+    }
+}
