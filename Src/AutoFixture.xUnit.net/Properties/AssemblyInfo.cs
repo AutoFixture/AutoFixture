ed3e003b8b56a786747ce4a17b4744fbbba0e1a5
Mark Seemann <mark@seemann.ms>
2012-11-14 23:46:48 +0100

    Incremented minor version to indicate new features.
    Omitter is a new class that enables a user to specifically opt out of
    AutoProperties based on a Specification.
    EqualRequestSpecification is an Adapter from IEqualityComparer to
    IRequestSpecification.

--- Src/AutoFixture.xUnit.net/Properties/AssemblyInfo.cs
+++ Src/AutoFixture.xUnit.net/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.13.5.0")]
-[assembly: AssemblyFileVersion("2.13.5.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
 
 [assembly: CLSCompliant(true)]
