6bebb1d09e2511e5d53e4125e817a7fca4b615c3
Mark Seemann <mark@seemann.ms>
2012-11-15 10:54:55 +0100

    Added SpecimenBuilderNodeFactory helper class,
    which is intended to be used to create well-known nodes (such as
    Composers or Typed Nodes) instead of relying on inheritance.

---
+++ Src/AutoFixture/Kernel/SpecimenBuilderNodeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Dsl;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class SpecimenBuilderNodeFactory
+    {
+        public static NodeComposer<T> CreateComposer<T>()
+        {
+            return new NodeComposer<T>(
+                new TypedNode(
+                    typeof(T),
+                    new MethodInvoker(
+                        new ModestConstructorQuery())));
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/SpecimenBuilderNodeFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture.Dsl;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class SpecimenBuilderNodeFactoryTests
+    {
+        [Fact]
+        public void CreateComposerReturnsCorrectResult()
+        {
+            // Fixture setup
+            // Exercise system
+            NodeComposer<int> actual = 
+                SpecimenBuilderNodeFactory.CreateComposer<int>();
+            // Verify outcome
+            var expected = new NodeComposer<int>(
+                new TypedNode(
+                    typeof(int),
+                    new MethodInvoker(
+                        new ModestConstructorQuery())));
+            Assert.True(expected.GraphEquals(actual, new NodeComparer()));
+            // Teardown
+        }
+    }
+}
