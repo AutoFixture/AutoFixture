d7be9698f2153e088f9b970364b5505c2dbeaa67
Mark Seemann <mark@seemann.ms>
2012-11-15 12:02:12 +0100

    Added documentation to ISpecimenBuilderNode.

--- Src/AutoFixture/Kernel/ISpecimenBuilderNode.cs
+++ Src/AutoFixture/Kernel/ISpecimenBuilderNode.cs
@@ -5,8 +5,56 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+    /// <summary>
+    /// A node in a graph of <see cref="ISpecimenBuilder" /> instances.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// AutoFixture's kernel basically works as a 'Graph of Responsibility'.
+    /// Each <see cref="ISpecimenBuilder" /> gets a chance to handle a request,
+    /// and if it can't do that, the next ISpecimenBuilder is asked to handle
+    /// the request. When an ISpecimenBuilder instance returns a useful
+    /// specimen, that instance is returned and the results (if any) from
+    /// lower-priority ISpecimenBuilder instances are ignored.
+    /// </para>
+    /// <para>
+    /// In theory, one could order all ISpecimenBuilder instances in a flat
+    /// Chain of Responsibility, but instead, the ISpecimenBuilderNode
+    /// interface defines the responsibilities of a 'parent' node in a deeper
+    /// graph. Each ISpecimenBuilder node constitute an intermediate node in a
+    /// graph, while itself being a parent to other nodes. In the degenerate
+    /// case when an ISpecimenBuilderNode has no child nodes, it effectively
+    /// becomes a leaf node. Otherwise, leaf nodes are typically
+    /// ISpecimenBuilder instances.
+    /// </para>
+    /// </remarks>
     public interface ISpecimenBuilderNode : ISpecimenBuilder, IEnumerable<ISpecimenBuilder>
     {
+        /// <summary>
+        /// Composes the supplied builders.
+        /// </summary>
+        /// <param name="builders">The builders to compose.</param>
+        /// <returns>A <see cref="ISpecimenBuilderNode" /> instance.</returns>
+        /// <remarks>
+        /// <para>
+        /// Note to implementers:
+        /// </para>
+        /// <para>
+        /// The intent of this method is to compose the supplied
+        /// <paramref name="builders" /> into a new instance of the type
+        /// implementing <see cref="ISpecimenBuilderNode" />. Thus, the
+        /// concrete return type is expected to the same type as the type
+        /// implementing the method. However, it is not considered a failure to
+        /// deviate from this idiom - it would just not be a mainstream
+        /// implementation.
+        /// </para>
+        /// <para>
+        /// The returned instance is normally expected to contain the builders
+        /// supplied as an argument, but again this is not strictly required.
+        /// The implementation may decide to filter the sequence or add to it
+        /// during composition.
+        /// </para>
+        /// </remarks>
         ISpecimenBuilderNode Compose(IEnumerable<ISpecimenBuilder> builders);
     }
 }
