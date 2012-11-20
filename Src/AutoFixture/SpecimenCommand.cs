d98a4e776f726f79566ef751a4e58a5e498e7e08
Mark Seemann <mark@seemann.ms>
2012-11-20 21:15:57 +0100

    Added XML comment documentation.

--- Src/AutoFixture/SpecimenCommand.cs
+++ Src/AutoFixture/SpecimenCommand.cs
@@ -6,8 +6,25 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// Provides convenience methods to perform (partially) anonymous Commands.
+    /// </summary>
     public static class SpecimenCommand
     {
+        /// <summary>
+        /// Invokes the supplied action with an anonymous parameter value.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the anonymous parameter.
+        /// </typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// builder is null
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// action is null
+        /// </exception>
         public static void Do<T>(
             this ISpecimenBuilder builder,
             Action<T> action)
@@ -20,6 +37,23 @@ namespace Ploeh.AutoFixture
             action(builder.Create<T>());
         }
 
+        /// <summary>
+        /// Invokes the supplied action with anonymous parameter values.
+        /// </summary>
+        /// <typeparam name="T1">
+        /// The type of the first anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T2">
+        /// The type of the second anonymous parameter.
+        /// </typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// builder is null
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// action is null
+        /// </exception>
         public static void Do<T1, T2>(
             this ISpecimenBuilder builder,
             Action<T1, T2> action)
@@ -32,6 +66,26 @@ namespace Ploeh.AutoFixture
             action(builder.Create<T1>(), builder.Create<T2>());
         }
 
+        /// <summary>
+        /// Invokes the supplied action with anonymous parameter values.
+        /// </summary>
+        /// /// <typeparam name="T1">
+        /// The type of the first anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T2">
+        /// The type of the second anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T3">
+        /// The type of the third anonymous parameter.
+        /// </typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// builder is null
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// action is null
+        /// </exception>
         public static void Do<T1, T2, T3>(
             this ISpecimenBuilder builder,
             Action<T1, T2, T3> action)
@@ -47,6 +101,29 @@ namespace Ploeh.AutoFixture
                 builder.Create<T3>());
         }
 
+        /// <summary>
+        /// Invokes the supplied action with anonymous parameter values.
+        /// </summary>
+        /// <typeparam name="T1">
+        /// The type of the first anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T2">
+        /// The type of the second anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T3">
+        /// The type of the third anonymous parameter.
+        /// </typeparam>
+        /// <typeparam name="T4">
+        /// The type of the fourth anonymous parameter.
+        /// </typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// builder is null
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// action is null
+        /// </exception>
         public static void Do<T1, T2, T3, T4>(
             this ISpecimenBuilder builder,
             Action<T1, T2, T3, T4> action)
