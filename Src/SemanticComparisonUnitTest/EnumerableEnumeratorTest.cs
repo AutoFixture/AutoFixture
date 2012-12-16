8d5217a49d0f41c084fef64261719226eed50624
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-12-16 23:20:41 +0100

    Added EnumerableEnumerator class along with tests

---
+++ Src/SemanticComparisonUnitTest/EnumerableEnumeratorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Ploeh.SemanticComparison.UnitTest
+{
+    public class EnumerableEnumeratorTest
+    {
+        [Fact]
+        public void GetEnumerator_ReturnsTheSpecifiedEnumerator()
+        {
+            // Fixture setup
+            var expected = Enumerable.Empty<object>().GetEnumerator();
+            var sut = new EnumerableEnumerator<object>(expected);
+
+            // Exercise system
+            var actual = sut.GetEnumerator();
+
+            // Verify outcome
+            Assert.Same(expected, actual);
+        }
+
+        [Fact]
+        public void InitializeWithNullEnumeratorThrows()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new EnumerableEnumerator<object>(null));
+        }
+
+        [Fact]
+        public void SutIsGenericIEnumerable()
+        {
+            // Exercise system
+            var sut = new EnumerableEnumerator<object>(Enumerable.Empty<object>().GetEnumerator());
+
+            // Verify outcome
+            Assert.IsAssignableFrom<IEnumerable<object>>(sut);
+        }
+    }
+}
