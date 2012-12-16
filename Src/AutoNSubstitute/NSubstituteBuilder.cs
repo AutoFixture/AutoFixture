ed9b7560545f62ab9eca2c5fda597e405d8950e5
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-12-16 20:50:55 +0100

    Added tests for NSubstituteBuilder and code to make them pass.

--- Src/AutoNSubstitute/NSubstituteBuilder.cs
+++ Src/AutoNSubstitute/NSubstituteBuilder.cs
@@ -1,5 +1,4 @@
-using System;
-using NSubstitute;
+﻿using System;
 using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.AutoNSubstitute
@@ -7,6 +6,32 @@ namespace Ploeh.AutoFixture.AutoNSubstitute
     public class NSubstituteBuilder : ISpecimenBuilder
     {
         private readonly ISpecimenBuilder builder;
+
+        /// <summary>Initializes a new instance of the <see cref="NSubstituteBuilder"/> class with an
+        ///     <see cref="ISpecimenBuilder"/> to decorate.</summary>
+        /// <param name="builder">The builder which must build mock instances.</param>
+        /// <remarks>
+        ///     <para>
+        ///         <paramref name="builder"/> is subsequently available through the <see cref="Builder"/>
+        ///         property.
+        ///     </para>
+        /// </remarks>
+        /// <seealso cref="Builder"/>
+        public NSubstituteBuilder(ISpecimenBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException("builder");
+
+            this.builder = builder;
+        }
+
+        /// <summary>Gets the decorated builder supplied through the constructor.</summary>
+        /// <seealso cref="NSubstituteBuilder(ISpecimenBuilder)"/>
+        public ISpecimenBuilder Builder
+        {
+            get { return builder; }
+        }
+
         /// <summary>Creates a new specimen based on a request.</summary>
         /// <param name="request">The request that describes what to create.</param>
         /// <param name="context">A context that can be used to create other specimens.</param>
@@ -17,7 +42,15 @@ namespace Ploeh.AutoFixture.AutoNSubstitute
         /// </remarks>
         public object Create(object request, ISpecimenContext context)
         {
-            throw new NotImplementedException();
+            var type = request as Type;
+            if (type == null || (!type.IsInterface && !type.IsAbstract))
+                return new NoSpecimen(request);
+
+            var substitute = Builder.Create(request, context);
+            if (substitute == null)
+                return new NoSpecimen(request);
+
+            return substitute;
         }
     }
 }
---
+++ Src/AutoNSubstitute/NSubstituteMethodQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.AutoNSubstitute
+{
+    public class NSubstituteMethodQuery : IMethodQuery
+    {
+        /// <summary>Selects the methods for the supplied type.</summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Methods for <paramref name="type"/>.</returns>
+        public IEnumerable<IMethod> SelectMethods(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
