2db14f33252098ea1efbe1c525cb09dae99902e3
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-12-16 22:03:03 +0100

    Added unit tests for NSubstituteMethodQuery and provided implementation for that class.

--- Src/AutoNSubstitute/NSubstituteMethodQuery.cs
+++ Src/AutoNSubstitute/NSubstituteMethodQuery.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.AutoNSubstitute
 {
+    /// <summary>Selects appropriate methods to create substitutes.</summary>
     public class NSubstituteMethodQuery : IMethodQuery
     {
         /// <summary>Selects the methods for the supplied type.</summary>
@@ -11,7 +13,13 @@ namespace Ploeh.AutoFixture.AutoNSubstitute
         /// <returns>Methods for <paramref name="type"/>.</returns>
         public IEnumerable<IMethod> SelectMethods(Type type)
         {
-            throw new NotImplementedException();
+            if (type.IsInterface)
+                return new[] { type.GetSubstituteFactoryMethod() };
+
+            return from ci in type.GetPublicAndProtectedConstructors()
+                   let parameters = ci.GetParameters()
+                   orderby parameters.Length ascending
+                   select type.GetSubstituteFactoryMethod(parameters);
         }
     }
 }
