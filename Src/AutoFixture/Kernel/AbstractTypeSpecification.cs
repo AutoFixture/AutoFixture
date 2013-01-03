f04ba0d0e818c5facffce91474323f14e482b848
Daniel Hilgarth <d.hilgarth@fire-development.com>
2013-01-03 17:38:19 +0100

    Added new request specification for abstract types

---
+++ Src/AutoFixture/Kernel/AbstractTypeSpecification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// A specification that evaluates whether a request is a request for an abstract type such as an abstract base class or an interface.
+    /// </summary>
+    public class AbstractTypeSpecification : IRequestSpecification
+    {
+        /// <summary>
+        /// Evaluates a request for a specimen.
+        /// </summary>
+        /// <param name="request">The specimen request.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="request"/> is a <see cref="Type"/> that represents an abstract base class or an interface;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsSatisfiedBy(object request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            var type = request as Type;
+            return type != null && type.IsAbstract;
+        }
+    }
+}
