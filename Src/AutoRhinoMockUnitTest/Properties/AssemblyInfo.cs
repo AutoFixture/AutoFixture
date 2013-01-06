c5847a7b29e4bc3f9f0d6a7c5ae644d340fd36ac
Mark Seemann <mark@seemann.ms>
2013-01-06 23:12:44 +0100

    Incremented minor version to indicate new features
    The addition of AutoNSubstitute is a rather major new feature, but since
    fortunately it isn't a breaking change, it doesn't warrant more than a
    minor version increase, according to Semantic Versioning.

--- Src/AutoRhinoMockUnitTest/Properties/AssemblyInfo.cs
+++ Src/AutoRhinoMockUnitTest/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.15.6.0")]
-[assembly: AssemblyFileVersion("2.15.6.0")]
+[assembly: AssemblyVersion("2.16.0.0")]
+[assembly: AssemblyFileVersion("2.16.0.0")]
