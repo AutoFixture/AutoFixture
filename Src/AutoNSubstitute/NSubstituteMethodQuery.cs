7b6d736fdab5a1c5b21cdcb8c80e4de6e0c05fec
Mark Seemann <mark@seemann.ms>
2013-01-06 22:53:03 +0100

    Removed the need for UnknownParametersCountMethod.

--- Src/AutoNSubstitute/NSubstituteMethodQuery.cs
+++ Src/AutoNSubstitute/NSubstituteMethodQuery.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using NSubstitute;
 using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture.AutoNSubstitute
@@ -17,12 +19,52 @@ namespace Ploeh.AutoFixture.AutoNSubstitute
                 throw new ArgumentNullException("type");
 
             if (type.IsInterface)
-                return new[] { type.GetSubstituteFactoryMethod() };
+                return new[] { SubstituteMethod.Create(type) };
 
             return from ci in type.GetPublicAndProtectedConstructors()
                    let parameters = ci.GetParameters()
                    orderby parameters.Length ascending
-                   select type.GetSubstituteFactoryMethod(parameters);
+                   select SubstituteMethod.Create(type, parameters);
         }
+
+        private static class SubstituteMethod
+        {
+            public static IMethod Create(Type type)
+            {
+                return SubstituteMethod.Create(type, new ParameterInfo[0]);
+            }
+
+            public static IMethod Create(
+                Type type,
+                IEnumerable<ParameterInfo> parameterInfos)
+            {
+                var constructedType =
+                    typeof(SubstituteMethod<>).MakeGenericType(type);
+                return (IMethod)Activator.CreateInstance(
+                    constructedType,
+                    parameterInfos);
+            }
+        }
+
+        private class SubstituteMethod<T> : IMethod where T : class
+        {
+            private readonly IEnumerable<ParameterInfo> parameterInfos;
+
+            public SubstituteMethod(IEnumerable<ParameterInfo> parameterInfos)
+            {
+                this.parameterInfos = parameterInfos;
+            }
+
+            public IEnumerable<ParameterInfo> Parameters
+            {
+                get { return this.parameterInfos; }
+            }
+
+            public object Invoke(IEnumerable<object> parameters)
+            {
+                return Substitute.For<T>(parameters.ToArray());
+            }
+        }
+
     }
 }
--- Src/AutoNSubstitute/NSubstituteType.cs
+++ Src/AutoNSubstitute/NSubstituteType.cs
@@ -9,34 +9,9 @@ namespace Ploeh.AutoFixture.AutoNSubstitute
 {
     internal static class NSubstituteType
     {
-        internal static IMethod GetSubstituteFactoryMethod(this Type type, params ParameterInfo[] paramInfos)
-        {
-            var builder = typeof(SubstituteBuilder<>).MakeGenericType(type);
-            var bindings = BindingFlags.Static | BindingFlags.NonPublic;
-
-            if (paramInfos.Any())
-                return new UnknownParametersCountMethod(new StaticMethod(builder.GetMethod("BuildWithArgumentsForConstructor", bindings)), paramInfos);
-
-            return new StaticMethod(builder.GetMethod("Build", bindings));
-        }
-
         internal static IEnumerable<ConstructorInfo> GetPublicAndProtectedConstructors(this Type type)
         {
             return type.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        }
-
-        private static class SubstituteBuilder<T>
-            where T : class
-        {
-            internal static T Build()
-            {
-                return Substitute.For<T>();
-            }
-
-            internal static T BuildWithArgumentsForConstructor(object[] argumentsForConstructor)
-            {
-                return Substitute.For<T>(argumentsForConstructor);
-            }
         }
     }
 }
