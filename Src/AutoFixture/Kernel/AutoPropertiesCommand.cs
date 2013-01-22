389e5921b7756f6c87dc30a71f0cdfdbcfbe5633
Mark Seemann <mark@seemann.ms>
2013-01-22 14:05:03 +0100

    Documented AutoPropertiesCommand.Execute.

--- Src/AutoFixture/Kernel/AutoPropertiesCommand.cs
+++ Src/AutoFixture/Kernel/AutoPropertiesCommand.cs
@@ -214,6 +214,16 @@ namespace Ploeh.AutoFixture.Kernel
                    select pi;
         }
 
+        /// <summary>
+        /// Assigns anonymous values to properties and fields on a specimen
+        /// </summary>
+        /// <param name="specimen">
+        /// The specimen on which property and field values will be assigned.
+        /// </param>
+        /// <param name="context">
+        /// An <see cref="ISpecimenContext"/> that is used to create property 
+        /// and field values.
+        /// </param>
         public void Execute(object specimen, ISpecimenContext context)
         {
             if (specimen == null)
