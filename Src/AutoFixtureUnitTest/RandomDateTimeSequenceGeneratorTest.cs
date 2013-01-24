acefdcef901c97324a84a03c73f6347a28f63fd2
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-01-24 11:27:28 +0100

    Adds a specimen builder that generates truly random dates.

---
+++ Src/AutoFixtureUnitTest/RandomDateTimeSequenceGeneratorTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class RandomDateTimeSequenceGeneratorTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullRequestReturnsNoSpecimen()
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = sut.Create(null, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNullContextDoesNotThrow()
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system and verify outcome
+            Assert.DoesNotThrow(
+                () => sut.Create(typeof(DateTime), null));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(default(int))]
+        [InlineData(default(bool))]
+        public void CreateWithNonTypeRequestReturnsNoSpecimen(object request)
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = sut.Create(request, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(request), result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(bool))]
+        public void CreateWithNonDateTimeTypeRequestReturnsNoSpecimen(Type request)
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = sut.Create(request, dummyContainer);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(request), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithDateTimeRequestReturnsDateTimeValue()
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = sut.Create(typeof(DateTime), dummyContainer);
+            // Verify outcome
+            Assert.IsAssignableFrom<DateTime>(result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithDateTimeRequestDoesNotReturnTodaysDate()
+        {
+            // Fixture setup
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = (DateTime)sut.Create(typeof(DateTime), dummyContainer);
+            // Verify outcome
+            Assert.NotEqual(DateTime.Today, result.Date);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithMultipleDateTimeRequestsReturnsDifferentDates()
+        {
+            // Fixture setup
+            const int requestCount = 10;
+            var times = Enumerable.Range(1, requestCount);
+            var sut = new RandomDateTimeSequenceGenerator();
+            // Exercise system
+            var dummyContainer = new DelegatingSpecimenContext();
+            var results = times
+                .Select(t => sut.Create(typeof(DateTime), dummyContainer))
+                .Cast<DateTime>();
+            // Verify outcome
+            Assert.Equal(requestCount, results.Distinct().Count());
+            // Teardown
+        }
+    }
+}
