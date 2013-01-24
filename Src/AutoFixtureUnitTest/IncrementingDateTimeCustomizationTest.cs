f335da55cff85a9c095acf74fe060d5bf91ad2f3
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-01-24 22:39:38 +0100

    Adds a customization that generates incrementing dates. This customization packages the generation strategy for DateTime specimens used by default in AutoFixture prior to version 3.0.

---
+++ Src/AutoFixtureUnitTest/IncrementingDateTimeCustomizationTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class IncrementingDateTimeCustomizationTest
+    {
+        [Fact]
+        public void SutIsCustomization()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new IncrementingDateTimeCustomization();
+            // Verify outcome
+            Assert.IsAssignableFrom<ICustomization>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeWithNullThrowsArgumentNullException()
+        {
+            // Fixture setup
+            var sut = new IncrementingDateTimeCustomization();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => sut.Customize(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeAddsIncrementingDateTimeGeneratorToTheFixture()
+        {
+            // Fixture setup
+            var fixture = new Fixture();
+            var sut = new IncrementingDateTimeCustomization();
+            // Exercise system
+            sut.Customize(fixture);
+            // Verify outcome
+            var containsIncrementingDateTimeGenerator = fixture
+                .Customizations
+                .OfType<StrictlyMonotonicallyIncreasingDateTimeGenerator>()
+                .Any();
+            Assert.True(containsIncrementingDateTimeGenerator);
+            // Teardown
+        }
+    }
+}
