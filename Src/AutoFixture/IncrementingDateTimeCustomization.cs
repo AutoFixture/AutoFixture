b5ce5ff775c0b825cf796c5f4e5608c0c0890fd9
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-01-24 23:19:57 +0100

    Adds XML docs to the random date generation types.

--- Src/AutoFixture/IncrementingDateTimeCustomization.cs
+++ Src/AutoFixture/IncrementingDateTimeCustomization.cs
@@ -2,8 +2,25 @@
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// A customization that enables <see cref="DateTime"/> specimens
+    /// to be based on an incrementing sequence of days starting from <see cref="DateTime.Now"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This customization reproduces the generation strategy for <see cref="DateTime"/> specimens
+    /// used in AutoFixture up to version 3.0.
+    /// </para>
+    /// </remarks>
     public class IncrementingDateTimeCustomization : ICustomization
     {
+        /// <summary>
+        /// Customizes the specified fixture.
+        /// </summary>
+        /// <param name="fixture">The fixture to customize.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="fixture"/> is <see langword="null"/>.
+        /// </exception>
         public void Customize(IFixture fixture)
         {
             if (fixture == null)
