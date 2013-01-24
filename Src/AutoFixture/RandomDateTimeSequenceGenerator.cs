ef428540f6c361bf41b0579682b63908cdbff66f
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-01-24 23:19:57 +0100

    Adds XML docs to the random date generation types.

--- Src/AutoFixture/RandomDateTimeSequenceGenerator.cs
+++ Src/AutoFixture/RandomDateTimeSequenceGenerator.cs
@@ -3,17 +3,37 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// Creates random <see cref="DateTime"/> specimens.
+    /// </summary>
+    /// <remarks>
+    /// The generated <see cref="DateTime"/> values will be between
+    /// <see cref="DateTime.MinValue"/> and <see cref="DateTime.MaxValue"/>
+    /// unless a different range has been specified in the constructor.
+    /// </remarks>
     public class RandomDateTimeSequenceGenerator : ISpecimenBuilder
     {
         private readonly DateTime minDate;
         private readonly DateTime maxDate;
         private readonly Random randomizer;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomDateTimeSequenceGenerator"/> class.
+        /// </summary>
         public RandomDateTimeSequenceGenerator()
             : this(DateTime.MinValue, DateTime.MaxValue)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomDateTimeSequenceGenerator"/> class
+        /// for a specific range of dates.
+        /// </summary>
+        /// <param name="minDate">The lower bound of the date range.</param>
+        /// <param name="maxDate">The uppder bound of the date range.</param>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="minDate"/> is greater than <paramref name="maxDate"/>.
+        /// </exception>
         public RandomDateTimeSequenceGenerator(DateTime minDate, DateTime maxDate)
         {
             if (minDate > maxDate)
@@ -26,6 +46,15 @@ namespace Ploeh.AutoFixture
             this.randomizer = new Random();
         }
 
+        /// <summary>
+        /// Creates a new <see cref="DateTime"/> specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">Not used.</param>
+        /// <returns>
+        /// A new <see cref="DateTime"/> specimen, if <paramref name="request"/> is a request for a
+        /// <see cref="DateTime"/> value; otherwise, a <see cref="NoSpecimen"/> instance.
+        /// </returns>
         public object Create(object request, ISpecimenContext context)
         {
             if (IsNotDateTimeRequest(request))
