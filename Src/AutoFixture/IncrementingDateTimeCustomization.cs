2e1780a7358434d6678ceddbd98a0d6d4f999bf1
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-01-24 22:39:38 +0100

    Adds a customization that generates incrementing dates. This customization packages the generation strategy for DateTime specimens used by default in AutoFixture prior to version 3.0.

---
+++ Src/AutoFixture/IncrementingDateTimeCustomization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ploeh.AutoFixture
+{
+    public class IncrementingDateTimeCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            if (fixture == null)
+            {
+                throw new ArgumentNullException("fixture");
+            }
+
+            fixture.Customizations.Add(new StrictlyMonotonicallyIncreasingDateTimeGenerator(DateTime.Now));
+        }
+    }
+}
