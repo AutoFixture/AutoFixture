c79f06e4fa7c1c7072b50ddfeddc776d6b6ac539
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-02-03 11:22:36 +0200

    Added XML documentation to RandomCharSequenceGenerator.

--- Src/AutoFixture/RandomCharSequenceGenerator.cs
+++ Src/AutoFixture/RandomCharSequenceGenerator.cs
@@ -3,16 +3,34 @@ using System;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// Creates a sequence of random printable ASCII characters (Dec 33-126).
+    /// </summary>
     public class RandomCharSequenceGenerator : ISpecimenBuilder
     {
         private readonly RandomNumericSequenceGenerator randomPrintableCharNumbers;
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="RandomCharSequenceGenerator"/> class.
+        /// </summary>
         public RandomCharSequenceGenerator()
         {
             this.randomPrintableCharNumbers =
                 new RandomNumericSequenceGenerator(33, 126);
         }
 
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.
+        /// </param>
+        /// <param name="context">A context that can be used to create other 
+        /// specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a 
+        /// <see cref="NoSpecimen"/> instance.
+        /// </returns>
         public object Create(object request, ISpecimenContext context)
         {
             if (!typeof(char).Equals(request))
