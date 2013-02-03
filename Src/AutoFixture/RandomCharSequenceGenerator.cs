2542d96540e8b71209146f035460a25232a870d9
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-02-03 11:17:05 +0200

    Added RandomCharSequenceGenerator.

---
+++ Src/AutoFixture/RandomCharSequenceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+
+namespace Ploeh.AutoFixture
+{
+    public class RandomCharSequenceGenerator : ISpecimenBuilder
+    {
+        private readonly RandomNumericSequenceGenerator randomPrintableCharNumbers;
+
+        public RandomCharSequenceGenerator()
+        {
+            this.randomPrintableCharNumbers =
+                new RandomNumericSequenceGenerator(33, 126);
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (!typeof(char).Equals(request))
+                return new NoSpecimen(request);
+
+            return Convert.ToChar(
+                this.randomPrintableCharNumbers.Create(typeof(long), context));
+        }
+    }
+}
