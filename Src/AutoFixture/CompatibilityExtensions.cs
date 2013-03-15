e6cd7b877ffff3cbacb7ac4f0824a3add50cc2a2
Jonathan M Robson <jonathan@robsonville.com>
2013-03-15 13:17:02 -0500

    Added extension method to ease migration from AutoFixture v2 to AutoFixture v3
    Extension method to ease the transition from the CreateAnonymous<>()
    methods to the new Create<>() methods

---
+++ Src/AutoFixture/CompatibilityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ploeh.AutoFixture
+{
+    public static class CompatibilityExtensions
+    {
+        /// <summary>
+        /// Creates an anonymous variable of the requested type.
+        /// </summary>
+        /// <typeparam name="T">The type of object to create.</typeparam>
+        /// <param name="fixture">
+        /// The fixture used to resolve the type request.
+        /// </param>
+        /// <returns>
+        /// An anonymous object of type <typeparamref name="T"/>.
+        /// </returns>
+        [Obsolete("For compatibility to AutoFixture version 2. This method will be removed, please move to using Create<T>()")]
+        public static T CreateAnonymous<T>(this IFixture fixture)
+        {
+            return fixture.Create<T>();
+        }
+    }
+}
