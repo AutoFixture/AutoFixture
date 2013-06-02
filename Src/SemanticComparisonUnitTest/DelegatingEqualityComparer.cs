a634988f5f451f76e3b98032ba6ff418debcedea
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-06-03 00:32:12 +0300

    Added DelegatingEqualityCompater class. This class is identical to the DelegatingEqualityCompater in AutoFixture. However, it wasn't available from SemanticComparison.

---
+++ Src/SemanticComparisonUnitTest/DelegatingEqualityComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Ploeh.SemanticComparison.UnitTest
+{
+    internal class DelegatingEqualityComparer : IEqualityComparer
+    {
+        internal DelegatingEqualityComparer()
+        {
+            this.OnEquals = (x, y) => false;
+        }
+
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            return this.OnEquals(x, y);
+        }
+
+        int IEqualityComparer.GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        internal Func<object, object, bool> OnEquals { get; set; }
+    }
+}
