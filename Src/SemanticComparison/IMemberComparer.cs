88486a130dd0667752e7754fa1aceb2d44956248
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-06-03 00:29:10 +0300

    Added IMemberComparer interface.

---
+++ Src/SemanticComparison/IMemberComparer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace Ploeh.SemanticComparison
+{
+    public interface IMemberComparer : IEqualityComparer
+    {
+        bool IsSatisfiedBy(PropertyInfo pi);
+        bool IsSatisfiedBy(FieldInfo fi);
+    }
+}
