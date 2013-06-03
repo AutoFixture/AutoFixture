31b8b85904a7fc32173b1e6aac379ac80496adc0
markwoodhall <mark.woodhall@gmail.com>
2013-06-03 22:13:56 +0100

    #92 Start of NoAutoPropertiesAttribute

---
+++ Src/AutoFixture.xUnit.net.UnitTest/NoAutoPropertiesAttributeTest.cs
@@ -0,0 +1,47 @@
+﻿
+using Xunit;
+using Ploeh.AutoFixture.Xunit;
+using System;
+using Ploeh.TestTypeFoundation;
+using System.Linq;
+
+namespace Ploeh.AutoFixture.Xunit.UnitTest
+{
+    public class NoAutoPropertiesAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new NoAutoPropertiesAttribute();
+            // Verify outcome
+            Assert.IsAssignableFrom<CustomizeAttribute>(sut);
+        }
+
+        [Fact]
+        public void GetCustomizationFromNullParameterThrows()
+        {
+            // Fixture setup
+            var sut = new NoAutoPropertiesAttribute();
+            // Exercise system and verify the outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                sut.GetCustomization(null));
+        }
+
+        [Fact]
+        public void GetCustomizationReturnsTheCorrectResult()
+        {
+            // Fixture setup
+            var sut = new NoAutoPropertiesAttribute();
+            var parameter = typeof(TypeWithOverloadedMembers)
+                .GetMethod("DoSomething", new[] { typeof(object) })
+                .GetParameters()
+                .Single();
+            // Exercise system
+            var result = sut.GetCustomization(parameter);
+            // Verify the outcome
+            Assert.IsAssignableFrom<NoAutoPropertiesCustomization>(result);
+        }
+    }
+}
