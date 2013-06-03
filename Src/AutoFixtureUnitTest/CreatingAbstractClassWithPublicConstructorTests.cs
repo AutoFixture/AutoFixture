d29fed1f9e22c73bd65c4e1b315d933aec40aa77
Mark Seemann <mark@ploeh.dk>
2013-06-03 07:56:28 +0200

    Added a test that demonstrates how to work around
    the issue of an abstract class with a public constructor.

--- Src/AutoFixtureUnitTest/CreatingAbstractClassWithPublicConstructorTests.cs
+++ Src/AutoFixtureUnitTest/CreatingAbstractClassWithPublicConstructorTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
 using Ploeh.TestTypeFoundation;
 using Xunit;
 
@@ -18,6 +19,26 @@ namespace Ploeh.AutoFixtureUnitTest
             // Exercise system
             Assert.Throws<ObjectCreationException>(() =>
                 sut.Create<AbstractClassWithPublicConstructor>());
+        }
+
+        [Fact]
+        public void MapAbstractClassWithPublicConstructorToTestDoubleToWorkAroundException()
+        {
+            // Fixture setup
+            var fixture = new Fixture();
+            fixture.Customizations.Add(
+                new TypeRelay(
+                    typeof(AbstractClassWithPublicConstructor),
+                    typeof(TestDouble)));
+            // Exercise system
+            var actual = fixture.Create<AbstractClassWithPublicConstructor>();
+            // Verify outcome
+            Assert.IsAssignableFrom<TestDouble>(actual);
+            // Teardown
+        }
+
+        private class TestDouble : AbstractClassWithPublicConstructor
+        {
         }
     }
 }
