67827d390ab36b66ef5f4928c62a9c0030e2f719
Mark Seemann <mark@ploeh.dk>
2013-06-18 08:25:29 +0200

    Implemented HandleRecursiveRequest.

--- Src/AutoFixtureUnitTest/Kernel/OmitOnRecursionHandlerTests.cs
+++ Src/AutoFixtureUnitTest/Kernel/OmitOnRecursionHandlerTests.cs
@@ -19,5 +19,20 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
             Assert.IsAssignableFrom<IRecursionHandler>(sut);
             // Teardown
         }
+
+        [Fact]
+        public void HandleRecursiveRequestReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new OmitOnRecursionHandler();
+            // Exercise system
+            var dummyRequest = new object();
+            var dummyRequests = Enumerable.Empty<object>();
+            var actual = sut.HandleRecursiveRequest(dummyRequest, dummyRequests);
+            // Verify outcome
+            var expected = new OmitSpecimen();
+            Assert.Equal(expected, actual);
+            // Teardown
+        }
     }
 }
