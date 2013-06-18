258907e8fa211cc53316dfab25d3e351250e6baf
Mark Seemann <mark@ploeh.dk>
2013-06-18 08:23:15 +0200

    Introduced OmitOnRecursionHandler,
    which will replace OmitOnRecursionGuard.

---
+++ Src/AutoFixtureUnitTest/Kernel/OmitOnRecursionHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class OmitOnRecursionHandlerTests
+    {
+        [Fact]
+        public void SutIsRecursionHandler()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new OmitOnRecursionHandler();
+            // Verify outcome
+            Assert.IsAssignableFrom<IRecursionHandler>(sut);
+            // Teardown
+        }
+    }
+}
