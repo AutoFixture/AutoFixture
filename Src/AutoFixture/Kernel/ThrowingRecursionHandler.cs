b8a8c5e9ad21aeba9479d18d621101326c99396b
Mark Seemann <mark@ploeh.dk>
2013-06-18 09:01:29 +0200

    Implemented HandleRecursiveRequest.

--- Src/AutoFixture/Kernel/ThrowingRecursionHandler.cs
+++ Src/AutoFixture/Kernel/ThrowingRecursionHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -11,7 +12,34 @@ namespace Ploeh.AutoFixture.Kernel
             object request,
             IEnumerable<object> recordedRequests)
         {
-            throw new NotImplementedException();
+            throw new ObjectCreationException(string.Format(
+                CultureInfo.InvariantCulture,
+                "AutoFixture was unable to create an instance of type {0} because the traversed object graph contains a circular reference. Information about the circular path follows below. This is the correct behavior when a Fixture is equipped with a ThrowingRecursionBehavior, which is the default. This ensures that you are being made aware of circular references in your code. Your first reaction should be to redesign your API in order to get rid of all circular references. However, if this is not possible (most likely because parts or all of the API is delivered by a third party), you can replace this default behavior with a different behavior: on the Fixture instance, remove the ThrowingRecursionBehavior from Fixture.Behaviors, and instead add an instance of OmitOnRecursionBehavior.{2}\tPath:{2}{1}",
+                recordedRequests.Cast<object>().First().GetType(),
+                GetFlattenedRequests(request, recordedRequests),
+                Environment.NewLine));
+        }
+
+        private static string GetFlattenedRequests(
+            object request,
+            IEnumerable<object> recordedRequests)
+        {
+            var requestInfos = new StringBuilder();
+            foreach (object r in recordedRequests)
+            {
+                Type type = r.GetType();
+                if (type.Assembly != typeof(RecursionGuard).Assembly)
+                {
+                    requestInfos.Append("\t\t");
+                    requestInfos.Append(r);
+                    requestInfos.AppendLine(" --> ");
+                }
+            }
+
+            requestInfos.Append("\t\t");
+            requestInfos.AppendLine(request.ToString());
+
+            return requestInfos.ToString();
         }
     }
 }
--- Src/AutoFixtureUnitTest/Kernel/ThrowingRecursionHandlerTests.cs
+++ Src/AutoFixtureUnitTest/Kernel/ThrowingRecursionHandlerTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture;
 
 namespace Ploeh.AutoFixtureUnitTest.Kernel
 {
@@ -17,6 +18,19 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
             var sut = new ThrowingRecursionHandler();
             // Verify outcome
             Assert.IsAssignableFrom<IRecursionHandler>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void HandleRecursiveRequestThrows()
+        {
+            // Fixture setup
+            var sut = new ThrowingRecursionHandler();
+            // Exercise system and verify outcome
+            Assert.Throws<ObjectCreationException>(
+                () => sut.HandleRecursiveRequest(
+                    new object(),
+                    new[] { new object() }));
             // Teardown
         }
     }
