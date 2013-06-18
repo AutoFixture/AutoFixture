26b1f5b4b1d1eb6cfca3b8df64ff3f0113ee5ed2
Mark Seemann <mark@ploeh.dk>
2013-06-18 08:17:32 +0200

    Introduced NullRecursionHandler,
    which is intended to replace NullRecursionGuard.

---
+++ Src/AutoFixtureUnitTest/Kernel/NullRecursionHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class NullRecursionHandlerTests
+    {
+        [Fact]
+        public void SutIsRecursionHandler()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new NullRecursionHandler();
+            // Verify outcome
+            Assert.IsAssignableFrom<IRecursionHandler>(sut);
+            // Teardown
+        }
+    }
+}
