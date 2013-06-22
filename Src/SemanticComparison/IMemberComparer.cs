c8afb3f7ae9966efdeabcfa81eb39a2692d5a45e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-06-22 17:09:45 +0300

    Added XML documentation to IMemberComparer interface.

--- Src/SemanticComparison/IMemberComparer.cs
+++ Src/SemanticComparison/IMemberComparer.cs
@@ -3,9 +3,27 @@ using System.Reflection;
 
 namespace Ploeh.SemanticComparison
 {
+    /// <summary>
+    /// Evaluates requests for comparison of a property and field for equality.
+    /// </summary>
     public interface IMemberComparer : IEqualityComparer
     {
-        bool IsSatisfiedBy(PropertyInfo pi);
-        bool IsSatisfiedBy(FieldInfo fi);
+        /// <summary>
+        /// Evaluates a request for comparison of a property.
+        /// </summary>
+        /// <param name="request">The request for comparison of a property.</param>
+        /// <returns><see langword="true"/> if <paramref name="request"/> is
+        /// satisfied by the comparison; otherwise, <see langword="false"/>.
+        /// </returns>
+        bool IsSatisfiedBy(PropertyInfo request);
+
+        /// <summary>
+        /// Evaluates a request for comparison of a field.
+        /// </summary>
+        /// <param name="request">The request for comparison of a field.</param>
+        /// <returns> <see langword="true"/> if <paramref name="request"/> is
+        /// satisfied by the comparison; otherwise, <see langword="false"/>.
+        /// </returns>
+        bool IsSatisfiedBy(FieldInfo request);
     }
 }
