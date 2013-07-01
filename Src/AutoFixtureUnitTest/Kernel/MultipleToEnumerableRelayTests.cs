0af426bd4e2d3133d99ac3fe4223fda7f3f3f571
Mark Seemann <mark@ploeh.dk>
2013-07-01 12:21:13 +0200

    Introduced MultipleToEnumerableRelay,
    which will provide an alternative handling of MultipleRequest, relaying
    that request to a request for a corresponding IEnumerable.

---
+++ Src/AutoFixtureUnitTest/Kernel/MultipleToEnumerableRelayTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class MultipleToEnumerableRelayTests
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            var sut = new MultipleToEnumerableRelay();
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+    }
+}
