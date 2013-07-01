9a0fa3b9df262f8093fe0f7b0d149a56e9f3be13
Mark Seemann <mark@ploeh.dk>
2013-07-01 13:30:06 +0200

    Introduced MapCreateManyToEnumerable
    as a customization that will apply MultipleToEnumerableRelay to a Fixture.

---
+++ Src/AutoFixture/MapCreateManyToEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture
+{
+    public class MapCreateManyToEnumerable : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/MapCreateManyToEnumerableTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class MapCreateManyToEnumerableTests
+    {
+        [Fact]
+        public void SutIsCustomization()
+        {
+            var sut = new MapCreateManyToEnumerable();
+            Assert.IsAssignableFrom<ICustomization>(sut);
+        }
+    }
+}
