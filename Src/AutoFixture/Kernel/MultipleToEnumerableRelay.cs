76400a7cc17e69e9e4fda19d97a0308b6bc5c585
Mark Seemann <mark@ploeh.dk>
2013-07-01 15:18:12 +0200

    Added XML documentation.

--- Src/AutoFixture/Kernel/MultipleToEnumerableRelay.cs
+++ Src/AutoFixture/Kernel/MultipleToEnumerableRelay.cs
@@ -5,8 +5,44 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+    /// <summary>
+    /// Relays a <see cref="MultipleRequest" /> to a request for
+    /// <see cref="IEnumerable{T}" />.
+    /// </summary>
+    /// <seealso cref="MultipleToEnumerableRelay.Create(object, ISpecimenContext)" />
+    /// <seealso cref="MapCreateManyToEnumerable" />
     public class MultipleToEnumerableRelay : ISpecimenBuilder
     {
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">
+        /// The request that describes what to create.
+        /// </param>
+        /// <param name="context">
+        /// A context that can be used to create other specimens.
+        /// </param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a
+        /// <see cref="NoSpecimen" /> instance.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// context is null
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="request" /> can be any object, but will often
+        /// be a <see cref="Type" /> or other
+        /// <see cref="System.Reflection.MemberInfo" /> instances. This 
+        /// particular implementation only handles
+        /// <see cref="MultipleRequest" /> instances.
+        /// <see cref="MultipleRequest.Request" /> must either be a Type
+        /// instance, or a <see cref="SeededRequest" /> with a Type as its
+        /// <see cref="SeededRequest.Request" />. If the request doesn't
+        /// satisfy these conditions, a <see cref="NoSpecimen" /> instance is
+        /// returned.
+        /// </para>
+        /// </remarks>
         public object Create(object request, ISpecimenContext context)
         {
             if (context == null)
--- Src/AutoFixture/MapCreateManyToEnumerable.cs
+++ Src/AutoFixture/MapCreateManyToEnumerable.cs
@@ -6,8 +6,45 @@ using Ploeh.AutoFixture.Kernel;
 
 namespace Ploeh.AutoFixture
 {
+    /// <summary>
+    /// Maps a call to
+    /// <see cref="SpecimenFactory.CreateMany{T}(ISpecimenBuilder)" /> to a
+    /// call for <see cref="IEnumerable{T}" />.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Normally,
+    /// <see cref="SpecimenFactory.CreateMany{T}(ISpecimenBuilder)" /> returns
+    /// its own implementation of <see cref="IEnumerable{T}" />, even if other
+    /// customizations have changed how IEnumerable&lt;T&gt; are handled. This
+    /// ensures that CreateMany&lt;T&gt; still returns many instances, even
+    /// when IEnumerable&lt;T&gt; have been changed to return an empty
+    /// sequence.
+    /// </para>
+    /// <para>
+    /// If you want to change this default behaviour, you can use this
+    /// customization.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="MultipleToEnumerableRelay" />
     public class MapCreateManyToEnumerable : ICustomization
     {
+        /// <summary>
+        /// Customizes the specified fixture.
+        /// </summary>
+        /// <param name="fixture">The fixture to customize.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// fixture is null
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// Customizes a <see cref="IFixture" /> so that
+        /// <see cref="SpecimenFactory.CreateMany{T}(ISpecimenBuilder)" />
+        /// exhibits the same behaviour as a request to create
+        /// <see cref="IEnumerable{T}" />.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="MapCreateManyToEnumerable" />
         public void Customize(IFixture fixture)
         {
             if (fixture == null)
