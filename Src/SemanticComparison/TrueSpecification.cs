1a649c5951d88489e428a94dc0ecbefd18852c18
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-03 23:39:19 +0300

    Introduced TrueSpecification class.

---
+++ Src/SemanticComparison/TrueSpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ploeh.SemanticComparison
+{
+    public class TrueSpecification<T> : ISpecification<T>
+    {
+        public bool IsSatisfiedBy(T request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
