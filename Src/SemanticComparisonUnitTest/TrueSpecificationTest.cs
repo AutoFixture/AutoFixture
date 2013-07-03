737f240867d728caf77be891c0bab628542b50c5
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-03 23:39:19 +0300

    Introduced TrueSpecification class.

---
+++ Src/SemanticComparisonUnitTest/TrueSpecificationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Ploeh.SemanticComparison.UnitTest
+{
+    public abstract class TrueSpecificationTest<T>
+    {
+        [Fact]
+        public void SutIsSpecification()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new TrueSpecification<T>();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecification<T>>(sut);
+            // Teardown
+        }
+    }
+
+    public class TrueSpecificationTestOfFieldInfo : TrueSpecificationTest<FieldInfo> { }
+    public class TrueSpecificationTestOfMemberInfo : TrueSpecificationTest<MemberInfo> { }
+    public class TrueSpecificationTestOfPropertyInfo : TrueSpecificationTest<PropertyInfo> { }
+}
