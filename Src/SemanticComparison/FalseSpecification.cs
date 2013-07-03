07492226eeb4bf89523510f6fb7417738b1ce1e4
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-03 23:46:34 +0300

    Introduced FalseSpecification class.

---
+++ Src/SemanticComparison/FalseSpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ploeh.SemanticComparison
+{
+    public class FalseSpecification<T> : ISpecification<T>
+    {
+        public bool IsSatisfiedBy(T request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/SemanticComparisonUnitTest/FalseSpecificationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Ploeh.SemanticComparison.UnitTest
+{
+    public abstract class FalseSpecificationTest<T>
+    {
+        [Fact]
+        public void SutIsSpecification()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new FalseSpecification<T>();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecification<T>>(sut);
+            // Teardown
+        }
+    }
+
+    public class FalseSpecificationTestOfFieldInfo : FalseSpecificationTest<FieldInfo> { }
+    public class FalseSpecificationTestOfMemberInfo : FalseSpecificationTest<MemberInfo> { }
+    public class FalseSpecificationTestOfPropertyInfo : FalseSpecificationTest<PropertyInfo> { }
+}
