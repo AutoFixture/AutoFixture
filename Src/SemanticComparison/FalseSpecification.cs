a041de7995447c0adadbde5a0512e51481494290
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-05 15:41:52 +0300

    Added XML documentation to Specification classes.

--- Src/SemanticComparison/FalseSpecification.cs
+++ Src/SemanticComparison/FalseSpecification.cs
@@ -2,8 +2,16 @@
 
 namespace Ploeh.SemanticComparison
 {
+    /// <summary>
+    /// A <see cref="ISpecification&lt;T&gt;"/> that is always <see langword="false"/>.
+    /// </summary>
     public class FalseSpecification<T> : ISpecification<T>
     {
+        /// <summary>
+        /// Determines whether the request is satisfied by the Specification.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns><see langword="false"/></returns>
         public bool IsSatisfiedBy(T request)
         {
             return false;
