987e6f5ec669eee3ff95a155738a583d25fc44f0
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-05 15:41:52 +0300

    Added XML documentation to Specification classes.

--- Src/SemanticComparison/TrueSpecification.cs
+++ Src/SemanticComparison/TrueSpecification.cs
@@ -2,8 +2,16 @@
 
 namespace Ploeh.SemanticComparison
 {
+    /// <summary>
+    /// A <see cref="ISpecification&lt;T&gt;"/> that is always <see langword="true"/>.
+    /// </summary>
     public class TrueSpecification<T> : ISpecification<T>
     {
+        /// <summary>
+        /// Determines whether the request is satisfied by the Specification.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns><see langword="true"/></returns>
         public bool IsSatisfiedBy(T request)
         {
             return true;
