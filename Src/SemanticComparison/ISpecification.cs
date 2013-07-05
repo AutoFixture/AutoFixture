4f8b71e2c32a122a407f9482708229a0fcc1ee19
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-07-05 15:40:58 +0300

    Added XML documentation to ISpecification<T> interface.

--- Src/SemanticComparison/ISpecification.cs
+++ Src/SemanticComparison/ISpecification.cs
@@ -1,7 +1,19 @@
 ﻿namespace Ploeh.SemanticComparison
 {
+    /// <summary>
+    /// A Specification that evaluates requests.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface ISpecification<T>
     {
+        /// <summary>
+        /// Determines whether the request is satisfied by the Specification.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>
+        ///   <c>true</c> if the request is satisfied by the Specification;
+        ///   otherwise, <c>false</c>.
+        /// </returns>
         bool IsSatisfiedBy(T request);
     }
 }
