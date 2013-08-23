1ca3761bbb3dffddf76ff4a284ea04066f86b3fa
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-23 20:24:21 +1000

    Added GlobalAssemblyInfo and added it too all projects

--- Src/AutoNSubstituteUnitTest/Properties/AssemblyInfo.cs
+++ Src/AutoNSubstituteUnitTest/Properties/AssemblyInfo.cs
@@ -1,35 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("AutoNSubstituteUnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ploeh")]
-[assembly: AssemblyProduct("AutoFixture")]
-[assembly: AssemblyCopyright("Copyright © Ploeh 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("AutoNSubstituteUnitTest")]
+[assembly: AssemblyProduct("AutoNSubstituteUnitTest")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("666949d5-59f5-4e91-a49a-5f4c854410df")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.6.0")]
-[assembly: AssemblyFileVersion("3.6.6.0")]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ Src/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ploeh")]
+[assembly: AssemblyCopyright("Copyright © Ploeh 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("3.6.6.0")]
+[assembly: AssemblyFileVersion("3.6.6.0")]
