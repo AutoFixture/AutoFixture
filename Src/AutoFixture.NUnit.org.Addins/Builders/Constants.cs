34ff5fdea861775f8f326c1d3d80ae12f9a10517
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-23 20:54:20 +1000

    Missed Constants

---
+++ Src/AutoFixture.NUnit.org.Addins/Builders/Constants.cs
@@ -0,0 +1,9 @@
+namespace Ploeh.AutoFixture.NUnit.org.Addins.Builders
+{
+    public static class Constants
+    {
+        public const string AutoDataAttribute = "Ploeh.AutoFixture.NUnit.org.AutoDataAttribute";
+
+        public const string DataAttribute = "Ploeh.AutoFixture.NUnit.org.DataAttribute";
+    }
+}
