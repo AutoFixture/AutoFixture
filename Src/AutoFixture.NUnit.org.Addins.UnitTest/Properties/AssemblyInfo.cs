f0264678ad30a5eefde761ad914feabf95140f44
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-23 21:09:42 +1000

    Added AutoFixture.NUnit.org.Addins.UnitTest

---
+++ Src/AutoFixture.NUnit.org.Addins.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoFixture.NUnit.org.Addins.UnitTest")]
+[assembly: AssemblyDescription("AutoFixture.NUnit.org.Addins.UnitTest")]
+[assembly: AssemblyProduct("AutoFixture.NUnit.org.Addins.UnitTest")]
+
+[assembly: Guid("40843926-4268-4b8b-9ffa-9de991802705")]
+
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
