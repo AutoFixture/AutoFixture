34c6a1ce86a01948711d0a87427966f1a546c1b1
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-25 10:20:14 +0300

    Introduced FoqRelay class.

---
+++ Src/AutoFoq/FoqRelay.fs
@@ -0,0 +1,15 @@
+﻿namespace Ploeh.AutoFixture.AutoFoq
+
+open Ploeh.AutoFixture.Kernel
+open System
+
+type FoqRelay(builder: ISpecimenBuilder, specification: IRequestSpecification) =
+    new(builder: ISpecimenBuilder) = 
+        FoqRelay(builder, AbstractTypeSpecification())
+    member this.Builder
+        with get() = builder
+    member this.Specification
+        with get() = specification
+    interface ISpecimenBuilder with
+        member this.Create (request, context) = 
+             raise (new NotImplementedException())
---
+++ Src/AutoFoqUnitTest/FoqRelayTest.fs
@@ -0,0 +1,51 @@
+﻿module Ploeh.AutoFixture.AutoFoq.UnitTest.FoqRelayTest
+ 
+open Foq
+open Ploeh.AutoFixture.Kernel
+open Ploeh.AutoFixture.AutoFoq
+open System
+open Xunit
+open Xunit.Extensions
+ 
+[<Fact>]
+let SutIsSpecimenBuilder() =
+    // Exercise system
+    let dummyBuilder = Mock<ISpecimenBuilder>().Create()
+    let sut = FoqRelay(dummyBuilder)
+    // Verify outcome
+    Assert.IsAssignableFrom<ISpecimenBuilder>(sut)
+    // Teardown
+
+[<Fact>]
+let BuilderIsCorrect() =
+    // Fixture setup
+    let expectedBuilder = Mock<ISpecimenBuilder>().Create()
+    let sut = FoqRelay(expectedBuilder)
+    // Exercise system
+    let result = sut.Builder
+    // Verify outcome
+    Assert.Equal(expectedBuilder, result)
+    // Teardown
+
+[<Fact>]
+let SpecificationIsCorrect() =
+    // Fixture setup
+    let dummyBuilder = Mock<ISpecimenBuilder>().Create()
+    let specification = TrueRequestSpecification()
+    let sut = FoqRelay(dummyBuilder, specification)
+    // Exercise system
+    let result = sut.Specification
+    // Verify outcome
+    Assert.Equal(specification, result :?> TrueRequestSpecification)
+    // Teardown
+
+[<Fact>]
+let InitializedWithDefaultConstructorHasCorrectSpecification() =
+    // Fixture setup
+    let dummyBuilder = Mock<ISpecimenBuilder>().Create()
+    let sut = FoqRelay(dummyBuilder)
+    // Exercise system
+    let result = sut.Specification
+    // Verify outcome
+    Assert.IsType<AbstractTypeSpecification>(result)
+    // Teardown
