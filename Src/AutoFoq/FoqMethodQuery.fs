4f7cfd0b1eb70ede8d257a278fa86de0a21220bb
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-27 00:02:48 +0300

    Introduced FoqMethodQuery class.

---
+++ Src/AutoFoq/FoqMethodQuery.fs
@@ -0,0 +1,9 @@
+﻿namespace Ploeh.AutoFixture.AutoFoq
+
+open Ploeh.AutoFixture.Kernel
+open System
+
+type FoqMethodQuery() =
+    interface IMethodQuery with
+        member this.SelectMethods destination =
+            raise (NotImplementedException())
---
+++ Src/AutoFoqUnitTest/FoqMethodQueryTest.fs
@@ -0,0 +1,15 @@
+﻿module Ploeh.AutoFixture.AutoFoq.UnitTest.FoqMethodQueryTest
+
+open Ploeh.AutoFixture.Kernel
+open Ploeh.AutoFixture.AutoFoq
+open System
+open Xunit
+
+[<Fact>]
+let SutIsMethodQuery() =
+    // Fixture setup
+    // Exercise system
+    let sut = FoqMethodQuery()
+    // Verify outcome
+    Assert.IsAssignableFrom<IMethodQuery>(sut)
+    // Teardown
