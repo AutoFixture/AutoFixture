f5e9caf75ed54e10c63f7999487f0ba89b36080c
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-27 18:05:21 +0300

    Added IMethodQuery protocol support for interfaces in FoqMethodQuery class.

--- Src/AutoFoq/FoqMethodQuery.fs
+++ Src/AutoFoq/FoqMethodQuery.fs
@@ -1,9 +1,36 @@
 ﻿namespace Ploeh.AutoFixture.AutoFoq
 
+open Foq
 open Ploeh.AutoFixture.Kernel
 open System
+open System.Linq
+open System.Reflection
+
+type private FoqMethod<'T when 'T : not struct>(parameterInfos) =
+    interface IMethod with
+        member this.Parameters = parameterInfos
+        member this.Invoke parameters = obj()
+
+[<AbstractClass; Sealed>]
+type private FoqMethod() =
+    static member Create(targetType: Type, parameterInfos: ParameterInfo[]) = 
+        Activator.CreateInstance(
+            typedefof<FoqMethod<_>>
+                .MakeGenericType(targetType), 
+            parameterInfos)
+    static member Create targetType = 
+        FoqMethod.Create(targetType, Array.empty)
 
 type FoqMethodQuery() =
+    member this.SelectMethods targetType = 
+        (this :> IMethodQuery).SelectMethods targetType
     interface IMethodQuery with
-        member this.SelectMethods destination =
-            raise (NotImplementedException())
+        member this.SelectMethods targetType = 
+            match targetType with
+            | null -> raise (ArgumentNullException("targetType"))
+            |  _   -> match targetType.IsInterface with
+                      | true  -> [| FoqMethod.Create(targetType) :?> IMethod |].AsEnumerable()
+                      | false -> Enumerable.Empty<IMethod>()
+
+
+
--- Src/AutoFoqUnitTest/FoqMethodQueryTest.fs
+++ Src/AutoFoqUnitTest/FoqMethodQueryTest.fs
@@ -2,7 +2,9 @@
 
 open Ploeh.AutoFixture.Kernel
 open Ploeh.AutoFixture.AutoFoq
+open Ploeh.TestTypeFoundation
 open System
+open System.Linq
 open Xunit
 
 [<Fact>]
@@ -13,3 +15,32 @@ let SutIsMethodQuery() =
     // Verify outcome
     Assert.IsAssignableFrom<IMethodQuery>(sut)
     // Teardown
+
+[<Fact>]
+let SelectMethodThrowsForNullType() =
+    // Fixture setup
+    let requestType = typeof<IInterface>
+    let sut = FoqMethodQuery()
+    // Exercise system and verify outcome
+    Assert.Throws<ArgumentNullException>(fun () -> 
+        sut.SelectMethods(null)|> ignore)
+
+[<Fact>]
+let SelectMethodReturnsMethodForInterface() =
+    // Fixture setup
+    let requestType = typeof<IInterface>
+    let sut = FoqMethodQuery()
+    // Exercise system
+    let result = sut.SelectMethods(requestType)
+    // Verify outcome
+    Assert.IsAssignableFrom<IMethod[]>(result)
+
+[<Fact>]
+let SelectMethodReturnsMethodWithoutParametersForInterface() =
+    // Fixture setup
+    let requestType = typeof<IInterface>
+    let sut = FoqMethodQuery()
+    // Exercise system
+    let result = sut.SelectMethods(requestType).First().Parameters
+    // Verify outcome
+    Assert.Empty(result)
