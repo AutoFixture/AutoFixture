3dec1f921ec812590b7b1cce74c10d5bf48276ac
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-28 15:49:37 +0300

    Introduced AutoFoqCustomization class.

---
+++ Src/AutoFoq/AutoFoqCustomization.fs
@@ -0,0 +1,10 @@
+﻿namespace Ploeh.AutoFixture.AutoFoq
+
+open Ploeh.AutoFixture
+open Ploeh.AutoFixture.Kernel
+open System
+
+type AutoFoqCustomization() =
+    interface ICustomization with
+        member this.Customize fixture = 
+            raise (NotImplementedException())
---
+++ Src/AutoFoqUnitTest/AutoFoqCustomizationTest.fs
@@ -0,0 +1,14 @@
+﻿module Ploeh.AutoFixture.AutoFoq.UnitTest.AutoFoqCustomizationTest
+
+open Ploeh.AutoFixture
+open Ploeh.AutoFixture.AutoFoq
+open Xunit
+ 
+[<Fact>]
+let SutIsCustomization() =
+    // Fixture setup
+    // Exercise system
+    let sut = AutoFoqCustomization()
+    // Verify outcome
+    Assert.IsAssignableFrom<ICustomization>(sut)
+    // Teardown
