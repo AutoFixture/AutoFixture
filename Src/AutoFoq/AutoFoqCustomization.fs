feb33658a17b90181978b1ed75ca9220eaf8b999
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-28 16:57:37 +0300

    Implemented AutoFoqCustomization class.

--- Src/AutoFoq/AutoFoqCustomization.fs
+++ Src/AutoFoq/AutoFoqCustomization.fs
@@ -4,7 +4,23 @@ open Ploeh.AutoFixture
 open Ploeh.AutoFixture.Kernel
 open System
 
-type AutoFoqCustomization() =
+type AutoFoqCustomization =
+    val Relay: ISpecimenBuilder
+    new(relay: ISpecimenBuilder) = 
+        { 
+            Relay = ((if relay = null 
+                      then raise(ArgumentNullException("relay"))); 
+                      relay)
+        }
+    new() = 
+        AutoFoqCustomization(
+            FoqRelay(
+                MethodInvoker(
+                    FoqMethodQuery())))
+    member this.Customize fixture = 
+        (this :> ICustomization).Customize fixture
     interface ICustomization with
         member this.Customize fixture = 
-            raise (NotImplementedException())
+            match fixture with
+            | null -> raise (ArgumentNullException("fixture"))
+            | _    -> fixture.ResidueCollectors.Add(this.Relay)
