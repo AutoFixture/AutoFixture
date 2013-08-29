1d41c74d78957b85158387a1b093e782814096e9
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-08-29 09:48:21 +0300

    Moved an internal type to a separate file.

---
+++ Src/AutoFoq/FoqMethod.fs
@@ -0,0 +1,22 @@
+﻿namespace Ploeh.AutoFixture.AutoFoq
+
+open Foq
+open Ploeh.AutoFixture.Kernel
+open System
+open System.Linq
+open System.Reflection
+
+type private FoqMethod<'T when 'T : not struct>(parameterInfos) =
+    interface IMethod with
+        member this.Parameters = parameterInfos
+        member this.Invoke parameters = Mock<'T>().Create(parameters.ToArray()) :> obj
+
+[<AbstractClass; Sealed>]
+type private FoqMethod() =
+    static member Create(targetType: Type, parameterInfos: ParameterInfo[]) = 
+        Activator.CreateInstance(
+            typedefof<FoqMethod<_>>
+                .MakeGenericType(targetType), 
+            parameterInfos)
+    static member Create targetType = 
+        FoqMethod.Create(targetType, Array.empty)
