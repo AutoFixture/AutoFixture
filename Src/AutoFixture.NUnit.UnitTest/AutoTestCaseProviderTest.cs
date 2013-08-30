3e7d870c18cc61189eb742e1eef9fc8d53535f7b
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-31 09:19:03 +1000

    Added comments

--- Src/AutoFixture.NUnit.UnitTest/AutoTestCaseProviderTest.cs
+++ Src/AutoFixture.NUnit.UnitTest/AutoTestCaseProviderTest.cs
@@ -16,19 +16,25 @@ namespace Ploeh.AutoFixture.NUnit.UnitTest
         [Fact]
         public void HasTestCasesForAutoTestCaseTestCaseProvider()
         {
+            // Fixture setup
+            // Exercise system
             var sut = new AutoTestCaseProvider();
             var actual = sut.HasTestCasesFor(this.method);
-
+            // Verify outcome
             Assert.True(actual);
+            // Teardown
         }
 
         [Fact]
         public void GetTestCasesForAutoTestCaseTestCaseBuilderReturnsCorrectly()
         {
+            // Fixture setup
+            // Exercise system
             var sut = new AutoTestCaseProvider();
             var actual = sut.GetTestCasesFor(this.method);
-
+            // Verify outcome
             Assert.NotNull(actual);
+            // Teardown
         }
     }
 }
