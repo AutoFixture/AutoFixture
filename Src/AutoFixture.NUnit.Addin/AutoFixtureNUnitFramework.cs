9f5bafebdec23a87c65f9f6d0724afcab84f9eb0
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-30 18:54:38 +1000

    Removed AutoFixture.NUnit.Addin and AutoFixture.NUnit.Addin.UnitTest projects

--- Src/AutoFixture.NUnit.Addin/AutoFixtureNUnitFramework.cs
+++
@@ -1,7 +0,0 @@
-namespace Ploeh.AutoFixture.NUnit.Addin
-{
-    public static class AutoFixtureNUnitFramework
-    {
-        public const string AutoTestCaseAttribute = "Ploeh.AutoFixture.NUnit.AutoTestCaseAttribute";
-    }
-}
