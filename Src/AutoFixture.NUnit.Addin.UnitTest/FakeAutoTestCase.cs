2e71dbbadfc796e92ad2514c345361ed62be1115
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-08-31 01:57:33 +1000

    Revert "Removed AutoFixture.NUnit.Addin and AutoFixture.NUnit.Addin.UnitTest projects"
    This reverts commit 9f5bafebdec23a87c65f9f6d0724afcab84f9eb0.

---
+++ Src/AutoFixture.NUnit.Addin.UnitTest/FakeAutoTestCase.cs
@@ -0,0 +1,10 @@
+namespace Ploeh.AutoFixture.NUnit.Addin.UnitTest
+{
+    public class FakeAutoTestCase
+    {
+        [AutoTestCase]
+        public void DoSomething(int number)
+        {
+        }
+    }
+}
---
+++ Src/AutoFixture.NUnit.Addin/AutoFixtureNUnitFramework.cs
@@ -0,0 +1,7 @@
+namespace Ploeh.AutoFixture.NUnit.Addin
+{
+    public static class AutoFixtureNUnitFramework
+    {
+        public const string AutoTestCaseAttribute = "Ploeh.AutoFixture.NUnit.AutoTestCaseAttribute";
+    }
+}
