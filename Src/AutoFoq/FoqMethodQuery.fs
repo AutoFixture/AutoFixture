4291d31b48e83578908c04c4c8bb67a599c2b1c6
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-09-04 12:58:30 +0300

    Added XML documentation to FoqMethodQuery class.

--- Src/AutoFoq/FoqMethodQuery.fs
+++ Src/AutoFoq/FoqMethodQuery.fs
@@ -5,9 +5,28 @@ open Ploeh.AutoFixture.Kernel
 open System
 open System.Reflection
 
+/// <summary>
+/// Selects appropriate methods to create <see cref="Foq.Mock&lt;T&gt;"/> 
+/// instances.
+/// </summary>
 type FoqMethodQuery() =
+    /// <summary>
+    /// Selects methods for the supplied type.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>
+    /// Constructors for <paramref name="type"/>.
+    /// </returns>
+    /// <remarks>
+    /// <para>
+    /// This method returns a sequence of <see cref="IMethod"/> according to
+    /// the public and protected constructors available on 
+    /// <paramref name="type"/>.
+    /// </para>
+    /// </remarks>
     member this.SelectMethods targetType = 
         (this :> IMethodQuery).SelectMethods targetType
+
     interface IMethodQuery with
         member this.SelectMethods targetType = 
             match targetType with
