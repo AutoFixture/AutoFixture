9a007c8040c2a3c7af9197295f57badeb8fb3ee2
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-09-05 07:50:36 +1000

    Remove GlobalAssemblyInfo.cs and its linked copies

--- Src/AutoFixture.NUnit/Properties/AssemblyInfo.cs
+++ Src/AutoFixture.NUnit/Properties/AssemblyInfo.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("AutoFixture.NUnit")]
-[assembly: AssemblyDescription("AutoFixture.NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ploeh")]
 [assembly: AssemblyProduct("AutoFixture.NUnit")]
+[assembly: AssemblyCopyright("Copyright © Ploeh 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("AutoFixture.NUnit.UnitTest")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3a54aa4b-be9b-4591-bf8c-0a463e91088f")]
 
-[assembly: InternalsVisibleTo("AutoFixture.NUnit.UnitTest")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.6.6.0")]
+[assembly: AssemblyFileVersion("3.6.6.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("3.6.6.0")]
-[assembly: AssemblyFileVersion("3.6.6.0")]
--- Src/GlobalAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ploeh")]
-[assembly: AssemblyCopyright("Copyright © Ploeh 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
