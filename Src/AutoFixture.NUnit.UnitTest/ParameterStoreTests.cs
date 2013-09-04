947194d123da9781d92f94fed3f29a4d5bc4a82a
BrianZell <brianzell256@gmail.com>
2013-09-04 14:09:24 -0500

    Updated ParameterStoreTests.cs for clarity.

--- Src/AutoFixture.NUnit.UnitTest/ParameterStoreTests.cs
+++ Src/AutoFixture.NUnit.UnitTest/ParameterStoreTests.cs
@@ -1,96 +1,84 @@
 ﻿using System.Linq;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace Ploeh.AutoFixture.NUnit.UnitTest.TestCases
 {
-    class ParameterStoreTests
+    /// <summary>
+    /// Note that the typeName constant MUST be different for each test because ParameterStore is static.
+    /// If the typeName is not different in each test, tests will interfere with each other.
+    /// </summary>
+    public class ParameterStoreTests
     {
-        [TestCase("AddOrGetExistingItem_WhenNoItemExists_ReturnsCreatedValue", "ParameterStoreTestsType1")]
-        public void AddOrGetExistingItem_WhenNoItemExists_ReturnsCreatedValue(string methodName, string typeName)
+        private const string MethodName = "TwoStringParameters";
+
+        private readonly ParameterInfo parameterInfo;
+        private readonly ParameterInfo otherParameterInfo;
+
+        public ParameterStoreTests()
+        {
+            this.parameterInfo = typeof(MyTestClass)
+                                    .GetMethod(MethodName)
+                                    .GetParameters()
+                                    .First();
+
+            this.otherParameterInfo = typeof(MyTestClass)
+                                        .GetMethod(MethodName)
+                                        .GetParameters()
+                                        .Last();
+        }
+
+
+        [Test]
+        public void AddOrGetExistingItem_WhenNoItemExists_ReturnsCreatedValue()
         {
             // Fixture setup
-            var parameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().First();
+            const string typeName = "ParameterStoreTestsType1";
             // Exercise system
-            var result = ParameterStore.GetOrAdd(typeName, parameterInfo, i => 1);
+            var result = ParameterStore.GetOrAdd(typeName, this.parameterInfo, i => 1);
             // Verify outcome
             Assert.That(result,Is.EqualTo(1));
             // Teardown
         }
 
-        [TestCase( "AddOrGetExistingItem_WhenExactMatchAlreadyCalled_ReturnsOldValue", "ParameterStoreTestsType2")]
-        public void AddOrGetExistingItem_WhenExactMatchAlreadyCalled_ReturnsOldValue(
-            string methodName, 
-            string typeName)
+        [Test]
+        public void AddOrGetExistingItem_WhenExactMatchAlreadyCalled_ReturnsOldValue()
         {
             // Fixture setup
-            var parameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().First();
+            const string typeName = "ParameterStoreTestsType2";
             // Exercise system
-            ParameterStore.GetOrAdd(typeName, parameterInfo, i => 1);
-            var result = ParameterStore.GetOrAdd(typeName, parameterInfo, i => 2);
+            ParameterStore.GetOrAdd(typeName, this.parameterInfo, i => 1);
+            var result = ParameterStore.GetOrAdd(typeName, this.parameterInfo, i => 2);
             // Verify outcome
             Assert.That(result, Is.EqualTo(1));
             // Teardown
         }
 
-        [TestCase("AddOrGetExistingItem_WhenDifferentParamAlreadyCalled_ReturnsNewValue", 
-            "ParameterStoreTestsType3", 
-            0)]
-        public void AddOrGetExistingItem_WhenDifferentParamAlreadyCalled_ReturnsNewValue(
-            string methodName, 
-            string typeName, 
-            int otherParam)
+        [Test]
+        public void AddOrGetExistingItem_WhenDifferentParamAlreadyCalled_ReturnsNewValue()
         {
             // Fixture setup
-            var firstParameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().First();
-            var lastParameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().Last();
+            const string typeName = "ParameterStoreTestsType3";
             // Exercise system
-            ParameterStore.GetOrAdd(typeName, firstParameterInfo, i => 1);
-            var result = ParameterStore.GetOrAdd(typeName, lastParameterInfo, i => 2);
+            ParameterStore.GetOrAdd(typeName, this.otherParameterInfo, i => 1);
+            var result = ParameterStore.GetOrAdd(typeName, this.parameterInfo, i => 2);
             // Verify outcome
             Assert.That(result, Is.EqualTo(2));
             // Teardown
         }
 
-        [TestCase("AddOrGetExistingItem_WhenDifferentTypeNameAlreadyCalled_ReturnsNewValue", 
-            "ParameterStoreTestsType4", 
-            "foo")]
-        public void AddOrGetExistingItem_WhenDifferentTypeNameAlreadyCalled_ReturnsNewValue(
-            string methodName, 
-            string typeName, 
-            string otherTypeName)
+        [Test]
+        public void AddOrGetExistingItem_WhenDifferentTypeNameAlreadyCalled_ReturnsNewValue()
         {
             // Fixture setup
-            var firstParameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().First();
-            var lastParameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().Last();
+            const string typeName = "ParameterStoreTestsType4";
+            const string OtherTypeName = "OtherType";
             // Exercise system
-            ParameterStore.GetOrAdd(otherTypeName, firstParameterInfo, i => 1);
-            var result = ParameterStore.GetOrAdd(typeName, lastParameterInfo, i => 2);
+            ParameterStore.GetOrAdd(OtherTypeName, this.parameterInfo, i => 1);
+            var result = ParameterStore.GetOrAdd(typeName, this.parameterInfo, i => 2);
             // Verify outcome
             Assert.That(result, Is.EqualTo(2));
             // Teardown
-        }
-
-        [TestCase("AddOrGetExistingItem_WhenDifferentMethodAlreadyCalled_ReturnsNewValue", 
-            "ParameterStoreTestsType5", 
-            "OtherMethod")]
-        public void AddOrGetExistingItem_WhenDifferentMethodAlreadyCalled_ReturnsNewValue(
-            string methodName, 
-            string typeName, 
-            string otherMethodName)
-        {
-            // Fixture setup
-            var firstParameterInfo = typeof(ParameterStoreTests).GetMethod(methodName).GetParameters().First();
-            var lastParameterInfo = typeof(ParameterStoreTests).GetMethod(otherMethodName).GetParameters().First();
-            // Exercise system
-            ParameterStore.GetOrAdd(typeName, firstParameterInfo, i => 1);
-            var result = ParameterStore.GetOrAdd(typeName, lastParameterInfo, i => 2);
-            // Verify outcome
-            Assert.That(result, Is.EqualTo(2));
-            // Teardown
-        }
-
-        public void OtherMethod(string methodName)
-        {
         }
     }
 }
