e830629f87fcda8c02c415d372bc9fef1ef16ac6
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-09-06 20:10:35 +0300

    Addressed a Code Analysis warning.

--- Src/AutoFoq/FoqMethod.fs
+++ Src/AutoFoq/FoqMethod.fs
@@ -12,10 +12,12 @@ type private FoqMethod<'T when 'T : not struct>(parameterInfos) =
 
 [<AbstractClass; Sealed>]
 type private FoqMethod() =
+    [<System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "This method is examined and instantiated with Reflection.")>]
     static member Create(targetType: Type, parameterInfos: ParameterInfo[]) = 
         Activator.CreateInstance(
             typedefof<FoqMethod<_>>
                 .MakeGenericType(targetType), 
             parameterInfos)
+    [<System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "This method is examined and instantiated with Reflection.")>]
     static member Create targetType = 
         FoqMethod.Create(targetType, Array.empty)
