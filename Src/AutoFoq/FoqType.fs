34a4fce1884f784e08d08ced9ed0b493dbcd5f8d
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-09-06 20:10:39 +0300

    Addressed a Code Analysis warning.

--- Src/AutoFoq/FoqType.fs
+++ Src/AutoFoq/FoqType.fs
@@ -4,6 +4,7 @@ open System
 open System.Reflection
 
 type Type with 
+    [<System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "This method is a Type Extension and it's module is declared as internal.")>]
     member this.GetPublicAndProtectedConstructors() = 
         this.GetConstructors(
             BindingFlags.Public ||| 
