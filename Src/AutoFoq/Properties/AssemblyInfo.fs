229f65166d14814105ce3e00159701202016b8a0
Mark Seemann <mark@ploeh.dk>
2013-09-14 17:52:16 +0200

    Incremented minor version,
    in order to indicate that the addition of AutoFixture.AutoFoq is a new
    feature. This resolves #56
    (https://github.com/AutoFixture/AutoFixture/issues/56).

--- Src/AutoFoq/Properties/AssemblyInfo.fs
+++ Src/AutoFoq/Properties/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-namespace Ploeh.AutoFixture.AutoFoq
+﻿namespace Ploeh.AutoFixture.AutoFoq
 
 open System
 open System.Reflection
@@ -13,7 +13,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("Ploeh")>]
 [<assembly: AssemblyProduct("AutoFixture")>]
-[<assembly: AssemblyCopyright("Copyright � Ploeh 2013")>]
+[<assembly: AssemblyCopyright("Copyright © Ploeh 2013")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -35,8 +35,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("3.6.6.0")>]
-[<assembly: AssemblyFileVersion("3.6.6.0")>]
+[<assembly: AssemblyVersion("3.7.0.0")>]
+[<assembly: AssemblyFileVersion("3.7.0.0")>]
 
 [<assembly: CLSCompliant(true)>]
 [<assembly: NeutralResourcesLanguage("en")>]
--- Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
+++ Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-namespace Ploeh.AutoFixture.AutoFoq.UnitTest
+﻿namespace Ploeh.AutoFixture.AutoFoq.UnitTest
 
 open System.Reflection
 open System.Runtime.InteropServices
@@ -33,7 +33,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("3.6.6.0")>]
-[<assembly: AssemblyFileVersion("3.6.6.0")>]
+[<assembly: AssemblyVersion("3.7.0.0")>]
+[<assembly: AssemblyFileVersion("3.7.0.0")>]
 
 ()
