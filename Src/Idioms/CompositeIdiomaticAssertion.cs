9c166420d4358cc7ed0e84afd0fda1bfec05b6a8
Adam Chester <adamchester@gmail.com>
2013-09-15 21:38:02 +0930

    Added CompositeIdiomaticAssertion
    Allows composing many idiomatic assertions together. Also, added a scenario that shows it's usage: VerifyCompositeEqualityBehaviourOnManyTypes

---
+++ Src/Idioms/CompositeIdiomaticAssertion.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Idioms
+{
+    /// <summary>
+    /// Composes an arbitrary number of <see cref="IdiomaticAssertion" /> instances.
+    /// </summary>
+    public class CompositeIdiomaticAssertion : IdiomaticAssertion
+    {
+        private readonly IEnumerable<IIdiomaticAssertion> assertions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeIdiomaticAssertion"/> class with
+        /// the supplied <see cref="IIdiomaticAssertion" /> instances.
+        /// </summary>
+        /// <param name="assertions">The encapsulated assertions.</param>
+        public CompositeIdiomaticAssertion(params IIdiomaticAssertion[] assertions)
+        {
+            this.assertions = assertions;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeIdiomaticAssertion"/> class with
+        /// the supplied <see cref="IIdiomaticAssertion" /> instances.
+        /// </summary>
+        /// <param name="assertions">The encapsulated assertions.</param>
+        public CompositeIdiomaticAssertion(IEnumerable<IIdiomaticAssertion> assertions)
+            : this(assertions.ToArray())
+        {
+        }
+
+        /// <summary>
+        /// Gets the assertions supplied via the constructor.
+        /// </summary>
+        public IEnumerable<IIdiomaticAssertion> Assertions
+        {
+            get { return this.assertions; }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the constructor by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="constructorInfo">The constructor whose behavior must be verified.</param>
+        public override void Verify(ConstructorInfo constructorInfo)
+        {
+ 	        foreach(var assertion in this.assertions)
+            {
+                assertion.Verify(constructorInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the constructor by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="methodInfo">The method whose behavior must be verified.</param>
+        public override void Verify(MethodInfo methodInfo)
+        {
+ 	        foreach(var assertion in this.assertions)
+            {
+                assertion.Verify(methodInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the property by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="propertyInfo">The property whose behavior must be verified.</param>
+        public override void Verify(PropertyInfo propertyInfo)
+        {
+ 	        foreach(var assertion in this.assertions)
+            {
+                assertion.Verify(propertyInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the field by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="fieldInfo">The field whose behavior must be verified.</param>
+        public override void Verify(FieldInfo fieldInfo)
+        {
+ 	        foreach(var assertion in this.assertions)
+            {
+                assertion.Verify(fieldInfo);
+            }
+        }
+    }
+}
---
+++ Src/IdiomsUnitTest/CompositeIdiomaticAssertionTest.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ploeh.AutoFixture.Idioms;
+using Xunit;
+using System.Reflection;
+using System;
+
+namespace Ploeh.AutoFixture.IdiomsUnitTest
+{
+    public class CompositeIdiomaticAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticAssertion()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new CompositeIdiomaticAssertion();
+            // Verify outcome
+            Assert.IsAssignableFrom<IIdiomaticAssertion>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void ConstructedWithArrayIdiomaticAssertionsIsCorrect()
+        {
+            // Fixture setup
+            var assertions = new[]
+            {
+                new DelegatingIdiomaticAssertion(),
+                new DelegatingIdiomaticAssertion(),
+                new DelegatingIdiomaticAssertion()
+            };
+
+            var sut = new CompositeIdiomaticAssertion(assertions);
+            // Exercise system
+            IEnumerable<IIdiomaticAssertion> result = sut.Assertions;
+            // Verify outcome
+            Assert.True(assertions.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void ConstructedWithEnumerableIdiomaticAssertionsIsCorrect()
+        {
+            // Fixture setup
+            var assertions = new[]
+            {
+                new DelegatingIdiomaticAssertion(),
+                new DelegatingIdiomaticAssertion(),
+                new DelegatingIdiomaticAssertion()
+            };
+
+            var sut = new CompositeIdiomaticAssertion(assertions);
+            // Exercise system
+            var result = sut.Assertions;
+            // Verify outcome
+            Assert.True(assertions.SequenceEqual(result));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyConstructorInfoVerifiesAllIdiomaticAssertions()
+        {
+            // Fixture setup
+            var observedConstructors = new List<ConstructorInfo>();
+            var expectations = Enumerable.Repeat(
+                new DelegatingIdiomaticAssertion { OnConstructorInfoVerify = observedConstructors.Add }, 3).ToArray();
+
+            var sut = new CompositeIdiomaticAssertion(expectations);
+            Type typeWithConstructor = typeof(Ploeh.TestTypeFoundation.UnguardedConstructorHost<object>);
+            ConstructorInfo ctor = typeWithConstructor.GetConstructors().First(); 
+            // Exercise system
+            sut.Verify(ctor);
+            // Verify outcome
+            Assert.Equal(expectations.Length, observedConstructors.Count(c => ctor.Equals(c)));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyMethodInfoVerifiesAllIdiomaticAssertions()
+        {
+            // Fixture setup
+            var observedMethods = new List<MethodInfo>();
+            var expectations = Enumerable.Repeat(
+                new DelegatingIdiomaticAssertion { OnMethodInfoVerify = observedMethods.Add }, 3).ToArray();
+
+            var sut = new CompositeIdiomaticAssertion(expectations);
+            Type typeWithMethod = typeof(Ploeh.TestTypeFoundation.TypeWithConcreteParameterMethod);
+            MethodInfo method = typeWithMethod.GetMethods().First();
+            // Exercise system
+            sut.Verify(method);
+            // Verify outcome
+            Assert.Equal(expectations.Length, observedMethods.Count(m => method.Equals(m)));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyPropertyInfoVerifiesAllIdiomaticAssertions()
+        {
+            // Fixture setup
+            var observedProperties = new List<PropertyInfo>();
+            var expectations = Enumerable.Repeat(
+                new DelegatingIdiomaticAssertion { OnPropertyInfoVerify = observedProperties.Add }, 3).ToArray();
+
+            var sut = new CompositeIdiomaticAssertion(expectations);
+            Type typeWithMethod = typeof(Ploeh.TestTypeFoundation.PropertyHolder<object>);
+            PropertyInfo property = typeWithMethod.GetProperties().First();
+            // Exercise system
+            sut.Verify(property);
+            // Verify outcome
+            Assert.Equal(expectations.Length, observedProperties.Count(p => property.Equals(p)));
+            // Teardown
+        }
+
+        [Fact]
+        public void VerifyFieldInfoVerifiesAllIdiomaticAssertions()
+        {
+            // Fixture setup
+            var observedFields = new List<FieldInfo>();
+            var expectations = Enumerable.Repeat(
+                new DelegatingIdiomaticAssertion { OnFieldInfoVerify = observedFields.Add }, 3).ToArray();
+
+            var sut = new CompositeIdiomaticAssertion(expectations);
+            Type typeWithField = typeof(Ploeh.TestTypeFoundation.FieldHolder<object>);
+            FieldInfo field = typeWithField.GetFields().First();
+            // Exercise system
+            sut.Verify(field);
+            // Verify outcome
+            Assert.Equal(expectations.Length, observedFields.Count(f => field.Equals(f)));
+            // Teardown
+        }
+    }
+}
