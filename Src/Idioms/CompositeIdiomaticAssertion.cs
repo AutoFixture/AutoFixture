fd114b05972e29ed7a4aa342a941655427bd11d9
Adam Chester <adamchester@gmail.com>
2013-09-22 16:18:05 +0930

    Made CompositeIdiomaticAssertion implement all IIdiomaticAssertion methods

--- Src/Idioms/CompositeIdiomaticAssertion.cs
+++ Src/Idioms/CompositeIdiomaticAssertion.cs
@@ -7,9 +7,9 @@ using System.Text;
 namespace Ploeh.AutoFixture.Idioms
 {
     /// <summary>
-    /// Composes an arbitrary number of <see cref="IdiomaticAssertion" /> instances.
+    /// Composes an arbitrary number of <see cref="IIdiomaticAssertion" /> instances.
     /// </summary>
-    public class CompositeIdiomaticAssertion : IdiomaticAssertion
+    public class CompositeIdiomaticAssertion : IIdiomaticAssertion
     {
         private readonly IEnumerable<IIdiomaticAssertion> assertions;
 
@@ -46,11 +46,154 @@ namespace Ploeh.AutoFixture.Idioms
         /// all <see cref="Assertions" />.
         /// </summary>
         /// <param name="constructorInfo">The constructor whose behavior must be verified.</param>
-        public override void Verify(ConstructorInfo constructorInfo)
+        public void Verify(ConstructorInfo constructorInfo)
         {
- 	        foreach(var assertion in this.assertions)
+            foreach (var assertion in this.assertions)
             {
                 assertion.Verify(constructorInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the constructors by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="constructorInfos">The constructors whose behavior must be verified.</param>
+        public void Verify(IEnumerable<ConstructorInfo> constructorInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(constructorInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the assemblies by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="assemblies">The assemblies whose behaviour must be verified.</param>
+        public void Verify(params Assembly[] assemblies)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(assemblies);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the assemblies by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="assemblies">The assemblies whose behaviour must be verified.</param>
+        public void Verify(IEnumerable<Assembly> assemblies)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(assemblies);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the assembly by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="assembly">The assembly whose behaviour must be verified.</param>
+        public void Verify(Assembly assembly)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(assembly);
+            }
+        }
+        
+        /// <summary>
+        /// Verifies the behaviour of the types by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="types">The types whose behaviour must be verified.</param>
+        public void Verify(params Type[] types)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(types);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the types by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="types">The types whose behaviour must be verified.</param>
+        public void Verify(IEnumerable<Type> types)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(types);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the type by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="type">The type whose behaviour must be verified.</param>
+        public void Verify(Type type)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(type);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the members by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="memberInfos">The members whose behaviour must be verified.</param>
+        public void Verify(params MemberInfo[] memberInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(memberInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the members by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="memberInfos">The members whose behaviour must be verified.</param>
+        public void Verify(IEnumerable<MemberInfo> memberInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(memberInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behaviour of the member by delegating the implementation to
+        /// all <see cref="Assertions"/>.
+        /// </summary>
+        /// <param name="memberInfo">The member whose behaviour must be verified.</param>
+        public void Verify(MemberInfo memberInfo)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(memberInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the constructors by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="constructorInfos">The constructors whose behavior must be verified.</param>
+        public void Verify(params ConstructorInfo[] constructorInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(constructorInfos);
             }
         }
 
@@ -59,11 +202,37 @@ namespace Ploeh.AutoFixture.Idioms
         /// all <see cref="Assertions" />.
         /// </summary>
         /// <param name="methodInfo">The method whose behavior must be verified.</param>
-        public override void Verify(MethodInfo methodInfo)
+        public void Verify(MethodInfo methodInfo)
         {
- 	        foreach(var assertion in this.assertions)
+            foreach (var assertion in this.assertions)
             {
                 assertion.Verify(methodInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the methods by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="methodInfos">The methods whose behavior must be verified.</param>
+        public void Verify(IEnumerable<MethodInfo> methodInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(methodInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the methods by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="methodInfos">The methods whose behavior must be verified.</param>
+        public void Verify(params MethodInfo[] methodInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(methodInfos);
             }
         }
 
@@ -72,11 +241,37 @@ namespace Ploeh.AutoFixture.Idioms
         /// all <see cref="Assertions" />.
         /// </summary>
         /// <param name="propertyInfo">The property whose behavior must be verified.</param>
-        public override void Verify(PropertyInfo propertyInfo)
+        public void Verify(PropertyInfo propertyInfo)
         {
- 	        foreach(var assertion in this.assertions)
+            foreach (var assertion in this.assertions)
             {
                 assertion.Verify(propertyInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the properties by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="propertyInfos">The properties whose behavior must be verified.</param>
+        public void Verify(IEnumerable<PropertyInfo> propertyInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(propertyInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the properties by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="propertyInfos">The properties whose behavior must be verified.</param>
+        public void Verify(params PropertyInfo[] propertyInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(propertyInfos);
             }
         }
 
@@ -85,11 +280,37 @@ namespace Ploeh.AutoFixture.Idioms
         /// all <see cref="Assertions" />.
         /// </summary>
         /// <param name="fieldInfo">The field whose behavior must be verified.</param>
-        public override void Verify(FieldInfo fieldInfo)
+        public void Verify(FieldInfo fieldInfo)
         {
  	        foreach(var assertion in this.assertions)
             {
                 assertion.Verify(fieldInfo);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the fields by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="fieldInfos">The fields whose behavior must be verified.</param>
+        public void Verify(IEnumerable<FieldInfo> fieldInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(fieldInfos);
+            }
+        }
+
+        /// <summary>
+        /// Verifies the behavior of the fields by delegating the implementation to
+        /// all <see cref="Assertions" />.
+        /// </summary>
+        /// <param name="fieldInfos">The fields whose behavior must be verified.</param>
+        public void Verify(params FieldInfo[] fieldInfos)
+        {
+            foreach (var assertion in this.assertions)
+            {
+                assertion.Verify(fieldInfos);
             }
         }
     }
