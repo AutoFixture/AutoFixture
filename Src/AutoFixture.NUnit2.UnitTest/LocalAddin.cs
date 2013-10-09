f518fd00c5d44158e258ac6d88d229acf00c62dd
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-10-09 21:07:23 +1000

    Added LocalAddin into AutoFixture.NUnit2.UnitTest

---
+++ Src/AutoFixture.NUnit2.UnitTest/LocalAddin.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Core.Extensibility;
+
+namespace Ploeh.AutoFixture.NUnit2.UnitTest
+{
+    [NUnitAddin]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {   
+    }
+}
