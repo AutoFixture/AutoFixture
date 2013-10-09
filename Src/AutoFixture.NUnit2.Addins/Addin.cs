dd28b6e2fd636741bf273bbc090a1ebb000474fd
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-10-09 22:08:50 +1000

    Removed unnecessary Resharper EventListenerWrapper

--- Src/AutoFixture.NUnit2.Addins/Addin.cs
+++ Src/AutoFixture.NUnit2.Addins/Addin.cs
@@ -22,12 +22,6 @@ namespace Ploeh.AutoFixture.NUnit2.Addins
         /// <returns>True if the add-in was installed, otehrwise false</returns>
         public bool Install(IExtensionHost host)
         {
-            var decorators = host.GetExtensionPoint("TestDecorators");
-            if (decorators == null) 
-                return false;
-            
-            decorators.Install(new TestDecorator());
-
             var providers = host.GetExtensionPoint("TestCaseProviders");
             if (providers == null) 
                 return false;
