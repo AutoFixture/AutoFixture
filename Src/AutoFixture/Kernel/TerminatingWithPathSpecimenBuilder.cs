14ce91e26b1794cf89b983522a9478978e3323f9
Adam Chester <adamchester@gmail.com>
2013-10-13 12:28:06 +0930

    throw ObjectCreationException with specimen request path information

---
+++ Src/AutoFixture/Kernel/TerminatingWithPathSpecimenBuilder.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Decorates an <see cref="ISpecimenBuilder"/> with a node which tracks specimen requests, and
+    /// when <see cref="NoSpecimen"/> is detected, throws an <see cref="ObjectCreationException"/>, which
+    /// includes a description of the request path.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix",
+        Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
+    public class TerminatingWithPathSpecimenBuilder : ISpecimenBuilderNode
+    {
+        private readonly TracingBuilder tracer;
+        private readonly Stack<object> path = new Stack<object>();
+
+        /// <summary>
+        /// Creates a new <see cref="TerminatingWithPathSpecimenBuilder"/> using the
+        /// supplied <see cref="TracingBuilder"/> to track specimen requests.
+        /// </summary>
+        /// <param name="tracer"></param>
+        public TerminatingWithPathSpecimenBuilder(TracingBuilder tracer)
+        {
+            if (tracer == null)
+                throw new ArgumentNullException("tracer");
+
+            this.tracer = tracer;
+            this.tracer.SpecimenRequested += OnSpecimenRequested;
+            this.tracer.SpecimenCreated += OnSpecimenCreated;
+        }
+
+        private void OnSpecimenCreated(object sender, SpecimenCreatedEventArgs e)
+        {
+            // Keep the final NoSpecimen in the list, even though we're about to throw
+            if (!(e.Specimen is NoSpecimen))
+                path.Pop();
+        }
+
+        private void OnSpecimenRequested(object sender, RequestTraceEventArgs e)
+        {
+            path.Push(e.Request);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="TracingBuilder"/> decorated by this instance.
+        /// </summary>
+        public TracingBuilder Tracer
+        {
+            get { return this.tracer; }
+        }
+
+        /// <summary>
+        /// Gets the observed specimen requests, in the order they were requested.
+        /// </summary>
+        public IEnumerable<object> SpecimenRequests
+        {
+            get { return this.path.Reverse(); }
+        }
+
+        /// <summary>
+        /// Creates a new specimen based on a request by delegating to its decorated builder.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "AutoFixture", Justification = "Workaround for a bug in CA: https://connect.microsoft.com/VisualStudio/feedback/details/521030/")]
+        public object Create(object request, ISpecimenContext context)
+        {
+            var result = this.tracer.Create(request, context);
+            if (result is NoSpecimen)
+            {
+                throw new ObjectCreationException(string.Format(
+                    CultureInfo.CurrentCulture,
+                    "AutoFixture was unable to create an instance from {0}, most likely because it has no " +
+                        "public constructor, is an abstract or non-public type.{1}{1}Request path:{1}{2}",
+                    request,
+                    Environment.NewLine,
+                    BuildRequestPathText(this.SpecimenRequests)));
+            }
+            return result;
+        }
+
+        private static string BuildRequestPathText(IEnumerable<object> recordedRequests)
+        {
+            var thisAssembly = MethodBase.GetCurrentMethod().DeclaringType.Assembly;
+            return recordedRequests
+                .Where(r => r.GetType().Assembly != thisAssembly)
+                .Select((r, i) => string.Format(CultureInfo.CurrentCulture, "\t{0} {1}", " ".PadLeft(i+1), r))
+                .Aggregate((s1, s2) => s1 + " --> " + Environment.NewLine + s2);
+        }
+
+        internal static ISpecimenBuilder ComposeIfMultiple(IEnumerable<ISpecimenBuilder> builders)
+        {
+            var isSingle = builders.Take(2).Count() == 1;
+            if (isSingle)
+                return builders.Single();
+
+            return new CompositeSpecimenBuilder(builders);
+        }
+
+        /// <summary>Composes the supplied builders.</summary>
+        /// <param name="builders">The builders to compose.</param>
+        /// <returns>
+        /// A new <see cref="ISpecimenBuilderNode" /> instance containing
+        /// <paramref name="builders" /> as child nodes.
+        /// </returns>
+        public virtual ISpecimenBuilderNode Compose(IEnumerable<ISpecimenBuilder> builders)
+        {
+            var builder = ComposeIfMultiple(builders);
+            return new TerminatingWithPathSpecimenBuilder(new TracingBuilder(builder));
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{ISpecimenBuilder}" /> that can be used to
+        /// iterate through the collection.
+        /// </returns>
+        public IEnumerator<ISpecimenBuilder> GetEnumerator()
+        {
+            yield return this.tracer.Builder;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/TerminatingWithPathSpecimenBuilderTest.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class TerminatingWithPathSpecimenBuilderTest
+    {
+        [Fact]
+        public void SutIsISpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new TerminatingWithPathSpecimenBuilder(new DelegatingTracingBuilder());
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutIsISpecimenBuilderNode()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new TerminatingWithPathSpecimenBuilder(new DelegatingTracingBuilder());
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilderNode>(sut);
+            // Teardown
+        }
+        
+        [Fact]
+        public void InitializeWithNullBuilderWillThrow()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new TerminatingWithPathSpecimenBuilder(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void TracerIsCorrect()
+        {
+            // Fixture setup
+            var expected = new DelegatingTracingBuilder();
+            // Exercise system
+            var sut = new TerminatingWithPathSpecimenBuilder(expected);
+            // Verify outcome
+            Assert.Same(expected, sut.Tracer);
+            // Teardown
+        }
+
+        [Fact]
+        public void SutYieldsCorrectSequence()
+        {
+            // Fixture setup
+            var expected = new DelegatingSpecimenBuilder();
+            var tracer = new DelegatingTracingBuilder(expected);
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system
+            // Verify outcome
+            Assert.Equal(expected, sut.Single());
+            Assert.Equal(expected, sut.Cast<object>().Single());
+            // Teardown
+        }
+
+        [Fact]
+        public void ComposeReturnsCorrectResult()
+        {
+            // Fixture setup
+            var tracer = new DelegatingTracingBuilder();
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system
+            var expectedBuilders = new[]
+            {
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder(),
+                new DelegatingSpecimenBuilder()
+            };
+            var actual = sut.Compose(expectedBuilders);
+            // Verify outcome
+            var tw = Assert.IsAssignableFrom<TerminatingWithPathSpecimenBuilder>(actual);
+            var composite = Assert.IsAssignableFrom<CompositeSpecimenBuilder>(tw.Tracer.Builder);
+            Assert.True(expectedBuilders.SequenceEqual(composite));
+            // Teardown
+        }
+
+        [Fact]
+        public void ComposeSingleItemReturnsCorrectResult()
+        {
+            // Fixture setup
+            var tracer = new DelegatingTracingBuilder();
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system
+            var expected = new DelegatingSpecimenBuilder();
+            var actual = sut.Compose(new[] { expected });
+            // Verify outcome
+            var tw = Assert.IsAssignableFrom<TerminatingWithPathSpecimenBuilder>(actual);
+            Assert.Equal(expected, tw.Tracer.Builder);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWillReturnCorrectResult()
+        {
+            // Fixture setup
+            var expectedSpecimen = new object();
+            var stubBuilder = new TracingBuilder(new DelegatingSpecimenBuilder
+            {
+                OnCreate = (r, c) => expectedSpecimen
+            });
+
+            var sut = new TerminatingWithPathSpecimenBuilder(stubBuilder);
+            // Exercise system
+            var dummyRequest = new object();
+            var dummyContainer = new DelegatingSpecimenContext();
+            var result = sut.Create(dummyRequest, dummyContainer);
+            // Verify outcome
+            Assert.Equal(expectedSpecimen, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWillInvokeDecoratedBuilderWithCorrectParameters()
+        {
+            // Fixture setup
+            var expectedRequest = new object();
+            var expectedContainer = new DelegatingSpecimenContext();
+
+            var verified = false;
+            var mockBuilder = new TracingBuilder(new DelegatingSpecimenBuilder
+            {
+                OnCreate = (r, c) => verified = expectedRequest == r && expectedContainer == c
+            });
+
+            var sut = new TerminatingWithPathSpecimenBuilder(mockBuilder);
+            // Exercise system
+            sut.Create(expectedRequest, expectedContainer);
+            // Verify outcome
+            Assert.True(verified, "Mock verified");
+            // Teardown
+        }
+
+        [Fact]
+        public void SpecimenRequestsWillInitiallyBeEmpty()
+        {
+            // Fixture setup
+            var tracer = new DelegatingTracingBuilder();
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system and verify outcome
+            Assert.Empty(sut.SpecimenRequests);
+            // Teardown
+        }
+
+        [Fact]
+        public void SpecimenRequestsRaisedFromTracerAreRecordedCorrectly()
+        {
+            // Fixture setup
+            var tracer = new DelegatingTracingBuilder();
+            var specimens = new[] { new object(), new object(), new object() };
+            var requestEvents = specimens.Select((o, i) => new RequestTraceEventArgs(o, i)).ToList();
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system
+            requestEvents.ForEach(tracer.RaiseSpecimenRequested);
+            // Verify outcome
+            Assert.True(specimens.SequenceEqual(sut.SpecimenRequests));
+            // Teardown
+        }
+
+        [Fact]
+        public void SpecimenRequestsAreEmptyWhenAllThatWereRequestedAreCreated()
+        {
+            // Fixture setup
+            var tracer = new DelegatingTracingBuilder();
+            var specimens = new[] { new object(), new object(), new object() };
+            var requestEvents = specimens.Select(
+                (o, i) => new RequestTraceEventArgs(o, i)).ToList();
+            var createdEvents = specimens.Reverse().Select(
+                (o, i) => new SpecimenCreatedEventArgs(o, null, i)).ToList();
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            // Exercise system
+            requestEvents.ForEach(tracer.RaiseSpecimenRequested);
+            createdEvents.ForEach(tracer.RaiseSpecimenCreated);
+            // Verify outcome
+            Assert.Empty(sut.SpecimenRequests);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateThrowsWhenNoSpecimenIsReturnedFromTheDecoratedGraph()
+        {
+            // Fixture setup
+            var requests = new[] {new object(), new object(), new object()};
+            var tracer = new DelegatingTracingBuilder(new DelegatingSpecimenBuilder
+            {
+                // Returns NoSpecimen only on the last specimen request
+                OnCreate = (r, c) => (r == requests[2]) ? new NoSpecimen() : new object(),
+            });
+
+            var sut = new TerminatingWithPathSpecimenBuilder(tracer);
+            var container = new SpecimenContext(sut);
+            // Exercise system and verify outcome
+            Assert.DoesNotThrow(() => sut.Create(requests[0], container));
+            Assert.DoesNotThrow(() => sut.Create(requests[1], container));
+            Assert.Throws<ObjectCreationException>(() => sut.Create(requests[2], container));
+            // Teardown
+        }
+    }
+}
