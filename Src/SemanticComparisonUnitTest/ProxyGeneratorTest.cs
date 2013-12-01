5e3de68864128b68a4892cd5bb5cc138ea4b8e1b
dlongest <dlongest@gmail.com>
2013-12-01 17:04:29 -0500

    Added two tests related to #174 - proxy generator throws exception when a source property is null.

---
+++ Src/SemanticComparisonUnitTest/ProxyGeneratorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.SemanticComparison.Fluent;
+
+namespace Ploeh.SemanticComparison.UnitTest
+{
+    public class ProxyGeneratorTest
+    {
+        [Fact]
+        public void CreateProxyDoesNotThrowExceptionWhenAllSourcePropertiesAreNotNull()
+        {
+            var obj = new SourceType(string.Empty);
+            Assert.DoesNotThrow(() => obj.AsSource().OfLikeness<DestinationType>().CreateProxy());            
+        }
+
+        [Fact]
+        public void CreateProxyDoesNotThrowExceptionWhenATopLevelSourcePropertyIsNull()      
+        {
+            var obj = new SourceType(null);
+            Assert.DoesNotThrow(() => obj.AsSource().OfLikeness<DestinationType>().CreateProxy());            
+        }
+    }
+       
+    public class SourceType
+    {
+        public SourceType(string aString)
+        {
+            this.TheString = aString;
+        }
+
+        public string TheString { get; set; }        
+    }
+
+    public class DestinationType
+    {
+        public DestinationType(string aString)
+        {
+            this.TheString = aString;
+        }
+
+        public string TheString { get; set; }       
+    }
+}
