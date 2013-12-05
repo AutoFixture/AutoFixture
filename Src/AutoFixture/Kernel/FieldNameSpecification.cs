fd9ce419619a2586d447461a71c0c0136028bf1f
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2013-12-05 18:29:48 +0000

    Adds a request specification for field names.

---
+++ Src/AutoFixture/Kernel/FieldNameSpecification.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class FieldNameSpecification : IRequestSpecification
+    {
+        private readonly string fieldName;
+
+        public FieldNameSpecification(string fieldName)
+        {
+            Require.IsNotNull(fieldName, "fieldName");
+            this.fieldName = fieldName;
+        }
+
+        public string FieldName
+        {
+            get { return fieldName; }
+        }
+
+        public bool IsSatisfiedBy(object request)
+        {
+            Require.IsNotNull(request, "request");
+
+            return IsRequestForField(request) &&
+                   FieldHasSpecifiedName(request);
+        }
+
+        private static bool IsRequestForField(object request)
+        {
+            return request is FieldInfo;
+        }
+
+        private bool FieldHasSpecifiedName(object request)
+        {
+            return ((FieldInfo)request).Name == this.fieldName;
+        }
+    }
+}
