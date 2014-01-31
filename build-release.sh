3e2e5dadd4af29cf53b6d0927121bb8501a967e6
Mark Seemann <mark@ploeh.dk>
2014-01-31 14:18:41 +0100

    Enabled build scripts to run with VS 2013.

--- build-release.sh
+++ build-release.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild
+
+function vsvers()
+{
+	if [ "$VS120COMNTOOLS" ]; then
+		echo " /property:VisualStudioVersion=12.0"
+	else
+		echo ""
+	fi
+}
+
+$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild `vsvers`
 
