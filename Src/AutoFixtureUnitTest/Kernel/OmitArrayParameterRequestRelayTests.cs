a6935fd0acb264237200fb4683c85f825c71767e
Mark Seemann <mark@ploeh.dk>
2014-03-08 16:10:42 +0100

    Returned specimen from context.

--- Src/AutoFixtureUnitTest/Kernel/OmitArrayParameterRequestRelayTests.cs
+++ Src/AutoFixtureUnitTest/Kernel/OmitArrayParameterRequestRelayTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Ploeh.AutoFixture.Kernel;
+using Xunit.Extensions;
+using Ploeh.TestTypeFoundation;
 
 namespace Ploeh.AutoFixtureUnitTest.Kernel
 {
@@ -14,6 +16,42 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
         {
             var sut = new OmitArrayParameterRequestRelay();
             Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+
+        [Theory]
+        [InlineData(typeof(object[]))]
+        [InlineData(typeof(string[]))]
+        [InlineData(typeof(int[]))]
+        [InlineData(typeof(Version[]))]
+        [InlineData(typeof(SingleParameterType<string>[]))]
+        public void CreateWithEnumerableParameterReturnsCorrectResult(
+            Type argumentType)
+        {
+            var parameterInfo =
+                typeof(SingleParameterType<>)
+                    .MakeGenericType(new[] { argumentType })
+                    .GetConstructors()
+                    .First()
+                    .GetParameters()
+                    .First();
+            var expected = new object();
+            var context = new DelegatingSpecimenContext
+            {
+                OnResolve = r =>
+                {
+                    Assert.Equal(
+                        new SeededRequest(
+                            parameterInfo.ParameterType,
+                            parameterInfo.Name),
+                        r);
+                    return expected;
+                }
+            };
+            var sut = new OmitArrayParameterRequestRelay();
+
+            var actual = sut.Create(parameterInfo, context);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
