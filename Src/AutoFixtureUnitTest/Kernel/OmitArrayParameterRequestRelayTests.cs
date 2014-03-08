288a940b2f1c8239116868e891a61eaddb940e7e
Mark Seemann <mark@ploeh.dk>
2014-03-08 16:07:50 +0100

    Introduced OmitArrayParameterRequestRelay,
    which will do the same as OmitEnumerableParameterRequestRelay, just for
    arrays instead of IEnumerable<T>.

---
+++ Src/AutoFixtureUnitTest/Kernel/OmitArrayParameterRequestRelayTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class OmitArrayParameterRequestRelayTests
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            var sut = new OmitArrayParameterRequestRelay();
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+    }
+}
