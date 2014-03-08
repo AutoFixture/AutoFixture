ccdaa51ad113ad1970f382a1b89001d70d6579c9
Mark Seemann <mark@ploeh.dk>
2014-03-08 12:57:03 +0100

    Introduced OmitEnumerableParameterRequestRelay,
    which will handle requests for IEnumerable<T> parameters, and return an
    empty array if the context resolves it as OmitSpecimen.

---
+++ Src/AutoFixtureUnitTest/Kernel/OmitEnumerableParameterRequestRelayTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class OmitEnumerableParameterRequestRelayTests
+    {
+        [Fact]
+        public void SutIsSpecimentBuilder()
+        {
+            var sut = new OmitEnumerableParameterRequestRelay();
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+    }
+}
