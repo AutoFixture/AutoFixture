8574705156c628e8cc8675344adb6a20e1a52f2b
Mark Seemann <mark@ploeh.dk>
2014-03-08 13:08:50 +0100

    Returned result from context.

--- Src/AutoFixtureUnitTest/Kernel/OmitEnumerableParameterRequestRelayTests.cs
+++ Src/AutoFixtureUnitTest/Kernel/OmitEnumerableParameterRequestRelayTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Ploeh.AutoFixture.Kernel;
+using Ploeh.TestTypeFoundation;
 
 namespace Ploeh.AutoFixtureUnitTest.Kernel
 {
@@ -14,6 +15,35 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
         {
             var sut = new OmitEnumerableParameterRequestRelay();
             Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+
+        [Fact]
+        public void CreateWithEnumerableParameterReturnsCorrectResult()
+        {
+            var parameterInfo = 
+                typeof(SingleParameterType<IEnumerable<string>>)
+                    .GetConstructors()
+                    .First()
+                    .GetParameters()
+                    .First();
+            var expected = new object();
+            var context = new DelegatingSpecimenContext
+            {
+                OnResolve = r =>
+                {
+                    Assert.Equal(
+                        new SeededRequest(
+                            parameterInfo.ParameterType,
+                            parameterInfo.Name),
+                        r);
+                    return expected;
+                }
+            };
+            var sut = new OmitEnumerableParameterRequestRelay();
+
+            var actual = sut.Create(parameterInfo, context);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
