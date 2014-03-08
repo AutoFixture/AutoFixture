f903053bc7c5fb51c8bd1a769407ae39f17bfc47
Mark Seemann <mark@ploeh.dk>
2014-03-08 17:05:17 +0100

    Documented OmitArrayParameterRequestRelay.

--- Src/AutoFixture/Kernel/OmitArrayParameterRequestRelay.cs
+++ Src/AutoFixture/Kernel/OmitArrayParameterRequestRelay.cs
@@ -6,8 +6,52 @@ using System.Text;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+    /// <summary>
+    /// Relays requests for array parameters, and returns an empty array if the
+    /// object returned from the context is an <see cref="OmitSpecimen" />
+    /// instance.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The <strong>OmitArrayParameterRequestRelay</strong> class works like
+    /// <see cref="ParameterRequestRelay" />, except that it only handles
+    /// <see cref="ParameterInfo" /> instances where
+    /// <see cref="ParameterInfo.ParameterType" /> is and array. If the value
+    /// returned from the context is an <see cref="OmitSpecimen" /> instance,
+    /// it returns an empty array.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="OmitArrayParameterRequestRelay.Create(object, ISpecimenContext)" />
+    /// <seealso cref="ParameterRequestRelay" />
+    /// <seealso cref="OmitEnumerableParameterRequestRelay" />
+    /// <seealso cref="OmitSpecimen" />
     public class OmitArrayParameterRequestRelay : ISpecimenBuilder
     {
+        /// <summary>
+        /// Creates a specimen based on a requested array parameter.
+        /// </summary>
+        /// <param name="request">
+        /// The request that describes what to create.
+        /// </param>
+        /// <param name="context">
+        /// A context that can be used to create other specimens.
+        /// </param>
+        /// <returns>
+        /// A specimen created from a <see cref="SeededRequest"/> encapsulating
+        /// the parameter type and name of the requested parameter, if
+        /// possible; otherwise, a <see cref="NoSpecimen" />.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// This method only handles <see cref="ParameterInfo" /> instances
+        /// where <see cref="ParameterInfo.ParameterType" /> is an array.  If
+        /// <paramref name="context" /> returns an <see cref="OmitSpecimen" />
+        /// instance, an empty array of the correct type is returned instead.
+        /// </para>
+        /// </remarks>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="context" /> is <see langword="null"/>
+        /// </exception>
         public object Create(object request, ISpecimenContext context)
         {
             if (context == null)
