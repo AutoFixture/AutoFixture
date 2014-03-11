e9d84a2286119a4f66b16103dafbd4fd2de74cc5
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-11 20:42:13 +0200

    Accepted an ISpecimenBuilder argument. This is abstraction is going to be used for object construction.

--- Src/Idioms.FsCheckUnitTest/ReturnValueMustNotBeNullAssertionTest.fs
+++ Src/Idioms.FsCheckUnitTest/ReturnValueMustNotBeNullAssertionTest.fs
@@ -4,10 +4,20 @@ open Ploeh.AutoFixture
 open Ploeh.AutoFixture.Idioms
 open Ploeh.AutoFixture.Idioms.FsCheck
 open Ploeh.AutoFixture.Idioms.FsCheckUnitTest.TestDsl
+open Ploeh.AutoFixture.Kernel
 open Xunit
 
 [<Fact>]
-let SutIsIdiomaticAssertion =
-    let sut = ReturnValueMustNotBeNullAssertion();
+let SutIsIdiomaticAssertion () =
+    let dummyBuilder = Fixture()
+    let sut = ReturnValueMustNotBeNullAssertion(dummyBuilder);
     verify <@ sut |> implements<IdiomaticAssertion> @>
-    
+
+[<Fact>]
+let BuilderIsCorrect () = 
+    let expected = Fixture() :> ISpecimenBuilder
+    let sut = ReturnValueMustNotBeNullAssertion(expected)
+
+    let actual = sut.Builder
+
+    verify <@ expected = actual @>
