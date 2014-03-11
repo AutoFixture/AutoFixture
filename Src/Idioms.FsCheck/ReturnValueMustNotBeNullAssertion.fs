714d811798735427de034b03658f0d08dbbc1777
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-11 20:48:45 +0200

    Added a Guard Clause.

--- Src/Idioms.FsCheck/ReturnValueMustNotBeNullAssertion.fs
+++ Src/Idioms.FsCheck/ReturnValueMustNotBeNullAssertion.fs
@@ -2,7 +2,9 @@
 
 open Ploeh.AutoFixture.Idioms
 open Ploeh.AutoFixture.Kernel
+open System
 
 type ReturnValueMustNotBeNullAssertion (builder : ISpecimenBuilder) = 
     inherit IdiomaticAssertion()
+    do if builder = null then raise (ArgumentNullException("builder"))
     member this.Builder = builder
