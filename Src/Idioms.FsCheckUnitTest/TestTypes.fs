d0bc8a717e1058f2f07d6daceaaa0f94594191f1
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-11 23:21:28 +0200

    Verified that no exception is thrown for members which do not return a value.

---
+++ Src/Idioms.FsCheckUnitTest/TestTypes.fs
@@ -0,0 +1,10 @@
+﻿namespace Ploeh.AutoFixture.Idioms.FsCheckUnitTest.TestTypes
+
+type AClass = 
+    member this.VoidMethod() = ()
+    member this.WriteOnlyProperty with set (value) = value
+
+[<AbstractClass; Sealed>]
+type AStaticClass =
+    static member VoidMethod() = ()
+    static member WriteOnlyProperty with set (value) = value
