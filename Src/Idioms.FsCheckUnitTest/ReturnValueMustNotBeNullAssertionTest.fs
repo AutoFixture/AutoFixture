bee237fe7de5980e37affbe8cb6f3f32fa937636
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-11 23:21:28 +0200

    Verified that no exception is thrown for members which do not return a value.

--- Src/Idioms.FsCheckUnitTest/ReturnValueMustNotBeNullAssertionTest.fs
+++ Src/Idioms.FsCheckUnitTest/ReturnValueMustNotBeNullAssertionTest.fs
@@ -4,11 +4,15 @@ open Ploeh.AutoFixture
 open Ploeh.AutoFixture.Idioms
 open Ploeh.AutoFixture.Idioms.FsCheck
 open Ploeh.AutoFixture.Idioms.FsCheckUnitTest.TestDsl
+open Ploeh.AutoFixture.Idioms.FsCheckUnitTest.TestTypes
 open Ploeh.AutoFixture.Kernel
 open Swensen.Unquote
 open System
 open System.Reflection
 open Xunit
+open Xunit.Extensions
+
+let doesNotThrow = Xunit.Assert.DoesNotThrow
 
 [<Fact>]
 let SutIsIdiomaticAssertion () =
@@ -40,3 +44,15 @@ let VerifyNullMethodThrows () =
     let dummyBuilder = Fixture()
     let sut = ReturnValueMustNotBeNullAssertion(dummyBuilder)
     raises<ArgumentNullException> <@ sut.Verify(null :> MethodInfo) @>
+
+[<Theory; PropertyData("VoidMembers")>]
+let VerifyVoidMembersDoesNotThrow (memberInfo : MemberInfo) =
+    let dummyBuilder = Fixture()
+    let sut = ReturnValueMustNotBeNullAssertion(dummyBuilder)
+    doesNotThrow |> fun _ -> sut.Verify(memberInfo)
+
+let VoidMembers : seq<MemberInfo[]> = seq { 
+    yield [| typeof<AClass>.GetMethod("VoidMethod") |]
+    yield [| typeof<AStaticClass>.GetMethod("VoidMethod") |]
+    yield [| typeof<AClass>.GetProperty("WriteOnlyProperty") |]
+    yield [| typeof<AStaticClass>.GetProperty("WriteOnlyProperty") |] }
