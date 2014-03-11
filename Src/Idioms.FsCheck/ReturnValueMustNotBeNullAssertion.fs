ef54c569bdf0e9128cc214289cb449775e01075a
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-11 20:53:49 +0200

    Added a Guard Clause when verifying null PropertyInfos.

--- Src/Idioms.FsCheck/ReturnValueMustNotBeNullAssertion.fs
+++ Src/Idioms.FsCheck/ReturnValueMustNotBeNullAssertion.fs
@@ -3,8 +3,14 @@
 open Ploeh.AutoFixture.Idioms
 open Ploeh.AutoFixture.Kernel
 open System
+open System.Reflection
 
 type ReturnValueMustNotBeNullAssertion (builder : ISpecimenBuilder) = 
     inherit IdiomaticAssertion()
-    do if builder = null then raise (ArgumentNullException("builder"))
+    
+    do if builder = null then raise <| ArgumentNullException("builder")
+    
     member this.Builder = builder
+    
+    override this.Verify (property : PropertyInfo) =
+        if property = null then raise <| ArgumentNullException("property")
