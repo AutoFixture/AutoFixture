b5192cbfd98fdbcaf9b05ce2ab988e1a760e6d83
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-12 21:15:46 +0200

    Reordered type members of test types.

--- Src/Idioms.FsCheckUnitTest/TestTypes.fs
+++ Src/Idioms.FsCheckUnitTest/TestTypes.fs
@@ -1,16 +1,16 @@
 ﻿namespace Ploeh.AutoFixture.Idioms.FsCheckUnitTest
 
 type AClass () = 
-    member this.VoidMethod () = ()
     member this.WriteOnlyProperty with set (value) = value |> ignore
+    member this.NullReturnValueProperty with get () = null
     member this.ReadOnlyProperty  with get () = obj()
     member this.MethodWithReturnValue () = obj()
-    member this.NullReturnValueProperty  with get () = null    
+    member this.VoidMethod () = ()
 
 [<AbstractClass; Sealed>]
 type AStaticClass () =
-    static member VoidMethod () = ()
     static member WriteOnlyProperty with set (value) = value |> ignore
+    static member NullReturnValueProperty  with get () = null
     static member ReadOnlyProperty with get () = obj()
     static member MethodWithReturnValue () = obj()
-    static member NullReturnValueProperty  with get () = null
+    static member VoidMethod () = ()
