3e93d72597ddd41de834d181d103de9e12886ac1
Haacked <haacked@gmail.com>
2014-03-14 15:37:42 -0700

    Add a GreedyMockConstructorQuery
    This just follows the pattern of GreedyConstructorQuery but
    for Moq.

---
+++ Src/AutoMoqUnitTest/GreedyMockConstructorQueryTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.TestTypeFoundation;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixture.AutoMoq.UnitTest
+{
+    public class GreedyMockConstructorQueryTest
+    {
+        [Fact]
+        public void SutIsMethodQuery()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new GreedyMockConstructorQuery();
+            // Verify outcome
+            Assert.IsAssignableFrom<IMethodQuery>(sut);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(AbstractType))]
+        [InlineData(typeof(IInterface))]
+        [InlineData(typeof(Mock))]
+        [InlineData(typeof(Mock<>))]
+        public void SelectMethodsReturnsCorrectResultForNonMockTypes(Type t)
+        {
+            // Fixture setup
+            var sut = new GreedyMockConstructorQuery();
+            // Exercise system
+            var result = sut.SelectMethods(t);
+            // Verify outcome
+            Assert.Empty(result);
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(Mock<AbstractType>))]
+        [InlineData(typeof(Mock<ConcreteType>))]
+        [InlineData(typeof(Mock<MultiUnorderedConstructorType>))]
+        public void SelectMethodsReturnsCorrectNumberOfConstructorsForTypesWithConstructors(Type t)
+        {
+            // Fixture setup
+            var mockType = t.GetGenericArguments().Single();
+            var expectedCount = mockType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Length;
+
+            var sut = new GreedyMockConstructorQuery();
+            // Exercise system
+            var result = sut.SelectMethods(t);
+            // Verify outcome
+            Assert.Equal(expectedCount, result.Count());
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(Mock<IInterface>))]
+        [InlineData(typeof(Mock<AbstractType>))]
+        [InlineData(typeof(Mock<ConcreteType>))]
+        [InlineData(typeof(Mock<MultiUnorderedConstructorType>))]
+        public void MethodsDefineCorrectParameters(Type t)
+        {
+            // Fixture setup
+            var mockType = t.GetGenericArguments().Single();
+            var mockTypeCtorArgs = from ci in mockType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                                   select ci.GetParameters();
+
+            var sut = new GreedyMockConstructorQuery();
+            // Exercise system
+            var result = sut.SelectMethods(t);
+            // Verify outcome
+            var actualArgs = from ci in result
+                             select ci.Parameters;
+            Assert.True(mockTypeCtorArgs.All(expectedParams =>
+                actualArgs.Any(expectedParams.SequenceEqual)));
+            // Teardown
+        }
+
+        [Theory]
+        [InlineData(typeof(Mock<AbstractType>))]
+        [InlineData(typeof(Mock<ConcreteType>))]
+        [InlineData(typeof(Mock<MultiUnorderedConstructorType>))]
+        public void MethodsAreReturnedInCorrectOrder(Type t)
+        {
+            // Fixture setup
+            var mockType = t.GetGenericArguments().Single();
+            var mockTypeCtorArgCounts = from ci in mockType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                                        let paramCount = ci.GetParameters().Length
+                                        orderby paramCount descending
+                                        select paramCount;
+
+            var sut = new GreedyMockConstructorQuery();
+            // Exercise system
+            var result = sut.SelectMethods(t);
+            // Verify outcome
+            var actualArgCounts = from ci in result
+                                  select ci.Parameters.Count();
+            Assert.True(mockTypeCtorArgCounts.SequenceEqual(actualArgCounts));
+            // Teardown
+        }
+    }
+}
