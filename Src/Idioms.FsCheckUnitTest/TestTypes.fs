841b881afb0f3694b5655bd99abf2e6340f97b04
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-14 11:55:15 +0200

    Separated each method in test-types with a blank line.

--- Src/Idioms.FsCheckUnitTest/TestTypes.fs
+++ Src/Idioms.FsCheckUnitTest/TestTypes.fs
@@ -4,17 +4,22 @@ open System
 
 type AClass () = 
     member this.WriteOnlyProperty with set (value) = value |> ignore
+
     member this.NullReturnValueProperty with get () = null
+
     member this.ReadOnlyProperty  with get () = obj()
 
     member this.NullReturnValueMethod () = null :> obj
+
     member this.NullReturnValueMethodWithParameters
         (s : string, i : int) =
         null :> obj
+
     member this.NullReturnValueMethodWithManyParameters
         (s : string, i : int, d : double, g : Guid) =
         null :> obj
     member this.MethodWithReturnValue () = obj()
+
     member this.VoidMethod () = ()
 
     member this.NullReturnValueMethodWithParametersAndBranching
@@ -31,17 +36,23 @@ type AClass () =
 [<AbstractClass; Sealed>]
 type AStaticClass () =
     static member WriteOnlyProperty with set (value) = value |> ignore
+
     static member NullReturnValueProperty  with get () = null
+
     static member ReadOnlyProperty with get () = obj()
 
     static member NullReturnValueMethod () = null :> obj
+
     static member NullReturnValueMethodWithParameters
         (s : string, i : int) =
         null :> obj
+
     static member NullReturnValueMethodWithManyParameters
         (s : string, i : int, d : double, g : Guid) =
         null :> obj
+
     static member MethodWithReturnValue () = obj()
+
     static member VoidMethod () = ()
 
     static member NullReturnValueMethodWithParametersAndBranching
