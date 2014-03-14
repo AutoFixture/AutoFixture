2224558d372feecd4572b45463bcd89f3d82c108
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-14 11:52:57 +0200

    Constrained long lines to 80 chars.

--- Src/Idioms.FsCheckUnitTest/TestTypes.fs
+++ Src/Idioms.FsCheckUnitTest/TestTypes.fs
@@ -7,15 +7,18 @@ type AClass () =
     member this.NullReturnValueProperty with get () = null
     member this.ReadOnlyProperty  with get () = obj()
 
-    member this.NullReturnValueMethod () = this.NullReturnValueProperty
-    member this.NullReturnValueMethodWithParameters (s : string, i : int) = this.NullReturnValueProperty
-    member this.NullReturnValueMethodWithManyParameters (s : string, i : int, d : double, g : Guid) = null :> obj
+    member this.NullReturnValueMethod () = null :> obj
+    member this.NullReturnValueMethodWithParameters
+        (s : string, i : int) =
+        null :> obj
+    member this.NullReturnValueMethodWithManyParameters
+        (s : string, i : int, d : double, g : Guid) =
+        null :> obj
     member this.MethodWithReturnValue () = obj()
     member this.VoidMethod () = ()
 
     member this.NullReturnValueMethodWithParametersAndBranching
         (s : string, i : int) =
-
         let returnValue =
             match s with
             | "" -> null
@@ -25,22 +28,24 @@ type AClass () =
                     |  _ -> obj()
         returnValue
 
-
 [<AbstractClass; Sealed>]
 type AStaticClass () =
     static member WriteOnlyProperty with set (value) = value |> ignore
     static member NullReturnValueProperty  with get () = null
     static member ReadOnlyProperty with get () = obj()
 
-    static member NullReturnValueMethod () = AStaticClass.NullReturnValueProperty
-    static member NullReturnValueMethodWithParameters (s : string, i : int) = AStaticClass.NullReturnValueProperty
-    static member NullReturnValueMethodWithManyParameters (s : string, i : int, d : double, g : Guid) = null :> obj
+    static member NullReturnValueMethod () = null :> obj
+    static member NullReturnValueMethodWithParameters
+        (s : string, i : int) =
+        null :> obj
+    static member NullReturnValueMethodWithManyParameters
+        (s : string, i : int, d : double, g : Guid) =
+        null :> obj
     static member MethodWithReturnValue () = obj()
     static member VoidMethod () = ()
 
     static member NullReturnValueMethodWithParametersAndBranching
         (s : string, i : int) =
-
         let returnValue =
             match s with
             | "" -> null
