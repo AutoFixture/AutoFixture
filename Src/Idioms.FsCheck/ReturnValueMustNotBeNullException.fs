389bec2da2ab728ca4b69d652bf0f00b9a5cb15f
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-17 11:15:56 +0200

    Added XML documentation to ReturnValueMustNotBeNullException class.

--- Src/Idioms.FsCheck/ReturnValueMustNotBeNullException.fs
+++ Src/Idioms.FsCheck/ReturnValueMustNotBeNullException.fs
@@ -2,4 +2,16 @@
 
 open System
 
-type ReturnValueMustNotBeNullException (message) = inherit Exception (message)
+/// <summary>
+/// Represents an error about a public Query (a method that returns a value)
+/// with null return value.
+/// </summary>
+type ReturnValueMustNotBeNullException (message) =
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="ReturnValueMustNotBeNullException"/> class.
+    /// </summary>
+    /// <param name="message">
+    /// The error message that explains the reason for the exception.
+    /// </param>
+    inherit Exception (message)
