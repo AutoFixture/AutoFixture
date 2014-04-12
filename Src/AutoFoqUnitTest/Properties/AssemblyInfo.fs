35b80f216a5f2ba43cf64546656f11c0850d471d
Mark Seemann <mark@ploeh.dk>
2014-04-12 16:19:17 +0200

    Incremented patch version,
    in order to indicate that better error messages, distinguishing between
    deferred and non-deferred Iterators, fixes a bug. This resolves #277
    (https://github.com/AutoFixture/AutoFixture/issues/277).

--- Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
+++ Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
@@ -33,7 +33,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("3.18.1.0")>]
-[<assembly: AssemblyFileVersion("3.18.1.0")>]
+[<assembly: AssemblyVersion("3.18.2.0")>]
+[<assembly: AssemblyFileVersion("3.18.2.0")>]
 
 ()
