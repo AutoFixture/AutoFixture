b876a6a561d392676a0f95733d2d285ebffc21b0
Mark Seemann <mark@ploeh.dk>
2014-04-24 07:33:55 +0200

    Incremented patch version,
    in order to indicate that supporting Int16 values for the [Range]
    attribute is a bux fix that resolves #280
    (https://github.com/AutoFixture/AutoFixture/issues/280).

--- Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
+++ Src/AutoFoqUnitTest/Properties/AssemblyInfo.fs
@@ -33,7 +33,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("3.18.3.0")>]
-[<assembly: AssemblyFileVersion("3.18.3.0")>]
+[<assembly: AssemblyVersion("3.18.4.0")>]
+[<assembly: AssemblyFileVersion("3.18.4.0")>]
 
 ()
