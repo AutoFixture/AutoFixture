1cc66677a078ea0d85fc9cedef4f2347221e790c
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-25 17:22:53 +0900

    Introduced MultidimensionalArrayRelay.

---
+++ Src/AutoFixture/Kernel/MultidimensionalArrayRelay.cs
@@ -0,0 +1,27 @@
+﻿namespace Ploeh.AutoFixture.Kernel
+{
+    /// <summary>
+    /// Relays a request for a multidimensional array to a jagged array and converts the result
+    /// to the desired multidimensional array type.
+    /// </summary>
+    public class MultidimensionalArrayRelay : ISpecimenBuilder
+    {
+        /// <summary>
+        /// Creates a new multidimensional array based on a request.
+        /// </summary>
+        /// <param name="request">
+        /// The request that describes what to create.
+        /// </param>
+        /// <param name="context">
+        /// A context that can be used to create other specimens.
+        /// </param>
+        /// <returns>
+        /// A multidimensional array of the requested type if possible; otherwise a
+        /// <see cref="NoSpecimen" /> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/MultidimensionalArrayRelayTest.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class MultidimensionalArrayRelayTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            var sut = new MultidimensionalArrayRelay();
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        } 
+    }
+}
