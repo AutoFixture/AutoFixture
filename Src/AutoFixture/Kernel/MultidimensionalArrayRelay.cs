3d0004672012da2a491ed14e578594e70110d36f
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-26 00:37:23 +0900

    Implemented the Create method of MultidimensionalArrayRelay.

--- Src/AutoFixture/Kernel/MultidimensionalArrayRelay.cs
+++ Src/AutoFixture/Kernel/MultidimensionalArrayRelay.cs
@@ -1,4 +1,7 @@
-﻿namespace Ploeh.AutoFixture.Kernel
+﻿using System;
+using System.Linq;
+
+namespace Ploeh.AutoFixture.Kernel
 {
     /// <summary>
     /// Relays a request for a multidimensional array to a jagged array and converts the result
@@ -21,7 +24,75 @@
         /// </returns>
         public object Create(object request, ISpecimenContext context)
         {
-            throw new System.NotImplementedException();
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            var arrayType = request as Type;
+            if (arrayType == null || !IsMultidimensionalArray(arrayType))
+                return new NoSpecimen(request);
+
+            return CreateMultidimensionalArray(arrayType, context);
+        }
+
+        private static bool IsMultidimensionalArray(Type type)
+        {
+            return type.IsArray && type.GetArrayRank() > 1;
+        }
+
+        private static object CreateMultidimensionalArray(Type arrayType, ISpecimenContext context)
+        {
+            Type elementType = arrayType.GetElementType();
+            int dimension = arrayType.GetArrayRank();
+
+            Type jaggedArrayType = MakeJaggedArrayType(elementType, dimension);
+            var jagged = (Array)context.Resolve(jaggedArrayType);
+
+            int length = GetInnerLength(jagged, dimension);
+            int[] lengths = Enumerable.Repeat(length, dimension).ToArray();
+
+            Array array = Array.CreateInstance(elementType, lengths);
+
+            Copy(array, dimension, jagged);
+
+            return array;
+        }
+
+        private static Type MakeJaggedArrayType(Type elementType, int dimension)
+        {
+            Type jaggedArrayType = elementType;
+            for (int i = 0; i < dimension; i++)
+                jaggedArrayType = jaggedArrayType.MakeArrayType();
+            
+            return jaggedArrayType;
+        }
+
+        private static int GetInnerLength(Array jagged, int dimension)
+        {
+            Array current = jagged;
+            for (int i = 0; i < dimension - 1; i++)
+                current = (Array)current.GetValue(0);
+
+            return current.Length;
+        }
+
+        private static void Copy(Array array, int dimension, Array jagged, params int[] indices)
+        {
+            if (dimension > 1)
+                CopyNextDemension(array, dimension, jagged, indices);
+            else
+                CopyLastDimension(array, jagged, indices);
+        }
+
+        private static void CopyNextDemension(Array array, int dimension, Array jagged, int[] indices)
+        {
+            for (int i = 0; i < jagged.Length; i++)
+                Copy(array, dimension - 1, (Array)jagged.GetValue(i), indices.Concat(new[] { i }).ToArray());
+        }
+
+        private static void CopyLastDimension(Array array, Array jagged, int[] indices)
+        {
+            for (int i = 0; i < jagged.Length; i++)
+                array.SetValue(jagged.GetValue(i), indices.Concat(new[] { i }).ToArray());
         }
     }
 }
