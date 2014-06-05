e9327afe1f463fe81bd90aae00d1b15f483b6c00
Mark Seemann <mark@ploeh.dk>
2014-06-05 17:29:46 +0200

    Incremented patch version,
    in order to indicate that support for multi-dimensional arrays is a bug
    fix, which resolves #289
    (https://github.com/AutoFixture/AutoFixture/issues/289).

--- Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
+++ Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.18.9.0")]
-[assembly: AssemblyFileVersion("3.18.9.0")]
+[assembly: AssemblyVersion("3.18.10.0")]
+[assembly: AssemblyFileVersion("3.18.10.0")]
