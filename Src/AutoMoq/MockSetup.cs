8e385415ad34e4662fef5c3c008f9a3c7f8a4edd
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-08-08 21:04:35 +0100

    Added MockSetup - runs a set of mock initializers

---
+++ Src/AutoMoq/MockSetup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.AutoMoq
+{
+    /// <summary>
+    /// Uses a set of mock initializers to setup a <see cref="Mock{T}"/>.
+    /// </summary>
+    public class MockSetup : ISpecimenBuilder
+    {
+        private readonly ISpecimenBuilder _builder;
+        private readonly IEnumerable<IMockInitializer> _initializers;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MockSetup"/>.
+        /// </summary>
+        /// <param name="builder">The builder used to create <see cref="Mock{T}"/> instances.</param>
+        /// <param name="initializers">The initializers used to setup the mock object.</param>
+        public MockSetup(ISpecimenBuilder builder, params IMockInitializer[] initializers)
+            : this(builder, initializers.AsEnumerable())
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MockSetup"/>.
+        /// </summary>
+        /// <param name="builder">The builder used to create <see cref="Mock{T}"/> instances.</param>
+        /// <param name="initializers">The initializers used to setup the mock object.</param>
+        public MockSetup(ISpecimenBuilder builder, IEnumerable<IMockInitializer> initializers)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+            if (initializers == null) throw new ArgumentNullException("initializers");
+
+            _builder = builder;
+            _initializers = initializers;
+        }
+
+        /// <summary>
+        /// Gets the mock initializers that will be used to setup a mock.
+        /// </summary>
+        public IEnumerable<IMockInitializer> Initializers
+        {
+            get { return _initializers; }
+        }
+
+        /// <summary>
+        /// Runs a set of initializers to setup a <see cref="Mock{T}"/> instance.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The specimen created by created by the underlying builder.
+        /// If the specimen is a <see cref="Mock{T}"/>, this instance will setup the mock before returning it.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            var specimen = _builder.Create(request, context);
+            if (specimen is NoSpecimen)
+                return specimen;
+
+            var mock = specimen as Mock;
+
+            foreach (var mockInitializer in _initializers)
+                mockInitializer.Setup(mock, context);
+
+            return mock;
+        }
+    }
+}
---
+++ Src/AutoMoqUnitTest/MockSetupTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixture.AutoMoq.UnitTest
+{
+    public class MockSetupTest
+    {
+        [Fact]
+        public void ConstructorThrowsWhenBuilderIsNull()
+        {
+            // Fixture setup
+            var initializers = Enumerable.Empty<IMockInitializer>();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new MockSetup(null, initializers));
+        }
+
+        [Fact]
+        public void ConstructorThrowsWhenInitializersAreNull()
+        {
+            // Fixture setup
+            var builder = new Mock<ISpecimenBuilder>().Object;
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new MockSetup(builder, null));
+        }
+
+        [Fact]
+        public void TriggersInitializersWhenUnderlyingBuilderHandlesRequest()
+        {
+            // Fixture setup
+            var request = new object();
+            var mock = new Mock<object>();
+            var context = new Mock<ISpecimenContext>();
+            var builder = new Mock<ISpecimenBuilder>();
+            var initializer1 = new Mock<IMockInitializer>();
+            var initializer2 = new Mock<IMockInitializer>();
+
+            builder.Setup(b => b.Create(request, context.Object))
+                   .Returns(mock);
+
+            var sut = new MockSetup(builder.Object, initializer1.Object, initializer2.Object);
+            // Exercise system
+            sut.Create(request, context.Object);
+            // Verify outcome
+            initializer1.Verify(i => i.Setup(mock, context.Object), Times.Once());
+            initializer2.Verify(i => i.Setup(mock, context.Object), Times.Once());
+            // Teardown
+        }
+
+        [Fact]
+        public void DoesNotTriggerInitializersWhenUnderlyingBuilderCantHandleRequest()
+        {
+            // Fixture setup
+            var request = new object();
+            var noSpecimen = new NoSpecimen();
+            var context = new Mock<ISpecimenContext>();
+            var builder = new Mock<ISpecimenBuilder>();
+            var initializer1 = new Mock<IMockInitializer>();
+            var initializer2 = new Mock<IMockInitializer>();
+
+            builder.Setup(b => b.Create(request, context.Object))
+                   .Returns(noSpecimen);
+
+            var sut = new MockSetup(builder.Object, initializer1.Object, initializer2.Object);
+            // Exercise system
+            sut.Create(request, context.Object);
+            // Verify outcome
+            initializer1.Verify(i => i.Setup(It.IsAny<Mock>(), context.Object), Times.Never());
+            initializer2.Verify(i => i.Setup(It.IsAny<Mock>(), context.Object), Times.Never());
+            // Teardown
+        }
+
+        [Fact]
+        public void ReturnsNoSpecimenCreatedByUnderlyingBuilder()
+        {
+            // Fixture setup
+            var request = new object();
+            var noSpecimen = new NoSpecimen();
+            var context = new Mock<ISpecimenContext>();
+            var builder = new Mock<ISpecimenBuilder>();
+            builder.Setup(b => b.Create(request, context.Object))
+                   .Returns(noSpecimen);
+
+            var sut = new MockSetup(builder.Object, new IMockInitializer[] {});
+            // Exercise system
+            var result = sut.Create(request, context.Object);
+            // Verify outcome
+            Assert.Same(noSpecimen, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void ReturnsMockCreatedByUnderlyingBuilder()
+        {
+            // Fixture setup
+            var request = new object();
+            var mock = new Mock<object>();
+            var context = new Mock<ISpecimenContext>();
+            var builder = new Mock<ISpecimenBuilder>();
+            builder.Setup(b => b.Create(request, context.Object))
+                   .Returns(mock);
+
+            var sut = new MockSetup(builder.Object, new IMockInitializer[] { });
+            // Exercise system
+            var result = sut.Create(request, context.Object);
+            // Verify outcome
+            Assert.Same(mock, result);
+            // Teardown
+        }
+    }
+}
