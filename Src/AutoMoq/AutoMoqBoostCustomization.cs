0030df962bdd1121276bad33beb6f5d74d4e5b39
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-08-09 08:32:03 +0100

    Added AutoMoqBoostCustomization

---
+++ Src/AutoMoq/AutoMoqBoostCustomization.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.AutoMoq
+{
+    /// <summary>
+    /// Enables auto-mocking and auto-setup with Moq.
+    /// Members of a mock will be automatically setup to retrieve the return values from a fixture.
+    /// </summary>
+    public class AutoMoqBoostCustomization : ICustomization
+    {
+        private readonly ISpecimenBuilder _relay;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AutoMoqBoostCustomization"/>.
+        /// </summary>
+        public AutoMoqBoostCustomization()
+            : this(new MockRelay())
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AutoMoqBoostCustomization"/>.
+        /// </summary>
+        /// <param name="relay">A mock relay to be added to <see cref="IFixture.ResidueCollectors"/></param>
+        public AutoMoqBoostCustomization(ISpecimenBuilder relay)
+        {
+            if (relay == null)
+                throw new ArgumentNullException("relay");
+
+            _relay = relay;
+        }
+
+        /// <summary>
+        /// Gets the relay that will be added to <see cref="IFixture.ResidueCollectors"/> when <see cref="Customize"/> is invoked.
+        /// </summary>
+        public ISpecimenBuilder Relay
+        {
+            get { return _relay; }
+        }
+
+        /// <summary>
+        /// Customizes a <see cref="IFixture"/> to enable auto-mocking and auto-setup with Moq.
+        /// Members of a mock will be automatically setup to retrieve the return values from <paramref name="fixture"/>.
+        /// </summary>
+        /// <param name="fixture">The fixture to customize.</param>
+        public void Customize(IFixture fixture)
+        {
+            if (fixture == null) throw new ArgumentNullException("fixture");
+
+            fixture.Customizations.Add(
+                new MockSetup(
+                    new MockPostprocessor(
+                        new MethodInvoker(
+                            new MockConstructorQuery())),
+                    new VirtualMethodInitializer(),
+                    new SealedPropertyInitializer()));
+
+            fixture.ResidueCollectors.Add(Relay);
+        }
+    }
+}
---
+++ Src/AutoMoqUnitTest/AutoMoqBoostCustomizationTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixture.AutoMoq.UnitTest
+{
+    public class AutoMoqBoostCustomizationTest
+    {
+        [Fact]
+        public void CtorThrowsWhenRelayIsNull()
+        {
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new AutoMoqBoostCustomization(null));
+        }
+
+        [Fact]
+        public void RelayIsMockRelayByDefault()
+        {
+            // Fixture setup
+            var sut = new AutoMoqBoostCustomization();
+            // Exercise system 
+            var relay = sut.Relay;
+            // Verify outcome
+            Assert.IsType<MockRelay>(relay);
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeThrowsWhenFixtureIsNull()
+        {
+            // Fixture setup
+            var sut = new AutoMoqBoostCustomization();
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(
+                () => sut.Customize(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeAddsMockSetupToCustomizations()
+        {
+            // Fixture setup
+            var customizations = new List<ISpecimenBuilder>();
+            var fixture = new Mock<IFixture> {DefaultValue = DefaultValue.Mock};
+            fixture.Setup(f => f.Customizations)
+                   .Returns(customizations);
+
+            var sut = new AutoMoqBoostCustomization();
+            // Exercise system
+            sut.Customize(fixture.Object);
+            // Verify outcome
+            Assert.True(customizations.Any(builder => builder is MockSetup));
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeAddsInitializersToMockSetup()
+        {
+            // Fixture setup
+            var customizations = new List<ISpecimenBuilder>();
+            var fixture = new Mock<IFixture> {DefaultValue = DefaultValue.Mock};
+            fixture.Setup(f => f.Customizations)
+                   .Returns(customizations);
+
+            var sut = new AutoMoqBoostCustomization();
+            // Exercise system
+            sut.Customize(fixture.Object);
+            // Verify outcome
+            var mockSetup = (MockSetup) customizations.Single(builder => builder is MockSetup);
+            Assert.True(mockSetup.Initializers.Any(init => init is VirtualMethodInitializer));
+            Assert.True(mockSetup.Initializers.Any(init => init is SealedPropertyInitializer));
+            // Teardown
+        }
+
+        [Fact]
+        public void CustomizeAddsRelayToResidueCollectors()
+        {
+            // Fixture setup
+            var relay = new Mock<ISpecimenBuilder>();
+            var collectors = new List<ISpecimenBuilder>();
+            var fixture = new Mock<IFixture> {DefaultValue = DefaultValue.Mock};
+            fixture.Setup(f => f.ResidueCollectors)
+                   .Returns(collectors);
+
+            var sut = new AutoMoqBoostCustomization(relay.Object);
+            // Exercise system
+            sut.Customize(fixture.Object);
+            // Verify outcome
+            Assert.Contains(relay.Object, collectors);
+            // Teardown
+        }
+    }
+}
