536b7e61df292521d84f027a59d30abb41428046
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-08-26 21:44:25 +0200

    Adds a request specification for interfaces.

---
+++ Src/TestTypeFoundation/NoopInterfaceImplementer.cs
@@ -0,0 +1,10 @@
+﻿namespace Ploeh.TestTypeFoundation
+{
+    public class NoopInterfaceImplementer : IInterface
+    {
+        public object MakeIt(object obj)
+        {
+            return obj;
+        }
+    }
+}
