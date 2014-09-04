fccabe2dd2a3649e84065b2fba2b53f600882089
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-09-04 11:40:56 +0200

    Considers type variance in param specification.

--- Src/AutoFixtureUnitTest/Kernel/ParameterSpecificationTest.cs
+++ Src/AutoFixtureUnitTest/Kernel/ParameterSpecificationTest.cs
@@ -7,14 +7,14 @@ using Xunit.Extensions;
 
 namespace Ploeh.AutoFixtureUnitTest.Kernel
 {
-    public class ParameterNameSpecificationTest
+    public class ParameterSpecificationTest
     {
         [Fact]
         public void SutIsRequestSpecification()
         {
             // Fixture setup
             // Exercise system
-            var sut = new ParameterNameSpecification("someName");
+            var sut = new ParameterSpecification(typeof(object), "someName");
             // Verify outcome
             Assert.IsAssignableFrom<IRequestSpecification>(sut);
             // Teardown
@@ -24,11 +24,23 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
         public void InitializeWithParameterNameShouldSetCorrespondingProperty()
         {
             // Fixture setup
-            var parameterName = "someName";
+            var type = typeof(object);
+            var name = "someName";
             // Exercise system
-            var sut = new ParameterNameSpecification(parameterName);
+            var sut = new ParameterSpecification(type, name);
             // Verify outcome
-            Assert.Equal(parameterName, sut.ParameterName);
+            Assert.Equal(type, sut.TargetType);
+            Assert.Equal(name, sut.TargetName);
+            // Teardown
+        }
+
+        [Fact]
+        public void InitializeWithNullParameterTypeShouldThrowArgumentNullException()
+        {
+            // Fixture setup
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() =>
+                new ParameterSpecification(null, "someName"));
             // Teardown
         }
 
@@ -38,7 +50,7 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
             // Fixture setup
             // Exercise system and verify outcome
             Assert.Throws<ArgumentNullException>(() =>
-                new ParameterNameSpecification(null));
+                new ParameterSpecification(typeof(object), null));
             // Teardown
         }
 
@@ -46,7 +58,7 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
         public void IsSatisfiedByWithNullRequestShouldThrowArgumentNullException()
         {
             // Fixture setup
-            var sut = new ParameterNameSpecification("someName");
+            var sut = new ParameterSpecification(typeof(object), "someName");
             // Exercise system and verify outcome
             Assert.Throws<ArgumentNullException>(() =>
                 sut.IsSatisfiedBy(null));
@@ -63,15 +75,33 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
             bool expectedResult)
         {
             // Fixture setup
-            var sut = new ParameterNameSpecification(parameterName);
+            var sut = new ParameterSpecification(typeof(object), parameterName);
             var request = typeof(SingleParameterType<object>)
-                              .GetConstructor(new[] { typeof(object) })
-                              .GetParameters()
-                              .Single(p => p.Name == requestedName);
+                          .GetConstructor(new[] { typeof(object) })
+                          .GetParameters()
+                          .Single(p => p.Name == requestedName);
             // Exercise system
             var result = sut.IsSatisfiedBy(request);
             // Verify outcome
             Assert.Equal(expectedResult, result);
+            // Teardown
+        }
+
+        [Fact]
+        public void IsSatisfiedByWithRequestForParameterWithSameNameAndIncompatibleTypeShouldReturnFalse()
+        {
+            // Fixture setup
+            var parameterName = "parameter";
+            var requestedName = "parameter";
+            var sut = new ParameterSpecification(typeof(object), parameterName);
+            var request = typeof(SingleParameterType<int>)
+                          .GetConstructor(new[] { typeof(int) })
+                          .GetParameters()
+                          .Single(p => p.Name == requestedName);
+            // Exercise system
+            var result = sut.IsSatisfiedBy(request);
+            // Verify outcome
+            Assert.False(result);
             // Teardown
         }
 
@@ -81,9 +111,8 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
             // Fixture setup
             var parameterName = "Parameter";
             var requestedName = "Parameter";
-            var sut = new ParameterNameSpecification(parameterName);
-            var request = typeof(SingleParameterType<object>)
-                          .GetProperty(requestedName);
+            var sut = new ParameterSpecification(typeof(object), parameterName);
+            var request = typeof(SingleParameterType<object>).GetProperty(requestedName);
             // Exercise system
             var result = sut.IsSatisfiedBy(request);
             // Verify outcome
@@ -99,7 +128,7 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
         public void IsSatisfiedByWithInvalidRequestShouldReturnFalse(object request)
         {
             // Fixture setup
-            var sut = new ParameterNameSpecification("someName");
+            var sut = new ParameterSpecification(typeof(object), "someName");
             // Exercise system
             var result = sut.IsSatisfiedBy(request);
             // Verify outcome
