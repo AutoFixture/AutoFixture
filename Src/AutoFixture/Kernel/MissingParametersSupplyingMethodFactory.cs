087634eb95967c41aba7695f48c661d85762b826
Marcio Rinaldi <mrinaldi4@gmail.com>
2014-10-19 19:00:09 -0200

    adding comments for MissingParametersSupplyingMethodFactory

--- Src/AutoFixture/Kernel/MissingParametersSupplyingMethodFactory.cs
+++ Src/AutoFixture/Kernel/MissingParametersSupplyingMethodFactory.cs
@@ -3,10 +3,19 @@ using System.Reflection;
 
 namespace Ploeh.AutoFixture.Kernel
 {
+    /// <summary>
+    /// Creates a new <see cref="MissingParametersSupplyingMethod" /> for an 
+    /// instance <see cref="MethodInfo"/>.
+    /// </summary>
     public class MissingParametersSupplyingMethodFactory : IMethodFactory
     {
         private readonly object owner;
 
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="MissingParametersSupplyingMethodFactory"/> class.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
         public MissingParametersSupplyingMethodFactory(object owner)
         {
             if (owner == null)
@@ -15,11 +24,21 @@ namespace Ploeh.AutoFixture.Kernel
             this.owner = owner;
         }
 
+        /// <summary>
+        /// Gets the owner originally supplied through the constructor.
+        /// </summary>
+        /// <seealso cref="MissingParametersSupplyingMethodFactory(object)" />
         public object Owner
         {
             get { return owner; }
         }
 
+        /// <summary>
+        /// Creates a <see cref="InstanceMethod" /> decorated with 
+        /// <see cref="MissingParametersSupplyingMethod" /> for the supplied methodInfo.
+        /// </summary>
+        /// <param name="methodInfo">The methodInfo.</param>
+        /// <returns>Method for <paramref name="methodInfo"/>.</returns>
         public IMethod Create(MethodInfo methodInfo)
         {
             return new MissingParametersSupplyingMethod(new InstanceMethod(methodInfo, Owner));
