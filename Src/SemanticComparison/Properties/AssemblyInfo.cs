a177824f05e911c70aac1a459b8da5bd206cf410
Mark Seemann <mark@ploeh.dk>
2014-11-13 11:16:58 +0100

    Incremented patch version,
    in order to indicate that a more specific verification of the result of
    calling a copy-and-update method is a bug fix, which resolves #326
    (https://github.com/AutoFixture/AutoFixture/issues/326).

--- Src/SemanticComparison/Properties/AssemblyInfo.cs
+++ Src/SemanticComparison/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.21.0.0")]
-[assembly: AssemblyFileVersion("3.21.0.0")]
+[assembly: AssemblyVersion("3.21.1.0")]
+[assembly: AssemblyFileVersion("3.21.1.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
