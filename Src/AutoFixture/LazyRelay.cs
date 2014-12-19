87c1ae021b838cea01f051ee5b854180f13d62ed
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-12-19 18:38:47 +0200

    Added LazyRelay.

---
+++ Src/AutoFixture/LazyRelay.cs
@@ -0,0 +1,16 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture
+{
+    public class LazyRelay : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/LazyRelayTest.cs
@@ -0,0 +1,24 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class LazyRelayTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new LazyRelay();
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+    }
+}
