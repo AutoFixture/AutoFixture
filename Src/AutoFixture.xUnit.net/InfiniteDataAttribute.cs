44392fc129d69cb27957bf92695f423ea9778c5f
Alex Gill <alexandermichaelfoxgill@hotmail.co.uk>
2015-03-01 20:27:34 +0000

    Added infinite data attribute

---
+++ Src/AutoFixture.xUnit.net/InfiniteDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Extensions;
+
+namespace Ploeh.AutoFixture.Xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class InfiniteDataAttribute : DataAttribute
+    {
+        private readonly DataAttribute _decorated;
+
+        public InfiniteDataAttribute(DataAttribute decorated)
+        {
+            _decorated = decorated;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+        {
+            while (true)
+            {
+                foreach (var dataSet in _decorated.GetData(methodUnderTest, parameterTypes))
+                    yield return dataSet;
+            }
+        }
+    }
+}
