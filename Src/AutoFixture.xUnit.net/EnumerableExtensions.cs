b1a21a021bdea492754442509ace0592e96f249a
Mark Seemann <mark@ploeh.dk>
2015-03-12 10:01:37 +0100

    Made extension methods internal.
    Even though the static class itself is already internal, it protects us
    better from accidental publication of these extension methods to
    explicitly make each of them internal. In this way, if we ever decide to
    make one of them public, it can be done in a gradual manner.

--- Src/AutoFixture.xUnit.net/EnumerableExtensions.cs
+++ Src/AutoFixture.xUnit.net/EnumerableExtensions.cs
@@ -14,7 +14,7 @@ namespace Ploeh.AutoFixture.Xunit
         /// <param name="sequences">The input sequences.</param>
         /// <param name="resultSelector">A function that specifies how to combine the corresponding elements of the sequences.</param>
         /// <returns>An <see cref="IEnumerable{T}"/> that contains elements of the input sequences, combined by resultSelector.</returns>
-        public static IEnumerable<TResult> Zip<T, TResult>(this IEnumerable<IEnumerable<T>> sequences,
+        internal static IEnumerable<TResult> Zip<T, TResult>(this IEnumerable<IEnumerable<T>> sequences,
             Func<IEnumerable<T>, TResult> resultSelector)
         {
             var enumerators = sequences.Select(s => s.GetEnumerator()).ToList();
@@ -30,7 +30,7 @@ namespace Ploeh.AutoFixture.Xunit
         /// <typeparam name="T">The type of the elements of the input sequences.</typeparam>
         /// <param name="sequences">The input sequences.</param>
         /// <returns>Items from each sequence in turn, yielding those from the first sequence first.</returns>
-        public static IEnumerable<T> Collapse<T>(this IEnumerable<IEnumerable<T>> sequences)
+        internal static IEnumerable<T> Collapse<T>(this IEnumerable<IEnumerable<T>> sequences)
         {
             var position = 0;
             foreach (var sequence in sequences)
