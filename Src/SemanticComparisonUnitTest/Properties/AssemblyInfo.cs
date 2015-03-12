52896fb78476c03fc8957f6dceaefd3db1f568d2
Mark Seemann <mark@ploeh.dk>
2015-03-12 10:07:09 +0100

    Incremented patch version,
    in order to indicate that a more sane implementation of
    CompositeDataAttribute fixes issues with the previous implementation.

--- Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
+++ Src/SemanticComparisonUnitTest/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.24.2.0")]
-[assembly: AssemblyFileVersion("3.24.2.0")]
+[assembly: AssemblyVersion("3.24.3.0")]
+[assembly: AssemblyFileVersion("3.24.3.0")]
