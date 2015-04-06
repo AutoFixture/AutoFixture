fe6a10880d396c7ebb410b325ffbb8ae276677e6
TeaDrivenDev <TeaDrivenDev@gmail.com>
2015-04-06 04:08:29 +0200

    Added AutoDataAttribute and tests

---
+++ Src/AutoFixture.xUnit.net2.UnitTest/DelegatingSpecimenBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.Xunit2.UnitTest
+{
+    internal class DelegatingSpecimenBuilder : ISpecimenBuilder
+    {
+        public DelegatingSpecimenBuilder()
+        {
+            this.OnCreate = (r, c) => new object();
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            return this.OnCreate(request, context);
+        }
+
+        internal Func<object, ISpecimenContext, object> OnCreate { get; set; }
+    }
+}
