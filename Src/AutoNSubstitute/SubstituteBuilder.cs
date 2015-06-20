3135fdceaf6c3e3a0faabd4b0eb1c4c8e69cffd0
Oleg Sych <oleg_sych@hotmail.com>
2015-06-20 11:42:48 -0700

    - Implement SubstituteBuilder.

---
+++ Src/AutoNSubstitute/SubstituteBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.AutoNSubstitute
+{
+    /// <summary>
+    /// Creates a substitute in response to the <see cref="SubstituteRequest"/>.
+    /// </summary>
+    /// <remarks>
+    /// This class serves as a fixture customization, responding to explicit substitute requests in the form of the
+    /// <see cref="SubstituteRequest"/> instances. The actual construction of the substitute is delegated to a 
+    /// substitute constructor, responsible for invoking an appropriate constructor for the target type with
+    /// automatically generated arguments.
+    /// </remarks>
+    public class SubstituteBuilder : ISpecimenBuilder
+    {
+        private readonly ISpecimenBuilder substituteConstructor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubstituteBuilder"/> class with the specified 
+        /// <paramref name="substituteConstructor"/>.
+        /// </summary>
+        /// <param name="substituteConstructor">
+        /// An <see cref="ISpecimenBuilder"/> responsible for creating a substitute instance from a requested 
+        /// <see cref="Type"/>.
+        /// </param>
+        public SubstituteBuilder(ISpecimenBuilder substituteConstructor)
+        {
+            if (substituteConstructor == null)
+            {
+                throw new ArgumentNullException("substituteConstructor");
+            }
+
+            this.substituteConstructor = substituteConstructor;
+        }
+
+        /// <summary>
+        /// Returns an <see cref="ISpecimenBuilder"/> responsible for creating a substitute instance based on a target
+        /// <see cref="Type"/> type.
+        /// </summary>
+        public ISpecimenBuilder SubstituteConstructor 
+        {
+            get { return this.substituteConstructor; }
+        }
+
+        /// <summary>
+        /// Creates a substitute when <paramref name="request"/> is an explicit <see cref="SubstituteRequest"/>.
+        /// </summary>
+        /// <returns>
+        /// A substitute created by the substitute constructor when <paramref name="request"/> is a 
+        /// <see cref="SubstituteRequest"/> or <see cref="NoSpecimen"/> for all other requests.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            var substituteRequest = request as SubstituteRequest;
+            if (substituteRequest == null)
+            {
+                return new NoSpecimen(request);
+            }
+
+            return this.substituteConstructor.Create(substituteRequest.TargetType, context);
+        }
+    }
+}
---
+++ Src/AutoNSubstituteUnitTest/SubstituteBuilderTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NSubstitute;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Ploeh.AutoFixture.AutoNSubstitute.UnitTest
+{
+    public class SubstituteBuilderTest
+    {
+        [Fact]
+        public void ClassImplementsISpecimenBuilderToServeAsFixtureCustomization()
+        {
+            // Fixture setup
+            // Exercise system
+            // Verify outcome
+            Assert.True(typeof(ISpecimenBuilder).IsAssignableFrom(typeof(SubstituteBuilder)));
+            // Teardown
+        }
+
+        [Fact]
+        public void ConstructorThrowsArgumentNullExceptionWhenSubstituteConstructorIsNull()
+        {
+            // Fixture setup
+            // Exercise system
+            var e = Assert.Throws<ArgumentNullException>(() => new SubstituteBuilder((ISpecimenBuilder)null));
+            // Verify outcome
+            Assert.Equal("substituteConstructor", e.ParamName);
+            // Teardown
+        }
+
+        [Fact]
+        public void SubstituteConstructorReturnsValueSpecifiedInConstructorToEnableTestingOfCustomizations()
+        {
+            // Fixture setup
+            var substituteConstructor = Substitute.For<ISpecimenBuilder>();
+            // Exercise system
+            var sut = new SubstituteBuilder(substituteConstructor);
+            // Verify outcome
+            Assert.Same(substituteConstructor, sut.SubstituteConstructor);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsNoSpecimenWhenRequestIsNotAnExplicitSubstituteRequest()
+        {
+            // Fixture setup
+            var constructor = Substitute.For<ISpecimenBuilder>();
+            var sut = new SubstituteBuilder(constructor);
+            var request = typeof(IComparable);
+            var context = Substitute.For<ISpecimenContext>();
+            // Exercise system
+            object result = sut.Create(request, context);
+            // Verify outcome
+            var noSpecimen = Assert.IsType<NoSpecimen>(result);
+            Assert.Same(request, noSpecimen.Request);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreatePassesRequestedSubstituteTypeAndSpecimenContextToSubstituteConstructorAndReturnsInstanceItCreates()
+        {
+            // Fixture setup
+            Type targetType = typeof(IComparable);
+            var constructedSubstute = Substitute.For(new[] { targetType}, new object[0]);
+            var constructor = Substitute.For<ISpecimenBuilder>();
+            constructor.Create(Arg.Any<object>(), Arg.Any<ISpecimenContext>()).Returns(constructedSubstute);
+            var sut = new SubstituteBuilder(constructor);
+            var request = new SubstituteRequest(targetType);
+            var context = Substitute.For<ISpecimenContext>();
+            // Exercise system
+            object result = sut.Create(request, context);
+            // Verify outcome
+            constructor.Received().Create(targetType, context);
+            Assert.Same(constructedSubstute, result);
+            // Teardown
+        }
+    }
+}
