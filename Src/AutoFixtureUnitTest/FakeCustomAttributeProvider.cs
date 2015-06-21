2741c7ebef63b3d5c6e339b99ff3840546fe7bac
Oleg Sych <oleg_sych@hotmail.com>
2015-06-21 11:59:24 -0700

    - Change FakeCustomAttributeProvider to correctly handle matching of derived and inherited attributes

--- Src/AutoFixtureUnitTest/FakeCustomAttributeProvider.cs
+++ Src/AutoFixtureUnitTest/FakeCustomAttributeProvider.cs
@@ -17,22 +17,32 @@ namespace Ploeh.AutoFixtureUnitTest
         public object[] GetCustomAttributes(bool inherit)
         {
             return (from p in this.providedAttributes
-                    where p.Inherited == inherit
+                    where IsMatching(p, inherit)
                     select p.Attribute).ToArray();
         }
 
         public object[] GetCustomAttributes(Type attributeType, bool inherit)
         {
             return (from p in this.providedAttributes
-                    where p.Attribute.GetType() == attributeType && p.Inherited == inherit
+                    where IsMatching(p, attributeType, inherit)
                     select p.Attribute).ToArray();
         }
 
         public bool IsDefined(Type attributeType, bool inherit)
         {
             return (from p in this.providedAttributes
-                    where p.Attribute.GetType() == attributeType && p.Inherited == inherit
+                    where IsMatching(p, attributeType, inherit)
                     select p.Attribute).Any();
+        }
+
+        private bool IsMatching(ProvidedAttribute p, Type attributeType, bool inherit)
+        {
+            return attributeType.IsAssignableFrom(p.Attribute.GetType()) && IsMatching(p, inherit);
+        }
+
+        private bool IsMatching(ProvidedAttribute p, bool inherit)
+        {
+            return !p.Inherited || p.Inherited == inherit;
         }
     }
 }
