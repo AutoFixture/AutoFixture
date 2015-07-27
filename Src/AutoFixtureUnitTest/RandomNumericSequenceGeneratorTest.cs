5866470fb2ee393a7bb5ea7164ec7b832d9d16a0
ryanmoseley <ryanmoseley@hotmail.com>
2015-07-27 17:36:42 -0400

    rollbacked change that will be addressed in #409.

--- Src/AutoFixtureUnitTest/RandomNumericSequenceGeneratorTest.cs
+++ Src/AutoFixtureUnitTest/RandomNumericSequenceGeneratorTest.cs
@@ -108,9 +108,9 @@ namespace Ploeh.AutoFixtureUnitTest
             // Fixture setup
             var unexpectedLimits = new long[]
             { 
-                (byte)10,
-                (Int16)50,
-                (Int32)20,
+                (byte)byte.MaxValue,
+                (Int16)Int16.MaxValue,
+                (Int32)Int32.MaxValue
             };
             var sut = new RandomNumericSequenceGenerator(
                 unexpectedLimits[0],
