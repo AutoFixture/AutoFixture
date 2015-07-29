8b343e8cf4705d9f116953c5e40ddf30fde8d430
cvbarros <carlosvitorbarros@gmail.com>
2015-07-29 17:36:50 -0300

    This commit is to address #427. Create InvariantCultureGenerator and add it to DefaultPrimitiveBuilders
    This enhancement to AutoFixture will make possible to provide CultureInfo instances without any further customizations on user's end.
    InvariantCultureGenerator will provide CultureInfo.InvariantCulture for any CultureInfo requests.
    This generator has been added to DefaultPrimitiveBuilders class that agreggates generators for primitive BCL types.

---
+++ Src/AutoFixture/InvariantCultureGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Handles creation requests for <see cref="CultureInfo"/> instances, 
+    /// returning always the same <see cref="CultureInfo.InvariantCulture"/>.
+    /// </summary>
+    public class InvariantCultureGenerator : ISpecimenBuilder
+    {
+        private readonly ExactTypeSpecification cultureTypeSpecification 
+			= new ExactTypeSpecification(typeof (CultureInfo));
+
+        /// <summary>
+        /// Generates a CultureInfo specimen based on a <see cref="CultureInfo"/> type request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        /// <remarks>>
+        /// The generated CultureInfo will always be the same as <see cref="CultureInfo.InvariantCulture"/>.
+        /// </remarks>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null)
+                return new NoSpecimen();
+
+            if (!cultureTypeSpecification.IsSatisfiedBy(request))
+                return new NoSpecimen(); ;
+
+            return CultureInfo.InvariantCulture;
+        }
+    }
+}
