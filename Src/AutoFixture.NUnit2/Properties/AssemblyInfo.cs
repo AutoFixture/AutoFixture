0a778cbff61d27e35a58f3abdad4b99b88e7a376
Mark Seemann <mark@ploeh.dk>
2015-09-05 19:34:11 +0200

    Removed a redundant [InternalsVisibleTo] attribute
    that never should have been there in the first place, but went unnoticed
    until now.

--- Src/AutoFixture.NUnit2/Properties/AssemblyInfo.cs
+++ Src/AutoFixture.NUnit2/Properties/AssemblyInfo.cs
@@ -20,7 +20,6 @@ using System.Resources;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("AutoFixture.NUnit2.UnitTest")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3a54aa4b-be9b-4591-bf8c-0a463e91088f")]
