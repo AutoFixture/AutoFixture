720c06e9c01e4ba3b9797e736d232d14c46a65d7
Mark Seemann <mark@ploeh.dk>
2015-09-10 14:35:14 +0200

    Introduced Criterion<T>,
    which will serve as a criterion that encapsulates a target value and a
    custom comparer. Candidates can be evaluated against this criterion to see
    if they match or not.

---
+++ Src/AutoFixture/Kernel/Criterion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class Criterion<T> : IEquatable<T>
+    {
+        public Criterion(T target, IEqualityComparer<T> comparer)
+        {
+        }
+
+        public bool Equals(T other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/CriterionTests.cs
@@ -0,0 +1,21 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class CriterionTests
+    {
+        [Fact]
+        public void SutIsEquatable()
+        {
+            var sut = new Criterion<string>(
+                "ploeh",
+                new DelegatingEqualityComparer<string>());
+            Assert.IsAssignableFrom<IEquatable<string>>(sut);
+        }
+    }
+}
