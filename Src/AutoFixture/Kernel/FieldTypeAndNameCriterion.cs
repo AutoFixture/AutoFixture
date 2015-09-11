3d9997ccfb07dd237171f573f8b54cc0d1cf8250
Mark Seemann <mark@ploeh.dk>
2015-09-11 19:19:58 +0200

    Gave FieldTypeAndNameCriterion structural equality

--- Src/AutoFixture/Kernel/FieldTypeAndNameCriterion.cs
+++ Src/AutoFixture/Kernel/FieldTypeAndNameCriterion.cs
@@ -29,5 +29,23 @@ namespace Ploeh.AutoFixture.Kernel
             return this.typeCriterion.Equals(other.FieldType)
                 && this.nameCriterion.Equals(other.Name);
         }
+
+        public override bool Equals(object obj)
+        {
+
+            var other = obj as FieldTypeAndNameCriterion;
+            if (other == null)
+                return base.Equals(obj);
+
+            return object.Equals(this.typeCriterion, other.typeCriterion)
+                && object.Equals(this.nameCriterion, other.nameCriterion);
+        }
+
+        public override int GetHashCode()
+        {
+            return 
+                this.typeCriterion.GetHashCode() ^
+                this.nameCriterion.GetHashCode();
+        }
     }
 }
