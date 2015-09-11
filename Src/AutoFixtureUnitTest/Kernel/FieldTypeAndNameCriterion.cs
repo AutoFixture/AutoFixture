c828d0443fc0e3cfa1f4343b5c2baa82c8f9f8a7
Mark Seemann <mark@ploeh.dk>
2015-09-11 18:25:48 +0200

    Introduced FieldTypeAndNameCriterion,
    which can be used as a comparison target for fields, just like
    PropertyTypeAndNameCriterion can be used as a comparison target for
    properties.

---
+++ Src/AutoFixtureUnitTest/Kernel/FieldTypeAndNameCriterion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class FieldTypeAndNameCriterion : IEquatable<FieldInfo>
+    {
+        public FieldTypeAndNameCriterion(
+            IEquatable<Type> typeCriterion, 
+            IEquatable<string> nameCriterion)
+        {
+        }
+
+        public bool Equals(FieldInfo other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/FieldTypeAndNameCriterionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class FieldTypeAndNameCriterionTests
+    {
+        [Fact]
+        public void SutIsFieldInfoEquatable()
+        {
+            var sut = new FieldTypeAndNameCriterion(
+                new DelegatingCriterion<Type>(),
+                new DelegatingCriterion<string>());
+            Assert.IsAssignableFrom<IEquatable<FieldInfo>>(sut);
+        }
+    }
+}
