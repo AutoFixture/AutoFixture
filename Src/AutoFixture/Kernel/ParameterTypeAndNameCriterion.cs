c778890c8a323e164d538f7bfb5a974f436ee4df
Mark Seemann <mark@ploeh.dk>
2015-09-12 08:40:55 +0200

    Implemented ParameterTypeAndNameCriterion.Equals.

--- Src/AutoFixture/Kernel/ParameterTypeAndNameCriterion.cs
+++ Src/AutoFixture/Kernel/ParameterTypeAndNameCriterion.cs
@@ -5,15 +5,21 @@ namespace Ploeh.AutoFixture.Kernel
 {
     public class ParameterTypeAndNameCriterion : IEquatable<ParameterInfo>
     {
+        private readonly IEquatable<Type> typeCriterion;
+        private readonly IEquatable<string> nameCriterion;
+
         public ParameterTypeAndNameCriterion(
             IEquatable<Type> typeCriterion,
             IEquatable<string> nameCriterion)
         {
+            this.typeCriterion = typeCriterion;
+            this.nameCriterion = nameCriterion;
         }
 
         public bool Equals(ParameterInfo other)
         {
-            throw new NotImplementedException();
+            return this.typeCriterion.Equals(other.ParameterType)
+                && this.nameCriterion.Equals(other.Name);
         }
     }
 }
--- Src/AutoFixtureUnitTest/Kernel/ParameterTypeAndNameCriterionTests.cs
+++ Src/AutoFixtureUnitTest/Kernel/ParameterTypeAndNameCriterionTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Ploeh.AutoFixtureUnitTest.Kernel
 {
@@ -17,6 +18,44 @@ namespace Ploeh.AutoFixtureUnitTest.Kernel
                 new DelegatingCriterion<Type>(),
                 new DelegatingCriterion<string>());
             Assert.IsAssignableFrom<IEquatable<ParameterInfo>>(sut);
+        }
+
+        [Theory]
+        [InlineData(false, false, false)]
+        [InlineData(true,  false, false)]
+        [InlineData(false, true,  false)]
+        [InlineData(true,  true,  true)]
+        public void EqualsReturnsCorrectResult(
+            bool typeResult,
+            bool nameResult,
+            bool expected)
+        {
+            var parameter = 
+                typeof(string).GetMethod("Contains").GetParameters().First();
+            var typeCriterion = new DelegatingCriterion<Type>
+            {
+                OnEquals = t =>
+                {
+                    Assert.Equal(parameter.ParameterType, t);
+                    return typeResult;
+                }
+            };
+            var nameCriterion = new DelegatingCriterion<string>
+            {
+                OnEquals = n =>
+                {
+                    Assert.Equal(parameter.Name, n);
+                    return nameResult;
+                }
+            };
+            var sut =
+                new ParameterTypeAndNameCriterion(
+                    typeCriterion,
+                    nameCriterion);
+
+            var actual = sut.Equals(parameter);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
