d0b44bac2d0fe7bdf4641121ea32ccef7b8079c9
Mark Seemann <mark@ploeh.dk>
2015-09-12 08:32:52 +0200

    ntroduced ParameterTypeAndNameCriterion,
    which can be used as a comparison target for parameters, just like
    PropertyTypeAndNameCriterion can be used as a comparison target for properties.

---
+++ Src/AutoFixture/Kernel/ParameterTypeAndNameCriterion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ploeh.AutoFixture.Kernel
+{
+    public class ParameterTypeAndNameCriterion : IEquatable<ParameterInfo>
+    {
+        public ParameterTypeAndNameCriterion(
+            IEquatable<Type> typeCriterion,
+            IEquatable<string> nameCriterion)
+        {
+        }
+
+        public bool Equals(ParameterInfo other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Kernel/ParameterTypeAndNameCriterionTests.cs
@@ -0,0 +1,22 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest.Kernel
+{
+    public class ParameterTypeAndNameCriterionTests
+    {
+        [Fact]
+        public void SutIsParameterInfoEquatable()
+        {
+            var sut = new ParameterTypeAndNameCriterion(
+                new DelegatingCriterion<Type>(),
+                new DelegatingCriterion<string>());
+            Assert.IsAssignableFrom<IEquatable<ParameterInfo>>(sut);
+        }
+    }
+}
