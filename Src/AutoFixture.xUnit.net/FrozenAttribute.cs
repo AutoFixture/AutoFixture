cbb058bbccf0f39d33f584720a7e144b9e7d3833
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2015-09-20 19:32:23 +0200

    Corrects the sample code in the obsolete warning.

--- Src/AutoFixture.xUnit.net/FrozenAttribute.cs
+++ Src/AutoFixture.xUnit.net/FrozenAttribute.cs
@@ -45,7 +45,7 @@ namespace Ploeh.AutoFixture.Xunit
         /// Gets or sets the <see cref="Type"/> that the frozen parameter value
         /// should be mapped to in the <see cref="IFixture"/>.
         /// </summary>
-        [Obsolete("The ability to map a frozen object to a specific type is deprecated and will likely be removed in the future. If you wish to map a frozen type to its implemented interfaces, use [Frozen(By = Matching.ImplementedInterfaces)].")]
+        [Obsolete("The ability to map a frozen object to a specific type is deprecated and will likely be removed in the future. If you wish to map a frozen type to its implemented interfaces, use [Frozen(Matching.ImplementedInterfaces)].")]
         public Type As { get; set; }
 
         /// <summary>
