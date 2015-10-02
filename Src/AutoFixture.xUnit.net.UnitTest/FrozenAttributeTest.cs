59a33b6df5b5e7b710753731ee2a3657ccac5e6b
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2015-10-02 15:21:56 +0200

    Removes overspecified tests.
    See the related discussion on https://github.com/AutoFixture/AutoFixture/pull/318#discussion_r40505383

--- Src/AutoFixture.xUnit.net.UnitTest/FrozenAttributeTest.cs
+++ Src/AutoFixture.xUnit.net.UnitTest/FrozenAttributeTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
-using Ploeh.AutoFixture.Kernel;
 using Ploeh.TestTypeFoundation;
 using Xunit;
 
@@ -70,74 +69,6 @@ namespace Ploeh.AutoFixture.Xunit.UnitTest
             var parameter = AParameter<ConcreteType>();
             // Exercise system and verify outcome
             Assert.Throws<ArgumentException>(() => sut.GetCustomization(parameter));
-            // Teardown
-        }
-
-        [Fact]
-        public void GetCustomizationShouldMatchByExactParameterType()
-        {
-            // Fixture setup
-            var parameter = AParameter<object>();
-            var sut = new FrozenAttribute();
-            // Exercise system
-            var customization = sut.GetCustomization(parameter);
-            // Verify outcome
-            var freezer = Assert.IsAssignableFrom<FreezeOnMatchCustomization>(customization);
-            var matcher = Assert.IsType<OrRequestSpecification>(freezer.Matcher);
-            var exactTypeMatcher = matcher.Specifications.OfType<ExactTypeSpecification>().SingleOrDefault();
-            Assert.NotNull(exactTypeMatcher);
-            Assert.Equal(parameter.ParameterType, exactTypeMatcher.TargetType);
-            // Teardown
-        }
-
-        [Fact]
-        public void GetCustomizationShouldMatchBySeedRequestForParameterType()
-        {
-            // Fixture setup
-            var parameter = AParameter<object>();
-            var sut = new FrozenAttribute();
-            // Exercise system
-            var customization = sut.GetCustomization(parameter);
-            // Verify outcome
-            var freezer = Assert.IsAssignableFrom<FreezeOnMatchCustomization>(customization);
-            var matcher = Assert.IsType<OrRequestSpecification>(freezer.Matcher);
-            var seedRequestMatcher = matcher.Specifications.OfType<SeedRequestSpecification>().SingleOrDefault();
-            Assert.NotNull(seedRequestMatcher);
-            Assert.Equal(parameter.ParameterType, seedRequestMatcher.TargetType);
-            // Teardown
-        }
-
-        [Fact]
-        public void GetCustomizationWithMatchingByDirectBaseTypeShouldMatchByBaseType()
-        {
-            // Fixture setup
-            var parameter = AParameter<object>();
-            var sut = new FrozenAttribute(Matching.DirectBaseType);
-            // Exercise system
-            var customization = sut.GetCustomization(parameter);
-            // Verify outcome
-            var freezer = Assert.IsAssignableFrom<FreezeOnMatchCustomization>(customization);
-            var matcher = Assert.IsType<OrRequestSpecification>(freezer.Matcher);
-            var directBaseTypeMatcher = matcher.Specifications.OfType<DirectBaseTypeSpecification>().SingleOrDefault();
-            Assert.NotNull(directBaseTypeMatcher);
-            Assert.Equal(parameter.ParameterType, directBaseTypeMatcher.TargetType);
-            // Teardown
-        }
-
-        [Fact]
-        public void GetCustomizationWithMatchingByImplementedInterfacesShouldMatchByImplementedInterfaces()
-        {
-            // Fixture setup
-            var parameter = AParameter<object>();
-            var sut = new FrozenAttribute(Matching.ImplementedInterfaces);
-            // Exercise system
-            var customization = sut.GetCustomization(parameter);
-            // Verify outcome
-            var freezer = Assert.IsAssignableFrom<FreezeOnMatchCustomization>(customization);
-            var matcher = Assert.IsType<OrRequestSpecification>(freezer.Matcher);
-            var interfaceTypeMatcher = matcher.Specifications.OfType<ImplementedInterfaceSpecification>().SingleOrDefault();
-            Assert.NotNull(interfaceTypeMatcher);
-            Assert.Equal(parameter.ParameterType, interfaceTypeMatcher.TargetType);
             // Teardown
         }
 
