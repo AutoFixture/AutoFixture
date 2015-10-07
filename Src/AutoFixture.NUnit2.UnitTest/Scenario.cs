35ea5dcb7c19fca88fd17aedef90318c55a4e247
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2015-10-07 15:34:04 +0200

    Makes freezing by implemented interface exclusive.
    Applies the same modifications as f88b80a with an additional test scenario
    for the NUnit2 Glue Library.

--- Src/AutoFixture.NUnit2.UnitTest/Scenario.cs
+++ Src/AutoFixture.NUnit2.UnitTest/Scenario.cs
@@ -190,6 +190,22 @@ namespace Ploeh.AutoFixture.NUnit2.UnitTest
         }
 
         [Test, AutoData]
+        public void FreezeFirstParameterByInterfaceShouldNotAssignSameInstanceToSecondParameterOfSameType(
+            [Frozen(Matching.ImplementedInterfaces)]NoopInterfaceImplementer p1,
+            NoopInterfaceImplementer p2)
+        {
+            Assert.AreNotEqual(p1, p2);
+        }
+
+        [Test, AutoData]
+        public void FreezeFirstParameterByDirectOrInterfaceShouldAssignSameInstanceToSecondParameterOfSameType(
+            [Frozen(Matching.ExactType | Matching.ImplementedInterfaces)]NoopInterfaceImplementer p1,
+            NoopInterfaceImplementer p2)
+        {
+            Assert.AreEqual(p1, p2);
+        }
+
+        [Test, AutoData]
         public void FreezeFirstParameterByParameterWithSameNameShouldAssignSameInstanceToSecondParameter(
             [Frozen(Matching.ParameterName)]string parameter,
             SingleParameterType<object> p2)
