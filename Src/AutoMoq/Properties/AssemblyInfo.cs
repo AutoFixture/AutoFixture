da6613f84b6d2010d3371f2b247c80453dc80081
Mark Seemann <mark@ploeh.dk>
2015-11-16 14:48:31 +0100

    Incremented patch version,
    in order to indicate that enabling FromFactory to return null values is a
    bug fix.

--- Src/AutoMoq/Properties/AssemblyInfo.cs
+++ Src/AutoMoq/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.36.10.0")]
-[assembly: AssemblyFileVersion("3.36.10.0")]
+[assembly: AssemblyVersion("3.36.11.0")]
+[assembly: AssemblyFileVersion("3.36.11.0")]
 
 [assembly: CLSCompliant(true)]
