62109582e7ec295c7e41bf4bb1f464fc182eb320
Mark Seemann <mark@ploeh.dk>
2015-11-16 14:38:24 +0100

    Added support for building using a strong name key
    file. If one is able to suplly the appropriate strong name key file, one
    can build an official release of AutoFixture from the command line.
    
    The extra function I copied from the Albedo project
    (https://github.com/ploeh/Albedo) where I've used it for quite a while
    with success.

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+function abspath()
+{
+	# Make an absolute path from a relative path, and convert it from Linux to Windows format
+	# Lifted from
+	# http://stackoverflow.com/a/3915420/126014
+	# http://stackoverflow.com/a/13701495/126014
+	echo $(cd $(dirname $1); pwd)/$(basename $1) | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/'
+}
+
 function vsvers()
 {
 	if [ "$VS140COMNTOOLS" ]; then
@@ -11,5 +20,8 @@ function vsvers()
 	fi
 }
 
-$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild `vsvers`
-
+if [ "$1" != "" ]; then
+	$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe /property:AssemblyOriginatorKeyFile=`abspath $1` `dirname $0`/BuildRelease.msbuild `vsvers`
+else
+	$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild `vsvers`
+fi
