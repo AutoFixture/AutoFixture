6f50f4c1ed3fbacd1a317993673478e49cf76964
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-11-18 22:14:42 +0100

    Add DomainNameGenerator,
    which creates DomainName's.

---
+++ Src/AutoFixture/DomainNameGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Creates new <see cref="DomainName"/> instances.
+    /// </summary>
+    public class DomainNameGenerator : ISpecimenBuilder
+    {
+        private readonly string[] fictitiousDomains =
+        {
+            "example.com",
+            "example.net",
+            "example.org"
+        };
+
+        private readonly Random random = new Random();
+
+        /// <summary>
+        /// Creates a new specimen based on a request.
+        /// </summary>
+        /// <param name="request">The request that describes what to create</param>
+        /// <param name="context">A context that can be used to create other specimens.</param>
+        /// <returns>
+        /// The requested specimen if possible; otherwise a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request == null || !typeof(DomainName).Equals(request))
+            {
+                return new NoSpecimen();
+            }
+
+            var index = random.Next(0, fictitiousDomains.Length);
+            return new DomainName(fictitiousDomains[index]);
+        }
+    }
+}
