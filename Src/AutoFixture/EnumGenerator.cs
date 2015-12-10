d992a83894715adb784b25eca77c608e631a76a7
Mark Seemann <mark@ploeh.dk>
2015-12-10 21:05:27 +0100

    Changed a use of NoSpecimen to the ctor w/out req.

--- Src/AutoFixture/EnumGenerator.cs
+++ Src/AutoFixture/EnumGenerator.cs
@@ -46,9 +46,7 @@ namespace Ploeh.AutoFixture
             var t = request as Type;
             if (!EnumGenerator.IsEnumType(t))
             {
-#pragma warning disable 618
-                return new NoSpecimen(request);
-#pragma warning restore 618
+                return new NoSpecimen();
             }
 
             lock (this.syncRoot)
