2fa054c644ce300c179323b335fb1074cb92c905
Mark Seemann <mark@ploeh.dk>
2015-12-17 20:34:31 +0100

    Changed a use of NoSpecimen to the ctor w/out req.

--- Src/AutoFixture/Kernel/PropertyRequestRelay.cs
+++ Src/AutoFixture/Kernel/PropertyRequestRelay.cs
@@ -29,9 +29,7 @@ namespace Ploeh.AutoFixture.Kernel
             var propertyInfo = request as PropertyInfo;
             if (propertyInfo == null)
             {
-#pragma warning disable 618
-                return new NoSpecimen(request);
-#pragma warning restore 618
+                return new NoSpecimen();
             }
 
             return context.Resolve(new SeededRequest(propertyInfo.PropertyType, propertyInfo.Name));
