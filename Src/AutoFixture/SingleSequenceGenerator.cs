757292d7a3a6316cd4d99c51b47c5bcbc027ff03
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-12-20 18:50:49 +0100

    Changed a use of NoSpecimen to the ctor w/out req.

--- Src/AutoFixture/SingleSequenceGenerator.cs
+++ Src/AutoFixture/SingleSequenceGenerator.cs
@@ -55,9 +55,7 @@ namespace Ploeh.AutoFixture
         {
             if (!typeof(float).Equals(request))
             {
-#pragma warning disable 618
-                return new NoSpecimen(request);
-#pragma warning restore 618
+                return new NoSpecimen();
             }
 
             return this.Create();
