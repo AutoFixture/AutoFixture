bc96ac912fe16bc8e56e6e5ba682f1f7abd5522e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2015-12-23 11:46:41 +0200

    Changed a use of NoSpecimen to the ctor w/out req.

--- Src/AutoNSubstituteUnitTest/SubstituteRequestHandlerTest.cs
+++ Src/AutoNSubstituteUnitTest/SubstituteRequestHandlerTest.cs
@@ -51,9 +51,7 @@ namespace Ploeh.AutoFixture.AutoNSubstitute.UnitTest
             // Exercise system
             object result = sut.Create(request, context);
             // Verify outcome
-#pragma warning disable 618
-            var expected = new NoSpecimen(request);
-#pragma warning restore 618
+            var expected = new NoSpecimen();
             Assert.Equal(expected, result);
             // Teardown
         }
