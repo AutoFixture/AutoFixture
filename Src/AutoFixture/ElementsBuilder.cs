62e21996d5e0436466e133ef548b3f6ca0bd7b0a
Philippe <pvlerick@gmail.com>
2015-12-27 21:01:07 +0100

    Added ElementsBuilder specimen builder

---
+++ Src/AutoFixture/ElementsBuilder.cs
@@ -0,0 +1,62 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture
+{
+    /// <summary>
+    /// Draws a random element from the given collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class ElementsBuilder<T> : ISpecimenBuilder
+    {
+        private readonly ICollection<T> elements;
+        private readonly RandomNumericSequenceGenerator sequence;
+
+        private readonly object syncRoot = new object();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElementsBuilder"/> class.
+        /// </summary>
+        /// <param name="elements">The collection from which elements should be drawn from.</param>
+        public ElementsBuilder(ICollection<T> elements)
+        {
+            if (elements == null)
+                throw new ArgumentNullException(nameof(elements));
+
+            if (elements.Count < 2)
+                throw new ArgumentException("The collection should at least hold two elements.", nameof(elements));
+
+            this.elements = elements;
+            this.sequence = new RandomNumericSequenceGenerator(0, elements.Count - 1);
+        }
+
+        /// <summary>
+        /// Returns one of the element present in the collection given when the object was constructed.
+        /// </summary>
+        /// <param name="request">The request that describes what to create.</param>
+        /// <param name="context">Not used.</param>
+        /// <returns>
+        /// One of the element present in the collection given to the constructor if <paramref name="request"/>
+        /// is a request for <typeparamref name="T"/>; otherwise, a <see cref="NoSpecimen"/> instance.
+        /// </returns>
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (!typeof(T).Equals(request))
+                return new NoSpecimen();
+
+            return this.elements.ElementAt(this.GetNext());
+        }
+
+        private int GetNext()
+        {
+            lock (this.syncRoot)
+            {
+                var i = (int)this.sequence.Create(typeof(int));
+                return i;
+            }
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/ElementsBuilderTest.cs
@@ -0,0 +1,105 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class ElementsBuilderTest
+    {
+        [Fact]
+        public void CreateWithNullShouldThrow()
+        {
+            // Fixture setup
+            // Exercise system & Verify outcome
+            Assert.Throws<ArgumentNullException>(() => new ElementsBuilder<int>(null));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithEmptyCollectionShouldThrow()
+        {
+            // Fixture setup
+            // Exercise system & Verify outcome
+            Assert.Throws<ArgumentException>(() => new ElementsBuilder<int>(Enumerable.Empty<int>().ToArray()));
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithOneElementCollectionShouldThrow()
+        {
+            // Fixture setup
+            // Exercise system & Verify outcome
+            Assert.Throws<ArgumentException>(() => new ElementsBuilder<int>(new[] { 42 }));
+            // Teardown
+        }
+
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new ElementsBuilder<int>(new[] { 42, 7 });
+            // Verify outcome
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithNonCorrectTypeRequestWillReturnNoSpecimen()
+        {
+            // Fixture setup
+            var dummyContext = new DelegatingSpecimenContext();
+            var sut = new ElementsBuilder<int>(new[] { 42, 7 });
+            // Exercise system
+            var result = sut.Create(typeof(string), dummyContext);
+            // Verify outcome
+            Assert.Equal(new NoSpecimen(), result);
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateWithCorrectTypeRequestWillReturnCorrectTypeSpecimen()
+        {
+            // Fixture setup
+            var dummyContext = new DelegatingSpecimenContext();
+            var sut = new ElementsBuilder<int>(new[] { 42, 7 });
+            // Exercise system
+            var result = sut.Create(typeof(int), dummyContext);
+            // Verify outcome
+            Assert.Equal(typeof(int), result.GetType());
+            // Teardown
+        }
+
+        [Fact]
+        public void CreateReturnsElementFromTheCollection()
+        {
+            // Fixture setup
+            var collection = new[] { "foo", "bar", "qux" };
+            var dummyContext = new DelegatingSpecimenContext();
+            var sut = new ElementsBuilder<string>(collection);
+            // Exercise system
+            var result = sut.Create(typeof(string), dummyContext);
+            // Verify outcome
+            Assert.Contains(result, collection);
+            // Teardown 
+        }
+
+        [Fact]
+        public void CreateDoesNotReturnTheSameElementTwiceWhenCalledTwoTimesWithTwoElementsInCollection()
+        {
+            // Fixture setup
+            var collection = new[] { 42, 7 };
+            var dummyContext = new DelegatingSpecimenContext();
+            var sut = new ElementsBuilder<int>(collection);
+            // Exercise system
+            var result1 = sut.Create(typeof(int), dummyContext);
+            var result2 = sut.Create(typeof(int), dummyContext);
+            // Verify outcome
+            Assert.NotEqual(result1, result2);
+            // Teardown 
+        }
+    }
+}
