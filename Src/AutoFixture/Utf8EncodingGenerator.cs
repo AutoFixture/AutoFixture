3e46a77b8576a499c21a1d0b2388788efc3a21cd
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-01-13 22:03:00 +0100

    Add Utf8EncodingGenerator class,
    similar to InvariantCultureGenerator, that satisfies Encoding requests and returns a Utf8 encoding instance.

---
+++ Src/AutoFixture/Utf8EncodingGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Ploeh.AutoFixture.Kernel;
+using System.Text;
+
+namespace Ploeh.AutoFixture
+{
+    public class Utf8EncodingGenerator : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (typeof(Encoding).Equals(request))
+            {
+                return Encoding.UTF8;
+            }
+            return new NoSpecimen();
+        }
+    }
+}
---
+++ Src/AutoFixtureUnitTest/Utf8EncodingGeneratorTest.cs
@@ -0,0 +1,68 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixtureUnitTest.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.AutoFixtureUnitTest
+{
+    public class Utf8EncodingGeneratorTest
+    {
+        [Fact]
+        public void SutIsSpecimenBuilder()
+        {
+            var sut = new Utf8EncodingGenerator();
+            Assert.IsAssignableFrom<ISpecimenBuilder>(sut);
+        }
+
+        [Fact]
+        public void CreateWithEncodingRequestWillReturnUtf8Encoding()
+        {
+            // Arrange
+            var sut = new Utf8EncodingGenerator();
+
+            // Act
+            var result = sut.Create(typeof(Encoding), new DelegatingSpecimenContext());
+
+            // Assert
+            Assert.Equal(Encoding.UTF8, result);
+        }
+
+        [Fact]
+        public void CreateWithNullRequestWillReturnNoSpecimen()
+        {
+            // Arrange
+            var sut = new Utf8EncodingGenerator();
+
+            // Act
+            var result = sut.Create(null, new DelegatingSpecimenContext());
+
+            // Assert
+            Assert.Equal(new NoSpecimen(), result);
+        }
+
+        [Fact]
+        public void CreateWithNullContextDoesNotThrow()
+        {
+            // Arrange
+            var sut = new Utf8EncodingGenerator();
+
+            // Act
+            Assert.DoesNotThrow(() => sut.Create(typeof(object), null));
+        }
+
+        [Fact]
+        public void CreateWithNonTypeRequestWillReturnNoSpecimen()
+        {
+            // Arrange
+            var sut = new Utf8EncodingGenerator();
+
+            // Act
+            var result = sut.Create(new object(), new DelegatingSpecimenContext());
+            // Assert
+        }
+    }
+}
