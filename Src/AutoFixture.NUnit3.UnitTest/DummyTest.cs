fe879595b58888466de5bd053d3dfbd008d3fcc6
Hackle Wayne <hacklew@hotmail.com>
2016-02-15 19:20:06 +1300

    Add dummy unit test

---
+++ Src/AutoFixture.NUnit3.UnitTest/DummyTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.AutoFixture.NUnit3.UnitTest
+{
+    public class DummyTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
