c3c895a5bac81ebeb0c1f9638e0ba82e1bd36565
Hackle Wayne <hacklew@hotmail.com>
2016-03-13 21:02:25 +1300

    Add Frozen and tests

---
+++ Src/AutoFixture.NUnit3.UnitTest/FrozenAttributeIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Ploeh.AutoFixture.NUnit3.UnitTest
+{
+    [TestFixture]
+    public class FrozenAttributeIntegrationTest
+    {
+        [Theory, AutoData]
+        public void FrozenParameterValuesAreKeptIdentical(
+            [Frozen]int anyInteger, 
+            [Frozen]int anyOtherInteger,
+            [Frozen] DateTime anyDateTime,
+            [Frozen] DateTime anyOtherDateTime,
+            [Frozen] CultureInfo anyCultureInfo,
+            [Frozen] CultureInfo anyOtherCultureInfo)
+        {
+            Assert.That(anyInteger, Is.EqualTo(anyOtherInteger));
+            Assert.That(anyDateTime, Is.EqualTo(anyOtherDateTime));
+            Assert.That(anyCultureInfo, Is.EqualTo(anyOtherCultureInfo));
+        }
+    }
+}
