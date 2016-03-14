81dbaac0704324824f763406ffecc3795f328475
Hackle Wayne <hacklew@hotmail.com>
2016-03-14 20:21:58 +1300

    Port over Scenario and attributes

---
+++ Src/AutoFixture.NUnit3/FavorEnumerablesAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.NUnit3
+{
+    /// <summary>
+    /// An attribute that can be applied to parameters in an <see cref="AutoDataAttribute"/>-driven
+    /// TestCase to indicate that the parameter value should be created using a constructor with one
+    /// or more <see cref="IEnumerable{T}" /> arguments, if applicable.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class FavorEnumerablesAttribute : CustomizeAttribute
+    {
+        /// <summary>
+        /// Gets a customization that associates a <see cref="EnumerableFavoringConstructorQuery"/>
+        /// with the <see cref="Type"/> of the parameter.
+        /// </summary>
+        /// <param name="parameter">The parameter for which the customization is requested.</param>
+        /// <returns>
+        /// A customization that associates a <see cref="EnumerableFavoringConstructorQuery"/> with
+        /// the <see cref="Type"/> of the parameter.
+        /// </returns>
+        public override ICustomization GetCustomization(ParameterInfo parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException("parameter");
+
+            return new ConstructorCustomization(parameter.ParameterType, new EnumerableFavoringConstructorQuery());
+        }
+    }
+}
---
+++ Src/AutoFixture.NUnit3/ModestAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture.NUnit3
+{
+    /// <summary>
+    /// An attribute that can be applied to parameters in an <see cref="AutoDataAttribute"/>-driven
+    /// TestCase to indicate that the parameter value should be created using the most modest
+    /// constructor that can be satisfied by an <see cref="IFixture"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class ModestAttribute : CustomizeAttribute
+    {
+        /// <summary>
+        /// Gets a customization that associates a <see cref="ModestConstructorQuery"/> with the
+        /// <see cref="Type"/> of the parameter.
+        /// </summary>
+        /// <param name="parameter">The parameter for which the customization is requested.</param>
+        /// <returns>
+        /// A customization that associates a <see cref="ModestConstructorQuery"/> with the
+        /// <see cref="Type"/> of the parameter.
+        /// </returns>
+        public override ICustomization GetCustomization(ParameterInfo parameter)
+        {
+            if (parameter == null)
+            {
+                throw new ArgumentNullException("parameter");
+            }
+
+            return new ConstructorCustomization(parameter.ParameterType, new ModestConstructorQuery());
+        }
+    }
+}
