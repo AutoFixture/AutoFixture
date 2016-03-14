5157f4184c21f80544f7ee9dd9b4b69491e7f6ed
Hackle Wayne <hacklew@hotmail.com>
2016-03-14 20:55:21 +1300

    Port over NoAutoProperties and dependencyConstraints

---
+++ Src/AutoFixture.NUnit3.UnitTest/DependencyConstraints.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Ploeh.AutoFixture.NUnit3.UnitTest
+{
+    [TestFixture]
+    public class DependencyConstraints
+    {
+        [TestCase("Moq")]
+        [TestCase("Rhino.Mocks")]
+        public void AutoFixtureNUnit3DoesNotReference(string assemblyName)
+        {
+            // Fixture setup
+            // Exercise system
+            var references = typeof(AutoDataAttribute).Assembly.GetReferencedAssemblies();
+            // Verify outcome
+            Assert.False(references.Any(an => an.Name == assemblyName));
+            // Teardown
+        }
+
+        [TestCase("Moq")]
+        [TestCase("Rhino.Mocks")]
+        public void AutoFixtureNUnit3UnitTestsDoNotReference(string assemblyName)
+        {
+            // Fixture setup
+            // Exercise system
+            var references = this.GetType().Assembly.GetReferencedAssemblies();
+            // Verify outcome
+            Assert.False(references.Any(an => an.Name == assemblyName));
+            // Teardown
+        }
+    }
+}
