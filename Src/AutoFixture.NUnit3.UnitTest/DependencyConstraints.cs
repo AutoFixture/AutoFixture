8b09c1957a6bdf750ad5d582d4c6787e3a0bd285
Hackle Wayne <hacklew@hotmail.com>
2016-03-16 20:34:06 +1300

    Add other mocking libs in dependency contraints

--- Src/AutoFixture.NUnit3.UnitTest/DependencyConstraints.cs
+++ Src/AutoFixture.NUnit3.UnitTest/DependencyConstraints.cs
@@ -6,8 +6,15 @@ namespace Ploeh.AutoFixture.NUnit3.UnitTest
     [TestFixture]
     public class DependencyConstraints
     {
+        [TestCase("FakeItEasy")]
+        [TestCase("Foq")]
+        [TestCase("FsCheck")]
         [TestCase("Moq")]
+        [TestCase("NSubstitute")]
         [TestCase("Rhino.Mocks")]
+        [TestCase("Unquote")]
+        [TestCase("xunit")]
+        [TestCase("xunit.extensions")]
         public void AutoFixtureNUnit3DoesNotReference(string assemblyName)
         {
             // Fixture setup
@@ -18,8 +25,15 @@ namespace Ploeh.AutoFixture.NUnit3.UnitTest
             // Teardown
         }
 
+        [TestCase("FakeItEasy")]
+        [TestCase("Foq")]
+        [TestCase("FsCheck")]
         [TestCase("Moq")]
+        [TestCase("NSubstitute")]
         [TestCase("Rhino.Mocks")]
+        [TestCase("Unquote")]
+        [TestCase("xunit")]
+        [TestCase("xunit.extensions")]
         public void AutoFixtureNUnit3UnitTestsDoNotReference(string assemblyName)
         {
             // Fixture setup
