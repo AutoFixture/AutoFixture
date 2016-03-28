da9b7285596b6c16b0440a8a44adc0a6c70020eb
Hackle Wayne <hacklew@hotmail.com>
2016-03-28 15:19:38 +1300

    Test: Can be extended

---
+++ Src/AutoFixture.NUnit3.UnitTest/InlineAutoDataAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Ploeh.AutoFixture.NUnit3.UnitTest
+{
+    [TestFixture]
+    public class InlineAutoDataAttributeTest
+    {
+        [Test]
+        public void InlineAutoDataInheritsAutoData()
+        {
+            Assert.That(new InlineAutoDataAttribute(), 
+                Is.AssignableTo(typeof(AutoDataAttribute)));
+        }
+
+        [Test]
+        public void InlineAutoDataCanBeExtendedWithImplementationOfIFixture()
+        {
+            var extended = new InlineAutoDataAttributeStub();
+
+            Assert.That(extended, Is.AssignableTo(typeof(InlineAutoDataAttribute)));
+        }
+
+        private class InlineAutoDataAttributeStub : InlineAutoDataAttribute
+        {
+            /// <summary>
+            /// Here we can use any implementation of <see cref="IFixture"/>, 
+            /// the use of <see cref="ThrowingStubFixture"/> is pure coincidence and inconsequential
+            /// </summary>
+            /// <param name="arguments"></param>
+            public InlineAutoDataAttributeStub(params object[] arguments) 
+                : base(new ThrowingStubFixture(), arguments)
+            {
+            }
+        }
+    }
+}
--- Src/AutoFixture.NUnit3/InlineAutoDataAttribute.cs
+++ Src/AutoFixture.NUnit3/InlineAutoDataAttribute.cs
@@ -13,10 +13,11 @@ namespace Ploeh.AutoFixture.NUnit3
     [CLSCompliant(false)]
     public class InlineAutoDataAttribute : AutoDataAttribute
     {
-        private readonly object[] _arguments;
+        private readonly object[] _existingParameterValues;
 
         /// <summary>
         /// Construct a <see cref="InlineAutoDataAttribute"/>
+        /// with parameter values for test method
         /// </summary>
         public InlineAutoDataAttribute(params object[] arguments)
             : this(new Fixture(), arguments)
@@ -25,10 +26,11 @@ namespace Ploeh.AutoFixture.NUnit3
 
         /// <summary>
         /// Construct a <see cref="InlineAutoDataAttribute"/> with an <see cref="IFixture"/> 
+        /// and parameter values for test method
         /// </summary>
         protected InlineAutoDataAttribute(IFixture fixture, params object[] arguments) : base(fixture)
         {
-            _arguments = arguments;
+            _existingParameterValues = arguments;
         }
 
         /// <summary>
@@ -36,9 +38,14 @@ namespace Ploeh.AutoFixture.NUnit3
         /// </summary>
         protected override IEnumerable<object> GetParameterValues(IEnumerable<IParameterInfo> parameters)
         {
-            var parametersWithoutValues = parameters.Skip(this._arguments.Count());
+            return this._existingParameterValues.Concat(GetMissingValues(parameters));
+        }
 
-            return this._arguments.Concat(parametersWithoutValues.Select(GetValueForParameter));
+        private IEnumerable<object> GetMissingValues(IEnumerable<IParameterInfo> parameters)
+        {
+            var parametersWithoutValues = parameters.Skip(this._existingParameterValues.Count());
+
+            return parametersWithoutValues.Select(GetValueForParameter);
         }
     } 
 }
