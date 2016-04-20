cc6cb8f596107a991c86c2bf4f1cb4ecea4e3997
Mark Seemann <mark@ploeh.dk>
2016-04-20 21:34:04 +0200

    Incremented patch version,
    in order to indicate that this version addresses a defect: using the
    Without method on a class with a base class field would throw an
    exception.

--- Src/AutoMoq/Properties/AssemblyInfo.cs
+++ Src/AutoMoq/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.44.0.0")]
-[assembly: AssemblyFileVersion("3.44.0.0")]
+[assembly: AssemblyVersion("3.44.1.0")]
+[assembly: AssemblyFileVersion("3.44.1.0")]
 
 [assembly: CLSCompliant(true)]
