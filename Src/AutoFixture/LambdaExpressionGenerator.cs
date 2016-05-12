24d3f5c126bc92013e7164da51aaf406f8923a2a
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-05-12 23:52:24 +0300

    Simplify the Expression<Func<>> specimen building

--- Src/AutoFixture/LambdaExpressionGenerator.cs
+++ Src/AutoFixture/LambdaExpressionGenerator.cs
@@ -1,7 +1,6 @@
 ﻿namespace Ploeh.AutoFixture
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
     using Kernel;
@@ -24,17 +23,13 @@
             var delegateType = requestType.GetGenericArguments().Single();
             var genericArguments = delegateType.GetGenericArguments().Select(Expression.Parameter).ToList();
 
-            if (delegateType.Name.StartsWith( "Action"))
+            if (delegateType.Name.StartsWith("Action"))
             {
                 return Expression.Lambda(Expression.Empty(), genericArguments);
             }
 
             var body = genericArguments.Last();
-            var parameters = new List<ParameterExpression>();
-            if (genericArguments.Count > 1)
-            {
-                parameters = genericArguments.Take(genericArguments.Count - 1).ToList();
-            }
+            var parameters = genericArguments.Except(new[] { body });
 
             return Expression.Lambda(body, parameters);
         }
