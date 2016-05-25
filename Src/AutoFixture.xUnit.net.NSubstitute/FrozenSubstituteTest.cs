6ad21433aeecafd358da27f536b4d70fa908f8d3
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-05-26 00:23:44 +0300

    Add POC implementation of the 'Frozen'-friendly [Substitute] attribute

--- Src/AutoFixture.xUnit.net.NSubstitute/FrozenSubstituteTest.cs
+++ Src/AutoFixture.xUnit.net.NSubstitute/FrozenSubstituteTest.cs
@@ -1,5 +1,8 @@
-﻿using Ploeh.AutoFixture;
+﻿using System;
+using System.Reflection;
+using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Kernel;
 using Ploeh.AutoFixture.Xunit;
 using Ploeh.TestTypeFoundation;
 using Xunit;
@@ -10,17 +13,16 @@ namespace AutoFixture.xUnit.net.NSubstitute
     public class FrozenSubstituteTest
     {
         [Theory, AutoNSubstituteData]
-        public void CreateConcreteTypeSubstituteSubstitute([Substitute] ConcreteType sut)
+        public void CreateConcreteTypeSubstitute([Substitute] ConcreteType sut)
         {
-            Assert.IsAssignableFrom<ConcreteType>(sut);
             Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
         }
 
         [Theory, AutoNSubstituteData]
-        public void CreateFrozenConcreteTypeSubstituteSubstitute([Frozen, Substitute] ConcreteType sut)
+        public void FreezeConcreteTypeSubstitute([Frozen, Substitute2] ConcreteType ct1, ConcreteType ct2)
         {
-            Assert.IsAssignableFrom<ConcreteType>(sut);
-            Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
+            Assert.True(ct1.GetType().BaseType == typeof(ConcreteType));
+            Assert.Same(ct1, ct2);
         }
 
         private class AutoNSubstituteDataAttribute : AutoDataAttribute
@@ -28,6 +30,34 @@ namespace AutoFixture.xUnit.net.NSubstitute
             public AutoNSubstituteDataAttribute()
               : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
             {
+            }
+        }
+
+        private class Substitute2Attribute : CustomizeAttribute
+        {
+            public override ICustomization GetCustomization(ParameterInfo parameter)
+            {
+                var type = typeof(ConcreteType);
+                return new ConcreteSubstituteCustomization(type);
+            }
+        }
+
+        public class ConcreteSubstituteCustomization : ICustomization
+        {
+            private readonly Type type;
+
+            public ConcreteSubstituteCustomization(Type type)
+            {
+                this.type = type;
+            }
+
+            public void Customize(IFixture fixture)
+            {
+                var builder = new FilteringSpecimenBuilder(
+                    new MethodInvoker(new NSubstituteMethodQuery()),
+                    new ExactTypeSpecification(type));
+
+                fixture.Customizations.Insert(0, builder);
             }
         }
     }
