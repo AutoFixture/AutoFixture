b569e9e57ae5b9a1517fb50bcdcc4a129277bce6
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-05-25 21:09:20 +0300

    Create xUnit NSubstitute project to combine integration tests

---
+++ Src/AutoFixture.xUnit.net.NSubstitute/FrozenSubstituteTest.cs
@@ -0,0 +1,34 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Xunit;
+using Ploeh.TestTypeFoundation;
+using Xunit;
+using Xunit.Extensions;
+
+namespace AutoFixture.xUnit.net.NSubstitute
+{
+    public class FrozenSubstituteTest
+    {
+        [Theory, AutoNSubstituteData]
+        public void CreateConcreteTypeSubstituteSubstitute([Substitute] ConcreteType sut)
+        {
+            Assert.IsAssignableFrom<ConcreteType>(sut);
+            Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void CreateFrozenConcreteTypeSubstituteSubstitute([Frozen, Substitute] ConcreteType sut)
+        {
+            Assert.IsAssignableFrom<ConcreteType>(sut);
+            Assert.True(sut.GetType().BaseType == typeof(ConcreteType));
+        }
+
+        private class AutoNSubstituteDataAttribute : AutoDataAttribute
+        {
+            public AutoNSubstituteDataAttribute()
+              : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
+            {
+            }
+        }
+    }
+}
