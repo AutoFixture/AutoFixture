1d6211908ebb5b436a8c98abc33ebce8dd6f2f2d
Mark Seemann <mark@ploeh.dk>
2016-06-28 19:51:58 +0200

    Incremented minor version,
    in order to indicate that the addition of the AutoFakeItEasy2 package is a
    new feature.

--- Src/AutoFixture.xUnit.net/Properties/AssemblyInfo.cs
+++ Src/AutoFixture.xUnit.net/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.47.8.0")]
-[assembly: AssemblyFileVersion("3.47.8.0")]
+[assembly: AssemblyVersion("3.48.0.0")]
+[assembly: AssemblyFileVersion("3.48.0.0")]
 
 [assembly: CLSCompliant(true)]
