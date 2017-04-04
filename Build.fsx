2ec1202b680bd1daa624dfac9f24191dd19b1030
Alex Povar <zvirja1@gmail.com>
2017-04-04 23:50:22 +0300

    Extend build script with NuGet push tasks

--- Build.fsx
+++ Build.fsx
@@ -138,7 +138,42 @@ Target "NuGetPack" (fun _ ->
                                                    SymbolPackage = NugetSymbolPackage.Nuspec }) f)
 )
 
+let publishPackagesToNuGet apiFeed symbolFeed nugetKey =
+    let packages = !! (sprintf "%s/*.nupkg" nuGetOutputFolder)     
+
+    packages
+    |> Seq.map (fun p ->
+        let isSymbolPackage = p.EndsWith "symbols.nupkg"
+        let feed =
+            match isSymbolPackage with
+            | true -> symbolFeed
+            | false -> apiFeed
+
+        let meta = GetMetaDataFromPackageFile p
+        let version = 
+            match isSymbolPackage with
+            | true -> sprintf "%s.symbols" meta.Version
+            | false -> meta.Version
+
+        (meta.Id, version, feed))
+    |> Seq.iter (fun (id, version, feed) -> NuGetPublish (fun p -> { p with PublishUrl = feed
+                                                                            AccessKey = nugetKey
+                                                                            OutputPath = nuGetOutputFolder
+                                                                            Project = id
+                                                                            Version = version }))    
+
+Target "PublishNuGetPreRelease" (fun _ -> publishPackagesToNuGet 
+                                            "https://www.myget.org/F/autofixture/api/v2/package" 
+                                            "https://www.myget.org/F/autofixture/symbols/api/v2/package"
+                                            (getBuildParam "NuGetPreReleaseKey"))
+
+Target "PublishNuGetRelease" (fun _ -> publishPackagesToNuGet
+                                            "https://www.nuget.org/api/v2/package"
+                                            "https://nuget.smbsrc.net/"
+                                            (getBuildParam "NuGetReleaseKey"))
+
 Target "CompleteBuild" (fun _ -> ())
+Target "PublishNuGetAll" (fun _ -> ()) 
 
 "CleanVerify"  ==> "CleanAll"
 "CleanRelease" ==> "CleanAll"
@@ -163,5 +198,12 @@ Target "CompleteBuild" (fun _ -> ())
 "CopyToReleaseFolder" ==> "NuGetPack"
 
 "NuGetPack" ==> "CompleteBuild"
+
+"NuGetPack" ==> "PublishNuGetRelease"
+"NuGetPack" ==> "PublishNuGetPreRelease"
+
+"PublishNuGetRelease"
+    ==> "PublishNuGetPreRelease"
+    ==> "PublishNuGetAll"
 
 RunTargetOrDefault "CompleteBuild"
