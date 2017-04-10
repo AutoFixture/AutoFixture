6d5c1f63842d99793f3215fa4f17efbbddd41653
Alex Povar <zvirja1@gmail.com>
2017-04-10 12:36:49 +0300

    Update FAKE to new version and switch to FAKE.Core package instead
    FAKE should be updated to support MSBuild from VS 2017.
    FAKE.Core package is used instead of full FAKE because it's smaller,
    while all the required functionality is present.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cls
 
-packages\FAKE.4.19.0\tools\FAKE.exe build.fsx %*
+packages\FAKE.Core\tools\FAKE.exe build.fsx %*
--- update-fake.bat
+++ update-fake.bat
@@ -1,3 +1,3 @@
 cls
-Packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe install "FAKE" -OutputDirectory "Packages" -ExcludeVersion
+Packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe install "FAKE.Core" -OutputDirectory "Packages" -ExcludeVersion
 @pause
