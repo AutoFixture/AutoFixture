ffc983046f40f3e14285f02f217c00f1c03e8c19
Alex Povar <zvirja1@gmail.com>
2017-04-11 15:24:29 +0300

    Add calculation of the current version based on git history
    Last tag and number of commits since last tag is used to determine
    NuGet version. Resolved version is written to the global variable to be
    used later during NuGet pack.
    
    This approach allows to simplify Release Management by controlling
    releases via tags creation only.
    
    Additionally we calculate AssemblyInformationalVersion as NuGet combined
    with sha of the commit. This allows to quickly determine package version
    and base commit having assembly only.

--- Build.fsx
+++ Build.fsx
@@ -3,12 +3,57 @@
 open Fake
 open Fake.Testing
 open System
+open System.Text.RegularExpressions
 
 let releaseFolder = "Release"
 let nunitToolsFolder = "Packages/NUnit.Runners.2.6.2/tools"
 let nuGetOutputFolder = "NuGetPackages"
 let solutionsToBuild = !! "Src/*.sln"
 let processorArchitecture = environVar "PROCESSOR_ARCHITECTURE"
+
+type BuildVersionInfo = { assemblyVersion:string; fileVersion:string; infoVersion:string; nugetVersion:string }
+let calculateVersionFromGit buildNumber =
+    // Example of output for a release tag: v3.50.2-288-g64fd5c5b, for a prerelease tag: v3.50.2-alpha1-288-g64fd5c5b
+    let desc = Git.CommandHelper.runSimpleGitCommand "" "describe --tags --long --match=v*"
+
+    let result = Regex.Match(desc,
+                             @"^v(?<maj>\d+)\.(?<min>\d+)\.(?<rev>\d+)(?<pre>-\w+\d*)?-(?<num>\d+)-g(?<sha>[a-z0-9]+)$",
+                             RegexOptions.IgnoreCase)
+                      .Groups
+    let getMatch (name:string) = result.[name].Value
+
+    let major, minor, revision, preReleaseSuffix, commitsNum, sha =
+        getMatch "maj" |> int, getMatch "min" |> int, getMatch "rev" |> int, getMatch "pre", getMatch "num" |> int, getMatch "sha"
+
+    
+    let assemblyVersion = sprintf "%d.%d.%d.0" major minor revision
+    let fileVersion = sprintf "%d.%d.%d.%d" major minor revision buildNumber
+    
+    // If number of commits since last tag is greater than zero, we append another identifier with number of commits.
+    // The produced version is larger than the last tag version.
+    // If we are on a tag, we use version specified modification.
+    // Examples of output: 3.50.2.1, 3.50.2.215, 3.50.1-rc1.3, 3.50.1-rc3.35
+    let nugetVersion = match commitsNum with
+                       | 0 -> sprintf "%d.%d.%d%s" major minor revision preReleaseSuffix
+                       | _ -> sprintf "%d.%d.%d%s.%d" major minor revision preReleaseSuffix commitsNum
+
+    let infoVersion = match commitsNum with
+                      | 0 -> nugetVersion
+                      | _ -> sprintf "%s-%s" nugetVersion sha
+
+    { assemblyVersion=assemblyVersion; fileVersion=fileVersion; infoVersion=infoVersion; nugetVersion=nugetVersion }
+
+// Calculate version that should be used for the build. Define globally as data might be required by multiple targets.
+// Please never name the build parameter with version as "Version" - it might be consumed by the MSBuild, override 
+// the defined properties and break some tasks (e.g. NuGet restore).
+let buildVersion = match getBuildParamOrDefault "BuildVersion" "git" with
+                   | "git"       -> calculateVersionFromGit (getBuildParamOrDefault "BuildNumber" "0" |> int)
+                   | assemblyVer -> { assemblyVersion = assemblyVer
+                                      fileVersion = getBuildParamOrDefault "BuildFileVersion" assemblyVer
+                                      infoVersion = getBuildParamOrDefault "BuildInfoVersion" assemblyVer
+                                      nugetVersion = getBuildParamOrDefault "BuildNugetVersion" assemblyVer }
+
+
 
 let build target configuration =
     let keyFile =
