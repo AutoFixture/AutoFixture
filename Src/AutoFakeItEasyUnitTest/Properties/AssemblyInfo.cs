964bfc8b886e120cb7d858d23deff19fbd67a93c
Alex Povar <zvirja1@gmail.com>
2017-06-23 15:34:09 +0300

    Migrate AutoFakeItEasyUnitTest

--- Src/AutoFakeItEasyUnitTest/Properties/AssemblyInfo.cs
+++ Src/AutoFakeItEasyUnitTest/Properties/AssemblyInfo.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoFakeItEasyUnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ploeh")]
-[assembly: AssemblyProduct("AutoFixture")]
-[assembly: AssemblyCopyright("Copyright © Ploeh 2011")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,16 +13,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e3570d02-ac62-4b21-826a-ae0b0f35f95e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
