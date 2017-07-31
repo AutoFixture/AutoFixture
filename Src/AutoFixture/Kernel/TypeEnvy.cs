460d9f1ac3376fa366acd5dd7a25099fe2f6e3de
Alex Povar <zvirja1@gmail.com>
2017-07-31 14:45:23 +0300

    Avoid Linq usage in TypeEnvy as this code is hot

--- Src/AutoFixture/Kernel/TypeEnvy.cs
+++ Src/AutoFixture/Kernel/TypeEnvy.cs
@@ -1,22 +1,25 @@
-﻿namespace Ploeh.AutoFixture.Kernel
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Reflection.Emit;
+﻿using System;
+using System.Globalization;
+using System.Reflection;
 
+namespace Ploeh.AutoFixture.Kernel
+{
     internal static class TypeEnvy
     {
         public static TAttribute GetAttribute<TAttribute>(object candidate)
             where TAttribute : Attribute
         {
+            // This is performance critical code and Linq isn't being used intentionally.
             var attributeProvider = candidate as ICustomAttributeProvider;
+            if (attributeProvider == null) return null;
 
-            return attributeProvider?
-                .GetCustomAttributes(typeof(TAttribute), true)
-                .Cast<TAttribute>()
-                .SingleOrDefault();
+            var attributes = attributeProvider.GetCustomAttributes(typeof(TAttribute), true);
+
+            if (attributes.Length == 0) return null;
+            if (attributes.Length == 1) return (TAttribute) attributes[0];
+
+            throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
+                "Member '{0}' contains more than one attribute of type '{1}'", candidate, typeof(TAttribute)));
         }
 
         public static Assembly Assembly(this Type type)
