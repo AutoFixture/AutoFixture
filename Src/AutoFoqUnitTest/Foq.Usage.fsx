6376f2139793c9f85d4870a691ae76da2351baa5
Alex Povar <zvirja1@gmail.com>
2017-08-14 23:09:20 +0300

    Migrate AutoFoqUnitTest to .NET Core SDK

--- Src/AutoFoqUnitTest/Foq.Usage.fsx
+++ Src/AutoFoqUnitTest/Foq.Usage.fsx
@@ -1,5 +1,5 @@
 ﻿#if INTERACTIVE // Emulate minimal NUnit in F# Interactive
-#r @"..\packages\Foq.1.5.1\Lib\net40\Foq.dll"
+#r @"..\..\Packages\Foq.1.5.1\Lib\net45\Foq.dll"
 
 type TestAttribute() = inherit System.Attribute()
 
