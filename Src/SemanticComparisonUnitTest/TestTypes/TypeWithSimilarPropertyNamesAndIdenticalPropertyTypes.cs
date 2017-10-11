2ad6243b08b2451e83bb41c9040c2bfb903ec8e9
Alex Povar <zvirja1@gmail.com>
2017-10-11 12:37:14 +0300

    Remove SemanticComparison project

--- Src/SemanticComparisonUnitTest/TestTypes/TypeWithSimilarPropertyNamesAndIdenticalPropertyTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ploeh.SemanticComparison.UnitTest.TestTypes
-{
-    public class TypeWithSimilarPropertyNamesAndIdenticalPropertyTypes
-    {
-        public int Property { get; set; }
-
-        public int Property2 { get; set; }
-    }
-}
