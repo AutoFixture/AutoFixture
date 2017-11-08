cb1d3df0a1eb4039987caa3c760ad5cf1fb9f970
Alex Povar <zvirja1@gmail.com>
2017-11-09 00:42:40 +0200

    Extract IsNumberType() to extension method

--- Src/AutoFixture/Kernel/TypeEnvy.cs
+++ Src/AutoFixture/Kernel/TypeEnvy.cs
@@ -66,5 +66,30 @@ namespace AutoFixture.Kernel
         {
             return type.GetTypeInfo().IsValueType;
         }
+        
+        public static bool IsNumberType(this Type type)
+        {
+            if(type.IsEnum()) return false;
+
+            var typeCode = Type.GetTypeCode(type);
+            switch (typeCode)
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
     }
 }
