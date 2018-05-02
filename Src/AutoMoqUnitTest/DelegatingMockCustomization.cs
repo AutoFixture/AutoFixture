c7413184b96a89cec3d1457917e238be9e9c09b6
Michal Kowal <michalkowal@users.noreply.github.com>
2018-05-02 11:05:45 +0200

    Add GreedyMockAttribute (#766)

---
+++ Src/AutoMoqUnitTest/DelegatingMockCustomization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AutoFixture.AutoMoq.UnitTest
+{
+    internal class DelegatingMockCustomization : ICustomization
+    {
+        internal DelegatingMockCustomization()
+        {
+            this.OnCustomize = f => { };
+        }
+
+        public void Customize(IFixture fixture)
+        {
+            this.OnCustomize(fixture);
+        }
+
+        internal Action<IFixture> OnCustomize { get; set; }
+    }
+}
