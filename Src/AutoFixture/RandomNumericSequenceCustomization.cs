3d94dbbaea4f9e8355581623b6cce2dd368592e0
Alex Povar <zvirja1@gmail.com>
2018-07-10 11:19:01 +0300

    Obsolete customizations which are no longer needed

--- Src/AutoFixture/RandomNumericSequenceCustomization.cs
+++ Src/AutoFixture/RandomNumericSequenceCustomization.cs
@@ -5,6 +5,8 @@ namespace AutoFixture
     /// <summary>
     /// A customization that enables numeric specimens to be random and unique.
     /// </summary>
+    [Obsolete("The RandomNumericSequenceGenerator is now used by default, therefore this customization " +
+              "is no longer needed and will be removed in future versions of AutoFixture.")]
     public class RandomNumericSequenceCustomization : ICustomization
     {
         /// <summary>
