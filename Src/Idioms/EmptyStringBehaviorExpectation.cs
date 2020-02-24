9842bf46d880930c74db023a1db55684a284ae1d
Andrei Ivascu <andrew.ivascu@gmail.com>
2020-02-24 23:03:49 +0200

    Adds documentation comments

--- Src/Idioms/EmptyStringBehaviorExpectation.cs
+++ Src/Idioms/EmptyStringBehaviorExpectation.cs
@@ -3,8 +3,27 @@ using System.Globalization;
 
 namespace AutoFixture.Idioms
 {
+    /// <summary>
+    /// Encapsulates the expected behavior when an <see cref="IGuardClauseCommand" /> (typically
+    /// representing a method or constructor) is invoked with a <see cref="string.Empty" /> argument.
+    /// </summary>
+    /// <seealso cref="Verify(IGuardClauseCommand)" />
     public class EmptyStringBehaviorExpectation : IBehaviorExpectation
     {
+        /// <summary>
+        /// Verifies the behavior of the command when invoked with <see cref="string.Empty" />.
+        /// </summary>
+        /// <param name="command">The command whose behavior must be examined.</param>
+        /// <remarks>
+        /// <para>
+        /// This method encapsulates the behavior which is expected when a method or constructor is
+        /// invoked with <see cref="string.Empty" /> as one of the method arguments. In that case it's
+        /// expected that invoking <paramref name="command" /> with string.Empty throws an
+        /// <see cref="ArgumentException" />, causing the Verify method to succeed. If other
+        /// exceptions are thrown, or no exception is thrown when invoking the command, the Verify
+        /// method throws an exception.
+        /// </para>
+        /// </remarks>
         public void Verify(IGuardClauseCommand command)
         {
             if (command is null)
--- Src/Idioms/WhiteSpaceStringBehaviorExpectation.cs
+++ Src/Idioms/WhiteSpaceStringBehaviorExpectation.cs
@@ -3,8 +3,27 @@ using System.Globalization;
 
 namespace AutoFixture.Idioms
 {
+    /// <summary>
+    /// Encapsulates the expected behavior when an <see cref="IGuardClauseCommand" /> (typically
+    /// representing a method or constructor) is invoked with a white space argument.
+    /// </summary>
+    /// <seealso cref="Verify(IGuardClauseCommand)" />
     public class WhiteSpaceStringBehaviorExpectation : IBehaviorExpectation
     {
+        /// <summary>
+        /// Verifies the behavior of the command when invoked with white space.
+        /// </summary>
+        /// <param name="command">The command whose behavior must be examined.</param>
+        /// <remarks>
+        /// <para>
+        /// This method encapsulates the behavior which is expected when a method or constructor is
+        /// invoked with white space as one of the method arguments. In that case it's
+        /// expected that invoking <paramref name="command" /> with white space throws an
+        /// <see cref="ArgumentException" />, causing the Verify method to succeed. If other
+        /// exceptions are thrown, or no exception is thrown when invoking the command, the Verify
+        /// method throws an exception.
+        /// </para>
+        /// </remarks>
         public void Verify(IGuardClauseCommand command)
         {
             if (command is null)
