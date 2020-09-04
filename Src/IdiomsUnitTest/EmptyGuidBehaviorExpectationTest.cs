36c55e0cb7ef95280ff3b12c652d5cc5b8faad88
Andrei Ivascu <7030530+aivascu@users.noreply.github.com>
2020-09-04 22:49:34 +0300

    Implements parameter name check for empty guid expectation (#1169)
    * Implements parameter name check for empty guid expectation

--- Src/IdiomsUnitTest/EmptyGuidBehaviorExpectationTest.cs
+++ Src/IdiomsUnitTest/EmptyGuidBehaviorExpectationTest.cs
@@ -118,5 +118,90 @@ namespace AutoFixture.IdiomsUnitTest
                 sut.Verify(cmd));
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void VerifyThrowsExpectedWhenCommandThrowsWithIncorrectParameterName()
+        {
+            // Arrange
+            var expected = new Exception();
+            var cmd = new DelegatingGuardClauseCommand
+            {
+                OnExecute = v => throw new ArgumentException(string.Empty, "wrongParamName"),
+                OnCreateExceptionWithFailureReason = (v, r, e) => expected,
+                RequestedParameterName = "expectedParamName",
+                RequestedType = typeof(Guid)
+            };
+            var sut = new EmptyGuidBehaviorExpectation();
+
+            // Act
+            var actual = Record.Exception(() => sut.Verify(cmd));
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void VerifyThrowsWithExpectedMessageWhenCommandThrowsWithIncorrectParameterName()
+        {
+            // Arrange
+            var cmd = new DelegatingGuardClauseCommand
+            {
+                OnExecute = v => throw new ArgumentException(string.Empty, "wrongParamName"),
+                OnCreateExceptionWithFailureReason = (v, r, e) => new Exception(r, e),
+                RequestedParameterName = "expectedParamName",
+                RequestedType = typeof(Guid)
+            };
+            var sut = new EmptyGuidBehaviorExpectation();
+
+            // Act
+            var actual = Record.Exception(() => sut.Verify(cmd));
+
+            // Assert
+            Assert.EndsWith(
+                string.Format("Expected parameter name: expectedParamName{0}Actual parameter name: wrongParamName",
+                              Environment.NewLine),
+                actual.Message);
+        }
+
+        [Fact]
+        public void VerifyThrowsWithExpectedInnerWhenCommandThrowsWithIncorrectParameterName()
+        {
+            // Arrange
+            var expected = new ArgumentException(string.Empty, "wrongParamName");
+            var cmd = new DelegatingGuardClauseCommand
+            {
+                OnExecute = v => throw expected,
+                OnCreateExceptionWithFailureReason = (v, r, e) => new Exception(r, e),
+                RequestedParameterName = "expectedParamName",
+                RequestedType = typeof(Guid)
+            };
+            var sut = new EmptyGuidBehaviorExpectation();
+
+            // Act
+            var actual = Record.Exception(() => sut.Verify(cmd));
+
+            // Assert
+            Assert.Equal(expected, actual.InnerException);
+        }
+
+        [Fact]
+        public void VerifyDoesNotThrowWhenCommandThrowsWithCorrectParameterName()
+        {
+            // Arrange
+            var cmd = new DelegatingGuardClauseCommand
+            {
+                OnExecute = v => throw new ArgumentException(string.Empty, "paramName"),
+                OnCreateExceptionWithFailureReason = (v, r, e) => new Exception(r, e),
+                RequestedParameterName = "paramName",
+                RequestedType = typeof(Guid)
+            };
+            var sut = new EmptyGuidBehaviorExpectation();
+
+            // Act
+            var actual = Record.Exception(() => sut.Verify(cmd));
+
+            // Assert
+            Assert.Null(actual);
+        }
     }
 }
