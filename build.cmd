18d1987d0a7b215aeb69496ccc4a3d05bda7c858
Andrei Ivascu <7030530+aivascu@users.noreply.github.com>
2020-10-25 10:05:00 +0200

    Enables running NUnit 2 tests from dotnet cli (#1205)

--- build.cmd
+++ build.cmd
@@ -2,22 +2,6 @@
 cls
 
 SET SCRIPT_DIR=%~dp0
-SET BUILD_DIR=%SCRIPT_DIR%\build
-SET TOOLS_DIR=%BUILD_DIR%\tools
-SET NUGET_PATH=%TOOLS_DIR%\nuget.exe
-
-IF NOT EXIST %TOOLS_DIR%\ (
-  mkdir %TOOLS_DIR%
-)
-
-IF NOT EXIST %NUGET_PATH% (
-  echo Downloading NuGet.exe ...
-  powershell -Command "Start-BitsTransfer -Source https://dist.nuget.org/win-x86-commandline/v4.3.0/nuget.exe -Destination %NUGET_PATH%"
-)
-
-IF NOT EXIST "%TOOLS_DIR%\NUnit.Runners.2.6.2\" (
-  %NUGET_PATH% install "NUnit.Runners" -Version 2.6.2 -OutputDirectory %TOOLS_DIR% 
-)
 
 dotnet tool restore
 
