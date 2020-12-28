74da7b8a7eb1a35e1ffe42ce74401ccad6b2de4a
notnotchris <notnotchris.github.com@cdm.me.uk>
2020-12-28 16:14:04 +0000

    Add restricted enum generator

---
+++ Src/AutoFixtureUnitTest/RestrictedEnumTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using TestTypeFoundation;
+using Xunit;
+
+namespace AutoFixtureUnitTest
+{
+    public class RestrictedEnumTest
+    {
+        [Fact]
+        public void InitializeWithEmptyEnumTypeThrows()
+        {
+            // Arrange
+            // Act & assert
+            Assert.Throws<ArgumentException>(() => RestrictedEnum.IncludeValues((EmptyEnum)0));
+        }
+
+        [Fact]
+        public void InitializeWithNoAvailableValuesThrows()
+        {
+            // Arrange
+            // Act & assert
+            Assert.Throws<ArgumentException>(() => RestrictedEnum.IncludeValues<TriState>());
+        }
+
+        [Fact]
+        public void InitializeWithNullValuesThrows()
+        {
+            // Arrange
+            // Act & assert
+            Assert.Throws<ArgumentNullException>(() => RestrictedEnum.IncludeValues<TriState>(null));
+        }
+    }
+}
