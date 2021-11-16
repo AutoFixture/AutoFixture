f08072d65c0d70cd1d618435c5d2e162a3bc061d
Casper Weiss Bang <casper.bang@centrica.com>
2021-11-16 08:23:23 +0100

    Corrected message with grammarly

--- Src/Idioms/EmptyStringBehaviorExpectation.cs
+++ Src/Idioms/EmptyStringBehaviorExpectation.cs
@@ -50,8 +50,8 @@ namespace AutoFixture.Idioms
                 throw command.CreateException(
                     "<empty string>",
                     string.Format(CultureInfo.InvariantCulture,
-                        "a Guard Clause prevented it, however the thrown exception contains invalid parameter name. " +
-                        "Ensure you pass correct parameter name to the ArgumentException constructor.{0}" +
+                        "a Guard Clause prevented it; however, the thrown exception contains an invalid parameter name. " +
+                        "Ensure you pass the correct parameter name to the ArgumentException constructor.{0}" +
                         "Expected parameter name: {1}{0}Actual parameter name: {2}",
                         Environment.NewLine,
                         command.RequestedParameterName,
--- Src/Idioms/NullReferenceBehaviorExpectation.cs
+++ Src/Idioms/NullReferenceBehaviorExpectation.cs
@@ -58,8 +58,8 @@ namespace AutoFixture.Idioms
                 throw command.CreateException(
                     "<null>",
                     string.Format(CultureInfo.InvariantCulture,
-                        "a Guard Clause prevented it, however the thrown exception contains invalid parameter name. " +
-                        "Ensure you pass correct parameter name to the ArgumentNullException constructor.{0}" +
+                        "a Guard Clause prevented it; however, the thrown exception contains an invalid parameter name. " +
+                        "Ensure you pass the correct parameter name to the ArgumentException constructor.{0}" +
                         "Expected parameter name: {1}{0}Actual parameter name: {2}",
                         Environment.NewLine,
                         command.RequestedParameterName,
