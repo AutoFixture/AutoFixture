b611fd79c733b92a942cff8c88eb6010fead9c2c
Andrei Ivascu <7030530+aivascu@users.noreply.github.com>
2025-01-06 00:52:32 +0200

    Fixed type conversion error

--- Src/AutoFixture.xUnit2.UnitTest/CompositeDataAttributeSufficientDataTest.cs
+++ Src/AutoFixture.xUnit2.UnitTest/CompositeDataAttributeSufficientDataTest.cs
@@ -31,103 +31,103 @@ namespace AutoFixture.Xunit2.UnitTest
         public IEnumerator<object[]> GetEnumerator()
         {
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 4, 5, 6 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 2, 3, 4 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 3, 4 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 3, 4, 5 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 5 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 }, new object[] { 4, 5, 6 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }, new object[] { 4, 5, 6 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 }, new object[] { 4, 5, 6 } }),
                     new FakeDataAttribute(this.method,
                         new[] { new object[] { 7, 8 }, new object[] { 9, 10 }, new object[] { 11, 12 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }, new object[] { 4, 5, 6 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method,
                         new[] { new object[] { 1, 2 }, new object[] { 3, 4 }, new object[] { 5, 6 } }),
                     new FakeDataAttribute(this.method,
                         new[] { new object[] { 7, 8, 9 }, new object[] { 10, 11, 12 }, new object[] { 13, 14, 15 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 9 }, new object[] { 3, 4, 12 }, new object[] { 5, 6, 15 }
                 });
 
             // Second attribute restricts
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 }, new object[] { 4, 5, 6 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 7, 8, 9 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }
                 });
 
             // Shortest data provider is limiting factor
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 4, 5, 6 }, new object[] { 7, 8, 9 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3 }
                 });
@@ -135,57 +135,57 @@ namespace AutoFixture.Xunit2.UnitTest
             // Test incorrect number of parameters - should just return what it's given
             // and let xUnit deal with counting parameters
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 3, 4 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 2, 3 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 3 },
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 2, 3 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 3 },
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 2 } }),
                     new FakeDataAttribute(this.method, new[] { new object[] { 3 } })
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1 }
                 });
 
             yield return CreateTestData(
-                data: new object[]
+                data: new DataAttribute[]
                 {
                     new FakeDataAttribute(this.method, new[] { new object[] { 1, 2, 3, 4 } }),
                 },
-                expected: new object[]
+                expected: new[]
                 {
                     new object[] { 1, 2, 3, 4 }
                 });
@@ -196,7 +196,7 @@ namespace AutoFixture.Xunit2.UnitTest
             return this.GetEnumerator();
         }
 
-        private static object[] CreateTestData(object[] data, object[] expected)
+        private static object[] CreateTestData(DataAttribute[] data, object[][] expected)
         {
             return new object[] { data, expected };
         }
