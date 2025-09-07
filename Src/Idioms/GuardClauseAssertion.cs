b3b489a4cbe0a7926ac60af9b87b36272fb830bb
bestsoftever <me@bestsoftever.dev>
2025-09-07 02:48:36 +0200

    work done

--- Src/Idioms/GuardClauseAssertion.cs
+++ Src/Idioms/GuardClauseAssertion.cs
@@ -238,69 +238,27 @@ namespace AutoFixture.Idioms
             }
         }
 
-        //private IEnumerable<ReflectionExceptionUnwrappingCommand> GetParameterGuardCommands(IMethod method)
-        //{
-        //    var arguments = this.GetParameters(method);
-        //    return from pi in method.Parameters
-        //           where !pi.IsOut
-        //           let expansion = new IndexedReplacement<object>(pi.Position, arguments)
-        //           select new MethodInvokeCommand(method, expansion, pi)
-        //           into command
-        //           select new ReflectionExceptionUnwrappingCommand(command);
-        //}
-
         private IEnumerable<ReflectionExceptionUnwrappingCommand> GetParameterGuardCommands(IMethod method)
         {
             var arguments = this.GetParameters(method);
             return from pi in method.Parameters
-                   where !pi.IsOut && !IsNullable(pi)
+                   where !pi.IsOut && !IsNullableReferenceType(pi)
                    let expansion = new IndexedReplacement<object>(pi.Position, arguments)
                    select new MethodInvokeCommand(method, expansion, pi)
                    into command
                    select new ReflectionExceptionUnwrappingCommand(command);
         }
 
-        // Helper method to detect nullable reference types
-        private static bool IsNullable(ParameterInfo parameter)
+        private static bool IsNullableReferenceType(ParameterInfo parameter)
         {
-            //// Value types are not reference types
-            //if (!parameter.ParameterType.IsClass && !parameter.ParameterType.IsInterface)
-            //    return false;
-
+#if NET6_0
             var nic = new NullabilityInfoContext();
             var nullabilityInfo = nic.Create(parameter);
             return nullabilityInfo.ReadState == NullabilityState.Nullable;
-
-            //// Check for [Nullable] attribute on the parameter
-            //var nullableAttr = parameter.GetCustomAttributes(false)
-            //    .FirstOrDefault(a => a.GetType().FullName == "System.Runtime.CompilerServices.NullableAttribute");
-            //if (nullableAttr != null)
-            //{
-            //    var field = nullableAttr.GetType().GetField("NullableFlags");
-            //    if (field != null)
-            //    {
-            //        var flags = field.GetValue(nullableAttr) as byte[];
-            //        if (flags != null && flags.Length > 0)
-            //            return flags[0] == 2; // 2 means nullable
-            //    }
-            //}
-
-            //// Check for [NullableContext] attribute on the declaring method or type
-            //var nullableContext = parameter.Member.GetCustomAttributes(false)
-            //    .Concat(parameter.Member.DeclaringType?.GetCustomAttributes(false) ?? Array.Empty<object>())
-            //    .FirstOrDefault(a => a.GetType().FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
-            //if (nullableContext != null)
-            //{
-            //    var field = nullableContext.GetType().GetField("Flag");
-            //    if (field != null)
-            //    {
-            //        var flag = field.GetValue(nullableContext);
-            //        if (flag is byte b)
-            //            return b == 2;
-            //    }
-            //}
-
-            //return false;
+#else
+            // Older frameworks does not support nullable reference types, so we assume it's not nullable.
+            return false;
+#endif
         }
 
         private List<object> GetParameters(IMethod method)
---
+++ Src/IdiomsUnitTest/GuardClauseAssertionTests.NullableParameters.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using AutoFixture.Idioms;
+using Xunit;
+
+namespace AutoFixture.IdiomsUnitTest
+{
+#pragma warning disable SA1601 // This is a test class, so it's OK to have unannotated public members
+    public partial class GuardClauseAssertionTest
+#pragma warning restore SA1601
+    {
+        [Fact]
+        public void VerifyThrowsForMissingGuardClauseOnNonNullableReferenceType()
+        {
+            var sut = new GuardClauseAssertion(new Fixture());
+            var method = typeof(NonNullableReferenceParameterClass)
+                .GetMethod(nameof(NonNullableReferenceParameterClass.Method));
+
+            Assert.Throws<GuardClauseException>(() => sut.Verify(method));
+        }
+
+        private class NonNullableReferenceParameterClass
+        {
+            public void Method(string foo) { /* no guard clause */ }
+        }
+
+        [Fact]
+        public void VerifyDoesNotThrowForMissingGuardClauseOnNullableReferenceType()
+        {
+            var sut = new GuardClauseAssertion(new Fixture());
+            var method = typeof(NullableReferenceParameterClass)
+                .GetMethod(nameof(NullableReferenceParameterClass.Method));
+
+            var ex = Record.Exception(() => sut.Verify(method));
+
+            Assert.Null(ex);
+        }
+
+        private class NullableReferenceParameterClass
+        {
+#nullable enable
+            public void Method(string? foo)
+            {
+                if (foo == null) throw new ArgumentNullException(nameof(foo));
+            }
+        }
+
+        [Fact]
+        public void VerifyDoesNotThrowForMissingGuardClauseOnNullableValueType()
+        {
+            var sut = new GuardClauseAssertion(new Fixture());
+            var method = typeof(NullableReferenceParameterClass).GetMethod(nameof(NullableReferenceParameterClass.Method));
+
+            var ex = Record.Exception(() => sut.Verify(method));
+
+            Assert.Null(ex);
+        }
+
+        private class NullableValueParameterClass
+        {
+            public void Method(int? foo)
+            {
+                if (foo == null) throw new ArgumentNullException(nameof(foo));
+            }
+        }
+    }
+}
