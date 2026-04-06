ab1c377fa994e19dacd4ef9d228198340771d3d4
Oleksandr Povar <misc@zvirja.com>
2026-04-06 02:10:54 +0200

    Remove AutoFixture.AutoFoq as no longer maintained (#1536)

--- tests/AutoFoqUnitTest/TestDsl.fs
+++
@@ -1,5 +0,0 @@
-module AutoFixture.AutoFoq.UnitTest.TestDsl
-
-let internal verify = Swensen.Unquote.Assertions.test
-let inline internal isNull (value : 'a) = match value with | null -> true | _ -> false
-let internal implements<'T> (sut : obj) = typeof<'T>.IsAssignableFrom(sut.GetType())
