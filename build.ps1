fc84eb5b8d4306dd906880c82bac8563ffed905e
Oleksandr Povar <acc@zvirja.com>
2026-04-09 23:35:26 +0200

    Disable telemetry for DotNet and NUKE

--- build.ps1
+++ build.ps1
@@ -20,8 +20,10 @@ $DotNetGlobalFile = "$PSScriptRoot\\global.json"
 $DotNetInstallUrl = "https://dot.net/v1/dotnet-install.ps1"
 $DotNetChannel = "STS"
 
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 $env:DOTNET_NOLOGO = 1
+$env:NUKE_TELEMETRY_OPTOUT = 1
 
 ###########################################################################
 # EXECUTION
